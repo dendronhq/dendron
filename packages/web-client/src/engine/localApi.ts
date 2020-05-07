332d3440cd531d8bbbbf218a9235ada64928cac6
kevinslin <kevin@thence.io>
2020-05-07 08:44:47 -0700

    chore(client): fix compiler issues

--- packages/web-client/src/engine/localApi.ts
+++ packages/web-client/src/engine/localApi.ts
@@ -1,34 +1,65 @@
-import { Scope, DendronEngine, NodeType, DataType, NodeQueryResp, Node, NodeDict, NodeGetResp, NodeStubDict, NodeGetBatchResp } from "../common/types";
+import {
+  DNode,
+  DataType,
+  DendronEngine,
+  NodeDict,
+  NodeGetBatchResp,
+  NodeGetResp,
+  NodeQueryResp,
+  NodeStubDict,
+  NodeType,
+  Scope,
+} from "../common/types";
+
 import Engine from "./engine";
 import InMemoryStore from "./inMemoryStore";
 
-export default class localAPI implements DendronEngine {
+export default class LocalAPI implements DendronEngine {
   public engine: Engine;
   constructor() {
     this.engine = new Engine(new InMemoryStore());
   }
-  async get (scope: Scope, id: string, nodeType: NodeType): Promise<NodeGetResp> {
-    let response = await this.engine.storage.getBatch(scope, [id], nodeType)
-    let x: any = { item: Object.values(response.item)[0], ...response }; // appease typechecker
+  async get(
+    scope: Scope,
+    id: string,
+    nodeType: NodeType
+  ): Promise<NodeGetResp> {
+    const response = await this.engine.storage.getBatch(scope, [id], nodeType);
+    const x: any = { item: Object.values(response.item)[0], ...response }; // appease typechecker
     return x;
   }
 
-  async getBatch (scope: Scope, ids: string[], nodeType: NodeType): Promise<NodeGetBatchResp> {
+  async getBatch(
+    scope: Scope,
+    ids: string[],
+    nodeType: NodeType
+  ): Promise<NodeGetBatchResp> {
     return this.engine.storage.getBatch(scope, ids, nodeType);
   }
 
-  async query (scope: Scope, queryString: string, nodeType: NodeType): Promise<NodeQueryResp> {
+  async query(
+    scope: Scope,
+    queryString: string,
+    nodeType: NodeType
+  ): Promise<NodeQueryResp> {
     return await this.engine.query(scope, queryString, nodeType);
   }
 
-  async write (scope: Scope, node: Node): Promise<void> {
-    let dict: NodeDict = {};
+  async queryLocal(
+    scope: Scope,
+    queryString: string,
+    nodeType: NodeType
+  ): Promise<NodeQueryResp> {
+    return await this.engine.query(scope, queryString, nodeType);
+  }
+
+  async write(scope: Scope, node: DNode): Promise<void> {
+    const dict: NodeDict = {};
     dict[node.id] = node;
     this.engine.storage.writeBatch(scope, dict);
   }
 
-  async writeBatch (scope: Scope, nodes: NodeDict): Promise<void> {
+  async writeBatch(scope: Scope, nodes: NodeDict): Promise<void> {
     this.engine.storage.writeBatch(scope, nodes);
   }
-  
 }

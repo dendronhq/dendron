4a40ed2212bfeaf865493815bb22d742e14fe416
kevinslin <kevin@thence.io>
2020-05-10 21:57:48 -0700

    client: update types

--- packages/web-client/src/common/types.ts
+++ packages/web-client/src/common/types.ts
@@ -1,74 +1,44 @@
+// === Node Types
 export interface DNode {
-  /**
-   * Local ID
-   */
   id: string;
-  data: NodeData;
-  // currently string, in the future, this might be a tree of nodes
-  body?: string;
-  parent: NodeStub | null;
-  children: NodeStub[];
-}
-
-interface NodeData {
   title: string;
   desc: string;
-  type: DataType;
+  type: DNodeType;
+  updated: string;
+  created: string;
+  parent: DNode | null;
+  children: DNode[];
+  body?: string;
 }
-export type NodeStub = Omit<DNode, "parent" | "children">;
-export type NodeStubDict = { [id: string]: NodeStub };
-export type NodeDict = { [id: string]: DNode };
+export type DNodeDict = { [id: string]: DNode };
+export interface DNodeRaw<T extends NoteData | SchemaData> {
+  id: string;
+  title: string;
+  desc: string;
+  type: string;
+  updated: string;
+  created: string;
+  parent: string | null;
+  children: string[];
+  data: T;
+  body?: string;
+}
+export type DNodeType = "note" | "schema";
 
-// --- Notes
-export interface NoteData extends NodeData {
+export type Note = DNode & NoteData;
+export type NoteData = {
   schemaId: string;
-}
-export type NoteNodeStub = Omit<NodeStub, "data"> & { data: NoteData };
-export type NoteStubDict = { [id: string]: NoteNodeStub };
-export function toStub(node: DNode | NodeStub): NodeStub {
-  return { id: node.id, body: node.body, data: node.data };
-}
+};
 
-// --- Schemas
-export interface SchemaData extends NodeData {
-  aliases?: string[];
-  kind?: SchemaNodeKind;
-  // match: SchemaNodeMatchRule[];
-  choices?: { [key: string]: NodeStub };
-  // links: SchemaNodeLink[];
-  /**
-   * If namespace exists, display it here
-   */
-  //namespace?: string;
-  //   language:
-  //     kind: namespace
-  //     choices:
-  //         python:
-  //         ruby:
-  //         ts:
-  //     children:
-  //         data
-  //         flow
-}
+export type Schema = DNode & SchemaData;
+export type SchemaData = {
+  pattern: string;
+};
 
-export type SchemaNode = Omit<DNode, "data"> & { data: SchemaData };
-export type SchemaNodeStub = Omit<NodeStub, "data"> & { data: SchemaData };
-export type SchemaNodeDict = { [id: string]: SchemaNode };
-export type SchemaDataKey = keyof SchemaData;
-
-// --- Other
-export type RequiredSchemaDataKey = "title" | "desc";
-export const RequiredSchemaDataKeyValues: RequiredSchemaDataKey[] = [
-  "title",
-  "desc",
-];
-
-export type OptionalSchemaDataKey = Exclude<
-  keyof SchemaData,
-  RequiredSchemaDataKey
->;
-export type SchemaNodeKind = "namespace";
-
+/**
+ * YAML reprsentation of a Schema
+ * Used in Kevin's schema notation
+ */
 export type SchemaYAMLRaw = {
   name: string;
   schema: { [key: string]: SchemaYAMLEntryRaw } | { root: SchemaYAMLEntryRaw };
@@ -77,53 +47,14 @@ export type SchemaYAMLEntryRaw = SchemaData & {
   children: { [key: string]: any };
 };
 
-/*
-global:
-  schema:
-    output:
-      aliases: [out]
-      desc: anything I want to put out. formerly known as report
-    usecase:
-      alias: use
-      desc: describes a way something is used
-    gotcha:
-      desc: something novel
-    journal:
-      desc: continuously updated
-      kind: namespace
-      choices:
-        meet: 
-        design:
-        changelog:
-    internal:
-      desc: internal impletation
-    templates:
-      alias: temp
-      desc: re-usable templates
-    notes:
-      desc: notes on something
-    todo:
-      desc: "things to update"
-      children:
-        tasks:
-        questions:
-    questions:
-      desc: "questions i have"
-    ref:
-      desc: catchall
-*/
-
-export type NodeType = "stub" | "full";
-export type DataType = "schema" | "note";
+// === Engine Types
 
 export type NodeGetResp = {
-  item: DNode | NodeStub;
-  nodeType: NodeType;
+  item: DNode;
 };
 
 export type NodeGetBatchResp = {
-  item: NodeDict | NodeStubDict;
-  nodeType: NodeType;
+  item: DNodeDict;
 };
 
 export type NodeGetRootResp = {
@@ -131,52 +62,30 @@ export type NodeGetRootResp = {
 };
 
 export interface NodeQueryResp {
-  item: NodeDict | NodeStubDict;
-  nodeType: NodeType;
+  item: DNodeDict;
   error: Error | null;
 }
 export interface Scope {
   username: string;
 }
 
-export interface DendronEngine {
+export interface DEngine {
   /**
    * Get node based on id
    * get(id: ...)
    */
-  get: (scope: Scope, id: string, nodeType: NodeType) => Promise<NodeGetResp>;
+  get: (scope: Scope, id: string) => Promise<NodeGetResp>;
 
-  getBatch: (
-    scope: Scope,
-    ids: string[],
-    nodeType: NodeType
-  ) => Promise<NodeGetBatchResp>;
-  // load alphacortex
-  /**
-   * NodeStorageAPI.getRoot()
-   * {
-   *   parent: null
-   *   children:
-   *     - Alphacortex
-   * }
-   *
-   * welcome page
-   * sider
-   */
-  // getRoot: (scope: Scope, dataType: DataType) => Promise<NodeGetRootResp>;
+  getBatch: (scope: Scope, ids: string[]) => Promise<NodeGetBatchResp>;
 
   /**
    * Get node based on query
-   * query(scope: {username: lukesernau}, queryString: "Project", nodeType: stub, dataType: note})
+   * query(scope: {username: lukesernau}, queryString: "project", nodeType: note)
    * - []
    * - [Node(id: ..., title: project, children: [])]
    *
    */
-  query: (
-    scope: Scope,
-    queryString: string,
-    nodeType: NodeType
-  ) => Promise<NodeQueryResp>;
+  query: (scope: Scope, queryString: string) => Promise<NodeQueryResp>;
 
   /**
    * Write node to db
@@ -186,7 +95,5 @@ export interface DendronEngine {
   /**
    * Write list of nodes
    */
-  writeBatch: (scope: Scope, nodes: NodeDict) => Promise<void>;
+  writeBatch: (scope: Scope, nodes: DNodeDict) => Promise<void>;
 }
-
-// convert to dictionary, in return

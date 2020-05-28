0aef6aefbdeb7219329aca583357b2b27c829e2f
kevinslin <kevin@thence.io>
2020-05-28 09:00:04 -0700

    check - add amplify config

---
+++ packages/common-all/src/config.ts
@@ -0,0 +1,8 @@
+export const global = {};
+export const dev = {
+  COGNITO_POOL_ID: "us-west-2_X6icVFghe",
+  COGNITO_CLIENT_ID: "19vkp969ss471e424pfh7trq33"
+};
+export const prod = { COGNITO_POOL_ID: "TODO", COGNITO_CLIENT_ID: "TODO" };
+export const config = { global, dev, prod };
+export type ConfigKey = keyof typeof dev;

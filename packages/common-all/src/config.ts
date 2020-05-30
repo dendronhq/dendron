5c6adb61e9c2ea88cf980a07a7d75ebc23bfd2b5
kevinslin <kevin@thence.io>
2020-05-30 12:13:22 -0700

    invest: add test stage

--- packages/common-all/src/config.ts
+++ packages/common-all/src/config.ts
@@ -1,4 +1,8 @@
 export const global: GlobalConfig = {};
+export const test: StageConfig = {
+  COGNITO_POOL_ID: "TODO",
+  COGNITO_CLIENT_ID: "TODO"
+};
 export const dev: StageConfig = {
   COGNITO_POOL_ID: "us-west-2_X6icVFghe",
   COGNITO_CLIENT_ID: "19vkp969ss471e424pfh7trq33"
@@ -7,7 +11,7 @@ export const prod: StageConfig = {
   COGNITO_POOL_ID: "TODO",
   COGNITO_CLIENT_ID: "TODO"
 };
-export const config = { global, dev, prod };
+export const config = { global, test, dev, prod };
 
 type GlobalConfig = {
   LOG_LEVEL?: string;

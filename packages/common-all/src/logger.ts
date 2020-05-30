389da636ab60ef0e06b63ec90c5176bb73b4d779
kevinslin <kevin@thence.io>
2020-05-30 11:22:30 -0700

    chore: update logger signature

--- packages/common-all/src/logger.ts
+++ packages/common-all/src/logger.ts
@@ -1,13 +1,13 @@
-import _ from "lodash";
 import { env } from "./env";
 import pino from "pino";
 
-const Logger = (name?: string) => {
+function createLogger(name?: string) {
   let level = env("LOG_LEVEL", { shouldThrow: false }) || "info";
   let nameClean = name || env("LOG_NAME");
   return pino({ name: nameClean, level });
-};
-export { Logger };
+}
+
+export { createLogger };
 
 export function logAndThrow(logger: pino.Logger, msg: any): never {
   logger.error(msg);

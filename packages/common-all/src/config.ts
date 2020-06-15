7c5ce6662fb7ed94576b440434fea1175464165c
kevinslin <kevin@thence.io>
2020-06-15 08:06:39 -0700

    snap

--- packages/common-all/src/config.ts
+++ packages/common-all/src/config.ts
@@ -8,8 +8,10 @@ export const dev: StageConfig = {
   COGNITO_CLIENT_ID: "19vkp969ss471e424pfh7trq33"
 };
 export const prod: StageConfig = {
-  COGNITO_POOL_ID: "TODO",
-  COGNITO_CLIENT_ID: "TODO"
+  COGNITO_POOL_ID: "us-west-2_X6icVFghe",
+  COGNITO_CLIENT_ID: "19vkp969ss471e424pfh7trq33"
+  // COGNITO_POOL_ID: "TODO",
+  // COGNITO_CLIENT_ID: "TODO"
 };
 export const config = { global, test, dev, prod };
 

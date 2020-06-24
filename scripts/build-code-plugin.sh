ad498a90d4e222c56c33b6ee682def67fd981568
kevinslin <kevin@thence.io>
2020-06-24 10:52:21 -0700

    chore: update npm

--- scripts/build-code-plugin.sh
+++ scripts/build-code-plugin.sh
@@ -3,4 +3,4 @@
 
 
 
-lerna run build --parallel  --scope @dendronhq/common-all --scope @dendronhq/common-client --scope @dendronhq/common-server --scope @dendronhq/plugin-core
+lerna run build --parallel  --scope @dendronhq/common-all --scope @dendronhq/common-client --scope @dendronhq/common-server --scope @dendronhq/engine-server --scope dendron-plugin-core
--- scripts/watch-code-plugin.sh
+++ scripts/watch-code-plugin.sh
@@ -1,4 +1,4 @@
 
 #!/usr/bin/env sh
-lerna run watch --parallel --scope @dendronhq/common-all --scope @dendronhq/common-client --scope @dendronhq/common-server --scope @dendronhq/plugin-core
+lerna run watch --parallel --scope @dendronhq/common-all --scope @dendronhq/common-client --scope @dendronhq/common-server --scope @dendronhq/engine-server --scope dendron-plugin-core
   

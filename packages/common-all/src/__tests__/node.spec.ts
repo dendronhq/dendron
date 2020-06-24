403301515cc53d277b0f042f47d4a4b54d3d9ee6
kevinslin <kevin@thence.io>
2020-06-24 10:02:49 -0700

    chore: minor updates

--- packages/common-all/src/__tests__/node.spec.ts
+++ packages/common-all/src/__tests__/node.spec.ts
@@ -1,56 +1,54 @@
-import { DNodeRaw, NodeBuilder, Schema } from '../node';
+import { Schema } from "../node";
 
-import { ISchemaOpts } from '../types';
-
-test('adds 1 + 2 to equal 3', () => {
+test("adds 1 + 2 to equal 3", () => {
   expect(1 + 2).toBe(3);
 });
 
 function setup() {
   const foo = new Schema({
-    id: 'foo.root',
-    title: 'foo.root',
-    fname: 'foo.root',
-    parent: null,
+    id: "foo.root",
+    title: "foo.root",
+    fname: "foo.root",
+    parent: null
   });
   const fooChild = new Schema({
-    id: 'foo.child',
-    title: 'foo.child',
-    fname: 'foo.child',
-    parent: null,
+    id: "foo.child",
+    title: "foo.child",
+    fname: "foo.child",
+    parent: null
   });
   const fooGrandChild = new Schema({
-    id: 'foo.grandchild',
-    title: 'foo.grandchild',
-    fname: 'foo.grandchild',
-    parent: null,
+    id: "foo.grandchild",
+    title: "foo.grandchild",
+    fname: "foo.grandchild",
+    parent: null
   });
   foo.addChild(fooChild);
   fooChild.addChild(fooGrandChild);
   return { foo, fooChild, fooGrandChild };
 }
 
-describe('schema', () => {
+describe("schema", () => {
   let schemas: ReturnType<typeof setup>;
 
   beforeEach(() => {
     schemas = setup();
   });
 
-  describe('domainRoot', () => {
-    test('at root', () => {
+  describe("domainRoot", () => {
+    test("at root", () => {
       const { foo } = schemas;
       expect(foo.domain.id).toEqual(foo.id);
-      expect(foo.renderBody()).toMatchSnapshot('foo_body');
+      expect(foo.renderBody()).toMatchSnapshot("foo_body");
     });
 
-    test('at child', () => {
+    test("at child", () => {
       const { fooChild, foo } = schemas;
       expect(fooChild.domain.id).toEqual(foo.id);
-      expect(fooChild.renderBody()).toMatchSnapshot('fooChild_body');
+      expect(fooChild.renderBody()).toMatchSnapshot("fooChild_body");
     });
 
-    test('at grand child', () => {
+    test("at grand child", () => {
       const { fooGrandChild, foo } = schemas;
       expect(fooGrandChild.domain.id).toEqual(foo.id);
     });

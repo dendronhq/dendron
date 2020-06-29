2c48ae29a3ca9b0d599f79857f0eafb1a513bba7
kevinslin <kevin@thence.io>
2020-06-29 16:05:54 -0700

    chore: update parse command

--- packages/plugin-core/src/commands/Parse.ts
+++ packages/plugin-core/src/commands/Parse.ts
@@ -1,30 +1,39 @@
 import { createLogger, getAllFiles } from "@dendronhq/common-server";
 import { FileParser, FileStorage } from "@dendronhq/engine-server";
 import { BaseCommand } from "./base";
+import _ from "lodash";
 
 const L = createLogger("ParseCommand");
 
 type ParseCommandOpts = {
     root: string
     exclude?: string[]
+    bailOnError?: boolean
     /**
      * Might need to run this multiple times until all issues are fixed
      */
     fixMissing?: boolean
 };
+
 export class ParseCommand extends BaseCommand<ParseCommandOpts> {
     async execute(opts: ParseCommandOpts) {
+        opts = _.defaults(opts, { bailOnError: false });
         L.info({ ctx: "execute:enter", opts });
         const storage = new FileStorage({ root: opts.root });
         const allFiles = getAllFiles({ root: opts.root }) as string[];
         const fp = new FileParser(storage, {
             errorOnEmpty: false,
-            errorOnBadParse: true,
+            errorOnBadParse: opts.bailOnError,
         });
         // @ts-ignore
         const _data = fp.parse(allFiles);
         const report = fp.report();
+        const errors = fp.errors;
+        const badParseErrors = errors.filter(e => e.status === "BAD_PARSE");
         L.info({ ctx: "execute:parse:post", report });
+        L.info({ ctx: "execute:parse:post", errors });
+        L.info({ ctx: "execute:parse:post", badParseErrors });
+        L.info({ ctx: "execute:parse:post", numParseErrors: badParseErrors.length });
         L.info({ ctx: "execute:exit" });
     }
 }

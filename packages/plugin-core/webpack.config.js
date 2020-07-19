efe80e15088c8b035c8b9eee35bd7826b617a8a2
kevinslin <kevin@thence.io>
2020-07-18 17:20:23 -0700

    chore: fix bundling issue with pino

--- packages/plugin-core/webpack.config.js
+++ packages/plugin-core/webpack.config.js
@@ -19,6 +19,7 @@ const config = {
   devtool: "source-map",
   externals: {
     vscode: "commonjs vscode", // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    "pino-pretty": "pino-pretty"
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader

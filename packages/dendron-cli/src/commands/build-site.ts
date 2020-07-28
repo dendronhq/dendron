41e3d7283bf5719a62f8f7f6f612dc9ad07370f7
kevinslin <kevin@thence.io>
2020-07-28 11:11:39 -0700

    feat: support custom root when publishing a site

--- packages/dendron-cli/src/commands/build-site.ts
+++ packages/dendron-cli/src/commands/build-site.ts
@@ -18,11 +18,30 @@ type CommandCommonOpts = {
 
 type CommandOutput = void;
 
-function note2JekyllMdFile(note: Note, opts: { notesDir: string }) {
+type DendronSiteConfig = {
+  root: string;
+};
+
+type DendronJekyllProps = {
+    hpath: string
+    permalink?: string
+}
+
+function note2JekyllMdFile(
+  note: Note,
+  opts: { notesDir: string } & DendronSiteConfig
+) {
   const meta = DNodeUtils.getMeta(note);
-  const jekyllProps = {
+  const jekyllProps: DendronJekyllProps = {
     hpath: note.path
   };
+  if (opts.root === meta.fname) {
+    jekyllProps['permalink'] = '/';
+  }
+  // pull children of root to the top
+  if (note.parent?.fname === opts.root) {
+      delete meta['parent'];
+  }
   const filePath = path.join(opts.notesDir, meta.fname + ".md");
   return fs.writeFile(
     filePath,
@@ -38,9 +57,25 @@ export class BuildSiteCommand extends BaseCommand<CommandOpts, CommandOutput> {
     fs.ensureDirSync(notesDirPath);
     // TODO: ask for confirmation
     fs.emptyDirSync(notesDirPath);
-    await Promise.all(_.values(engine.notes).map(n => {
-      return note2JekyllMdFile(n, { notesDir: notesDirPath });
-    }));
+    const config: DendronSiteConfig = {
+      root: "dendron"
+    };
+    const root: Note = _.find(engine.notes, { fname: config.root }) as Note;
+    if (_.isUndefined(root)) {
+      throw Error(`root ${root} not found`);
+    }
+    const nodes: Note[] = [root];
+    const out = [];
+    while (!_.isEmpty(nodes)) {
+        const node = nodes.pop() as Note;
+        out.push(note2JekyllMdFile(node, { notesDir: notesDirPath, ...config }));
+        node.children.forEach(n => nodes.push(n as Note));
+    }
+    await Promise.all(nodes);
+    //   _.values(engine.notes).map(n => {
+    //     return note2JekyllMdFile(n, { notesDir: notesDirPath });
+    //   })
+    // );
     return;
   }
 }

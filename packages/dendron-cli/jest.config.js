14e574143d5ca916558aa2de45549716099a0fd4
kevinslin <kevin@thence.io>
2020-07-28 08:30:54 -0700

    chore: add dendron-cli

---
+++ packages/dendron-cli/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    preset: "ts-jest",
+    testEnvironment: "node",
+    roots: ["<rootDir>/src"],
+    moduleFileExtensions: ["ts", "tsx", "js"],
+    transform: {
+      "^.+\\.tsx?$": "ts-jest",
+    },
+    globals: {
+      "ts-jest": {
+        //tsConfig: "tsconfig.build.json",
+        diagnostics: false,
+      },
+    },
+  };
+  

482b37e19c0de35e6e8b172a9e8bac51dff78b1c
kevinslin <kevin@thence.io>
2020-07-30 12:34:47 -0700

    chore: add travis_terminate

--- scripts/testAll.sh
+++ scripts/testAll.sh
@@ -11,4 +11,5 @@ fi
 # cmd="lerna run test --parallel --ignore @dendronhq/plugin-core -- $flags 2>&1 | tee /tmp/testAll.log"
 # eval $cmd
 # use below to update snapshots
-npx lerna run test --parallel --ignore @dendronhq/plugin-core -- -- -u 2>&1 | tee /tmp/testAll.log
+npx lerna run test --parallel --ignore @dendronhq/plugin-core -- -- -u 2>&1 
+travis_terminate 0 || true

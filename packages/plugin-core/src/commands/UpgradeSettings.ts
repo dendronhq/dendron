187c584fe35939b7fbf8dfe202e5f9b023193df8
kevinslin <kevin@thence.io>
2020-08-03 18:06:42 -0700

    chore: update error message

--- packages/plugin-core/src/commands/UpgradeSettings.ts
+++ packages/plugin-core/src/commands/UpgradeSettings.ts
@@ -9,30 +9,42 @@ import { BaseCommand } from "./base";
 const L = createLogger("UpgradeSettingsCommand");
 
 type UpgradeSettingsCommandOpts = {
-  settingOpts: SettingsUpgradeOpts
+  settingOpts: SettingsUpgradeOpts;
 };
 
-export class UpgradeSettingsCommand extends BaseCommand<UpgradeSettingsCommandOpts, any> {
+export class UpgradeSettingsCommand extends BaseCommand<
+  UpgradeSettingsCommandOpts,
+  any
+> {
   async execute(opts: UpgradeSettingsCommandOpts) {
     const ctx = "execute";
     L.info({ ctx, opts });
     const config = DendronWorkspace.configuration();
     if (!config) {
-        throw Error("no ws config found");
+      throw Error("no ws config found");
     }
 
-    const newConfig = WorkspaceConfig.update(path.dirname(DendronWorkspace.workspaceFile().fsPath));
-    const badExtensions: Extension<any>[] = newConfig.extensions.unwantedRecommendations?.map(ext => {
-      return extensions.getExtension(ext)
-    }).filter(Boolean) as Extension<any>[]|| [];
-    this.L.info({ctx, badExtensions});
+    const newConfig = WorkspaceConfig.update(
+      path.dirname(DendronWorkspace.workspaceFile().fsPath)
+    );
+    const badExtensions: Extension<any>[] =
+      (newConfig.extensions.unwantedRecommendations
+        ?.map((ext) => {
+          return extensions.getExtension(ext);
+        })
+        .filter(Boolean) as Extension<any>[]) || [];
+    this.L.info({ ctx, badExtensions });
     if (!_.isEmpty(badExtensions)) {
       const msg = [
         "Manual action needed!",
         "The following extensions need to be uninstalled: ",
-      ].concat([badExtensions.map(ext => ext.packageJSON.displayName).join(", ")]).concat([
-        "Reload the window afterwards and Dendron will offer to install the Dendron version of the extension"
-      ]);
+      ]
+        .concat([
+          badExtensions.map((ext) => ext.packageJSON.displayName).join(", "),
+        ])
+        .concat([
+          "- Reload the window afterwards and Dendron will offer to install the Dendron version of the extension",
+        ]);
       console.log(msg);
       window.showWarningMessage(msg.join(" "));
     }

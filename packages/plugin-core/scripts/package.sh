f7afc5a178f11bf4eff9898a9ff20b83a12b0206
kevinslin <kevin@thence.io>
2020-08-03 17:16:59 -0700

    chore: update plugin install script

--- packages/plugin-core/scripts/package.sh
+++ packages/plugin-core/scripts/package.sh
@@ -7,6 +7,9 @@ git pull
 sed  -ibak 's/@dendronhq.plugin-core/dendron/' package.json
 sed  -ibak 's/out\/extension/dist\/extension/' package.json
 
+cat package.json | jq '.repository = { "url": "https://github.com/dendronhq/dendron.git", "type": "git" }' > tmp.json
+mv tmp.json package.json
+
 ./scripts/sync_vault.sh
 npm install
 vsce package

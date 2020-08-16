3ca8dfd9a8e4c5c805b08d69652a52c2908b1c5b
kevinslin <kevin@thence.io>
2020-08-15 19:52:11 -0700

    chore: update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,6 +4,7 @@
 #echo "gen readme..."
 #ansible-playbook playbooks/genReadme.yml
 echo "upgrading..."
+npm run release -- --dry-run > /tmp/updates.txt
 lerna version patch
 lerna publish from-package --ignore-scripts -y
 git push

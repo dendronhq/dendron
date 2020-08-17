c8e9a3371c075a262d4d395e13ec3795ff1b8d00
kevinslin <kevin@thence.io>
2020-08-17 10:05:24 -0700

    tests: run integ tests on travis

--- scripts/testAll.sh
+++ scripts/testAll.sh
@@ -11,11 +11,17 @@ fi
 # cmd="lerna run test --parallel --ignore @dendronhq/plugin-core -- $flags 2>&1 | tee /tmp/testAll.log"
 # eval $cmd
 # use below to update snapshots
-npx lerna run test --parallel --ignore @dendronhq/plugin-core -- -- -u 2>&1 
+if [[ -n $CI ]]; then
+    echo "run local test..."
+    npx lerna run test --parallel --ignore @dendronhq/plugin-core -- -- -u 2>&1
+else
+    echo "run CI test..."
+    npx lerna run test --parallel -- -- -u 2>&1
+fi
 
 travis_terminate() {
   set +e
   pkill -9 -P $$ &> /dev/null || true
   exit $1
 }
-travis_terminate 0 
+travis_terminate 0

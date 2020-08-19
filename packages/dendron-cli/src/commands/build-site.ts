bb71089334b59f41a6c430f5207eb478181938f2
kevinslin <kevin@thence.io>
2020-08-19 11:22:35 -0700

    chore: test change

--- packages/dendron-cli/src/commands/build-site.ts
+++ packages/dendron-cli/src/commands/build-site.ts
@@ -78,6 +78,7 @@ async function note2JekyllMdFile(
   if (note.parent?.fname === opts.noteRoot) {
     delete meta["parent"];
   }
+  // TEST
   // delete parent from root
   note.body = stripSiteOnlyTags(note);
   note.body = stripLocalOnlyTags(note.body);

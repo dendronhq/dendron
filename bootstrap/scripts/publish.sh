641676bb887d38b280d28b90b95f3476b5a29b86
kevinslin <kevin@thence.io>
2020-08-23 18:11:19 -0700

    chore: update publish script

--- bootstrap/scripts/publish.sh
+++ bootstrap/scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "upgrading..."
-npm run release -- --dry-run > /tmp/updates.txt
+npm run release -- --patch --no-verify --no-git-tag-version --no-push
 lerna version patch
 lerna publish from-package --ignore-scripts -y
 git push

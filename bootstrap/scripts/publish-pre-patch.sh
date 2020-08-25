88d118a1c963c8ca014b2e0df91f55f8cf1739fb
kevinslin <kevin@thence.io>
2020-08-25 09:22:58 -0700

    chore: update lerna publish

--- bootstrap/scripts/publish-pre-patch.sh
+++ bootstrap/scripts/publish-pre-patch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "upgrading..."
-npm run release -- --prerelease alpha --no-verify --no-git-tag-version --no-push
 lerna version prerelease
 lerna publish from-package --ignore-scripts -y
 git push

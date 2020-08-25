4a9cd916a18ed427e8dcfc6dac53b8be39c51180
kevinslin <kevin@thence.io>
2020-08-25 08:22:27 -0700

    chore: update lerna

--- bootstrap/scripts/publish.sh
+++ bootstrap/scripts/publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "upgrading..."
-npm run release -- --patch --no-verify --no-git-tag-version --no-push
+# npm run release -- --patch --no-verify --no-git-tag-version --no-push
 lerna version patch
 lerna publish from-package --ignore-scripts -y
 git push

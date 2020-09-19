8f298489c6d33901658ddace668170cf50a264eb
kevinslin <kevin@thence.io>
2020-09-18 19:20:21 -0700

    chore: comment out lsp server

--- packages/plugin-core/src/client.ts
+++ packages/plugin-core/src/client.ts
@@ -1,76 +1,70 @@
-import { fstat } from "fs";
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
+// import * as path from "path";
+// import { workspace, ExtensionContext } from "vscode";
 
-import * as path from "path";
-import { workspace, ExtensionContext } from "vscode";
+// import {
+//   LanguageClient,
+//   LanguageClientOptions,
+//   ServerOptions,
+//   TransportKind,
+// } from "vscode-languageclient";
+// import { Logger } from "./logger";
+// import fs from "fs-extra";
 
-import {
-  LanguageClient,
-  LanguageClientOptions,
-  ServerOptions,
-  TransportKind,
-} from "vscode-languageclient";
-import { Logger } from "./logger";
-import fs from "fs-extra";
+// let client: LanguageClient;
 
-let client: LanguageClient;
+// export function startClient(context: ExtensionContext) {
+//   const isDev = fs.existsSync(
+//     path.join("node_modules", "@dendronhq", "lsp-server", "lib", "index.js")
+//   );
+//   let serverModule: string;
 
-export function startClient(context: ExtensionContext) {
-  const isDev = fs.existsSync(
-    path.join("node_modules", "@dendronhq", "lsp-server", "lib", "index.js")
-  );
-  let serverModule: string;
+//   if (isDev) {
+//     serverModule = context.asAbsolutePath(
+//       path.join("node_modules", "@dendronhq", "lsp-server", "lib", "index.js")
+//     );
+//   } else {
+//     serverModule = context.asAbsolutePath(
+//       path.join("dist", "lsp-server", "lib", "index.js")
+//     );
+//   }
 
-  if (isDev) {
-    serverModule = context.asAbsolutePath(
-      path.join("node_modules", "@dendronhq", "lsp-server", "lib", "index.js")
-    );
-  } else {
-    serverModule = context.asAbsolutePath(
-      path.join("dist", "lsp-server", "lib", "index.js")
-    );
-  }
+//   Logger.info({ serverModule, msg: "starting client" });
+//   // The debug options for the server
+//   // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+//   let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
 
-  Logger.info({ serverModule, msg: "starting client" });
-  // The debug options for the server
-  // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-  let debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+//   // If the extension is launched in debug mode then the debug server options are used
+//   // Otherwise the run options are used
+//   let serverOptions: ServerOptions = {
+//     run: { module: serverModule, transport: TransportKind.ipc },
+//     debug: {
+//       module: serverModule,
+//       transport: TransportKind.ipc,
+//       options: debugOptions,
+//     },
+//   };
 
-  // If the extension is launched in debug mode then the debug server options are used
-  // Otherwise the run options are used
-  let serverOptions: ServerOptions = {
-    run: { module: serverModule, transport: TransportKind.ipc },
-    debug: {
-      module: serverModule,
-      transport: TransportKind.ipc,
-      options: debugOptions,
-    },
-  };
+//   let clientOptions: LanguageClientOptions = {
+//     documentSelector: [{ scheme: "file", language: "markdown" }],
+//     synchronize: {
+//       fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
+//     },
+//   };
 
-  let clientOptions: LanguageClientOptions = {
-    documentSelector: [{ scheme: "file", language: "markdown" }],
-    synchronize: {
-      fileEvents: workspace.createFileSystemWatcher("**/.clientrc"),
-    },
-  };
+//   // Create the language client and start the client.
+//   client = new LanguageClient(
+//     "dendron.lsp",
+//     "Dendron LSP",
+//     serverOptions,
+//     clientOptions
+//   );
 
-  // Create the language client and start the client.
-  client = new LanguageClient(
-    "dendron.lsp",
-    "Dendron LSP",
-    serverOptions,
-    clientOptions
-  );
+//   client.start();
+// }
 
-  client.start();
-}
-
-export function deactivate(): Thenable<void> | undefined {
-  if (!client) {
-    return undefined;
-  }
-  return client.stop();
-}
+// export function deactivate(): Thenable<void> | undefined {
+//   if (!client) {
+//     return undefined;
+//   }
+//   return client.stop();
+// }

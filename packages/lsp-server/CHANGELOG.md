facc5b1eccca7c2d468042fc3ff339a9d63e17b4
kevinslin <kevin@thence.io>
2020-09-24 12:49:35 -0700

    chore: rename files

--- packages/lsp-server/CHANGELOG.md
+++ packages/lsp-server/CHANGELOG.md
@@ -3,22 +3,22 @@
 All notable changes to this project will be documented in this file.
 See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
-## [0.12.3-alpha.2](https://github.com/dendronhq/dendron/compare/v0.12.3-alpha.1...v0.12.3-alpha.2) (2020-09-24)
+## [0.12.3-alpha.2](https://github.com/Microsoft/vscode-extension-samples/compare/v0.12.3-alpha.1...v0.12.3-alpha.2) (2020-09-24)
 
-**Note:** Version bump only for package @dendronhq/lsp-client-server
+**Note:** Version bump only for package @dendronhq/lsp-server
 
-## [0.12.3-alpha.1](https://github.com/dendronhq/dendron/compare/v0.12.3-alpha.0...v0.12.3-alpha.1) (2020-09-24)
+## [0.12.3-alpha.1](https://github.com/Microsoft/vscode-extension-samples/compare/v0.12.3-alpha.0...v0.12.3-alpha.1) (2020-09-24)
 
-**Note:** Version bump only for package @dendronhq/lsp-client-server
+**Note:** Version bump only for package @dendronhq/lsp-server
 
-## [0.12.3-alpha.0](https://github.com/dendronhq/dendron/compare/v0.12.2...v0.12.3-alpha.0) (2020-09-24)
+## [0.12.3-alpha.0](https://github.com/Microsoft/vscode-extension-samples/compare/v0.12.2...v0.12.3-alpha.0) (2020-09-24)
 
-**Note:** Version bump only for package @dendronhq/lsp-client-server
+**Note:** Version bump only for package @dendronhq/lsp-server
 
-## [0.12.2](https://github.com/dendronhq/dendron/compare/v0.12.2-alpha.0...v0.12.2) (2020-09-24)
+## [0.12.2](https://github.com/Microsoft/vscode-extension-samples/compare/v0.12.2-alpha.0...v0.12.2) (2020-09-24)
 
-**Note:** Version bump only for package @dendronhq/lsp-client-server
+**Note:** Version bump only for package @dendronhq/lsp-server
 
-## [0.12.2-alpha.0](https://github.com/dendronhq/dendron/compare/v0.12.1...v0.12.2-alpha.0) (2020-09-24)
+## [0.12.2-alpha.0](https://github.com/Microsoft/vscode-extension-samples/compare/v0.12.1...v0.12.2-alpha.0) (2020-09-24)
 
-**Note:** Version bump only for package @dendronhq/lsp-client-server
+**Note:** Version bump only for package @dendronhq/lsp-server
---
+++ packages/lsp-server/out/server.js
@@ -0,0 +1,198 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const engine_server_1 = require("@dendronhq/engine-server");
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+const vscode_languageserver_1 = require("vscode-languageserver");
+// import {app} from "@dendronhq/express-server";
+const vscode_languageserver_textdocument_1 = require("vscode-languageserver-textdocument");
+const vscode_uri_1 = require("vscode-uri");
+const utils_1 = require("./utils");
+// Create a connection for the server, using Node's IPC as a transport.
+// Also include all preview / proposed LSP features.
+let connection = vscode_languageserver_1.createConnection(vscode_languageserver_1.ProposedFeatures.all);
+utils_1.LSPUtils.instance(connection);
+// Create a simple text document manager.
+let documents = new vscode_languageserver_1.TextDocuments(vscode_languageserver_textdocument_1.TextDocument);
+let hasConfigurationCapability = false;
+let hasWorkspaceFolderCapability = false;
+let hasDiagnosticRelatedInformationCapability = false;
+connection.onInitialize((params) => {
+    connection.console.log("conneciton initializing");
+    let capabilities = params.capabilities;
+    // Does the client support the `workspace/configuration` request?
+    // If not, we fall back using global settings.
+    hasConfigurationCapability = !!(capabilities.workspace && !!capabilities.workspace.configuration);
+    hasWorkspaceFolderCapability = !!(capabilities.workspace && !!capabilities.workspace.workspaceFolders);
+    hasDiagnosticRelatedInformationCapability = !!(capabilities.textDocument &&
+        capabilities.textDocument.publishDiagnostics &&
+        capabilities.textDocument.publishDiagnostics.relatedInformation);
+    const result = {
+        capabilities: {
+            textDocumentSync: vscode_languageserver_1.TextDocumentSyncKind.Incremental,
+            // Tell the client that this server supports code completion.
+            completionProvider: {
+                resolveProvider: true,
+            },
+        },
+    };
+    if (hasWorkspaceFolderCapability) {
+        result.capabilities.workspace = {
+            workspaceFolders: {
+                supported: true,
+            },
+        };
+    }
+    // app.listen(3000, () => {
+    //   console.log("express server started");
+    // });
+    return result;
+});
+connection.onInitialized(async () => {
+    if (hasConfigurationCapability) {
+        // Register for all configuration changes.
+        connection.client.register(vscode_languageserver_1.DidChangeConfigurationNotification.type, undefined);
+    }
+    if (hasWorkspaceFolderCapability) {
+        connection.workspace.onDidChangeWorkspaceFolders((_event) => {
+            connection.console.log("Workspace folder change event received.");
+        });
+    }
+    const wsFolders = await utils_1.LSPUtils.wsFolders();
+    if (wsFolders && wsFolders[0]) {
+        const uri = vscode_uri_1.URI.parse(wsFolders[0].uri);
+        engine_server_1.DendronEngine.getOrCreateEngine({ root: uri.fsPath, forceNew: true });
+        connection.console.log("engine initialized");
+        // initialize express
+    }
+});
+// The global settings, used when the `workspace/configuration` request is not supported by the client.
+// Please note that this is not the case when using this server with the client provided in this example
+// but could happen with other clients.
+const defaultSettings = { maxNumberOfProblems: 1000 };
+let globalSettings = defaultSettings;
+// Cache the settings of all open documents
+let documentSettings = new Map();
+connection.onDidChangeConfiguration((change) => {
+    if (hasConfigurationCapability) {
+        // Reset all cached document settings
+        documentSettings.clear();
+    }
+    else {
+        globalSettings = ((change.settings.languageServerExample || defaultSettings));
+    }
+    // Revalidate all open text documents
+    documents.all().forEach(validateTextDocument);
+});
+function getDocumentSettings(resource) {
+    if (!hasConfigurationCapability) {
+        return Promise.resolve(globalSettings);
+    }
+    let result = documentSettings.get(resource);
+    if (!result) {
+        result = connection.workspace.getConfiguration({
+            scopeUri: resource,
+            section: "languageServerExample",
+        });
+        documentSettings.set(resource, result);
+    }
+    return result;
+}
+// Only keep settings for open documents
+documents.onDidClose((e) => {
+    documentSettings.delete(e.document.uri);
+});
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent((change) => {
+    validateTextDocument(change.document);
+});
+async function validateTextDocument(textDocument) {
+    connection.console.info("v2");
+    // In this simple example we get the settings for every validate run.
+    let settings = globalSettings;
+    // await getDocumentSettings(textDocument.uri);
+    // The validator creates diagnostics for all uppercase words length 2 and more
+    let text = textDocument.getText();
+    let pattern = /\b[A-Z]{2,}\b/g;
+    let m;
+    let problems = 0;
+    let diagnostics = [];
+    while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
+        problems++;
+        let diagnostic = {
+            severity: vscode_languageserver_1.DiagnosticSeverity.Warning,
+            range: {
+                start: textDocument.positionAt(m.index),
+                end: textDocument.positionAt(m.index + m[0].length),
+            },
+            message: `${m[0]} is all uppercase.`,
+            source: "ex",
+        };
+        if (hasDiagnosticRelatedInformationCapability) {
+            diagnostic.relatedInformation = [
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: "Spelling matters",
+                },
+                {
+                    location: {
+                        uri: textDocument.uri,
+                        range: Object.assign({}, diagnostic.range),
+                    },
+                    message: "Particularly for names",
+                },
+            ];
+        }
+        diagnostics.push(diagnostic);
+    }
+    // Send the computed diagnostics to VSCode.
+    connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+connection.onDidChangeWatchedFiles((_change) => {
+    // Monitored files have change in VSCode
+    connection.console.log("We received an file change event");
+});
+// This handler provides the initial list of the completion items.
+connection.onCompletion((_textDocumentPosition) => {
+    connection.console.error("BOND!");
+    // The pass parameter contains the position of the text document in
+    // which code complete got requested. For the example we ignore this
+    // info and always provide the same completion items.
+    return [
+        {
+            label: "TypeScript!!",
+            kind: vscode_languageserver_1.CompletionItemKind.Text,
+            data: 1,
+        },
+        {
+            label: "JavaScript!!",
+            kind: vscode_languageserver_1.CompletionItemKind.Text,
+            data: 2,
+        },
+    ];
+});
+// This handler resolves additional information for the item selected in
+// the completion list.
+connection.onCompletionResolve((item) => {
+    if (item.data === 1) {
+        item.detail = "TypeScript details!!";
+        item.documentation = "TypeScript documentation";
+    }
+    else if (item.data === 2) {
+        item.detail = "JavaScript details!!";
+        item.documentation = "JavaScript documentation";
+    }
+    return item;
+});
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+// Listen on the connection
+connection.listen();
+//# sourceMappingURL=server.js.map

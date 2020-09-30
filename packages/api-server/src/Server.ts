b3577c27aaf0cfdcd85c3c8c0e86dd890a0ad496
kevinslin <kevin@thence.io>
2020-09-30 12:42:13 -0700

    enhance(server): return version

--- packages/api-server/src/Server.ts
+++ packages/api-server/src/Server.ts
@@ -1,7 +1,10 @@
+import { findInParent } from "@dendronhq/common-server";
 import express, { Request, Response, NextFunction } from "express";
 import { BAD_REQUEST } from "http-status-codes";
 import morgan from "morgan";
 import { baseRouter } from "./routes";
+import fs from "fs-extra";
+import path from "path";
 
 const app = express();
 
@@ -13,6 +16,15 @@ if (process.env.NODE_ENV !== "production") {
 
 app.get("/health", async (_req: Request, res: Response) => {
   return res.json({ ok: 1 });
+});
+
+app.get("/version", async (_req: Request, res: Response) => {
+  const pkg = findInParent(__dirname, "package.json");
+  if (!pkg) {
+    throw Error("no pkg found");
+  }
+  const version = fs.readJSONSync(path.join(pkg, "package.json")).version;
+  return res.json({ version });
 });
 
 app.use("/api", baseRouter);

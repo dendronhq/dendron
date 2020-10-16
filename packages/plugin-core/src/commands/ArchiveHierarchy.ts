54c3cd7a0b91cf5c7200e34b326130603e5cd52e
kevinslin <kevin@thence.io>
2020-10-16 10:27:59 -0700

    enhance(server): migrate archive command

--- packages/plugin-core/src/commands/ArchiveHierarchy.ts
+++ packages/plugin-core/src/commands/ArchiveHierarchy.ts
@@ -1,8 +1,10 @@
 import { DNodeUtils } from "@dendronhq/common-all";
 import _ from "lodash";
 import { VSCodeUtils } from "../utils";
+import { DendronWorkspace } from "../workspace";
 import { BasicCommand } from "./base";
 import { RefactorHierarchyCommand } from "./RefactorHierarchy";
+import { RefactorHierarchyCommandV2 } from "./RefactorHierarchyV2";
 
 type CommandOpts = {
   match: string;
@@ -18,11 +20,15 @@ export class ArchiveHierarchyCommand extends BasicCommand<
   CommandOpts,
   CommandOutput
 > {
-  private refactorCmd: RefactorHierarchyCommand;
+  private refactorCmd: RefactorHierarchyCommand | RefactorHierarchyCommandV2;
 
   constructor(name?: string) {
     super(name);
-    this.refactorCmd = new RefactorHierarchyCommand();
+    if (DendronWorkspace.lsp()) {
+      this.refactorCmd = new RefactorHierarchyCommandV2();
+    } else {
+      this.refactorCmd = new RefactorHierarchyCommand();
+    }
   }
 
   async gatherInputs(): Promise<CommandInput | undefined> {

34e7a076abb87c1ae03d3b3092c6f082f82ea475
kevinslin <kevin@thence.io>
2020-10-24 11:40:02 -0700

    chore: fix compiler issues

--- packages/common-test-utils/src/utils.ts
+++ packages/common-test-utils/src/utils.ts
@@ -1,19 +1,29 @@
 import {
   DEngineClientV2,
   DEngineInitRespV2,
+  DLink,
+  DNodeTypeV2,
+  EngineDeleteOptsV2,
   EngineUpdateNodesOptsV2,
   EngineWriteOptsV2,
+  GetNoteOptsV2,
+  NotePropsDictV2,
   NotePropsV2,
+  QueryOptsV2,
   RenameNoteOptsV2,
+  SchemaModuleDictV2,
+  SchemaModulePropsV2,
 } from "@dendronhq/common-all";
 import { DendronAPI } from "@dendronhq/common-server";
 import { TestResult } from "./types";
 
-// @ts-ignore
 export class EngineAPIShim implements DEngineClientV2 {
   public api: DendronAPI;
   public wsRoot: string;
   public vaults: string[];
+  public notes: NotePropsDictV2;
+  public schemas: SchemaModuleDictV2;
+  public links: DLink[];
 
   constructor({
     api,
@@ -27,6 +37,9 @@ export class EngineAPIShim implements DEngineClientV2 {
     this.api = api;
     this.wsRoot = wsRoot;
     this.vaults = vaults;
+    this.notes = {};
+    this.schemas = {};
+    this.links = [];
   }
   async init() {
     const { api, wsRoot, vaults } = this;
@@ -39,6 +52,31 @@ export class EngineAPIShim implements DEngineClientV2 {
     };
     const resp = await api.workspaceInit(payload);
     return resp as DEngineInitRespV2;
+  }
+
+  async deleteNote(_id: string, _opts?: EngineDeleteOptsV2) {
+    return {} as any;
+  }
+  async deleteSchema(_id: string, _opts?: EngineDeleteOptsV2) {
+    return {} as any;
+  }
+
+  async getNoteByPath(_opts: GetNoteOptsV2) {
+    return {} as any;
+  }
+  async getSchema(_qs: string) {
+    return {} as any;
+  }
+
+  async querySchema(_qs: string) {
+    return {} as any;
+  }
+  async queryNotes?(_qs: string) {
+    return {} as any;
+  }
+
+  async query(_queryString: string, _mode: DNodeTypeV2, _opts?: QueryOptsV2) {
+    return {} as any;
   }
 
   async renameNote(opts: RenameNoteOptsV2) {
@@ -57,6 +95,12 @@ export class EngineAPIShim implements DEngineClientV2 {
       opts,
     });
     return resp;
+  }
+  async writeSchema(_schema: SchemaModulePropsV2) {
+    return {} as any;
+  }
+  async updateSchema(_schema: SchemaModulePropsV2) {
+    return {} as any;
   }
 }
 

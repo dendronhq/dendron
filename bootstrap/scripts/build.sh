d7f22aa61e9b80ed92cc851ea637782b0d5001d7
kevinslin <kevin@thence.io>
2020-11-08 16:07:26 -0800

    chore:  optimize build script

--- bootstrap/scripts/build.sh
+++ bootstrap/scripts/build.sh
@@ -7,12 +7,11 @@ npx lerna run build --scope @dendronhq/common-server  || exit 1
 npx lerna run build --scope @dendronhq/common-test-utils || exit 1
 
 npx lerna run build --scope @dendronhq/engine-server  || exit 1
+
 npx lerna run build --scope @dendronhq/lsp-server  || exit 1
 npx lerna run build --scope @dendronhq/api-server  || exit 1
 npx lerna run build --scope @dendronhq/dendron-next-server  || exit 1
 
-npx lerna run build --scope @dendronhq/dendron-cli  || exit 1
-npx lerna run build --scope @dendronhq/pods-core  || exit 1
-npx lerna run build --scope @dendronhq/seeds-core  || exit 1
+npx lerna run build --parallel --scope @dendronhq/dendron-cli  --scope @dendronhq/pods-core --scope @dendronhq/seeds-core || exit 1
 
 npx lerna run build --scope @dendronhq/plugin-core  || exit 1

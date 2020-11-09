1f6ed7e10967f52dc96a28c9a716b1ed05a3c204
kevinslin <kevin@thence.io>
2020-11-08 21:55:26 -0800

    chore: remove unused dep

--- packages/dendron-next-server/pages/_app.tsx
+++ packages/dendron-next-server/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
 import "../styles/globals.css";
-import 'react-sortable-tree/style.css'; // This only needs to be imported once in your app
 import 'antd/dist/antd.css'
 
 function App({ Component, pageProps }) {

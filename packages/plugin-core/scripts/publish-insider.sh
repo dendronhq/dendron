e61bf152f169a1beb5181887a5b2af8fb4cb5fcc
kevinslin <kevin@thence.io>
2020-11-12 06:55:29 -0800

    chore: update insiders script

--- packages/plugin-core/scripts/publish-insider.sh
+++ packages/plugin-core/scripts/publish-insider.sh
@@ -3,4 +3,4 @@ name=`cat package.json | jq ".name" -r`
 package="$name-$version.vsix"
 
 aws s3 cp $package s3://artifacts-prod-artifactb7980f61-19orqnnuurvwy/publish/$package
-echo artifacts-prod-artifactb7980f61-19orqnnuurvwy.s3.amazonaws.com/publish/$package
+echo https://artifacts-prod-artifactb7980f61-19orqnnuurvwy.s3.amazonaws.com/publish/$package

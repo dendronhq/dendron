4cc94a7bad039aa9860d2860d455c9eeb934b2f2
kevinslin <kevin@thence.io>
2020-11-14 08:41:58 -0800

    spike(ui): add stub for config interface

---
+++ packages/dendron-next-server/pages/workspace/configSample.tsx
@@ -0,0 +1,30 @@
+import {DendronConfig} from "@dendronhq/common-all";
+
+const getConfigData = (): {data: DendronConfig} => {
+    return {
+        data: {
+            vaults: [{
+                fsPath: "vault"
+            }],
+            site: {
+                siteHierarchies: ["dendron"],
+                siteRootDir: "docs"
+            }
+        }
+    }
+}
+
+const saveConfigData = (data: DendronConfig) => {
+    console.log("data saved")
+}
+
+export default function ConfigSamplePage() {
+    const {data} = getConfigData() 
+    return <div>
+        <h1>Config Data</h1>
+        <pre>
+            {JSON.stringify(data)}
+        </pre>
+        <button onClick={()=>(saveConfigData(data))}>Save Config</button>
+    </div>
+}

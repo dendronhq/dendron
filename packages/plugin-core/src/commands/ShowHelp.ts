02fc08a907e196b39c23db36b82565c15588673f
kevinslin <kevin@thence.io>
2020-12-13 09:27:23 -0800

    fix: help command

--- packages/plugin-core/src/commands/ShowHelp.ts
+++ packages/plugin-core/src/commands/ShowHelp.ts
@@ -1,6 +1,4 @@
-import _ from "lodash";
-import path from "path";
-import { env, Uri, window } from "vscode";
+import { env, Uri } from "vscode";
 import { DENDRON_COMMANDS } from "../constants";
 import { BasicCommand } from "./base";
 
@@ -13,22 +11,7 @@ type CommandOutput = void;
 export class ShowHelpCommand extends BasicCommand<CommandOpts, CommandOutput> {
   static key = DENDRON_COMMANDS.SHOW_HELP.key;
   async gatherInputs(): Promise<CommandInput | undefined> {
-    const resp = await window.showInputBox({
-      prompt: "Select your folder for dendron",
-      ignoreFocusOut: true,
-      validateInput: (input: string) => {
-        if (!path.isAbsolute(input)) {
-          if (input[0] !== "~") {
-            return "must enter absolute path";
-          }
-        }
-        return undefined;
-      },
-    });
-    if (_.isUndefined(resp)) {
-      return;
-    }
-    return;
+    return {};
   }
   async execute() {
     env.openExternal(

9632457bb3b02749e50568d8fc405a72ebd8fcb3
kevinslin <kevin@thence.io>
2020-12-21 10:43:25 -0800

    chore: update tests

--- packages/engine-server/src/markdown/remark/__tests__/utils.spec.ts
+++ packages/engine-server/src/markdown/remark/__tests__/utils.spec.ts
@@ -10,6 +10,54 @@ import { createEngine, createProcTests } from "./utils";
 
 const IMAGE_LINK = `![alt-text](image-url.jpg)`;
 
+const WITH_ASSET_PREFIX_UNDEFINED = createProcTests({
+  name: "asset_prefix undefined",
+  setupFunc: async ({ engine, vaults, extra }) => {
+    const proc = await MDUtilsV4.procFull({
+      engine,
+      dest: extra.dest,
+      vault: vaults[0],
+      publishOpts: {
+        assetsPrefix: undefined,
+      },
+    });
+    const resp = proc.processSync(IMAGE_LINK);
+    return { resp, proc };
+  },
+  verifyFuncDict: {
+    [DendronASTDest.MD_REGULAR]: async ({ extra }) => {
+      const { resp } = extra;
+      expect(resp).toMatchSnapshot();
+      return [
+        {
+          actual: _.trim(resp.toString()),
+          expected: "![alt-text](image-url.jpg)",
+        },
+      ];
+    },
+    [DendronASTDest.HTML]: async ({ extra }) => {
+      const { resp } = extra;
+      expect(resp).toMatchSnapshot();
+      return [
+        {
+          actual: _.trim(resp.toString()),
+          expected: "![alt-text](image-url.jpg)",
+        },
+      ];
+    },
+    [DendronASTDest.MD_ENHANCED_PREVIEW]: async ({ extra }) => {
+      const { resp } = extra;
+      return [
+        {
+          actual: _.trim(resp.toString()),
+          expected: "![alt-text](image-url.jpg)",
+        },
+      ];
+    },
+  },
+  preSetupHook: ENGINE_HOOKS.setupBasic,
+});
+
 const WITH_ASSET_PREFIX = createProcTests({
   name: "asset_prefix",
   setupFunc: async ({ engine, vaults, extra }) => {
@@ -58,7 +106,7 @@ const WITH_ASSET_PREFIX = createProcTests({
   preSetupHook: ENGINE_HOOKS.setupBasic,
 });
 
-const ALL_TEST_CASES = [...WITH_ASSET_PREFIX];
+const ALL_TEST_CASES = [...WITH_ASSET_PREFIX, ...WITH_ASSET_PREFIX_UNDEFINED];
 
 describe("html dest", () => {
   test.each(

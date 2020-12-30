716c161db2553c0e6f4d140dd2f8eb8dadb90889
Luke Carrier <luke@carrier.im>
2020-12-30 12:25:27 +0000

    chore: use npm consistently, not yarn
    yarn doesn't offer an equivalent to npm ci which makes it difficult to guarantee
    that dependency versions match those resolved on developer systems. Using the
    two also led to inconsustencies in resolved versions.

--- packages/plugin-core/scripts/sync_static.sh
+++ packages/plugin-core/scripts/sync_static.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-
-
 echo "building next-server"
 pushd ../dendron-next-server
-yarn
-yarn build
+npm install
+npm build
 
 rsync -avq out/ ../plugin-core/assets/static/ --delete
 popd

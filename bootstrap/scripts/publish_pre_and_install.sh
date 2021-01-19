b17ebfbcb434f67761973220dbb079b385ede1cb
kevinslin <kevin@thence.io>
2021-01-18 18:53:52 -0800

    chore: update publish script

--- bootstrap/scripts/publish_pre_and_install.sh
+++ bootstrap/scripts/publish_pre_and_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-./publish-pre-patch.sh
+./bootstrap/scripts/publish-pre-patch.sh
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Error"
     exit
 fi
-./sleep.sh
+./bootstrap/scripts/sleep.sh
 
-pushd ../../build/dendron/packages/plugin-core
+pushd build/dendron/packages/plugin-core
 ./scripts/pack_and_install.sh
 popd

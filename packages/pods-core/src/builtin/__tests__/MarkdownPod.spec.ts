94103fdbf476c331373a6be2359b092940629229
Mark Hyunik Choi <hikchoi@gmail.com>
2021-01-26 03:06:42 +0900

    chore: add more tests for MarkdownPod (#458)
    * chore: add more tests for MarkdownPod
    
    * chore: remove unnecessary MarkdownPublishPod tests
    
    * chore: add tests for dendron specific markdown.

--- packages/pods-core/src/builtin/__tests__/MarkdownPod.spec.ts
+++ packages/pods-core/src/builtin/__tests__/MarkdownPod.spec.ts
@@ -78,4 +78,107 @@ describe("MarkdownPublishPod", () => {
       }
     );
   });
+
+  test("empty note", async () => {
+    await runEngineTestV5(
+      async ({ engine, wsRoot, vaults }) => {
+        const fname = "empty";
+        const vault = vaults[0];
+        const vaultName = VaultUtils.getName(vault);
+        const resp = await new MarkdownPublishPod().execute({
+          engine,
+          vaults,
+          wsRoot,
+          config: {
+            fname,
+            vault: vaultName,
+            dest: "stdout",
+          },
+        });
+        expect(resp).toEqual("");
+      },
+      {
+        expect,
+        preSetupHook: ENGINE_HOOKS.setupEmpty,
+      }
+    );
+  });
+
+  test("notes with links", async () => {
+    await runEngineTestV5(
+      async ({ engine, wsRoot, vaults }) => {
+        const vault = vaults[0];
+        const vaultName = VaultUtils.getName(vault);
+        const fnames = ["alpha", "beta", "omega"];
+        const resp = await Promise.all(
+          fnames.map(async (fname) => {
+            return new MarkdownPublishPod().execute({
+              engine,
+              vaults,
+              wsRoot,
+              config: {
+                fname,
+                vault: vaultName,
+                dest: "stdout",
+              },
+            });
+          })
+        );
+        expect(resp).toEqual([
+          "[[beta]]",
+          "[[alpha#h3]]",
+          "[[some label|beta]]",
+        ]);
+      },
+      {
+        expect,
+        preSetupHook: ENGINE_HOOKS.setupLinks,
+      }
+    );
+  });
+
+  test("notes with refs", async () => {
+    await runEngineTestV5(
+      async ({ engine, wsRoot, vaults }) => {
+        const vault = vaults[0];
+        const vaultName = VaultUtils.getName(vault);
+        const fnames = [
+          "simple-note-ref",
+          "simple-block-ref",
+          "simple-block-range-ref",
+          "ref-offset",
+          "wildcard-child-ref",
+          "wildcard-header-ref",
+          "wildcard-complex-ref",
+        ];
+        const resp = await Promise.all(
+          fnames.map(async (fname) => {
+            return new MarkdownPublishPod().execute({
+              engine,
+              vaults,
+              wsRoot,
+              config: {
+                fname,
+                vault: vaultName,
+                dest: "stdout",
+              },
+            });
+          })
+        );
+        expect(resp).toEqual([
+          "![[simple-note-ref.one]]",
+          "![[simple-block-ref.one#intro]]",
+          "![[simple-block-range-ref.one#head1:#head3]]",
+          "![[ref-offset.one#head1,1]]",
+          "![[wildcard-child-ref.*]]",
+          "![[wildcard-header-ref.one#head1:#*]]",
+          "![[wildcard-complex.*#head1,1]]",
+        ]);
+      },
+      {
+        expect,
+        preSetupHook: ENGINE_HOOKS.setupRefs,
+      }
+    );
+  });
 });

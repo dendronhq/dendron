1398c0cfbc806de8b6e77f951e3ee71c81e7af35
hikchoi <hikchoi@gmail.com>
2021-02-15 07:44:45 +0900

    chore: add FrontmatterFoldingRangeProvidere docs

--- packages/plugin-core/src/features/FrontmatterFoldingRangeProvider.ts
+++ packages/plugin-core/src/features/FrontmatterFoldingRangeProvider.ts
@@ -3,6 +3,11 @@ import vscode from "vscode";
 
 export default class FrontmatterFoldingRangeProvider
   implements vscode.FoldingRangeProvider {
+  /**
+   * Returns the folding range of the frontmatter section of a markdown note.
+   * @param document The document we want to find the folding range.
+   * @returns The frontmatter folding range of given Dendron note as an array.
+   */
   public async provideFoldingRanges(
     document: vscode.TextDocument
   ): Promise<vscode.FoldingRange[]> {

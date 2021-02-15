00b20213af3ffe9a699659362f58696edf452b37
kevinslin <kevin@thence.io>
2021-02-15 15:59:34 -0800

    chore: update plugin dep

--- packages/plugin-core/src/utils/site.ts
+++ packages/plugin-core/src/utils/site.ts
@@ -16,7 +16,7 @@ const packageJson = {
   main: "index.js",
   license: "MIT",
   dependencies: {
-    "@dendronhq/dendron-11ty": "^1.28.5",
+    "@dendronhq/dendron-11ty": "^1.28.6-alpha.0",
   },
 };
 

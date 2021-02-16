a2fba1d06b0ba110be1f95d72229eb361565a21f
kevinslin <kevin@thence.io>
2021-02-15 19:00:46 -0800

    chore: bump site dep

--- packages/plugin-core/src/utils/site.ts
+++ packages/plugin-core/src/utils/site.ts
@@ -16,7 +16,7 @@ const packageJson = {
   main: "index.js",
   license: "MIT",
   dependencies: {
-    "@dendronhq/dendron-11ty": "^1.28.6-alpha.0",
+    "@dendronhq/dendron-11ty": "^1.29.0",
   },
 };
 

ab386adb823897be874ac367e1fca98565494a09
Nikita Voloboev <nikita.voloboev@gmail.com>
2021-02-25 02:44:27 +0100

    show backlins only if they exist

--- packages/engine-server/src/markdown/remark/backlinks.ts
+++ packages/engine-server/src/markdown/remark/backlinks.ts
@@ -1,13 +1,16 @@
 import { NoteUtilsV2 } from "@dendronhq/common-all";
 import { MDUtilsV4 } from "@dendronhq/engine-server";
-import { link, paragraph, text } from "mdast-builder";
+import { Root } from "mdast";
+import { paragraph } from "mdast-builder";
 import Unified, { Plugin } from "unified";
+import { Node } from "unist";
 import u from "unist-builder";
 
-// Plugin that adds backlinks at the end each page if they exist
+// Plugin that adds backlinks at the end of each page if they exist
 const plugin: Plugin = function (this: Unified.Processor) {
   const proc = this;
-  function transformer(tree) {
+  function transformer(tree: Node) {
+    let root = tree as Root;
     const { fname, vault } = MDUtilsV4.getDendronData(proc);
     const { engine } = MDUtilsV4.getEngineFromProc(proc);
     const note = NoteUtilsV2.getNotesByFname({
@@ -17,19 +20,27 @@ const plugin: Plugin = function (this: Unified.Processor) {
     });
 
     if (note[0].links.length > 0) {
-      tree.children.push(u("heading", { depth: 2 }, [u("text", "Backlinks")]));
-
-      let backlinks = [];
+      let backlinks: Array<Node> = [];
       note[0].links.map((mdLink) => {
         if (mdLink.type === "backlink" && mdLink.from.fname) {
           backlinks.push(
-            paragraph(
-              link(mdLink.from.fname, undefined, text(mdLink.from.fname))
-            )
+            paragraph({
+              type: "wikiLink",
+              value: mdLink.from.fname,
+              data: {
+                alias: mdLink.from.fname,
+                vault: note[0].vault,
+              },
+            })
           );
         }
       });
-      tree.children.push(paragraph(backlinks));
+      if (backlinks.length > 0) {
+        root.children.push(
+          u("heading", { depth: 2 }, [u("text", "Backlinks")])
+        );
+        root.children.push(paragraph(backlinks));
+      }
     }
   }
   return transformer;

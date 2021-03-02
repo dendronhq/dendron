d9ca2e1ae0688fa96d1f767923b678f89eb91a76
Nikita Voloboev <nikita.voloboev@gmail.com>
2021-03-03 00:39:57 +0100

    wip

--- packages/engine-server/src/changelog/changelog.ts
+++ packages/engine-server/src/changelog/changelog.ts
@@ -5,35 +5,91 @@ import execa from "execa";
 export async function generateChangelog(engine: DEngineClientV2) {
   // TODO: works only on one vault, make it work for multiple
   let vault = engine.vaultsv3[0].fsPath;
-  getChanges(vault).then(function (changes) {
+  // console.log(engine.wsRoot, "root")
+  getChanges(engine.wsRoot, vault).then(function (changes) {
     console.log(changes, "changes");
   });
 }
 
-// get files changed/added for a given vault
-async function getChanges(vault: any) {
-  let filesChanged: string[] = [];
-  let filesAdded: string[] = [];
+// {
+//   commitDate: 'Mar 2 2021',
+//   commitHash: "'2602c8d5'"
+//   changes: [{
+//       action: "added",
+//       fname: "new.md",
+//       diff: "..."
+//   },
+//   ...
+//   ]
+// }
+
+// get files changed/added for a repo for the last commit
+async function getChanges(path: string, vault: string) {
+  // let filesChanged: string[] = [];
+  // let filesAdded: string[] = [];
   let commitDate: string = "";
   let commitHash: string = "";
+  let changes: any[] = [];
+  let filesChanged: string[] = [];
 
+  // get last commit hash
   try {
-    const { stdout } = await execa("git", ["status", vault]);
+    const { stdout } = await execa(
+      "git",
+      [`log`, `--pretty=format:'%h'`, `-n`, `1`],
+      { cwd: path }
+    );
+    commitHash = stdout;
+  } catch (error) {
+    console.log(error);
+  }
+
+  // get files changed/added
+  try {
+    const { stdout } = await execa("git", ["show", "--name-status"], {
+      cwd: path,
+    });
     let status = stdout.split("\n");
     status.map((result) => {
-      if (result.startsWith(" M")) {
-        filesChanged.push(result.replace(` M ${vault}/`, ""));
-      } else if (result.startsWith("??")) {
-        filesAdded.push(result.replace(`?? ${vault}/`, ""));
+      if (result.startsWith("M")) {
+        let filePath = result.split(" ")[0].substring(2);
+        filesChanged.push(filePath);
+        changes.push({
+          action: "modified",
+          fname: filePath,
+        });
+      } else if (result.startsWith("A")) {
+        let filePath = result.split(" ")[0].substring(2);
+        filesChanged.push(filePath);
+        changes.push({
+          action: "added",
+          fname: filePath,
+        });
       }
     });
   } catch (error) {
     console.log(error);
   }
 
+  changes.map((change) => {
+    try {
+      const { stdout } = await execa("git", [
+        "show",
+        commitHash,
+        `-- ${change.fname}`,
+      ]);
+      console.log(stdout, "DIFF");
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
   // get date of last commit
   try {
-    const { stdout } = await execa("git", ["log", "-1", "--format=%cd"]);
+    const { stdout } = await execa("git", ["log", "-1", "--format=%cd"], {
+      cwd: path,
+    });
+    console.log(stdout, "OUTT");
     let date = stdout.split(/\s+/).slice(1, 5);
     let day = date[0];
     let month = date[1];
@@ -43,24 +99,12 @@ async function getChanges(vault: any) {
     console.log(error);
   }
 
-  // get last commit hash
-  try {
-    const { stdout } = await execa("git", [
-      `log`,
-      `--pretty=format:'%h'`,
-      `-n`,
-      `1`,
-    ]);
-    commitHash = stdout;
-  } catch (error) {
-    console.log(error);
-  }
-
   return {
-    filesAdded: filesAdded,
-    filesChanged: filesChanged,
     commitDate: commitDate,
     commitHash: commitHash,
+    changes: changes,
+    // filesAdded: filesAdded,
+    // filesChanged: filesChanged,
   };
 }
 

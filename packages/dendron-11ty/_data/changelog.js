013b3f61e49dcd200658e52d0a2b1664b2eb189c
Nikita Voloboev <nikita.voloboev@gmail.com>
2021-03-05 12:57:33 +0100

    clean

--- packages/dendron-11ty/_data/changelog.js
+++ packages/dendron-11ty/_data/changelog.js
@@ -2,8 +2,6 @@ const fs = require("fs-extra");
 
 async function getChangelog() {
   const changes = fs.readJSONSync("/tmp/changes.json");
-  console.log(changes.commits, "commits")
-  // let changes = []
   return changes.commits
 }
 

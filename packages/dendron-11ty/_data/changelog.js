11d0dcaf47aa7f1ad771e37bcb1ca5ee37986595
kevinslin <kevin@thence.io>
2021-03-06 18:23:00 -0800

    chore: no crash on missing changes

--- packages/dendron-11ty/_data/changelog.js
+++ packages/dendron-11ty/_data/changelog.js
@@ -2,6 +2,10 @@ const fs = require("fs-extra");
 const {env} = require("../libs/utils")
 
 async function getChangelog() {
+  const changePath = env().wsRoot + "/build/changes.json";
+  if (!fs.existsSync(changePath)) {
+    return [];
+  }
   const changes = fs.readJSONSync(env().wsRoot + "/build/changes.json");
   if (changes.commits.length > 0) {
     return changes.commits

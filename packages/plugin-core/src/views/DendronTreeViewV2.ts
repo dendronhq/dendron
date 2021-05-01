7cf17e4ecaddab4c5fb4b44130489fb63d45a460
kevinslin <kevinslin8@gmail.com>
2021-04-30 20:00:50 -0700

    spike: treeview work

---
+++ packages/plugin-core/src/views/DendronTreeViewV2.ts
@@ -0,0 +1,90 @@
+import path from "path";
+import * as vscode from "vscode";
+
+export class DendronTreeViewV2 implements vscode.WebviewViewProvider {
+  public static readonly viewType = "dendron.treeViewV2";
+
+  private _view?: vscode.WebviewView;
+
+  constructor(private readonly _extensionUri: vscode.Uri) {}
+
+  public resolveWebviewView(
+    webviewView: vscode.WebviewView,
+    _context: vscode.WebviewViewResolveContext,
+    _token: vscode.CancellationToken
+  ) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage((data) => {
+      switch (data.type) {
+        default:
+          console.log("got data", data);
+          break;
+      }
+    });
+  }
+
+  public addColor() {
+    if (this._view) {
+      this._view.show?.(true); // `show` is not implemented in 1.49 but is for 1.50 insiders
+      this._view.webview.postMessage({ type: "addColor" });
+    }
+  }
+
+  private _getHtmlForWebview(webview: vscode.Webview) {
+    const baseDir = path.join(
+      this._extensionUri.fsPath,
+      "assets",
+      "dendronTreeViewV2"
+    );
+    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.file(path.join(baseDir, "js", "main.js"))
+    );
+    // Do the same for the stylesheet.
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    const nonce = getNonce();
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+				<title>Cat Colors</title>
+			</head>
+			<body>
+                <iframe src="http://localhost:3000/vscode/tree-view" style="border: none;">
+			</body>
+			</html>`;
+  }
+}
+
+function getNonce() {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}

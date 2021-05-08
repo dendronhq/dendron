5ae57faea62799deae7e054c8f9cad6630fffd25
kevinslin <kevinslin8@gmail.com>
2021-05-08 09:22:53 -0700

    chore: add comments

--- packages/common-all/src/types/foundation.ts
+++ packages/common-all/src/types/foundation.ts
@@ -29,20 +29,62 @@ export type DNodePointer = string;
  * Props are the official interface for a node
  */
 export type DNodeProps<T = any, TCustom = any> = {
+  /**
+   * Unique id of a note
+   */
   id: string;
+  /**
+   * Node title
+   */
   title: string;
+  /**
+   * Node description
+   */
   desc: string;
+  /**
+   * Node links (eg. backlinks, wikilinks, etc)
+   */
   links: DLink[];
+  /**
+   * Name of the node. This corresponds to the name of the file minus the extension
+   */
   fname: string;
+  /**
+   * Whether this node is a note or a schema
+   */
   type: DNodeType;
+  /**
+   * Last updated
+   */
   updated: number;
+  /**
+   * Created
+   */
   created: number;
+  /**
+   * Determines whether this node is a {@link stub https://wiki.dendron.so/notes/c6fd6bc4-7f75-4cbb-8f34-f7b99bfe2d50.html#stubs}
+   */
   stub?: boolean;
+  /**
+   @deprecated
+   */
   schemaStub?: boolean;
+  /**
+   * Immediate parent
+   */
   parent: DNodePointer | null;
+  /**
+   * Immediate children
+   */
   children: DNodePointer[];
   data: T;
+  /**
+   * Body of the note
+   */
   body: string;
+  /**
+   * Custom frontmatter
+   */
   custom?: TCustom;
   schema?: { moduleId: string; schemaId: string };
   vault: DVault;

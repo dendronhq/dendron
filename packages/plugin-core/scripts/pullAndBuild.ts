4df490da67264a32fecb618c2b60e3f285ff33bc
Kevin S Lin <kevin@dendron.so>
2021-05-22 20:45:57 +0000

    chore: better build script

--- packages/plugin-core/scripts/pullAndBuild.ts
+++ packages/plugin-core/scripts/pullAndBuild.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
 import yargs from "yargs";
+import fs from "fs-extra";
 import { execa } from "@dendronhq/dendron-cli";
 
 const $ = execa.commandSync;
 
 function checkoutInteg() {
+  console.log("checkout...");
   $(`echo "checkout..."`);
   $(`git reset --hard`);
   $(`git clean -f`);
@@ -15,20 +17,49 @@ function checkoutInteg() {
   $(`git checkout --track origin/integ-publish`);
 }
 
+function syncStatic() {
+  console.log("sync static...");
+  const { stdout, stderr } = $(`./scripts/sync_static.sh`);
+  console.log({ stdout, stderr });
+}
+
+function updatePkgJSON() {
+  console.log("update pkg...");
+  $(`rm ../../package.json`);
+  const pkg = fs.readJSONSync("package.json");
+  pkg.name = "dendron";
+  pkg.main = "./dist/extension.js";
+  pkg.repository = {
+    url: "https://github.com/dendronhq/dendron.git",
+    type: "git",
+  };
+  fs.writeJSONSync("package.json", pkg, { spaces: 4 });
+}
+
+function installAndPkg() {
+  console.log("installing...");
+  $(`yarn install --no-lockfile`);
+  console.log("packaging...");
+  $(`vsce package --yarn`);
+}
+
+function getVersion() {
+  return fs.readJSONSync("package.json").version;
+}
+
 function main() {
-  let args = yargs
-    .option("input", {
-      alias: "i",
-      demand: true,
-    })
-    .option("year", {
-      alias: "y",
-      description: "Year number",
-      demand: true,
-    }).argv;
-  console.log(args);
+  let args = yargs.option("--no-sync-static", {
+    alias: "S",
+    description: "don't compile static assets",
+  }).argv;
   checkoutInteg();
-  console.log("done");
+  if (!args["--no-sync-static"]) {
+    syncStatic();
+  }
+  updatePkgJSON();
+  installAndPkg();
+  const version = getVersion();
+  console.log(`build ${version}`);
 }
 
 main();

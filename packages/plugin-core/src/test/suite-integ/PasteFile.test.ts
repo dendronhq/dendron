61407df5bac4282c480ff521ab39f6568e306ee1
Kevin Lin <kevin@dendron.so>
2021-05-26 09:40:43 -0700

    fix: handle file names with non-ascii in paste file

--- packages/plugin-core/src/test/suite-integ/PasteFile.test.ts
+++ packages/plugin-core/src/test/suite-integ/PasteFile.test.ts
@@ -8,6 +8,7 @@ import { clipboard, VSCodeUtils } from "../../utils";
 import { runLegacyMultiWorkspaceTest, setupBeforeAfter } from "../testUtilsV3";
 import { vault2Path } from "@dendronhq/common-server";
 import { ERROR_STATUS } from "@dendronhq/common-all";
+import _ from "lodash";
 
 suite("PasteFile", function () {
   let ctx: vscode.ExtensionContext;
@@ -44,6 +45,7 @@ suite("PasteFile", function () {
 
         // run cmd
         await new PasteFileCommand().run();
+
         const dstPath = path.join(
           vault2Path({ vault: note.vault, wsRoot }),
           "assets",
@@ -56,6 +58,41 @@ suite("PasteFile", function () {
         const pos2 = new vscode.Position(8, 33);
         expect(editor.document.getText(new vscode.Range(pos1, pos2))).toEqual(
           `[apples.pdf](${path.join("assets", "apples.pdf")})`
+        );
+        done()
+      },
+    });
+  });
+
+  test("with space in file name", (done) => {
+    runLegacyMultiWorkspaceTest({
+      ctx,
+      preSetupHook: ENGINE_HOOKS.setupBasic,
+      onInit: async ({ engine, wsRoot }) => {
+        const tmpRoot = FileTestUtils.tmpDir().name;
+        const fname = "red apples~";
+        const fakeAsset = path.join(tmpRoot, `${fname}.pdf`);
+        fs.writeFileSync(fakeAsset, "data");
+        clipboard.writeText(fakeAsset);
+        const note = engine.notes["foo"];
+        const editor = await VSCodeUtils.openNote(note);
+        editor.selection = new vscode.Selection(8, 0, 8, 12);
+
+        // run cmd
+        await new PasteFileCommand().run();
+        const cleanFileName = _.kebabCase(fname) + ".pdf";
+        const dstPath = path.join(
+          vault2Path({ vault: note.vault, wsRoot }),
+          "assets",
+          cleanFileName,
+        );
+        expect(fs.existsSync(dstPath)).toBeTruthy();
+        expect(fs.readFileSync(dstPath, { encoding: "utf8" })).toEqual("data");
+        editor.selection = new vscode.Selection(8, 0, 8, 12);
+        const pos1 = new vscode.Position(8, 0);
+        const pos2 = new vscode.Position(8, 50);
+        expect(editor.document.getText(new vscode.Range(pos1, pos2))).toEqual(
+          `[red-apples.pdf](${path.join("assets", cleanFileName)})`
         );
         done();
       },

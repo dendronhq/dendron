88fd453c24e8a7a1f8983d82635e75318e848cd9
Henry Fellerhoff <henryfellerhoff@gmail.com>
2021-05-31 13:20:15 -0500

    feat: open note on node select

--- packages/plugin-core/src/commands/ShowNoteGraph.ts
+++ packages/plugin-core/src/commands/ShowNoteGraph.ts
@@ -1,10 +1,16 @@
 import _ from "lodash";
-import { DendronWebViewKey } from "@dendronhq/common-all";
+import {
+  DendronWebViewKey,
+  GraphViewMessage,
+  GraphViewMessageType,
+} from "@dendronhq/common-all";
 import { ViewColumn, window } from "vscode";
 import { DENDRON_COMMANDS } from "../constants";
 import { WebViewUtils } from "../views/utils";
 import { BasicCommand } from "./base";
-import { getWS } from "../workspace";
+import { getEngine, getWS } from "../workspace";
+import { GotoNoteCommand } from "./GotoNote";
+import { VSCodeUtils } from "../utils";
 
 type CommandOpts = {};
 
@@ -19,25 +25,30 @@ export class ShowNoteGraphCommand extends BasicCommand<
     return {};
   }
   async execute() {
-    const title = "Dendron Note Graph";
+    const title = "Dendron Graph";
 
     // Get workspace information
     const ws = getWS();
 
     // If panel already exists
     const existingPanel = ws.getWebView(DendronWebViewKey.NOTE_GRAPH);
+
     if (!_.isUndefined(existingPanel)) {
-      existingPanel.reveal();
-      return;
+      try {
+        // If error, panel disposed and needs to be recreated
+        existingPanel.reveal();
+        return;
+      } catch {}
     }
 
     // If panel does not exist
     const panel = window.createWebviewPanel(
       "dendronIframe", // Identifies the type of the webview. Used internally
       title, // Title of the panel displayed to the user
-      ViewColumn.One, // Editor column to show the new webview panel in.
+      ViewColumn.Two, // Editor column to show the new webview panel in.
       {
         enableScripts: true,
+        retainContextWhenHidden: true,
       } // Webview options. More on these later.
     );
 
@@ -47,6 +58,48 @@ export class ShowNoteGraphCommand extends BasicCommand<
     });
 
     panel.webview.html = resp;
+
+    // Listener
+    panel.webview.onDidReceiveMessage(async (msg: GraphViewMessage) => {
+      switch (msg.type) {
+        case GraphViewMessageType.onSelect: {
+          const note = getEngine().notes[msg.data.id];
+          await new GotoNoteCommand().execute({
+            qs: note.fname,
+            vault: note.vault,
+          });
+          break;
+        }
+        // case GraphViewMessageType.onGetActiveEditor: {
+        //   const document = VSCodeUtils.getActiveTextEditor()?.document;
+        //   if (document) {
+        //     if (
+        //       !getWS().workspaceService?.isPathInWorkspace(document.uri.fsPath)
+        //     ) {
+        //       // not in workspace
+        //       return;
+        //     }
+        //     const note = VSCodeUtils.getNoteFromDocument(document);
+        //     if (note) {
+        //       // refresh note
+        //       this.refresh(note);
+        //     }
+        //   }
+        //   break;
+        // }
+        // case GraphViewMessageType.onReady: {
+        //   const profile = getDurationMilliseconds(start);
+        //   Logger.info({ ctx, msg: "treeViewLoaded", profile, start });
+        //   AnalyticsUtils.track(VSCodeEvents.TreeView_Ready, {
+        //     duration: profile,
+        //   });
+        //   break;
+        // }
+        default:
+          console.log("got data", msg);
+          break;
+      }
+    });
 
     // Update workspace-wide graph panel
     ws.setWebView(DendronWebViewKey.NOTE_GRAPH, panel);

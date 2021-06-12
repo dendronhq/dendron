486709b2d24a6f9311162f0989e6a417875c0483
Kevin Lin <kevin@dendron.so>
2021-06-11 17:18:05 -0700

    chore: bump site deps

--- packages/engine-test-utils/src/index.ts
+++ packages/engine-test-utils/src/index.ts
@@ -3,3 +3,19 @@ export * from "./engine";
 export * from "./topics";
 export { GitTestUtils, checkVaults } from "./utils";
 export * from "./presets";
+import {
+  ENGINE_HOOKS,
+  ENGINE_HOOKS_MULTI,
+  ENGINE_QUERY_PRESETS,
+  ENGINE_RENAME_PRESETS,
+  ENGINE_WRITE_PRESETS,
+  PODS_CORE,
+} from "./presets";
+export {
+  ENGINE_HOOKS,
+  ENGINE_HOOKS_MULTI,
+  PODS_CORE,
+  ENGINE_QUERY_PRESETS,
+  ENGINE_WRITE_PRESETS,
+  ENGINE_RENAME_PRESETS,
+};

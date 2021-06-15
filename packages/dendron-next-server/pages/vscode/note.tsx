7f25ae3e75db8c336e20fe969e5c645bcf015286
Samuel Hobl <nam@hobl.at>
2021-06-15 11:21:41 +0200

    spike: add clickhandler for anchore tags

--- packages/dendron-next-server/pages/vscode/note.tsx
+++ packages/dendron-next-server/pages/vscode/note.tsx
@@ -16,23 +16,47 @@ function Note({ engine, ide }: DendronProps) {
 
   const dispatch = engineHooks.useEngineAppDispatch();
 
-  const noteId = ide.noteActive?.id;
+  const { noteActive } = ide;
+  const noteId = noteActive?.id || "05774b2e-ebf7-4bbc-8171-ad191ba0ae0a";
+  const noteContent = noteId && engine.notesRendered[noteId];
 
   React.useEffect(() => {
     if (!noteId) {
       logger.info({ msg: "no noteId" });
       return;
     }
-    const maybeNoteContent = engine.notesRendered[noteId];
-    if (!maybeNoteContent) {
+    if (!noteContent) {
       dispatch(engineSlice.renderNote({ ...getWsAndPort(), id: noteId }));
     }
-  }, [ide.noteActive, engine.notesRendered]);
+  }, [noteId, noteContent]);
+
+  const onClickHandler = React.useCallback(
+    (event: Event) => {
+      if ((event.target as Element).tagName === "A") {
+        logger.info({
+          msg: "click#a",
+          event: (event.target as Element).tagName,
+        });
+        event.preventDefault();
+        event.stopPropagation();
+        // postVSCodeMessage(...);
+      }
+    },
+    [noteId]
+  );
+
+  React.useEffect(() => {
+    window.addEventListener("click", onClickHandler);
+
+    return () => {
+      window.removeEventListener("click", onClickHandler);
+    };
+  }, [onClickHandler]);
 
   if (!noteId) {
     return <>Loading..</>;
   }
-  if (!engine.notesRendered[noteId]) {
+  if (!noteContent) {
     return <>Loading..</>;
   }
   return (

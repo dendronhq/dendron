93142ce1c962ca21001f27b2118f67fbd391cc81
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2021-06-19 02:33:51 +0530

    chore: added testcases

--- packages/plugin-core/src/test/suite-integ/CreateDailyJournalNote.test.ts
+++ packages/plugin-core/src/test/suite-integ/CreateDailyJournalNote.test.ts
@@ -1,14 +1,26 @@
 import * as vscode from "vscode";
 import { CreateDailyJournalCommand } from "../../commands/CreateDailyJournal";
-import { getActiveEditorBasename } from "../testUtils";
 import { expect } from "../testUtilsv2";
-import { runLegacyMultiWorkspaceTest, setupBeforeAfter } from "../testUtilsV3";
+import {
+  runLegacyMultiWorkspaceTest,
+  setupBeforeAfter,
+  withConfig,
+  EditorUtils,
+} from "../testUtilsV3";
+import { DVault } from "@dendronhq/common-all";
+import { sinon } from "@dendronhq/common-test-utils";
+import _ from "lodash";
+import { PickerUtilsV2 } from "../../components/lookup/utils";
+import { getActiveEditorBasename } from "../testUtils";
 
-suite("CreateDailyJournal", function () {
+const stubVaultPick = (vaults: DVault[]) => {
+  const vault = _.find(vaults, { fsPath: vaults[2].fsPath });
+  sinon.stub(PickerUtilsV2, "promptVault").returns(Promise.resolve(vault));
+  return vault;
+};
+
+suite("Daily Journal Suite", function () {
   let ctx: vscode.ExtensionContext;
-
-  ctx = setupBeforeAfter(this, {});
-
   test("basic", (done) => {
     runLegacyMultiWorkspaceTest({
       ctx,
@@ -16,6 +28,59 @@ suite("CreateDailyJournal", function () {
         await new CreateDailyJournalCommand().run();
         expect(
           getActiveEditorBasename().startsWith("daily.journal")
+        ).toBeTruthy();
+        done();
+      },
+    });
+  });
+});
+
+suite("CreateDailyJournal", function () {
+  let ctx: vscode.ExtensionContext;
+
+  ctx = setupBeforeAfter(this, {});
+
+  test("default journal vault", (done) => {
+    runLegacyMultiWorkspaceTest({
+      ctx,
+      onInit: async ({ wsRoot, vaults }) => {
+        withConfig(
+          (config) => {
+            config.lookupConfirmVaultOnCreate = false;
+            config.defaultDailyJournalVault = vaults[0].fsPath;
+            return config;
+          },
+          { wsRoot }
+        );
+        stubVaultPick(vaults);
+        await new CreateDailyJournalCommand().run();
+        expect(
+          (await EditorUtils.getURIForActiveEditor()).fsPath.includes(
+            vaults[0].fsPath
+          )
+        ).toBeTruthy();
+        done();
+      },
+    });
+  });
+
+  test("default journal vault with lookup Confirm", (done) => {
+    runLegacyMultiWorkspaceTest({
+      ctx,
+      onInit: async ({ wsRoot, vaults }) => {
+        withConfig(
+          (config) => {
+            config.lookupConfirmVaultOnCreate = true;
+            config.defaultDailyJournalVault = vaults[0].fsPath;
+            return config;
+          },
+          { wsRoot }
+        );
+        await new CreateDailyJournalCommand().run();
+        expect(
+          (await EditorUtils.getURIForActiveEditor()).fsPath.includes(
+            vaults[2].fsPath
+          )
         ).toBeTruthy();
         done();
       },

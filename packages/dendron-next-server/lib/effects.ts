618084df2df61ea7b013a058d3a98a17d204d1eb
Antar R (re:cruit) <eng@recruit.so>
2021-06-25 21:06:51 +0600

    add ajv and save to yml file but not getting updates on reload

--- packages/dendron-next-server/lib/effects.ts
+++ packages/dendron-next-server/lib/effects.ts
@@ -1,14 +1,24 @@
-import { DendronConfig } from "@dendronhq/common-all";
-import { api } from "./config";
+import { DendronConfig, DendronApiV2 } from "@dendronhq/common-all";
+import { createAsyncThunk, } from "@reduxjs/toolkit";
+import { createLogger } from "@dendronhq/common-frontend";
 
-export const configWrite = (config: DendronConfig) => {
-  const body = JSON.stringify({ config });
-  return fetch(api("writeConfig"), {
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    method: "post",
-    body,
-  });
-};
+export const configWrite = createAsyncThunk(
+  "config/write",
+  async ({config, ws, port}:{config: DendronConfig, ws: string, port: number}) => {
+    const logger = createLogger("configWriteThunk");
+    const endpoint = `http://localhost:${port}`;
+    logger.info({ state: "enter", endpoint});
+    console.log({endpoint});
+    const api = new DendronApiV2({
+      endpoint,
+      apiPath: "api",
+      logger,
+    });
+    logger.info({ state: "pre:configWrite" });
+    const response = await api.configWrite({config, ws});
+    logger.info({ state: "post:configWrite" });
+    if (response.error) {
+      // TODO: dispatch what?
+    }
+  }
+)
--- packages/dendron-next-server/pages/vscode/configure.tsx
+++ packages/dendron-next-server/pages/vscode/configure.tsx
@@ -1,21 +1,61 @@
 import { createLogger, engineSlice } from "@dendronhq/common-frontend";
-import { List, Typography } from "antd";
+import { List, Typography, Button } from "antd";
+import { useRouter } from "next/router";
 import { FieldArray, Formik } from "formik";
-import { Field, Form, Input } from "formik-antd";
+import { Form, Input, Switch, ResetButton, SubmitButton } from "formik-antd";
 import React from "react";
 const { Title, Paragraph, Text, Link } = Typography;
+import { MinusOutlined } from "@ant-design/icons";
+import _ from "lodash";
+import Ajv, { JSONSchemaType } from "ajv";
+import { useRef } from "react";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { engineHooks } from "@dendronhq/common-frontend";
+import { configWrite } from "../../lib/effects";
+
+interface MyData {
+  siteRootDir: string;
+}
+
+const schema: JSONSchemaType<MyData> = {
+  type: "object",
+  properties: {
+    // TODO: this regex only works with unix systems
+    siteRootDir: {
+      type: "string",
+      minLength: 1,
+      pattern: "(^/$|(^(?=/)|^.|^..)(/(?=[^/\0])[^/\0]+)*/?$)|()",
+    },
+    siteAssetsPrefix: { type: "string", minLength: 5 },
+  },
+  required: ["siteRootDir", "siteAssetsPrefix"],
+  additionalProperties: true,
+};
 
 const createFormItem = ({
   name,
   label,
+  required,
+  helperText,
+  error,
 }: {
   name: string;
   placeholder?: string;
   label: string;
+  required?: boolean;
+  helperText?: string;
+  error?: string;
 }) => {
   return (
-    <Form.Item name={name} label={label}>
-      <Input name={name} />
+    <Form.Item name={name} style={{ justifyContent: "center" }}>
+      <Title level={3}>
+        {label}
+        {required && <span style={{ color: "red" }}> *</span>}
+      </Title>
+      <Input size="large" name={name} />
+      <Text type="secondary">{helperText}</Text>
+      <br />
+      <Text type="danger">{error}</Text>
     </Form.Item>
   );
 };
@@ -24,20 +64,21 @@ const renderArray = (arrayEnts: any[], arrayHelpers: any) => {
   const data = arrayEnts
     .map((_ent, idx) => (
       <>
-        <Field
+        <Input
           key={idx}
+          size="large"
           name={`site.siteHierarchies.${idx}`}
-          style={{ color: "black" }}
+          addonBefore={idx + 1 + "."}
+          addonAfter={
+            <MinusOutlined onClick={() => arrayHelpers.remove(idx)} />
+          }
         />
-        <button type="button" onClick={() => arrayHelpers.remove(idx)}>
-          -
-        </button>
       </>
     ))
     .concat(
-      <button type="button" onClick={() => arrayHelpers.push("")}>
+      <Button type="primary" size="large" onClick={() => arrayHelpers.push("")}>
         +
-      </button>
+      </Button>
     );
   return (
     <List
@@ -56,6 +97,10 @@ export default function Config({
 }: {
   engine: engineSlice.EngineState;
 }) {
+  const router = useRouter();
+  const { ws, port } = router.query;
+  const dispatch = engineHooks.useEngineAppDispatch();
+  const ajv = useRef(new Ajv({ allErrors: true }));
   const logger = createLogger("Config");
   if (!engine.config) {
     return <></>;
@@ -73,24 +118,115 @@ export default function Config({
   };
 
   return (
-    <Formik initialValues={engine.config} onSubmit={() => {}}>
-      {({ handleSubmit, handleChange, handleBlur, values, errors }) => (
-        <Form {...formItemLayout}>
-          <Typography>
-            <Title>Publishing </Title>
-          </Typography>
-
-          <Form.Item name="siteHierarchies" label="Site Hierarchies">
-            <FieldArray
-              name="site.siteHierarchies"
-              render={(arrayHelpers) =>
-                renderArray(values.site.siteHierarchies, arrayHelpers)
-              }
-            />
-          </Form.Item>
-          {createFormItem({ name: "site.siteRootDir", label: "Site Root Dir" })}
-        </Form>
-      )}
-    </Formik>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        width: "100%",
+      }}
+    >
+      <Formik
+        initialValues={engine.config}
+        onSubmit={(data) => {
+          dispatch(configWrite({ config: data, ws, port }));
+          return;
+        }}
+        validate={(values) => {
+          let errors: any = {};
+          const { site } = values;
+          const validate = ajv.current.compile(schema);
+          validate(site);
+          const { errors: ajvErrors } = validate;
+          ajvErrors?.forEach((error) => {
+            const { instancePath } = error;
+            let message = "";
+            if (error.keyword === "minLength") {
+              message = `Must have at least ${error.params.limit} characters`;
+            }
+            if (instancePath !== "") {
+              errors[`${instancePath.substring(1)}`] = message;
+            }
+          });
+          return errors;
+        }}
+        validateOnChange={true}
+      >
+        {({ handleSubmit, handleChange, handleBlur, values, errors }) => (
+          <Form {...formItemLayout}>
+            <Typography style={{ textAlign: "center" }}>
+              <Title>Dendron Configuration </Title>
+            </Typography>
+            <Form.Item
+              name="siteHierarchies"
+              style={{ justifyContent: "center" }}
+            >
+              <Title level={3}>Site Hierarchy</Title>
+              <FieldArray
+                name="site.siteHierarchies"
+                render={(arrayHelpers) =>
+                  renderArray(values.site.siteHierarchies, arrayHelpers)
+                }
+              />
+            </Form.Item>
+            {createFormItem({
+              name: "site.siteRootDir",
+              label: "Site root directory",
+              required: true,
+              helperText:
+                "Where your site will be published, Relative to Dendron workspace.",
+              error: errors.siteRootDir,
+            })}
+            {createFormItem({
+              name: "site.siteNotesDir",
+              label: "Site notes directory",
+              helperText:
+                "Folder where your notes will be kept. By default, `notes`",
+            })}
+            {createFormItem({
+              name: "site.siteAssetsPrefix",
+              label: "Assets Prefix",
+              helperText: "If set, add prefix to all links.",
+            })}
+            {createFormItem({
+              name: "site.siteRepoDir",
+              label: "Site repo directory",
+              helperText:
+                "Location of the github repo where your site notes are located. By defualt, this is assumed to be your `workspaceRoot` if not set",
+            })}
+            <Form.Item
+              style={{ justifyContent: "center" }}
+              name="usePrettyRefSwitch"
+            >
+              <Title level={3}>Use Pretty Refs?</Title>
+              <Switch name="site.siteUsePrettyRefs" />
+              <br />
+              <Text type="secondary">
+                Pretty refs help you identify when content is embedded from
+                elsewhere and provide links back to the source.
+              </Text>
+            </Form.Item>
+            <Form.Item
+              style={{ justifyContent: "center" }}
+              name="siteRepoDirCheckedSwitch"
+            >
+              <Title level={3}>Site Repo directory</Title>
+              <Switch name="site.useSiteRepoDir" />
+              <br />
+              <Text type="secondary">
+                If enabled, assets will be copied from the vault to the site
+              </Text>
+            </Form.Item>
+            <Form.Item name="submit" style={{ justifyContent: "center" }}>
+              <Button.Group size="large">
+                <ResetButton type="text">Cancel</ResetButton>
+                <SubmitButton type="primary" disabled={!_.isEmpty(errors)}>
+                  Submit
+                </SubmitButton>
+              </Button.Group>
+            </Form.Item>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }

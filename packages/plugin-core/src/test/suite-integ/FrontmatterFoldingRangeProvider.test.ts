b7f6fab109a9f9bb22a6eb769f675e6f1f3a85ad
Kaan Genç <kaan@kaangenc.me>
2021-06-29 11:06:47 -0400

    fix: horizontal line folded as frontmatter (#886)
    #883

--- packages/plugin-core/src/test/suite-integ/FrontmatterFoldingRangeProvider.test.ts
+++ packages/plugin-core/src/test/suite-integ/FrontmatterFoldingRangeProvider.test.ts
@@ -1,11 +1,16 @@
 import { vault2Path } from "@dendronhq/common-server";
 import { ENGINE_HOOKS } from "@dendronhq/engine-test-utils";
+import { NoteTestUtilsV4 } from "@dendronhq/common-test-utils";
 import path from "path";
 import * as vscode from "vscode";
 import FrontmatterFoldingRangeProvider from "../../features/FrontmatterFoldingRangeProvider";
 import { VSCodeUtils } from "../../utils";
 import { expect } from "../testUtilsv2";
-import { runLegacySingleWorkspaceTest, setupBeforeAfter } from "../testUtilsV3";
+import {
+  runLegacyMultiWorkspaceTest,
+  runLegacySingleWorkspaceTest,
+  setupBeforeAfter,
+} from "../testUtilsV3";
 
 async function provide(editor: vscode.TextEditor) {
   const doc = editor?.document as vscode.TextDocument;
@@ -33,7 +38,40 @@ suite("FrontmatterFoldingRangeProvider", function () {
           vscode.Uri.file(path.join(vaultDir, "foo.md"))
         );
         const foldingRange = (await provide(editor!)) as vscode.FoldingRange[];
-        expect(foldingRange).toEqual([new vscode.FoldingRange(0, 6)]);
+        expect(foldingRange).toEqual([
+          new vscode.FoldingRange(0, 6, vscode.FoldingRangeKind.Region),
+        ]);
+        done();
+      },
+    });
+  });
+
+  test("with horizontal line", (done) => {
+    runLegacyMultiWorkspaceTest({
+      ctx,
+      preSetupHook: async ({ wsRoot, vaults }) => {
+        await NoteTestUtilsV4.createNote({
+          wsRoot,
+          vault: vaults[0],
+          fname: "foo",
+          body: [
+            "Doloremque illo exercitationem error ab. Dicta architecto quis voluptatem. Numquam in est voluptatem quia impedit iusto repellendus magnam.",
+            "",
+            "---",
+            "",
+            "Aperiam in cupiditate temporibus id voluptas qui. Qui doloremque error odio eligendi quia. Quis ipsa aliquid voluptatem sunt.",
+          ].join("\n"),
+        });
+      },
+      onInit: async ({ wsRoot, vaults }) => {
+        const vaultDir = vault2Path({ vault: vaults[0], wsRoot });
+        const editor = await VSCodeUtils.openFileInEditor(
+          vscode.Uri.file(path.join(vaultDir, "foo.md"))
+        );
+        const foldingRange = (await provide(editor!)) as vscode.FoldingRange[];
+        expect(foldingRange).toEqual([
+          new vscode.FoldingRange(0, 6, vscode.FoldingRangeKind.Region),
+        ]);
         done();
       },
     });

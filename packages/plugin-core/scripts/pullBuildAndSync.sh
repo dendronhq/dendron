9db4a2443cab1a9385bd4f79a1484ad4bc58a04f
Kevin <kevin@dendron.so>
2021-07-01 11:32:06 -0700

    chore: update pkg script

--- packages/plugin-core/scripts/pullBuildAndSync.sh
+++ packages/plugin-core/scripts/pullBuildAndSync.sh
@@ -1,12 +1,5 @@
 
 #!/bin/bash
-
-git reset --hard
-git clean -f
-git checkout master
-git fetch
-git branch -D integ-publish
-git checkout --track origin/integ-publish
-echo "syncing..."
-./scripts/sync_static.sh
+./scripts/prep.sh
+./sync_static.sh
 ./scripts/pack_and_install.sh 2>&1 | tee /tmp/out.txt

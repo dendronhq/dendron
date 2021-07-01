0f80f213d7d7fdb8054ae06de1aa2e78164b5432
Kevin <kevin@dendron.so>
2021-07-01 11:00:38 -0700

    fix: remove circular dep on workspace in cmd

--- packages/plugin-core/src/commands/VaultAddCommand.ts
+++ packages/plugin-core/src/commands/VaultAddCommand.ts
@@ -8,7 +8,6 @@ import {
   assignJSONWithComment,
   GitUtils,
   readJSONWithComments,
-  SimpleGit,
   simpleGit,
   writeJSONWithComments,
 } from "@dendronhq/common-server";
@@ -41,12 +40,8 @@ type SourceQuickPickEntry = QuickPickItem & { src: string };
 export class VaultAddCommand extends BasicCommand<CommandOpts, CommandOutput> {
   key = DENDRON_COMMANDS.VAULT_ADD.key;
 
-  public git: SimpleGit;
-
   constructor() {
     super();
-    const baseDir = DendronWorkspace.wsRoot();
-    this.git = simpleGit({ baseDir });
   }
 
   generateRemoteEntries = (): SourceQuickPickEntry[] => {
@@ -145,7 +140,6 @@ export class VaultAddCommand extends BasicCommand<CommandOpts, CommandOutput> {
   async handleRemoteRepo(
     opts: CommandOpts
   ): Promise<{ vaults: DVault[]; workspace?: DWorkspace }> {
-    const baseDir = DendronWorkspace.wsRoot();
     const { vaults, workspace } = await window.withProgress(
       {
         location: ProgressLocation.Notification,
@@ -156,7 +150,9 @@ export class VaultAddCommand extends BasicCommand<CommandOpts, CommandOutput> {
         progress.report({
           message: "cloning repo",
         });
-        await this.git.clone(opts.pathRemote!, opts.path);
+        const baseDir = DendronWorkspace.wsRoot();
+        const git = simpleGit({ baseDir });
+        await git.clone(opts.pathRemote!, opts.path);
         const { vaults, workspace } = GitUtils.getVaultsFromRepo({
           repoPath: path.join(baseDir, opts.path),
           wsRoot: DendronWorkspace.wsRoot(),

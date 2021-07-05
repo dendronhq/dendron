376953bbd431d92fbc8e29d793843b812d348d27
Samnan <namanush.rsr.16@gmail.com>
2021-07-05 21:04:24 +0600

    rewrite rendering algorithm

--- packages/dendron-next-server/pages/vscode/configure.tsx
+++ packages/dendron-next-server/pages/vscode/configure.tsx
@@ -1,17 +1,15 @@
+import React, { useRef, ReactNode } from "react";
 import { createLogger, engineSlice } from "@dendronhq/common-frontend";
-import { List, Typography, Button, Space, message } from "antd";
+import { List, Typography, Button, Card, message } from "antd";
 import { useRouter } from "next/router";
 import { FieldArray, Formik } from "formik";
 import { Form, Input, Switch, ResetButton, SubmitButton } from "formik-antd";
-import React from "react";
 const { Title, Paragraph, Text, Link } = Typography;
 import { MinusCircleOutlined } from "@ant-design/icons";
 import _ from "lodash";
 import Ajv, { JSONSchemaType } from "ajv";
-import { useRef } from "react";
 import { engineHooks } from "@dendronhq/common-frontend";
 import { configWrite } from "../../lib/effects";
-import { m } from "framer-motion";
 import get from "lodash/get";
 
 interface MyData {
@@ -21,89 +19,85 @@ interface MyData {
   copyAssets: boolean;
 }
 
-const siteConfig: ConfigType[] = [
-  {
-    label: "Site Root Directory",
-    name: "siteRootDir",
-    type: "string",
-    required: true,
-    helperText:
-      "Where your site will be published. Relative to Dendron workspace",
+const siteConfig: ObjectConfig = {
+  type: "object",
+  data: {
+    siteRootDir: {
+      label: "Site Root Directory",
+      type: "string",
+      required: true,
+      helperText:
+        "Where your site will be published. Relative to Dendron workspace",
+    },
+    assetPrefix: {
+      label: "Asset Prefix",
+      type: "string",
+      helperText: "If set, add prefix to all asset links",
+    },
+    copyAssets: {
+      label: "Copy Assets",
+      type: "boolean",
+      helperText: "Copy assets from vault to site.",
+    },
+    siteRepotDir: {
+      label: "Site Repo Directory",
+      type: "string",
+      helperText:
+        "Location of the github repo where your site notes are located. By default, this is assumed to be your `workspaceRoot` if not set.",
+    },
+    usePrettyRefs: {
+      label: "Use Pretty Refs?",
+      type: "boolean",
+      helperText:
+        "Pretty refs help you identify when content is embedded from elsewhere and provide links back to the source.",
+    },
   },
-  {
-    label: "Asset Prefix",
-    name: "assetPrefix",
-    type: "string",
-    helperText: "If set, add prefix to all asset links",
-  },
-  {
-    label: "Copy Assets",
-    name: "copyAssets",
-    type: "boolean",
-    helperText: "Copy assets from vault to site.",
-  },
-  {
-    label: "Site Repo Directory",
-    name: "siteRepotDir",
-    type: "string",
-    helperText:
-      "Location of the github repo where your site notes are located. By default, this is assumed to be your `workspaceRoot` if not set.",
-  },
-  {
-    label: "Use Pretty Refs?",
-    name: "usePrettyRefs",
-    type: "boolean",
-    helperText:
-      "Pretty refs help you identify when content is embedded from elsewhere and provide links back to the source.",
-  },
-];
+};
 
-const vault: ConfigType[] = [
-  {
-    label: "Filesystem Path",
-    name: "fsPath",
-    type: "string",
-    helperText: "Filesystem path to vault",
-  },
-  {
-    label: "Visibility",
-    name: "visibility",
-    type: "string",
-    helperText: "Visibility of the vault",
-  },
-];
-
-const dendronConfig: ConfigType[] = [
-  {
-    label: "Site Config",
-    name: "site.siteHierarchies",
-    type: "array",
-    helperText: "Site configuration",
-  },
-  {
-    label: "No Caching?",
-    name: "noCaching",
-    type: "boolean",
-    helperText: "Disable caching behavior",
-  },
-  {
-    label: "No telemetry?",
-    name: "noTelemetry",
-    type: "boolean",
-    helperText: "Disable telemetry",
-  },
-  {
-    name: "site",
+const vault: ArrayConfig = {
+  type: "array",
+  data: {
     type: "object",
-    data: siteConfig,
+    data: {
+      fsPath: {
+        label: "Filesystem Path",
+        type: "string",
+        helperText: "Filesystem path to vault",
+      },
+      visibility: {
+        label: "Visibility",
+        type: "string",
+        helperText: "Visibility of the vault",
+      },
+    },
   },
-  {
-    label: "Vault",
-    name: "vaults",
-    type: "array",
-    data: vault,
+};
+
+const dendronConfig: ObjectConfig = {
+  type: "object",
+  data: {
+    "site.siteHierarchies": {
+      type: "array",
+      data: {
+        type: "string",
+        label: "Site Config",
+        helperText: "Site configuration",
+      },
+    },
+    noCaching: {
+      label: "No Caching?",
+      type: "boolean",
+      helperText: "Disable caching behavior",
+    },
+    noTelemetry: {
+      label: "No telemetry?",
+      type: "boolean",
+      helperText: "Disable telemetry",
+    },
+    site: siteConfig,
+    vaults: vault,
   },
-];
+};
 
 type ConfigType = {
   label?: string;
@@ -114,17 +108,269 @@ type ConfigType = {
   data?: ConfigType[];
 };
 
-const flatten = (
-  data: ConfigType[] | ConfigType,
-  prefix: string[] = []
-): ConfigType[] | ConfigType => {
-  if (Array.isArray(data)) return data.flatMap((next) => flatten(next, prefix));
-  return data.type !== "object"
-    ? {
-        ...data,
-        name: prefix.concat(data.name).join("."),
-      }
-    : data.data.flatMap((next: any) => flatten(next, prefix.concat(data.name)));
+type FieldType =
+  | "string"
+  | "boolean"
+  | "number"
+  | "enum"
+  | "array"
+  | "object"
+  | "record";
+
+type CommonConfig = {
+  required?: boolean;
+  helperText?: string;
+  label?: string;
+};
+
+type BooleanConfig = CommonConfig & { type: "boolean" };
+
+type StringConfig = CommonConfig & { type: "string" };
+
+type NumberConfig = CommonConfig & { type: "number" };
+
+type EnumConfig = CommonConfig & { type: "enum"; data: string[] };
+
+type ArrayConfig = CommonConfig & {
+  type: "array";
+  data: Config;
+};
+
+type RecordConfig = CommonConfig & {
+  type: "record";
+  data: Config;
+};
+
+type ObjectConfig = CommonConfig & {
+  type: "object";
+  data: Record<string, Config>;
+};
+
+type Config =
+  | ArrayConfig
+  | BooleanConfig
+  | EnumConfig
+  | StringConfig
+  | NumberConfig
+  | RecordConfig
+  | ObjectConfig;
+
+type ConfigInputType = {
+  data: Config;
+  prefix: string[];
+  errors?: any;
+  values?: any;
+};
+
+type InputType = {
+  label?: string;
+  name: string;
+  errors?: string;
+  placeholder?: string;
+  required?: boolean;
+  helperText?: string;
+};
+type BaseInputType = InputType & { children?: ReactNode };
+type SimpleInputType = InputType & { type: "string" | "number" };
+type ArrayInputType = InputType & { data: Config; values: any };
+
+const BaseInput = ({
+  label,
+  name,
+  errors,
+  required,
+  helperText,
+  children,
+}: BaseInputType) => (
+  <Form.Item
+    name={name}
+    style={{ justifyContent: "center" }}
+    rules={[{ required: required, message: `This information is required!` }]}
+  >
+    <Title level={3} style={{ textTransform: "capitalize" }}>
+      {label}
+    </Title>
+    {children}
+    <br />
+    <Text type="secondary">{helperText}</Text>
+    <br />
+    <Text type="danger">{get(errors, name)}</Text>
+  </Form.Item>
+);
+
+const SimpleInput = ({
+  name,
+  type,
+  label,
+  placeholder,
+  required,
+  helperText,
+  errors,
+}: SimpleInputType) => {
+  return (
+    <BaseInput {...{ name, label, required, helperText, errors }}>
+      <Input
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        required={required}
+      />
+    </BaseInput>
+  );
+};
+
+const BooleanInput = ({
+  name,
+  label,
+  required,
+  helperText,
+  errors,
+}: InputType) => {
+  return (
+    <BaseInput {...{ name, label, required, helperText, errors }}>
+      <Switch name={name} />
+    </BaseInput>
+  );
+};
+
+const ArrayInput = ({
+  name,
+  values,
+  data,
+  label,
+  required,
+  helperText,
+  errors,
+}: ArrayInputType) => {
+  return (
+    <BaseInput {...{ name, label, required, helperText, errors }}>
+      <FieldArray
+        name={name}
+        render={(arrayHelpers) =>
+          renderArray(values, data, name, arrayHelpers, errors)
+        }
+      />
+    </BaseInput>
+  );
+};
+
+const renderArray = (
+  values: any,
+  dataDefinition: Config,
+  name: any,
+  arrayHelpers: any,
+  errors: any
+) => {
+  const dataSource =
+    dataDefinition.data.type === "object"
+      ? values.map((value: any, index: number) => (
+          <Card
+            key={`${name}.${index}`}
+            size="small"
+            title={index + 1}
+            extra={
+              <MinusCircleOutlined onClick={() => arrayHelpers.remove(index)} />
+            }
+          >
+            {Object.keys(dataDefinition.data.data).map((key) => (
+              <ConfigInput
+                key={`${name}.${index}.${key}`}
+                values={values}
+                data={dataDefinition.data.data[key]}
+                errors={errors}
+                prefix={[name, `${index}`, key]}
+              />
+            ))}
+          </Card>
+        ))
+      : values.map((value: any, index: number) => (
+          <Card
+            key={`${name}.${index}`}
+            size="small"
+            title={index + 1}
+            extra={
+              <MinusCircleOutlined onClick={() => arrayHelpers.remove(index)} />
+            }
+          >
+            <ConfigInput
+              values={values}
+              data={dataDefinition.data}
+              errors={errors}
+              prefix={[name, `${index}`]}
+            />
+          </Card>
+        ));
+
+  dataSource.concat(
+    <Button type="primary" size="large" onClick={() => arrayHelpers.add()}>
+      +
+    </Button>
+  );
+
+  return (
+    <List
+      itemLayout="vertical"
+      dataSource={dataSource}
+      bordered={false}
+      renderItem={(item: ReactNode) => (
+        <List.Item style={{ borderBottom: "none" }}>{item}</List.Item>
+      )}
+    />
+  );
+};
+
+const ConfigInput = ({ data, values, errors, prefix }: ConfigInputType) => {
+  const { type, required, helperText, label } = data;
+  if (type === "string" || type === "number") {
+    return (
+      <SimpleInput
+        name={prefix.join(".")}
+        type={type}
+        label={label}
+        required={required}
+        helperText={helperText}
+        errors={errors}
+      />
+    );
+  }
+  if (type === "boolean") {
+    return (
+      <BooleanInput
+        name={prefix.join(".")}
+        label={label}
+        required={required}
+        helperText={helperText}
+        errors={errors}
+      />
+    );
+  }
+
+  if (type === "array") {
+    const name = prefix.join(".");
+    return (
+      <ArrayInput
+        values={get(values, name)}
+        label={prefix[prefix.length - 1].split(".").pop()}
+        {...{ name, data, required, helperText, errors }}
+      />
+    );
+  }
+  if (type === "enum") return <></>;
+  if (type === "record") return <></>;
+
+  return (
+    <>
+      {Object.keys(data.data).map((key) => (
+        <ConfigInput
+          key={prefix.join(".") + "." + key}
+          values={values}
+          data={data.data[key]}
+          errors={errors}
+          prefix={[...prefix, key]}
+        />
+      ))}
+    </>
+  );
 };
 
 const schema: JSONSchemaType<MyData> = {
@@ -145,111 +391,6 @@ const schema: JSONSchemaType<MyData> = {
   additionalProperties: true,
 };
 
-const FormItem = ({
-  name,
-  label,
-  type,
-  data,
-  values,
-  required,
-  helperText,
-  error,
-}: {
-  name: string;
-  placeholder?: string;
-  label: string;
-  data?: any;
-  values?: any;
-  type: string;
-  required?: boolean;
-  helperText?: string;
-  error?: string;
-}) => (
-  <Form.Item name={name} style={{ justifyContent: "center" }}>
-    {console.log({ name, values }, "yoooooo")}
-    <Title level={3}>
-      {console.log(type)}
-      {label}
-      {required && <span style={{ color: "red" }}> *</span>}
-    </Title>
-    <Text type="secondary">{helperText}</Text>
-    {type === "string" && <Input size="large" name={name} />}
-    {type === "boolean" && (
-      <>
-        <br />
-        <Switch name={name} />
-        <br />
-      </>
-    )}
-    {type === "array" && (
-      <FieldArray
-        name={name}
-        render={(arrayHelpers) => renderArray(values, data, name, arrayHelpers)}
-      />
-    )}
-    <br />
-    <Text type="danger">{error}</Text>
-  </Form.Item>
-);
-
-const renderArray = (
-  arrayEnts: any[],
-  dataDefinition: any,
-  name: string,
-  arrayHelpers: any
-) => {
-  const data = arrayEnts
-    ?.map((_ent, idx) => (
-      <>
-        {typeof _ent !== "object" ? (
-          <Input
-            key={idx}
-            size="large"
-            name={`${name}.${idx}`}
-            addonBefore={idx + 1 + "."}
-            addonAfter={
-              <MinusCircleOutlined onClick={() => arrayHelpers.remove(idx)} />
-            }
-          />
-        ) : (
-          <Space
-            key={name}
-            style={{ display: "flex", marginBottom: 8 }}
-            align="baseline"
-          >
-            {dataDefinition
-              .map(({ name: itemName, label }) => (
-                <Input
-                  key={`${idx}.${itemName}`}
-                  size="large"
-                  name={`${name}.${idx}.${itemName}`}
-                  placeholder={label}
-                />
-              ))
-              .concat(
-                <MinusCircleOutlined onClick={() => arrayHelpers.remove(idx)} />
-              )}
-          </Space>
-        )}
-      </>
-    ))
-    .concat(
-      <Button type="primary" size="large" onClick={() => arrayHelpers.push("")}>
-        +
-      </Button>
-    );
-  return (
-    <List
-      itemLayout="vertical"
-      dataSource={data}
-      bordered={false}
-      renderItem={(item) => (
-        <List.Item style={{ borderBottom: "none" }}>{item}</List.Item>
-      )}
-    />
-  );
-};
-
 export default function Config({
   engine,
 }: {
@@ -263,8 +404,6 @@ export default function Config({
   if (!engine.config || !ws || !port) {
     return <></>;
   }
-
-  console.log(engine.config, "yooooo");
 
   const formItemLayout = {
     labelCol: {
@@ -327,14 +466,12 @@ export default function Config({
             <Typography style={{ textAlign: "center" }}>
               <Title>Dendron Configuration </Title>
             </Typography>
-            {flatten(dendronConfig).map(({ name, ...rest }) => (
-              <FormItem
-                key={name}
-                name={name}
-                {...rest}
-                values={get(values, name)}
-              />
-            ))}
+            <ConfigInput
+              data={dendronConfig}
+              values={values}
+              errors={errors}
+              prefix={[]}
+            />
             <Form.Item name="submit" style={{ justifyContent: "center" }}>
               <Button.Group size="large">
                 <ResetButton type="text">Clear changes</ResetButton>

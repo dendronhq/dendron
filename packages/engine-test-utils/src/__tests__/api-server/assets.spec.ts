9f9ca2f5a75cd4841fbbc43bd655e9bafb19a062
Kevin <kevin@dendron.so>
2021-07-06 09:38:19 -0700

    chore: respond to https://github.com/dendronhq/dendron/pull/919

--- packages/engine-test-utils/src/__tests__/api-server/assets.spec.ts
+++ packages/engine-test-utils/src/__tests__/api-server/assets.spec.ts
@@ -38,6 +38,24 @@ describe("assets/get", () => {
     );
   });
 
+  test("fail: path not exist", async () => {
+    await runEngineTestV5(
+      async ({ wsRoot, vaults }) => {
+        const { port } = await createServer({ wsRoot, vaults });
+        const api = new DendronAPI({
+          endpoint: `http://localhost:${port}`,
+          apiPath: "api",
+        });
+        await initRemoteWorkspace({ wsRoot, vaults, api });
+        const vpath = vault2Path({ vault: vaults[0], wsRoot });
+        const fpath = path.join(vpath, "not-exist.md");
+        const resp = await api.assetGet({ fpath, ws: wsRoot });
+        await checkString((resp as DendronError).message, "does not exist");
+      },
+      { expect, preSetupHook: ENGINE_HOOKS.setupBasic }
+    );
+  });
+
   test("ok", async () => {
     await runEngineTestV5(
       async ({ wsRoot, vaults }) => {

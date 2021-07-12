c3f41c7b060f5633d00b74c8da1db8a52f051cd1
Mark Choi <hikchoi@gmail.com>
2021-07-12 09:47:21 +0900

    fix: correctly select range after link conversion

--- packages/plugin-core/src/commands/ConvertLink.ts
+++ packages/plugin-core/src/commands/ConvertLink.ts
@@ -1,7 +1,7 @@
 import { DENDRON_COMMANDS } from "../constants";
 import { BasicCommand } from "./base";
 import _ from "lodash";
-import { commands, Location, Selection } from "vscode";
+import { commands, Location, Position, Selection } from "vscode";
 import { VSCodeUtils } from "../utils";
 // import vscode from "vscode";
 
@@ -29,11 +29,22 @@ export class ConvertLinkCommand extends BasicCommand<
     await commands.executeCommand("vscode.open", location.uri);
     const editor = VSCodeUtils.getActiveTextEditor()!;
     const selection = editor.document.getText(location.range);
+    const preConversionOffset = selection.indexOf(text);
     const convertedSelection = selection.replace(text, `\[\[${text}\]\]`);
     await editor.edit((editBuilder) => {
       editBuilder.replace(location.range, convertedSelection);
     });
-    editor.selection = new Selection(location.range.start, location.range.end);
+    const postConversionSelectionRange = new Selection(
+      new Position(
+        location.range.start.line,
+        location.range.start.character + preConversionOffset
+      ),
+      new Position(
+        location.range.end.line,
+        location.range.start.character + preConversionOffset + text.length + 4
+      )
+    );
+    editor.selection = postConversionSelectionRange;
     return;
   }
 }

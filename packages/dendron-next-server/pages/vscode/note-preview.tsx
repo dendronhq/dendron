d48bc03a110e34ae05561ae91c9159027939467a
Kevin <kevin@dendron.so>
2021-07-13 15:10:43 -0700

    spike: mermaid renders on first load

--- packages/dendron-next-server/pages/vscode/note-preview.tsx
+++ packages/dendron-next-server/pages/vscode/note-preview.tsx
@@ -1,4 +1,8 @@
-import { DMessageSource, NoteViewMessageType } from "@dendronhq/common-all";
+import {
+  DendronConfig,
+  DMessageSource,
+  NoteViewMessageType,
+} from "@dendronhq/common-all";
 import {
   createLogger,
   engineHooks,
@@ -6,7 +10,9 @@ import {
   postVSCodeMessage,
 } from "@dendronhq/common-frontend";
 import { Col, Layout, Row } from "antd";
+import _ from "lodash";
 import * as React from "react";
+import { EngineSliceUtils } from "../../../common-frontend/lib/features/engine/slice";
 import { getWsAndPort } from "../../lib/env";
 import { DendronProps } from "../../lib/types";
 
@@ -16,6 +22,19 @@ function isHTMLAnchorElement(element: Element): element is HTMLAnchorElement {
   return element.nodeName === "A";
 }
 
+type Mermaid = {
+  init: () => undefined;
+  initialize: any;
+};
+
+function getMermaid(window: Window): Mermaid | undefined {
+  // NOTE: a mermaid h3 header will result in window.mermaid being defined
+  // @ts-ignore
+  if (window.mermaid && window.mermaid.initialize) {
+    // @ts-ignore
+    return window.mermaid as Mermaid;
+  }
+}
 function AntLayout(props: React.PropsWithChildren<any>) {
   return (
     <Layout>
@@ -34,37 +53,60 @@ function AntLayout(props: React.PropsWithChildren<any>) {
   );
 }
 
-function docReady(fn: () => any) {
+function mermaidReady(fn: () => any) {
   // see if DOM is already available
-  if (
-    document.readyState === "complete" ||
-    document.readyState === "interactive"
-  ) {
+  if (getMermaid(window)) {
     // call on next available tick
     setTimeout(fn, 1);
   } else {
-    document.addEventListener("DOMContentLoaded", fn);
+    setTimeout(() => {
+      mermaidReady(fn);
+    }, 1000);
   }
 }
 
-function Note({ engine, ide }: DendronProps) {
-  logger.info({
-    state: "enter",
-  });
+/**
+ * Initialize mermaid if it is enabled
+ * @param param0
+ */
+const useMermaid = ({ config }: { config?: DendronConfig }) => {
+  React.useEffect(() => {
+    if (config?.mermaid) {
+      mermaidReady(() => {
+        const mermaid = getMermaid(window);
+        mermaid!.initialize({
+          startOnLoad: false,
+          cloneCssStyles: false,
+        });
+      });
+    }
+  }, [config]);
+};
 
+function Note({ engine, ide }: DendronProps) {
+  logger.info(
+    JSON.stringify({
+      msg: "enter",
+      noteActive: ide.noteActive,
+    })
+  );
+
+  // apply initial hooks
   const dispatch = engineHooks.useEngineAppDispatch();
+  useMermaid({ config: engine.config });
 
   const { noteActive } = ide;
   const { id: noteId = "9eae08fb-5e3f-4a7e-a989-3f206825d490", contentHash } =
     noteActive || {};
-  const noteContent = engine.notesRendered[noteId || ""];
+  let noteContent = engine.notesRendered[noteId || ""];
 
   // remember note contentHash from last "render to markdown"
   const renderedNoteContentHash = React.useRef<string>();
 
+  // hook: update note render
   React.useEffect(() => {
     if (!noteId) {
-      logger.info({ msg: "no noteId" });
+      logger.info(JSON.stringify({ msg: "no noteId" }));
       return;
     }
     // if no "render to markdown" has happended or the note body changed
@@ -72,35 +114,19 @@ function Note({ engine, ide }: DendronProps) {
       renderedNoteContentHash.current = contentHash;
       dispatch(engineSlice.renderNote({ ...getWsAndPort(), id: noteId }));
     }
-
-    console.log("BOND: use effect called");
-    docReady(() => {
-      console.log("BOND2: doc ready");
-      // @ts-ignore
-      if (window.mermaid) {
-        try {
-          console.log("BOND3: mermaid");
-          // @ts-ignore
-          window.mermaid.initialize({ startOnLoad: true });
-          console.log("BOND4: mermaid fin!");
-        } catch (err) {
-          console.log("error", err);
-          debugger;
-        }
-      }
-    });
-    // debugger;
   }, [noteId, contentHash]);
 
   const onClickHandler = React.useCallback(
     (event: Event) => {
       const target = event.target as Element;
       if (isHTMLAnchorElement(target)) {
-        logger.info({
-          ctx: `onClickHandler#${target.nodeName}`,
-          event,
-          target,
-        });
+        logger.info(
+          JSON.stringify({
+            ctx: `onClickHandler#${target.nodeName}`,
+            event,
+            target,
+          })
+        );
         event.preventDefault();
         event.stopPropagation();
         postVSCodeMessage({
@@ -116,20 +142,29 @@ function Note({ engine, ide }: DendronProps) {
     [noteId]
   );
 
-  // add effects
+  // hook: update click listener
   React.useEffect(() => {
     window.addEventListener("click", onClickHandler);
-
     return () => {
       window.removeEventListener("click", onClickHandler);
     };
   }, [onClickHandler]);
 
+  // hook: apply mermaid
+  React.useEffect(() => {
+    if (engine?.config?.mermaid) {
+      mermaidReady(() => {
+        const mermaid = getMermaid(window);
+        mermaid!.init();
+      });
+    }
+  }, [noteContent, engine.config]);
+
   if (!noteId) {
-    return null;
+    return <></>;
   }
   if (!noteContent) {
-    return null;
+    return <></>;
   }
   return (
     <AntLayout>
@@ -138,4 +173,24 @@ function Note({ engine, ide }: DendronProps) {
   );
 }
 
+function areEqual(prevProps: DendronProps, nextProps: DendronProps) {
+  const logger = createLogger("treeViewContainer");
+  const isDiff = _.some([
+    // active note changed
+    prevProps.ide.noteActive?.id !== nextProps.ide.noteActive?.id,
+    // active note content changed
+    prevProps.ide.noteActive?.contentHash !==
+      nextProps.ide.noteActive?.contentHash,
+    // engine initialized for first time
+    _.isUndefined(prevProps.engine.notes) ||
+      (_.isEmpty(prevProps.engine.notes) && !_.isEmpty(nextProps.engine.notes)),
+    // engine just went from pending to loading
+    prevProps.engine.loading === "pending" &&
+      nextProps.engine.loading === "idle",
+  ]);
+  logger.info({ state: "areEqual", isDiff, prevProps, nextProps });
+  return !isDiff;
+}
+
+const NoteContainer = React.memo(Note, areEqual);
 export default Note;

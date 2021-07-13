b223de28c0467590a781ece0a02832d608d29fe4
Kevin <kevin@dendron.so>
2021-07-13 12:07:50 -0700

    chore: fix tests for jest@26

--- packages/engine-test-utils/src/__tests__/engine-server/enginev2.spec.ts
+++ packages/engine-test-utils/src/__tests__/engine-server/enginev2.spec.ts
@@ -31,17 +31,20 @@ describe("engine, schemas/", () => {
   const nodeType = "SCHEMAS";
   ENGINE_PRESETS.forEach((pre) => {
     const { name, presets } = pre;
-    describe(name, () => {
-      test.each(
-        // @ts-ignore
-        _.map(presets[nodeType], (v, k) => {
-          return [k, v];
-        })
-      )("%p", async (_key, TestCase) => {
-        const { testFunc, ...opts } = TestCase;
-        await runEngineTestV5(testFunc, { ...opts, expect });
+    // @ts-ignore
+    const presetByNodeType = presets[nodeType];
+    if (!_.isEmpty(presetByNodeType)) {
+      describe(name, () => {
+        test.each(
+          _.map(presetByNodeType, (v, k) => {
+            return [k, v];
+          })
+        )("%p", async (_key, TestCase) => {
+          const { testFunc, ...opts } = TestCase;
+          await runEngineTestV5(testFunc, { ...opts, expect });
+        });
       });
-    });
+    }
   });
 });
 

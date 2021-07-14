3e21ffa741371833b8d94430d11ad4a8f83fc484
Kevin <kevin@dendron.so>
2021-07-14 16:24:47 -0700

    chore: update sync_static

--- packages/plugin-core/scripts/sync_static.sh
+++ packages/plugin-core/scripts/sync_static.sh
@@ -6,6 +6,7 @@ yarn  --ignore-lockfile
 yarn build
 yarn gen:theme
 rsync -avq out/ ../plugin-core/assets/static/ --delete
+rsync -avq assets/js ../plugin-core/assets/static/
 popd
 echo "sync server assets..."
 pushd ../api-server

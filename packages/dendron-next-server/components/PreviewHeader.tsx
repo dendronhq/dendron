a63e89bd72bd723d6a72dec250eec723b4a99b14
Kevin <kevin@dendron.so>
2021-07-14 16:42:27 -0700

    chore: add local jquery

--- packages/dendron-next-server/components/PreviewHeader.tsx
+++ packages/dendron-next-server/components/PreviewHeader.tsx
@@ -45,9 +45,8 @@ function PreviewHeader({ engine }: DendronProps) {
   return (
     <Head>
       <script
-        src="https://code.jquery.com/jquery-3.6.0.min.js"
-        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
-        crossOrigin="anonymous"
+        key="jquery"
+        src="/js/jquery.3-60.min.js"
       ></script>
       {engine?.config?.useKatex && MathJaxHeaders()}
       {engine?.config?.mermaid && MermaidHeaders()}

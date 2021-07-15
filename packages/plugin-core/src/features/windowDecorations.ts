4e1653b53856fa605e82bacb592d6b59449aa149
Kaan Genç <kaan@dendron.so>
2021-07-15 12:39:15 -0400

    feat: editor highlighting for wikilinks & references (#960)
    * feat: editor highlighting for wikilinks & references
    
    * fix: Wikilinks with URIs like `http://` don't highlight as missing
    
    Co-authored-by: Kevin Lin <kevinslin8@gmail.com>

--- packages/plugin-core/src/features/windowDecorations.ts
+++ packages/plugin-core/src/features/windowDecorations.ts
@@ -5,15 +5,18 @@ import {
   DendronASTTypes,
   MDUtilsV5,
   ProcMode,
+  WikiLinkNoteV4,
+  NoteRefNoteV4,
 } from "@dendronhq/engine-server";
-import { DecorationOptions, Range, TextEditor, window } from "vscode";
+import { DecorationOptions, DecorationRangeBehavior, Range, TextEditor, TextEditorDecorationType, ThemeColor, window, TextDocument } from "vscode";
 import visit from "unist-util-visit";
 import _ from "lodash";
-import { isNotUndefined, NoteUtils } from "@dendronhq/common-all";
+import { isNotUndefined, NoteUtils, Position, VaultUtils } from "@dendronhq/common-all";
 import { DateTime } from "luxon";
-import { getConfigValue } from "../workspace";
+import { getConfigValue, getWS } from "../workspace";
 import { CodeConfigKeys, DateTimeFormat } from "../types";
 import { VSCodeUtils } from "../utils";
+import { containsNonDendronUri } from "../utils/md";
 
 export function updateDecorations(activeEditor: TextEditor) {
   const text = activeEditor.document.getText();
@@ -25,37 +28,57 @@ export function updateDecorations(activeEditor: TextEditor) {
     { dest: DendronASTDest.MD_DENDRON }
   );
   const tree = proc.parse(text);
-  const blockAnchorDecorations: DecorationOptions[] = [];
-  let timestampDecorations: DecorationOptions[] = [];
+  const decorations: Map<TextEditorDecorationType, DecorationOptions[]> = new Map([
+    [DECORATION_TYPE_TIMESTAMP, []],
+    [DECORATION_TYPE_WIKILINK, []],
+    [DECORATION_TYPE_ALIAS, []],
+    [DECORATION_TYPE_BROKEN_WIKILINK, []],
+    [DECORATION_TYPE_BLOCK_ANCHOR, []],
+  ]);
 
   visit(tree, (node) => {
     switch (node.type) {
       case DendronASTTypes.FRONTMATTER: {
-        const decoration = decorateTimestamps(node as FrontmatterContent);
-        if (isNotUndefined(decoration)) timestampDecorations = decoration;
+        for (const decoration of decorateTimestamps(node as FrontmatterContent)) {
+          decorations.get(DECORATION_TYPE_TIMESTAMP)?.push(decoration);
+        }
         break;
       }
       case DendronASTTypes.BLOCK_ANCHOR: {
         const decoration = decorateBlockAnchor(node as BlockAnchor);
-        if (isNotUndefined(decoration)) blockAnchorDecorations.push(decoration);
+        if (isNotUndefined(decoration)) decorations.get(DECORATION_TYPE_BLOCK_ANCHOR)?.push(decoration);
         break;
       }
+      case DendronASTTypes.WIKI_LINK: {
+        for (const [type, decoration] of decorateWikiLink(node as WikiLinkNoteV4, activeEditor.document)) {
+          decorations.get(type)?.push(decoration);
+        }
+        break;
+      }
+      case DendronASTTypes.REF_LINK_V2:
+      case DendronASTTypes.REF_LINK: {
+        const out = decorateReference(node as NoteRefNoteV4);
+        if (out) {
+          const [type, decoration] = out;
+          decorations.get(type)?.push(decoration);
+        }
+        break;
+      }
+      default: /* nothing */
     }
   });
 
-  activeEditor.setDecorations(
-    DECORATION_TYPE_BLOCK_ANCHOR,
-    blockAnchorDecorations
-  );
-  activeEditor.setDecorations(DECORATION_TYPE_TIMESTAMP, timestampDecorations);
-  return { blockAnchorDecorations, timestampDecorations };
+  for (const [type, items] of decorations.entries()) {
+    activeEditor.setDecorations(type, items);
+  }
+  return decorations;
 }
 
-const DECORATION_TYPE_TIMESTAMP = window.createTextEditorDecorationType({});
+export const DECORATION_TYPE_TIMESTAMP = window.createTextEditorDecorationType({});
 
 function decorateTimestamps(frontmatter: FrontmatterContent) {
   const { value: contents, position } = frontmatter;
-  if (_.isUndefined(position)) return; // should never happen
+  if (_.isUndefined(position)) return []; // should never happen
   const tsConfig = getConfigValue(
     CodeConfigKeys.DEFAULT_TIMESTAMP_DECORATION_FORMAT
   ) as DateTimeFormat;
@@ -92,8 +115,9 @@ function decorateTimestamps(frontmatter: FrontmatterContent) {
     .filter(isNotUndefined);
 }
 
-const DECORATION_TYPE_BLOCK_ANCHOR = window.createTextEditorDecorationType({
+export const DECORATION_TYPE_BLOCK_ANCHOR = window.createTextEditorDecorationType({
   opacity: "40%",
+  rangeBehavior: DecorationRangeBehavior.ClosedOpen,
 });
 
 function decorateBlockAnchor(blockAnchor: BlockAnchor) {
@@ -104,4 +128,90 @@ function decorateBlockAnchor(blockAnchor: BlockAnchor) {
     range: VSCodeUtils.position2VSCodeRange(position),
   };
   return decoration;
+}
+
+/** Decoration for wikilinks that point to valid notes. */
+export const DECORATION_TYPE_WIKILINK = window.createTextEditorDecorationType({
+  color: new ThemeColor("editorLink.activeForeground"),
+  rangeBehavior: DecorationRangeBehavior.ClosedClosed,
+});
+
+/** Decoration for wikilinks that do *not* point to valid notes (e.g. broken). */
+export const DECORATION_TYPE_BROKEN_WIKILINK = window.createTextEditorDecorationType({
+  color: new ThemeColor("editorWarning.foreground"),
+  backgroundColor: new ThemeColor("editorWarning.background"),
+  rangeBehavior: DecorationRangeBehavior.ClosedClosed,
+});
+
+
+function doesLinkedNoteExist({fname, vaultName}: {fname: string, vaultName?: string}) {
+  const {notes, vaults} = getWS().getEngine();
+  const vault = vaultName ? VaultUtils.getVaultByName({vname: vaultName, vaults}) : undefined;
+  // Vault specified, but can't find it.
+  if (vaultName && !vault) return false;
+  const found = NoteUtils.getNotesByFname({
+    fname,
+    vault,
+    notes,
+  });
+  return found.length > 0;
+}
+
+/** Decoration for the alias part of wikilinks. */
+export const DECORATION_TYPE_ALIAS = window.createTextEditorDecorationType({
+  fontStyle: "italic",
+});
+
+const RE_ALIAS = /(?<beforeAlias>\[\[)(?<alias>[^|]+)\|/;
+
+function decorateWikiLink(wikiLink: WikiLinkNoteV4, document: TextDocument) {
+  const position = wikiLink.position;
+  if (_.isUndefined(position)) return []; // should never happen
+
+  const foundNote = doesLinkedNoteExist({fname: wikiLink.value, vaultName: wikiLink.data.vaultName});
+  const wikiLinkrange = VSCodeUtils.position2VSCodeRange(position);
+  const options: DecorationOptions = {
+    range: wikiLinkrange,
+  };
+  const decorations: [TextEditorDecorationType, DecorationOptions][] = [];
+  
+  // Highlight the alias part
+  const linkText = document.getText(options.range);
+  const aliasMatch = linkText.match(RE_ALIAS);
+  if (aliasMatch && aliasMatch.groups?.beforeAlias && aliasMatch.groups?.alias) {
+    const {beforeAlias, alias} = aliasMatch.groups;
+    decorations.push([DECORATION_TYPE_ALIAS, { range: new Range(
+      wikiLinkrange.start.line,
+      wikiLinkrange.start.character + beforeAlias.length,
+      wikiLinkrange.start.line,
+      wikiLinkrange.start.character + beforeAlias.length + alias.length,
+    )}]);
+  }
+
+  if (foundNote || containsNonDendronUri(wikiLink.value)) {
+    decorations.push(
+      [DECORATION_TYPE_WIKILINK, options]
+    );
+  } else {
+    decorations.push(
+      [DECORATION_TYPE_BROKEN_WIKILINK, options]
+    );
+  }
+  return decorations;
+}
+
+function decorateReference(reference: NoteRefNoteV4): [TextEditorDecorationType, DecorationOptions] | undefined {
+  const position = reference.position as Position;
+  if (_.isUndefined(position)) return undefined;
+
+  const foundNote = doesLinkedNoteExist({fname: reference.data.link.from.fname, vaultName: reference.data.link.data.vaultName});
+  const options: DecorationOptions = {
+    range: VSCodeUtils.position2VSCodeRange(position),
+  };
+
+  if (foundNote) {
+    return [DECORATION_TYPE_WIKILINK, options];
+  } else {
+    return [DECORATION_TYPE_BROKEN_WIKILINK, options]
+  }
 }

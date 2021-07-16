1115f4a12292bc2cd2724dda3a63e2f048144a58
Kevin <kevin@dendron.so>
2021-07-15 19:56:07 -0700

    chore: compiler fixes

--- packages/plugin-core/src/test/suite-integ/NoteLookupCommand.test.ts
+++ packages/plugin-core/src/test/suite-integ/NoteLookupCommand.test.ts
@@ -10,8 +10,8 @@ import { describe } from "mocha";
 // // You can import and use all API from the 'vscode' module
 // // as well as import your extension to test it
 import * as vscode from "vscode";
-import { LookupNoteTypeEnum } from "../../commands/LookupCommand";
 import { NoteLookupCommand } from "../../commands/NoteLookupCommand";
+import { LookupNoteTypeEnum } from "../../commands/LookupCommand";
 import { PickerUtilsV2 } from "../../components/lookup/utils";
 import { VSCodeUtils } from "../../utils";
 import { expect } from "../testUtilsv2";

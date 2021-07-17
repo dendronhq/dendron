cae6641eab77953b061a6a4fc89e2240af3fc01d
kevinslin <kevinslin8@gmail.com>
2021-07-17 12:03:09 -0700

    chore: add static layouts

---
+++ packages/common-frontend/src/components/layouts/BasicLayout.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { Col, Layout, Row } from "antd";
+
+export default function BasicLayout(props: React.PropsWithChildren<any>) {
+  return (
+    <Layout>
+      <br />
+      <br />
+      <Layout id="main-content-wrap" className="main-content-wrap">
+        <Row gutter={16}>
+          <Col className="gutter-row" span={2}/>
+          <Col className="gutter-row" span={20}>
+            <Layout.Content
+              id="main-content"
+              className="main-content"
+              role="main"
+            >
+              {props.children}
+            </Layout.Content>
+          </Col>
+          <Col className="gutter-row" span={2}/>
+        </Row>
+      </Layout>
+    </Layout>
+  );
+}

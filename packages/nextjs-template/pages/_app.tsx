dbf95e93016d2dcb14332fdbf4a8767e709c731c
kevinslin <kevinslin8@gmail.com>
2021-07-17 12:30:52 -0700

    spike: add ant layout

--- packages/nextjs-template/pages/_app.tsx
+++ packages/nextjs-template/pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { BasicLayout } from "@dendronhq/common-frontend";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <BasicLayout>
+      <Component {...pageProps} />
+    </BasicLayout>
+  );
 }
-export default MyApp
+export default MyApp;

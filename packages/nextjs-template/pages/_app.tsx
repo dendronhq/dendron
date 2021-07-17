eef885e3807d50ae22b7c13e02ae91af1e88fafd
kevinslin <kevinslin8@gmail.com>
2021-07-17 12:33:18 -0700

    spike: add theme

--- packages/nextjs-template/pages/_app.tsx
+++ packages/nextjs-template/pages/_app.tsx
@@ -1,12 +1,21 @@
 import "../styles/globals.css";
+import { ThemeSwitcherProvider } from "react-css-theme-switcher";
 import type { AppProps } from "next/app";
 import { BasicLayout } from "@dendronhq/common-frontend";
 
+const themes = {
+  dark: `/dark-theme.css`,
+  light: `/light-theme.css`,
+};
+
 function MyApp({ Component, pageProps }: AppProps) {
+  const defaultTheme = "light";
   return (
-    <BasicLayout>
-      <Component {...pageProps} />
-    </BasicLayout>
+    <ThemeSwitcherProvider themeMap={themes} defaultTheme={defaultTheme}>
+      <BasicLayout>
+        <Component {...pageProps} />
+      </BasicLayout>
+    </ThemeSwitcherProvider>
   );
 }
 export default MyApp;

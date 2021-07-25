a0f9ff06a5eb5c074ba9b7c06f40e8ac3c3222e4
Kevin <kevin@dendron.so>
2021-07-25 08:54:55 -0700

    enhance(preview-v2): handle missing note ref
    commit b2c671f8736c5ebb16b770e3edfb1c3d1be65930
    Author: Kevin <kevin@dendron.so>
    Date:   Fri Jul 23 12:44:25 2021 -0700
    
        enhance: gracefully handle missing note refs
    
    commit 774f55872bc6bf33d1263f107d6aaa5a0b6ab963
    Author: Kevin <kevin@dendron.so>
    Date:   Fri Jul 23 12:14:44 2021 -0700
    
        enhance: add local assets to preview
    
    commit a366265813894ba274c60600cdea0728ed6b042c
    Author: Kevin <kevin@dendron.so>
    Date:   Fri Jul 23 12:33:38 2021 -0700
    
        chore: updates
    
    commit 9d334a825af848db5f6c56d013d769ff2d0294b6
    Author: Kevin <kevin@dendron.so>
    Date:   Fri Jul 23 11:50:00 2021 -0700
    
        chore: address pr comments from pr 1008
    
        https://github.com/dendronhq/dendron/pull/1008

--- packages/engine-test-utils/src/__tests__/engine-server/markdown/utilsv5.spec.ts
+++ packages/engine-test-utils/src/__tests__/engine-server/markdown/utilsv5.spec.ts
@@ -1,11 +1,11 @@
 import {
   NoteTestUtilsV4,
-  TestPresetEntryV4
+  TestPresetEntryV4,
 } from "@dendronhq/common-test-utils";
 import {
   DendronASTDest,
   Processor,
-  ProcFlavor
+  ProcFlavor,
 } from "@dendronhq/engine-server";
 import path from "path";
 import { createEngineFromServer, runEngineTestV5 } from "../../../engine";
@@ -18,129 +18,170 @@ const getOpts = (opts: any) => {
   return _copts;
 };
 
-const IMAGE_WITH_LEAD_FORWARD_SLASH = createProcCompileTests({
-  name: "IMAGE_WITH_LEAD_FORWARD_SLASH",
-  setup: async (opts) => {
-    const { proc } = getOpts(opts);
-    const txt = `![foo alt txt](/assets/foo.jpg)`;
-    const resp = await proc.process(txt);
-    return { resp, proc };
-  },
-  verify: {
-    [DendronASTDest.HTML]: {
-      [ProcFlavor.REGULAR]: async (opts) => {
-        const {
-          extra: { resp },
-        } = cleanVerifyOpts(opts);
-        await checkString(
-          resp.contents,
-          `<img src="/assets/foo.jpg" alt="foo alt txt">`
-        );
-      },
-      [ProcFlavor.PREVIEW]: async (opts) => {
-        const {
-          extra: { resp },
-        } = cleanVerifyOpts(opts);
-        await checkString(resp.contents, `assets%2Ffoo.jpg`, "localhost");
-      },
-      [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
-    },
-    [DendronASTDest.MD_REGULAR]: {
-      [ProcFlavor.HOVER_PREVIEW]: async (opts) => {
-        const {
-          extra: { resp },
-        } = cleanVerifyOpts(opts);
-        await checkString(resp.contents,
-          `![foo alt txt](${path.join(opts.wsRoot, opts.vaults[0].fsPath, '/assets/foo.jpg')})`,
-        );
-      },
-    },
-  },
-  preSetupHook: async (opts) => {
-    await ENGINE_HOOKS.setupBasic(opts);
-  },
-});
-const IMAGE_NO_LEAD_FORWARD_SLASH = createProcCompileTests({
-  name: "IMAGE_NO_LEAD_FORWARD_SLASH",
-  setup: async (opts) => {
-    const { proc } = getOpts(opts);
-    const txt = `![foo alt txt](assets/foo.jpg)`;
-    const resp = await proc.process(txt);
-    return { resp, proc };
-  },
-  verify: {
-    [DendronASTDest.HTML]: {
-      [ProcFlavor.REGULAR]: async (opts) => {
-        const {
-          extra: { resp },
-        } = cleanVerifyOpts(opts);
-        await checkString(
-          resp.contents,
-          `<img src="assets/foo.jpg" alt="foo alt txt">`
-        );
-      },
-      [ProcFlavor.PREVIEW]: async (opts) => {
-        const {
-          extra: { resp },
-        } = cleanVerifyOpts(opts);
-        await checkString(resp.contents, `assets%2Ffoo.jpg`, "localhost");
-      },
-      [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
-    },
-    [DendronASTDest.MD_REGULAR]: {
-      [ProcFlavor.HOVER_PREVIEW]: async (opts) => {
-        const {
-          extra: { resp },
-        } = cleanVerifyOpts(opts);
-        await checkString(resp.contents,
-          `![foo alt txt](${path.join(opts.wsRoot, opts.vaults[0].fsPath, '/assets/foo.jpg')})`,
-        );
-      },
-    },
-  },
-  preSetupHook: async (opts) => {
-    await ENGINE_HOOKS.setupBasic(opts);
-  },
-});
-const NOTE_REF_BASIC_WITH_REHYPE = createProcCompileTests({
-  name: "NOTE_REF_WITH_REHYPE",
-  setup: async (opts) => {
-    const { proc } = getOpts(opts);
-    const txt = `![[alpha.md]]`;
-    const resp = await proc.process(txt);
-    return { resp, proc };
-  },
-  verify: {
-    [DendronASTDest.HTML]: {
-      [ProcFlavor.REGULAR]: async ({ extra }) => {
-        const { resp } = extra;
-        expect(resp).toMatchSnapshot();
-        await checkString(
-          resp.contents,
-          // should have id for link
-          `<a href="alpha-id.html"`,
-          // title should be fname,
-          "Alpha</h1>",
-          // html quoted
-          `<p><a href="bar.html">Bar</a></p>`
-        );
-      },
-      [ProcFlavor.PREVIEW]: ProcFlavor.REGULAR,
-      [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
-    },
-  },
-  preSetupHook: async (opts) => {
-    await ENGINE_HOOKS.setupBasic({ ...opts, extra: { idv2: true } });
-    await NoteTestUtilsV4.createNote({fname: "alpha", body: "[[bar]]", vault: opts.vaults[0], wsRoot: opts.wsRoot, props: {id: "alpha-id"}});
-  },
-});
-const ALL_TEST_CASES = [
-  ...IMAGE_NO_LEAD_FORWARD_SLASH,
-  ...IMAGE_WITH_LEAD_FORWARD_SLASH,
-  ...NOTE_REF_BASIC_WITH_REHYPE,
-];
-
 describe("MDUtils.proc", () => {
+  const IMAGE_WITH_LEAD_FORWARD_SLASH = createProcCompileTests({
+    name: "IMAGE_WITH_LEAD_FORWARD_SLASH",
+    setup: async (opts) => {
+      const { proc } = getOpts(opts);
+      const txt = `![foo alt txt](/assets/foo.jpg)`;
+      const resp = await proc.process(txt);
+      return { resp, proc };
+    },
+    verify: {
+      [DendronASTDest.HTML]: {
+        [ProcFlavor.REGULAR]: async (opts) => {
+          const {
+            extra: { resp },
+          } = cleanVerifyOpts(opts);
+          await checkString(
+            resp.contents,
+            `<img src="/assets/foo.jpg" alt="foo alt txt">`
+          );
+        },
+        [ProcFlavor.PREVIEW]: async (opts) => {
+          const {
+            extra: { resp },
+          } = cleanVerifyOpts(opts);
+          await checkString(resp.contents, `assets%2Ffoo.jpg`, "localhost");
+        },
+        [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
+      },
+      [DendronASTDest.MD_REGULAR]: {
+        [ProcFlavor.HOVER_PREVIEW]: async (opts) => {
+          const {
+            extra: { resp },
+          } = cleanVerifyOpts(opts);
+          await checkString(
+            resp.contents,
+            `![foo alt txt](${path.join(
+              opts.wsRoot,
+              opts.vaults[0].fsPath,
+              "/assets/foo.jpg"
+            )})`
+          );
+        },
+      },
+    },
+    preSetupHook: async (opts) => {
+      await ENGINE_HOOKS.setupBasic(opts);
+    },
+  });
+  const IMAGE_NO_LEAD_FORWARD_SLASH = createProcCompileTests({
+    name: "IMAGE_NO_LEAD_FORWARD_SLASH",
+    setup: async (opts) => {
+      const { proc } = getOpts(opts);
+      const txt = `![foo alt txt](assets/foo.jpg)`;
+      const resp = await proc.process(txt);
+      return { resp, proc };
+    },
+    verify: {
+      [DendronASTDest.HTML]: {
+        [ProcFlavor.REGULAR]: async (opts) => {
+          const {
+            extra: { resp },
+          } = cleanVerifyOpts(opts);
+          await checkString(
+            resp.contents,
+            `<img src="assets/foo.jpg" alt="foo alt txt">`
+          );
+        },
+        [ProcFlavor.PREVIEW]: async (opts) => {
+          const {
+            extra: { resp },
+          } = cleanVerifyOpts(opts);
+          await checkString(resp.contents, `assets%2Ffoo.jpg`, "localhost");
+        },
+        [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
+      },
+      [DendronASTDest.MD_REGULAR]: {
+        [ProcFlavor.HOVER_PREVIEW]: async (opts) => {
+          const {
+            extra: { resp },
+          } = cleanVerifyOpts(opts);
+          await checkString(
+            resp.contents,
+            `![foo alt txt](${path.join(
+              opts.wsRoot,
+              opts.vaults[0].fsPath,
+              "/assets/foo.jpg"
+            )})`
+          );
+        },
+      },
+    },
+    preSetupHook: async (opts) => {
+      await ENGINE_HOOKS.setupBasic(opts);
+    },
+  });
+  const NOTE_REF_BASIC_WITH_REHYPE = createProcCompileTests({
+    name: "NOTE_REF_WITH_REHYPE",
+    setup: async (opts) => {
+      const { proc } = getOpts(opts);
+      const txt = `![[alpha.md]]`;
+      const resp = await proc.process(txt);
+      return { resp, proc };
+    },
+    verify: {
+      [DendronASTDest.HTML]: {
+        [ProcFlavor.REGULAR]: async ({ extra }) => {
+          const { resp } = extra;
+          expect(resp).toMatchSnapshot();
+          await checkString(
+            resp.contents,
+            // should have id for link
+            `<a href="alpha-id.html"`,
+            // title should be fname,
+            "Alpha</h1>",
+            // html quoted
+            `<p><a href="bar.html">Bar</a></p>`
+          );
+        },
+        [ProcFlavor.PREVIEW]: ProcFlavor.REGULAR,
+        [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
+      },
+    },
+    preSetupHook: async (opts) => {
+      await ENGINE_HOOKS.setupBasic({ ...opts, extra: { idv2: true } });
+      await NoteTestUtilsV4.createNote({
+        fname: "alpha",
+        body: "[[bar]]",
+        vault: opts.vaults[0],
+        wsRoot: opts.wsRoot,
+        props: { id: "alpha-id" },
+      });
+    },
+  });
+  const NOTE_REF_MISSING  = createProcCompileTests({
+    name: "NOTE_REF_MISSING",
+    setup: async (opts) => {
+      const { proc } = getOpts(opts);
+      const txt = `![[alpha.md]]`;
+      const resp = await proc.process(txt);
+      return { resp, proc };
+    },
+    verify: {
+      [DendronASTDest.HTML]: {
+        [ProcFlavor.REGULAR]: async ({ extra }) => {
+          const { resp } = extra;
+          await checkString(
+            resp.contents,
+            "Did not find",
+          );
+        },
+        [ProcFlavor.PREVIEW]: ProcFlavor.REGULAR,
+        [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
+      },
+    },
+    preSetupHook: async (opts) => {
+      await ENGINE_HOOKS.setupBasic({ ...opts, extra: { idv2: true } });
+    },
+  });
+  const ALL_TEST_CASES = [
+    ...IMAGE_NO_LEAD_FORWARD_SLASH,
+    ...IMAGE_WITH_LEAD_FORWARD_SLASH,
+    ...NOTE_REF_BASIC_WITH_REHYPE,
+    ...NOTE_REF_MISSING,
+  ];
   test.each(
     ALL_TEST_CASES.map((ent) => [
       `${ent.dest}: ${ent.name}: ${ent.flavor}`,

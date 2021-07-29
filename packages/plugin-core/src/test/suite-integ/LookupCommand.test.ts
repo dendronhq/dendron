4b816c46f60f508e906409a60e0ac52e7d0fadab
Kevin <kevin@dendron.so>
2021-07-29 10:39:49 -0700

    chore: fix sinon issues

--- packages/plugin-core/src/test/suite-integ/LookupCommand.test.ts
+++ packages/plugin-core/src/test/suite-integ/LookupCommand.test.ts
@@ -12,7 +12,6 @@ import {
   FileTestUtils,
   NOTE_PRESETS_V4,
   runJestHarnessV2,
-  sinon,
   TestPresetEntryV4,
 } from "@dendronhq/common-test-utils";
 import { DendronEngineV2 } from "@dendronhq/engine-server";
@@ -25,6 +24,7 @@ import fs from "fs-extra";
 import _ from "lodash";
 import { describe } from "mocha";
 import path from "path";
+import sinon from "sinon";
 // // You can import and use all API from the 'vscode' module
 // // as well as import your extension to test it
 import * as vscode from "vscode";

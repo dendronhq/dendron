c288ae22782b93df2c3e46fa51cd5f3ad3ad072a
Kevin <kevin@dendron.so>
2021-08-01 14:51:16 -0700

    chore: optimize nextjs pod

--- packages/engine-test-utils/src/presets/engine-server/render.ts
+++ packages/engine-test-utils/src/presets/engine-server/render.ts
@@ -1,4 +1,5 @@
-import { AssertUtils, TestPresetEntryV4 } from "@dendronhq/common-test-utils";
+import { AssertUtils, NoteTestUtilsV4, TestPresetEntryV4 } from "@dendronhq/common-test-utils";
+import { checkString } from "../../utils";
 import { ENGINE_HOOKS } from "./utils";
 
 const NOTES = {
@@ -21,6 +22,26 @@ const NOTES = {
     {
       preSetupHook: async (opts) => {
         return ENGINE_HOOKS.setupBasic(opts);
+      },
+    }
+  ),
+  EMPTY_NOTE: new TestPresetEntryV4(
+    async ({ engine }) => {
+      const { data } = await engine.renderNote({
+        id: "empty",
+      });
+      expect(data).toMatchSnapshot();
+      return [
+        {
+          actual: true,
+          expected: checkString(data!, ""),
+          msg: "empty string",
+        },
+      ];
+    },
+    {
+      preSetupHook: async (opts) => {
+        return NoteTestUtilsV4.createNote({...opts, fname: "empty", vault: opts.vaults[0]});
       },
     }
   ),

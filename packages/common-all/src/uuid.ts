8c4600163f69c88764cf2a64c8ad214941824c6b
Kaan Genc <genc.5@osu.edu>
2021-08-02 17:21:51 -0400

    chore: use 21 character nanoids for notes
    Note IDs use 21 character ids, the default for a nanoid.
    
    The insecure generator, which is used in limited cases like block anchors and assets in markdown imports, use much shorter 12 character ids. This is still safe since these are used in limited cases where conflicts are unlikely, e.g. you would need over 9 billion block anchors within the same note before you have just 1% chance of a conflict.
    
    #1044

--- packages/common-all/src/uuid.ts
+++ packages/common-all/src/uuid.ts
@@ -4,16 +4,18 @@ import { customAlphabet as nanoidInsecure } from "nanoid/non-secure";
 import { alphanumeric } from "nanoid-dictionary";
 
 /** Using this length, according to [nanoid collision calculator](https://zelark.github.io/nano-id-cc/),
- * generating 1000 IDs per second, it would take around 981 years to have 1 percent chance of a single collision.
- * This is actually a higher chance than UUID (and default of nanoid), but still very safe for our purposes.
+ * generating 1000 IDs per hour, it would take around 919 years to have 1 percent chance of a single collision.
+ * This is okay for the "insecure" generator, which is used in limited cases where collisions are less likely.
  */
-const ID_LENGTH = 16;
+const SHORT_ID_LENGTH = 12;
+/** Default length for nanoids. */
+const LONG_ID_LENGTH = 21;
 
 /** Generates a random identifier.
  *
  * @returns A url-safe, random identifier.
  */
-export const genUUID = nanoid(alphanumeric, ID_LENGTH);
+export const genUUID = nanoid(alphanumeric, LONG_ID_LENGTH);
 
 /** Generates a random identifier asynchronously.
  *
@@ -21,14 +23,15 @@ export const genUUID = nanoid(alphanumeric, ID_LENGTH);
  *
  * @returns A url-safe, random identifier.
  */
-export const genUUIDasync = nanoidAsync(alphanumeric, ID_LENGTH);
+export const genUUIDasync = nanoidAsync(alphanumeric, LONG_ID_LENGTH);
 
-/** Generates a random identifier, faster but with potential cryptographic risks.
+/** Generates a shorter random identifier, faster but with potential cryptographic risks.
  *
  * Uses an insecure random generator for faster generation.
- * This increases the risk of collision attacks.
- * Only use this if performance is critical and collisions are relatively unimportant.
+ * Also shortens the length of the generated IDs to 16 characters.
+ * This increases the risk of collisions.
+ * Only use this if performance is important and collisions are relatively unimportant.
  *
  * @returns A url-safe, random identifier.
  */
-export const genUUIDInsecure = nanoidInsecure(alphanumeric, ID_LENGTH);
+export const genUUIDInsecure = nanoidInsecure(alphanumeric, SHORT_ID_LENGTH);

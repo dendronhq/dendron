f49163a6f4ba658620e81f745a397084b14b5c97
Kevin <kevin@dendron.so>
2021-08-02 10:48:37 -0700

    chore: optimize nextjs page gen

--- packages/nextjs-template/utils/index.ts
+++ packages/nextjs-template/utils/index.ts
@@ -1,5 +1,14 @@
 import fs from "fs-extra";
 import path from "path";
+import { NoteProps, NotePropsDict } from "@dendronhq/common-all";
+import _ from "lodash";
+
+export type NoteData = {
+	notes: NotePropsDict;
+}
+
+const NOTE_META_DIR = "meta";
+const NOTE_BODY_DIR = "notes";
 
 export function getDataDir(): string {
 	const dataDir = process.env.DATA_DIR;
@@ -11,6 +20,23 @@ export function getDataDir(): string {
 
 export function getNoteBody(id: string) {
 	const dataDir = getDataDir();
-	const body = fs.readFileSync(path.join(dataDir, "notes", `${id}.html`), {encoding: "utf8"})
+	const body = fs.readFile(path.join(dataDir, NOTE_BODY_DIR, `${id}.html`), {encoding: "utf8"})
 	return body;
+}
+
+let _NOTES_CACHE: NoteData|undefined;
+
+export function getNotes() {
+	console.log("getNotes")
+	if (_.isUndefined(_NOTES_CACHE)) {
+		console.log("getNotes:readFromDisk")
+		const dataDir = getDataDir();
+		_NOTES_CACHE = fs.readJSONSync(path.join(dataDir, "notes.json")) as NoteData
+	}
+	return _NOTES_CACHE;
+}
+
+export function getNoteMeta(id: string) {
+	const dataDir = getDataDir();
+	return fs.readJSON(path.join(dataDir, NOTE_META_DIR, `${id}.json`)) as Promise<NoteProps>;
 }

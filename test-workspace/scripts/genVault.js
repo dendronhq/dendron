db0208b75377da988eef8008133d11b1c9894942
Kevin <kevin@dendron.so>
2021-08-02 14:14:51 -0700

    chore: update tsconfig json

--- test-workspace/scripts/genVault.js
+++ test-workspace/scripts/genVault.js
@@ -14,9 +14,8 @@ function createVault() {
 }
 
 async function main() {
-	const vaultRoot = path.join(__dirname, "..", "..", "test-workspace-10-profile", "vault");
-	await Promise.all(_.range(0, 10).map(async idx => {
-		console.log(idx);
+	const vaultRoot = path.join(__dirname, "..", "..", "test-workspace-5k-profile", "vault");
+	await Promise.all(_.range(0, 5000).map(async idx => {
 		return fs.writeFile(path.join(vaultRoot, `${idx}.md`), generateNote({generatedLength: 5}));
 	}));
 }

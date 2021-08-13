ecd77d40255df460db4f00c24d37d49f64c746ec
Mark Choi <hikchoi@gmail.com>
2021-08-13 23:33:15 +0900

    chore: add basic test for InsertNoteIndexCommand

--- packages/plugin-core/src/test/suite-integ/InsertNoteIndexCommand.test.ts
+++ packages/plugin-core/src/test/suite-integ/InsertNoteIndexCommand.test.ts
@@ -1,6 +1,9 @@
 import { ENGINE_HOOKS } from "@dendronhq/engine-test-utils";
+import { AssertUtils } from "@dendronhq/common-test-utils";
 import * as vscode from "vscode";
+import { describe } from "mocha";
 import { InsertNoteIndexCommand } from "../../commands/InsertNoteIndexCommand";
+import { VSCodeUtils } from "../../utils";
 import { TIMEOUT } from "../testUtils";
 import { expect } from "../testUtilsv2";
 import { runLegacyMultiWorkspaceTest, setupBeforeAfter } from "../testUtilsV3";
@@ -16,13 +19,29 @@ suite("InsertNoteIndex", function () {
         preSetupHook: async ({ wsRoot, vaults }) => {
           await ENGINE_HOOKS.setupBasic({ wsRoot, vaults });
         },
-        onInit: async () => {
+        onInit: async ({ engine }) => {
+          const notes = engine.notes;
           const cmd = new InsertNoteIndexCommand();
+
+          await VSCodeUtils.openNote(notes["foo"]);
+          const editor = VSCodeUtils.getActiveTextEditorOrThrow();
+          editor.selection = new vscode.Selection(9, 0, 9, 0);
           await cmd.execute({});
-          expect(undefined).toBeFalsy();
+          const body = editor.document.getText();
+          expect(
+            await AssertUtils.assertInString({
+              body, 
+              match: [
+                [
+                  "## Index",
+                  "- [[Ch1|foo.ch1]]"
+                ].join("\n")
+              ]
+            })
+          );
           done();
         }
-      })
+      });
     });
   });
 });

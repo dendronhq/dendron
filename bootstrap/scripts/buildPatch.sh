89eba58b2a58d192c2425decf724ab751a202cbc
Jonathan Yeung <jonathanyeung@outlook.com>
2021-08-21 18:29:13 +0800

    spike: fixing sh compat

--- bootstrap/scripts/buildPatch.sh
+++ bootstrap/scripts/buildPatch.sh
@@ -1,4 +1,4 @@
-if [ $PUBLISH_ENDPOINT == "local" ]; then
+if [ $PUBLISH_ENDPOINT = "local" ]; then
 	echo "start verdaccio"
 	verdaccio &
 	FOO_PID=$!
@@ -10,7 +10,7 @@ echo "building... upgrade: $UPGRADE_TYPE, endpoint: $PUBLISH_ENDPOINT"
 
 LOG_LEVEL=info dendron dev build --upgradeType $UPGRADE_TYPE --publishEndpoint $PUBLISH_ENDPOINT
 
-if [ $PUBLISH_ENDPOINT == "local" ]; then
+if [ $PUBLISH_ENDPOINT = "local" ]; then
 	echo "killing "
 	kill $FOO_PID
 fi

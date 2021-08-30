1e2e1fb32d8d50eefff7145d82bc92a3a20cb388
Samuel Hobl <nam@hobl.at>
2021-08-30 22:45:32 +0200

    nextjs template style improvements 2 (#1240)
    * spike: adjust layout to be centered
    
    The initial intend was to only sets a content max-width using ant breakpoints.
    This would leave a big space on the right on wider screens. Centereing
    the layout solves that and keeps the layout of the old template.
    
    * spike: change main content font-size
    
    The old template has font-size set to 16px for main content.
    16px is better than 14px for legibility.

--- packages/nextjs-template/styles/constants.ts
+++ packages/nextjs-template/styles/constants.ts
@@ -17,6 +17,14 @@ const HEADER = {
 
 const LAYOUT = {
   PADDING: 24,
+  BREAKPOINTS: {
+    xs: "480px",
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px",
+    xxl: "1600px",
+  },
 };
 
 export const DENDRON_STYLE_CONSTANTS = {

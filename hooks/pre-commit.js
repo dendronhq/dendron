c9abf6ef4fbef44a7f9cea58043a676c8a2ebce6
nickolay-kondratyev <nickolay.kondratyev@gmail.com>
2021-08-31 12:45:55 +0700

    chore: add pre-commit husky check to avoid direct imports from packages

--- hooks/pre-commit.js
+++ hooks/pre-commit.js
@@ -1,6 +1,16 @@
 const {checkToken} = require("./common");
 const {exec} = require("./exec");
 
+const ErrorMessages = {
+  AVOID_DIRECT_IMPORT_FROM_PACKAGES:[
+    `Direct import from a package.`,
+    `This check catches direct imports such as:`,
+    `import { milliseconds } from "@dendronhq/common-all/lib/timing";`,
+    `While the above import should look as:`,
+    `import { milliseconds } from "@dendronhq/common-all";`
+  ].join('\n')
+}
+
 function main() {
   const gitCommand = `git diff --staged --name-only`;
   const stagedFiles = exec(gitCommand).stdout.split('\n');
@@ -9,6 +19,10 @@ function main() {
     filesToCheck: stagedFiles,
     forbiddenTokens: {
       ".localhost": { rgx: /localhost:/, fileRgx: /\.lock$/ },
+
+      [ErrorMessages.AVOID_DIRECT_IMPORT_FROM_PACKAGES]: {
+        rgx: /((common-frontend|common-all|common-server|engine-server|dendron-cli|pods-core|api-server|common-test-utils|engine-test-utils|dendron-next-server)\/)/,
+        fileRgx: /\.ts[x]?$/ },
     }
   });
 }

8ccc860982d271868d5fe30a929ca716688eb44f
Mark Choi <hikchoi@gmail.com>
2021-09-09 10:47:40 +0900

    chore: change generic type for DendronConfigEntry

--- packages/common-all/src/types/configs/base.ts
+++ packages/common-all/src/types/configs/base.ts
@@ -1,5 +1,3 @@
-export type DendronConfigValueType = string | boolean | number;
-
 /**
  * DendronConfigEntry
  * Holds the value, label, and description of individual configuration entries.
@@ -7,8 +5,8 @@ export type DendronConfigValueType = string | boolean | number;
  * For config entries that can be an arbitrary value, only specify the label and description.
  * For config entries that have pre-defined choices, provide the value as well as label and description specific to that value.
  */
-export type DendronConfigEntry<DendronConfigValueType> = {
-  value?: DendronConfigValueType;
+export type DendronConfigEntry<T> = {
+  value?: T;
   label: string;
   desc: string;
 };

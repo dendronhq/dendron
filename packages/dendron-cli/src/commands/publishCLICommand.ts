10586b3760b6c593e1032308997b7e25765ccefa
Kevin <kevin@dendron.so>
2021-09-11 10:47:38 -0700

    chore: fix compiler issues

--- packages/dendron-cli/src/commands/publishCLICommand.ts
+++ packages/dendron-cli/src/commands/publishCLICommand.ts
@@ -90,7 +90,7 @@ export class PublishCLICommand extends CLICommand<CommandOpts, CommandOutput> {
         default:
           return assertUnreachable();
       }
-    } catch (err) {
+    } catch (err: any) {
       this.L.error(err);
       if (err instanceof DendronError) {
         this.print(["status:", err.status, err.message].join(" "));

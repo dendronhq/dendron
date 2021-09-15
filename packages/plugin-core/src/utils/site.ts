9ae7cac9b0b0131271bfdef2214aec6cc3f1a02b
Kevin <kevin@dendron.so>
2021-09-14 19:34:11 -0700

    chore: bump 11ty

--- packages/plugin-core/src/utils/site.ts
+++ packages/plugin-core/src/utils/site.ts
@@ -16,7 +16,7 @@ const packageJson = {
   main: "index.js",
   license: "MIT",
   dependencies: {
-    "@dendronhq/dendron-11ty": "^1.59.2-alpha.1",
+    "@dendronhq/dendron-11ty": "^1.59.2-alpha.2",
   },
 };
 

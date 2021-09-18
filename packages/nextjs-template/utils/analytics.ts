b2afd81721f7c5f1caf1202c0bcd44c43a685017
Leo <leo@senrok.com>
2021-09-18 09:27:06 +0800

    refactor(publishing): remove undefined check

--- packages/nextjs-template/utils/analytics.ts
+++ packages/nextjs-template/utils/analytics.ts
@@ -1,22 +1,14 @@
-import ReactGA from "react-ga";
+import { initialize, set, pageview, event, exception } from "react-ga";
 
-export const initGA = async (gaTrackingId: string) => {
-  ReactGA.initialize(gaTrackingId);
-};
+export const initGA = async (gaTrackingId: string) => initialize(gaTrackingId);
 
 export const logPageView = () => {
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+  set({ page: window.location.pathname });
+  pageview(window.location.pathname);
 };
 
-export const logEvent = (category = "", action = "") => {
-  if (category && action) {
-    ReactGA.event({ category, action });
-  }
-};
+export const logEvent = (category = "", action = "") =>
+  event({ category, action });
 
-export const logException = (description = "", fatal = false) => {
-  if (description) {
-    ReactGA.exception({ description, fatal });
-  }
-};
+export const logException = (description = "", fatal = false) =>
+  exception({ description, fatal });

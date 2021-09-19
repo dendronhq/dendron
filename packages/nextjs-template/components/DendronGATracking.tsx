fe79fcde47118c7a26bdfca446ed9347f5d83f20
viz <viztor@users.noreply.github.com>
2021-09-19 08:12:35 +0800

    chore(publish): convert Google Analytics comp to hook. (#1353)
    * refactor(publishing): GATracking & _app
    
    * refactor(publishing): remove undefined check

--- packages/nextjs-template/components/DendronGATracking.tsx
+++ packages/nextjs-template/components/DendronGATracking.tsx
@@ -1,38 +1,31 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { initGA, logPageView } from "../utils/analytics";
-import { NoteData } from "../utils/types";
 import { getStage } from "@dendronhq/common-all";
 import { useEngineAppSelector } from "../features/engine/hooks";
 import _ from "lodash";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
-export default function DendronGATracking(
-  props: React.PropsWithChildren<Partial<NoteData>>
-) {
+export const useDendronGATracking = () => {
   const [initialized, setInitialized] = useState(false);
   const engine = useEngineAppSelector((state) => state.engine);
-  const router = useRouter()
-
-  
-
+  const router = useRouter();
   useEffect(() => {
     const { config } = engine;
-    if(!_.isUndefined(config)){
-      const { ga_tracking } = config.site
-      if (ga_tracking && !initialized && getStage() !== "dev") {
-        initGA(ga_tracking);
+    if (!_.isUndefined(config)) {
+      const { ga_tracking: gaTracking } = config.site;
+      if (gaTracking && !initialized && getStage() !== "dev") {
+        initGA(gaTracking);
         setInitialized(true);
       }
-    }  
+    }
   }, [engine]);
 
   useEffect(() => {
-  
-    router.events.on('routeChangeComplete', () => logPageView())
+    router.events.on("routeChangeComplete", () => logPageView());
     return () => {
-      router.events.off('routeChangeComplete', () => logPageView())
-    }
-  }, [router.events])
+      router.events.off("routeChangeComplete", () => logPageView());
+    };
+  }, [router.events]);
+};
 
-  return <div>{props.children}</div>;
-}
+export default useDendronGATracking;

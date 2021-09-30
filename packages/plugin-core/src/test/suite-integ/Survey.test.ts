427101007c565c8a1f52bbdbc2c1936b51c1f5b8
Mark Choi <hikchoi@gmail.com>
2021-09-30 18:24:40 +0900

    chore: add tests for lapsed user survey

--- packages/plugin-core/src/test/suite-integ/Survey.test.ts
+++ packages/plugin-core/src/test/suite-integ/Survey.test.ts
@@ -1,4 +1,3 @@
-// import { DendronSurvey } from "../../survey"
 import { describe } from "mocha";
 import sinon from "sinon";
 import { expect } from "../testUtilsv2";
@@ -7,6 +6,9 @@ import { TutorialInitializer } from "../../workspace/tutorialInitializer";
 import { getDWorkspace } from "../../workspace";
 import { StateService } from "../../services/stateService";
 import { SurveyUtils } from "../../survey";
+import { showLapsedUserMessage } from "../../_extension";
+import { WSUtils } from "../../utils";
+import * as vscode from "vscode";
 
 suite("SurveyUtils", function () {
   const ctx = setupBeforeAfter(this);
@@ -47,4 +49,52 @@ suite("SurveyUtils", function () {
       });
     });
   });
+
+  describe("showLapsedUserSurvey", () => {
+    describe("GIVEN: user rejects lapsed user message", () => {
+      describe("WHEN: Global state for lapsed user survey isn't set", () => {
+        test("THEN: showLapsedUserSurvey is called", (done) => {
+          runLegacyMultiWorkspaceTest({
+            ctx,
+            preSetupHook: async () => {},
+            onInit: async () => {
+              sinon.stub(StateService.instance(), "getGlobalState").resolves(undefined);
+              const lapsedSurveySpy = sinon.spy(SurveyUtils, "showLapsedUserSurvey");
+              sinon.stub(vscode.window, "showInformationMessage").resolves({ title: "foo"});
+              await showLapsedUserMessage(WSUtils.getAssetUri(ctx));
+              await new Promise((resolve: any) => {
+                setTimeout(() => {
+                  resolve();
+                }, 1);
+              });
+              expect(lapsedSurveySpy.calledOnce).toBeTruthy();
+              done();
+            }
+          })
+        });
+      });
+
+      describe("WHEN: Global state for lapsed user survey is set", () => {
+        test("THEN: showLapsedUserSurvey is not called", (done) => {
+          runLegacyMultiWorkspaceTest({
+            ctx,
+            preSetupHook: async () => {},
+            onInit: async () => {
+              sinon.stub(StateService.instance(), "getGlobalState").resolves("submitted");
+              const lapsedSurveySpy = sinon.spy(SurveyUtils, "showLapsedUserSurvey");
+              sinon.stub(vscode.window, "showInformationMessage").resolves({ title: "foo"});
+              await showLapsedUserMessage(WSUtils.getAssetUri(ctx));
+              await new Promise((resolve: any) => {
+                setTimeout(() => {
+                  resolve();
+                }, 1);
+              });
+              expect(lapsedSurveySpy.calledOnce).toBeFalsy();
+              done();
+            }
+          })
+        });
+      })
+    })
+  })
 })

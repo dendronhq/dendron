ac3afb2464cf537e7c5edb3832721a7daac7861d
jonathanyeung <jonathanyeung@users.noreply.github.com>
2021-09-30 23:46:42 +0800

    chore: fix autobuild vsix on remote (#1445)
    * fix: differentiating between local and remote in ci publishing for npm login
    
    * spike: rename build vsix step

--- bootstrap/scripts/buildPatch.sh
+++ bootstrap/scripts/buildPatch.sh
@@ -9,8 +9,13 @@ fi
 SCRIPT_BUILD_ENV=${BUILD_ENV:-local}
 echo "building... upgrade: $UPGRADE_TYPE, endpoint: $PUBLISH_ENDPOINT build environment: $SCRIPT_BUILD_ENV"
 if [ $SCRIPT_BUILD_ENV = "ci" ]; then
-	echo "npm login"
-	yarn setup:npmlogin
+  if [ $PUBLISH_ENDPOINT = "local" ]; then
+  	echo "npm login with local account"
+	  yarn setup:npmlogin:local
+	elif [ $PUBLISH_ENDPOINT = "remote" ]; then
+  	echo "npm login with remote npm registry account"
+	  yarn setup:npmlogin:remote
+	fi
 fi
 
 DENDRON_CLI=dendron

bb163ecbcb50f7cf47f3d9a358692fe76a0aeccb
Stedag <stephen.dages@gmail.com>
2021-09-30 13:22:57 -0400

    enhance(commands): OpenLink can open more links on partial selection (#1317)
    * Updated OpenLink.ts
    
    * Correction to OpenLink.ts
    
    * Moved OpenLink functionality to utils/md
    
    * Fixed return type and test format issues
    
    * Corrected partial selection behavior
    
    * stubbed "openExternal" to avoid popup
    
    * Removed unnecessary notification
    
    * Add support for filepaths of various OSs

--- packages/plugin-core/src/commands/OpenLink.ts
+++ packages/plugin-core/src/commands/OpenLink.ts
@@ -7,9 +7,10 @@ import path from "path";
 import { PickerUtilsV2 } from "../components/lookup/utils";
 import { DENDRON_COMMANDS } from "../constants";
 import { resolvePath, VSCodeUtils } from "../utils";
-import { isAnythingSelected } from "../utils/editor";
+import { getURLAt } from "../utils/md";
 import { getExtension, getDWorkspace } from "../workspace";
 import { BasicCommand } from "./base";
+import { env, Uri, window } from "vscode";
 
 type CommandOpts = {};
 
@@ -25,47 +26,56 @@ export class OpenLinkCommand extends BasicCommand<CommandOpts, CommandOutput> {
   async execute() {
     const ctx = DENDRON_COMMANDS.OPEN_LINK;
     this.L.info({ ctx });
-    if (!isAnythingSelected()) {
+
+    let text = "";
+
+    text = getURLAt(VSCodeUtils.getActiveTextEditor());
+
+    if (_.isUndefined(text) || text === "") {
       const error = DendronError.createFromStatus({
         status: ERROR_STATUS.INVALID_STATE,
-        message: `nothing selected`,
+        message: `no valid path or URL selected`,
       });
       this.L.error({ error });
       return { error };
     }
-    const { text } = VSCodeUtils.getSelection();
-    if (_.isUndefined(text)) {
-      const error = DendronError.createFromStatus({
-        status: ERROR_STATUS.INVALID_STATE,
-        message: `nothing selected`,
-      });
-      this.L.error({ error });
-      return { error };
-    }
-    const wsRoot = getDWorkspace().wsRoot;
     let assetPath: string;
-    if (text.startsWith("asset")) {
-      const vault = PickerUtilsV2.getOrPromptVaultForOpenEditor();
-      assetPath = path.join(vault2Path({ vault, wsRoot }), text);
+    if (
+      text.indexOf(":/") !== -1 ||
+      text.indexOf("/") === 0 ||
+      text.indexOf(":\\") !== -1
+    ) {
+      window.showInformationMessage(
+        "the selection reads as a full URI or filepath so an attempt will be made to open it"
+      );
+      env.openExternal(Uri.parse(text.replace("\\", "/"))); // make sure vscode doesn't choke on "\"s
+      assetPath = text;
     } else {
-      assetPath = resolvePath(text, getExtension().rootWorkspace.uri.fsPath);
-    }
-    if (!fs.existsSync(assetPath)) {
-      const error = DendronError.createFromStatus({
-        status: ERROR_STATUS.INVALID_STATE,
-        message: `${assetPath} does not exist`,
+      const wsRoot = getDWorkspace().wsRoot;
+
+      if (text.startsWith("asset")) {
+        const vault = PickerUtilsV2.getOrPromptVaultForOpenEditor();
+        assetPath = path.join(vault2Path({ vault, wsRoot }), text);
+      } else {
+        assetPath = resolvePath(text, getExtension().rootWorkspace.uri.fsPath);
+      }
+      if (!fs.existsSync(assetPath)) {
+        const error = DendronError.createFromStatus({
+          status: ERROR_STATUS.INVALID_STATE,
+          message: `no valid path or URL selected`,
+        });
+        this.L.error({ error });
+        return { error };
+      }
+      await open(assetPath).catch((err) => {
+        const error = DendronError.createFromStatus({
+          status: ERROR_STATUS.UNKNOWN,
+          error: err,
+        });
+        this.L.error({ error });
+        return { error };
       });
-      this.L.error({ error });
-      return { error };
     }
-    await open(assetPath).catch((err) => {
-      const error = DendronError.createFromStatus({
-        status: ERROR_STATUS.UNKNOWN,
-        error: err,
-      });
-      this.L.error({ error });
-      return { error };
-    });
-    return { filePath: assetPath };
+    return { filepath: assetPath };
   }
 }
--- packages/plugin-core/src/test/suite-integ/OpenLink.test.ts
+++ packages/plugin-core/src/test/suite-integ/OpenLink.test.ts
@@ -1,26 +1,107 @@
 import { vault2Path } from "@dendronhq/common-server";
 import { ENGINE_HOOKS } from "@dendronhq/engine-test-utils";
+import { NoteTestUtilsV4 } from "@dendronhq/common-test-utils";
 import fs from "fs-extra";
 import path from "path";
 import * as vscode from "vscode";
+import sinon from "sinon";
 import { OpenLinkCommand } from "../../commands/OpenLink";
 import { expect } from "../testUtilsv2";
 import { runLegacyMultiWorkspaceTest, setupBeforeAfter } from "../testUtilsV3";
+import { VSCodeUtils } from "../../utils";
+import { NoteProps } from "@dendronhq/common-all";
 
 suite("OpenLink", function () {
-  let ctx: vscode.ExtensionContext;
-  ctx = setupBeforeAfter(this);
+  const ctx = setupBeforeAfter(this, {});
 
-  test("error: nothing selected", (done) => {
+  test("error: cursor on non-link", (done) => {
     runLegacyMultiWorkspaceTest({
       ctx,
       preSetupHook: async ({ wsRoot, vaults }) => {
         ENGINE_HOOKS.setupBasic({ wsRoot, vaults });
       },
-      onInit: async ({}) => {
+      onInit: async () => {
         const cmd = new OpenLinkCommand();
         const { error } = await cmd.execute();
-        expect(error!.message).toEqual("nothing selected");
+        expect(error!.message).toEqual("no valid path or URL selected");
+        done();
+      },
+    });
+  });
+
+  test("With an invalid character in the selection.", (done) => {
+    let noteWithLink: NoteProps;
+    runLegacyMultiWorkspaceTest({
+      ctx,
+      preSetupHook: async ({ wsRoot, vaults }) => {
+        noteWithLink = await NoteTestUtilsV4.createNote({
+          fname: "OpenLinkTest",
+          vault: vaults[0],
+          wsRoot,
+          body: "Here we have some example text to search for URLs within",
+        });
+      },
+      onInit: async () => {
+        // Open and select some text
+        const editor = await VSCodeUtils.openNote(noteWithLink);
+        editor.selection = new vscode.Selection(7, 1, 7, 10);
+        const cmd = new OpenLinkCommand();
+        const { error } = await cmd.execute();
+        expect(error!.message).toEqual("no valid path or URL selected");
+        done();
+      },
+    });
+  });
+
+  test("grab a URL under the cursor.", (done) => {
+    let noteWithLink: NoteProps;
+    runLegacyMultiWorkspaceTest({
+      ctx,
+      preSetupHook: async ({ wsRoot, vaults }) => {
+        noteWithLink = await NoteTestUtilsV4.createNote({
+          fname: "OpenLinkTest",
+          vault: vaults[0],
+          wsRoot,
+          body:
+            "Here we have some example text to search for URLs within\n" +
+            "https://www.dendron.so/",
+        });
+      },
+      onInit: async () => {
+        // Open and select some text
+        const editor = await VSCodeUtils.openNote(noteWithLink);
+        editor.selection = new vscode.Selection(8, 1, 8, 5);
+        const cmd = new OpenLinkCommand();
+        const avoidPopUp = sinon.stub(vscode.env, "openExternal");
+        const text = await cmd.run();
+        expect(text).toEqual({ filepath: "https://www.dendron.so/" });
+        avoidPopUp.restore();
+        done();
+      },
+    });
+  });
+
+  test("with a partially selected URL", (done) => {
+    let noteWithLink: NoteProps;
+    runLegacyMultiWorkspaceTest({
+      ctx,
+      preSetupHook: async ({ wsRoot, vaults }) => {
+        noteWithLink = await NoteTestUtilsV4.createNote({
+          fname: "OpenLinkTest",
+          vault: vaults[0],
+          wsRoot,
+          body:
+            "Here we have some example text to search for URLs within\n" +
+            "check out [dendron](https://www.dendron.so/)",
+        });
+      },
+      onInit: async () => {
+        // Open and select some text
+        const editor = await VSCodeUtils.openNote(noteWithLink);
+        editor.selection = new vscode.Selection(8, 15, 8, 25);
+        const cmd = new OpenLinkCommand();
+        const { error } = await cmd.execute();
+        expect(error!.message).toEqual("no valid path or URL selected");
         done();
       },
     });

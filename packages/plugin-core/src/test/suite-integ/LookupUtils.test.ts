0b56a9ce0f5f707565dd7e85572af0e85910c9bd
Kevin <kevin@dendron.so>
2021-10-03 14:32:33 -0700

    chore: update tests

--- packages/plugin-core/src/test/suite-integ/LookupUtils.test.ts
+++ packages/plugin-core/src/test/suite-integ/LookupUtils.test.ts
@@ -82,10 +82,10 @@ suite("Lookup Utils Test", function runSuite() {
             vault: vaultCtx,
             fname: "hello",
           });
-          expect(recs!.length === 3);
+          expect(recs.length === 3);
 
-          expect(recs![0].vault.fsPath).toEqual(vaultCtx.fsPath);
-          expect(recs![0].detail).toEqual(CONTEXT_DETAIL);
+          expect(recs[0].vault.fsPath).toEqual(vaultCtx.fsPath);
+          expect(recs[0].detail).toEqual(CONTEXT_DETAIL);
           done();
         },
       });
@@ -104,10 +104,10 @@ suite("Lookup Utils Test", function runSuite() {
             vault: vaultCtx,
             fname: "alpha.one",
           });
-          expect(recs!.length === 1);
+          expect(recs.length === 1);
 
-          expect(recs![0].vault.fsPath).toEqual(vaultCtx.fsPath);
-          expect(recs![0].detail).toEqual(FULL_MATCH_DETAIL);
+          expect(recs[0].vault.fsPath).toEqual(vaultCtx.fsPath);
+          expect(recs[0].detail).toEqual(FULL_MATCH_DETAIL);
           done();
         },
       });
@@ -126,13 +126,13 @@ suite("Lookup Utils Test", function runSuite() {
             vault: vaultCtx,
             fname: "alpha.one",
           });
-          expect(recs!.length === 3);
+          expect(recs.length === 3);
 
-          expect(recs![0].vault.fsPath).toEqual(vaults[0].fsPath);
-          expect(recs![0].detail).toEqual(HIERARCHY_MATCH_DETAIL);
+          expect(recs[0].vault.fsPath).toEqual(vaults[0].fsPath);
+          expect(recs[0].detail).toEqual(HIERARCHY_MATCH_DETAIL);
 
-          expect(recs![1].vault.fsPath).toEqual(vaultCtx.fsPath);
-          expect(recs![1].detail).toEqual(CONTEXT_DETAIL);
+          expect(recs[1].vault.fsPath).toEqual(vaultCtx.fsPath);
+          expect(recs[1].detail).toEqual(CONTEXT_DETAIL);
 
           done();
         },
@@ -192,16 +192,16 @@ suite("Lookup Utils Test", function runSuite() {
             vault: vaultCtx,
             fname: "alpha.one",
           });
-          expect(recs!.length === 3);
+          expect(recs.length === 3);
 
-          expect(recs![0].vault.fsPath).toEqual(vaults[0].fsPath);
-          expect(recs![0].detail).toEqual(HIERARCHY_MATCH_DETAIL);
+          expect(recs[0].vault.fsPath).toEqual(vaults[0].fsPath);
+          expect(recs[0].detail).toEqual(HIERARCHY_MATCH_DETAIL);
 
-          expect(recs![1].vault.fsPath).toEqual(vaults[1].fsPath);
-          expect(recs![1].detail).toEqual(HIERARCHY_MATCH_DETAIL);
+          expect(recs[1].vault.fsPath).toEqual(vaults[1].fsPath);
+          expect(recs[1].detail).toEqual(HIERARCHY_MATCH_DETAIL);
 
-          expect(recs![2].vault.fsPath).toEqual(vaultCtx.fsPath);
-          expect(recs![2].detail).toEqual(CONTEXT_DETAIL);
+          expect(recs[2].vault.fsPath).toEqual(vaultCtx.fsPath);
+          expect(recs[2].detail).toEqual(CONTEXT_DETAIL);
           done();
         },
       });

30025b4e368d8f63e2a97f38be3f560f7a9c6ac0
Mark Choi <hikchoi@gmail.com>
2021-10-05 15:16:22 +0900

    chore: refine intermediate config type

--- packages/common-all/src/types/intermediateConfigs.ts
+++ packages/common-all/src/types/intermediateConfigs.ts
@@ -3,9 +3,13 @@
  * Holds part of both old and new configs
  * During the migration period.
  */
-import { DendronConfig as DendronConfigV1 } from "./workspace";
+import { 
+  DendronConfig as DendronConfigV1,
+  LegacyRandomNoteConfig,
+} from "./workspace";
 import { DendronConfig as DendronConfigV2 } from "./configs/dendronConfig";
 import { genDefaultCommandConfig } from "./configs/commands/commands";
+import { RandomNoteConfig } from "./configs/commands";
 
 export * from "./configs";
 export type IntermediateDendronConfig = IntermediateOldConfig & IntermediateNewConfig;
@@ -18,13 +22,46 @@ export type IntermediateDendronConfig = IntermediateOldConfig & IntermediateNewC
 //   | "insertNoteIndex"
 //   | "randomNote"
 // >;
-type IntermediateOldConfig = DendronConfigV1;
+type IntermediateOldConfig = Partial<DendronConfigV1> 
+  & Required<Pick<DendronConfigV1, 
+  | "version"
+  | "site"
+  | "journal"
+  | "vaults"
+  | "lookup"
+>>;
 
 // 
-type IntermediateNewConfig = Pick<DendronConfigV2,
+type IntermediateNewConfig = Partial<Pick<DendronConfigV2,
   | "commands"
->
+>>
+
+/**
+ * Given an intermediate dendron config, return 
+ * new random note config if command namespace exists
+ * old random note config (or undefined) otherwise
+ * @param config 
+ * @returns 
+ */
+function getRandomNoteConfig(
+  config: IntermediateDendronConfig
+): RandomNoteConfig | LegacyRandomNoteConfig | undefined {
+  const keys = Object.keys(config);
+  // new config exists
+  if (keys.includes("commands")) {
+    return config["commands"]!["randomNote"];
+  }
+
+  // old config exists
+  if (keys.includes("randomNote")) {
+    return config["randomNote"] as LegacyRandomNoteConfig;
+  }
+
+  // doesn't exist.
+  return;
+}
 
 export const IntermediateDendronConfigUtils = {
-  genDefaultCommandConfig
+  genDefaultCommandConfig,
+  getRandomNoteConfig,
 };

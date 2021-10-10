a02fe43d98588ad24002baeb71a2a271a13fce52
Kevin <kevin@dendron.so>
2021-10-10 13:32:58 -0700

    enhance(publish): hide toc on mobile
    Squashed commit of the following:
    
    commit 12ac2cdd2e54b5df559045a5c3717cdb3d801211
    Author: Kevin <kevin@dendron.so>
    Date:   Sun Oct 10 10:07:31 2021 -0700
    
        enhance: capitalize toc
    
    commit f312470a914debb919c3ead8ed4f0aa8bc3d189c
    Author: Kevin <kevin@dendron.so>
    Date:   Sun Oct 10 10:05:24 2021 -0700
    
        enhance: hide toc on mobile
    
        - also merge index page and regular page logic

---
+++ packages/nextjs-template/components/DendronNotePage.tsx
@@ -0,0 +1,120 @@
+import { IntermediateDendronConfig, NoteProps } from "@dendronhq/common-all";
+import {
+  createLogger,
+  DendronNote,
+  LoadingStatus,
+} from "@dendronhq/common-frontend";
+import { Col, Row } from "antd";
+import _ from "lodash";
+import React from "react";
+import { DendronCollectionItem } from "../components/DendronCollection";
+import DendronCustomHead from "../components/DendronCustomHead";
+import DendronSEO from "../components/DendronSEO";
+import DendronSpinner from "../components/DendronSpinner";
+import { DendronTOC } from "../components/DendronTOC";
+import { useCombinedDispatch, useCombinedSelector } from "../features";
+import { browserEngineSlice } from "../features/engine";
+import { DENDRON_STYLE_CONSTANTS } from "../styles/constants";
+import { useDendronRouter, useIsMobile } from "../utils/hooks";
+
+const { HEADER } = DENDRON_STYLE_CONSTANTS;
+
+export type DendronNotePageProps = {
+  // `InferGetStaticPropsType` doesn't get right types for some reason, hence the manual override here
+  customHeadContent: string | null;
+  noteIndex: NoteProps;
+  note: NoteProps;
+  body: string;
+  collectionChildren: NoteProps[] | null;
+  config: IntermediateDendronConfig;
+};
+
+export default function Note({
+  note,
+  body,
+  collectionChildren,
+  noteIndex,
+  customHeadContent,
+  config,
+}: DendronNotePageProps) {
+  const logger = createLogger("Note");
+  const { getActiveNoteId } = useDendronRouter();
+  const [bodyFromState, setBody] =
+    React.useState<string | undefined>(undefined);
+  let id = getActiveNoteId();
+  if (id === "root") {
+    id = noteIndex.id;
+  }
+
+  // --- Hooks
+  const dispatch = useCombinedDispatch();
+  const engine = useCombinedSelector((state) => state.engine);
+  logger.info({ ctx: "enter", id });
+  const { isMobile } = useIsMobile();
+
+  // setup body
+  React.useEffect(() => {
+    logger.info({ ctx: "updateNoteBody:enter", id });
+    if (_.isUndefined(id)) {
+      logger.info({ ctx: "updateNoteBody:exit", id, state: "id undefined" });
+      return;
+    }
+    // loaded page statically
+    if (id === note.id) {
+      dispatch(
+        browserEngineSlice.actions.setLoadingStatus(LoadingStatus.FULFILLED)
+      );
+      logger.info({ ctx: "updateNoteBody:exit", id, state: "id = note.id" });
+      return;
+    }
+    logger.info({ ctx: "updateNoteBody:fetch:pre", id });
+    // otherwise, dynamically fetch page
+    fetch(`/data/notes/${id}.html`).then(async (resp) => {
+      logger.info({ ctx: "updateNoteBody:fetch:post", id });
+      const contents = await resp.text();
+      setBody(contents);
+      dispatch(
+        browserEngineSlice.actions.setLoadingStatus(LoadingStatus.FULFILLED)
+      );
+    });
+  }, [id]);
+
+  const noteBody = id === note.id ? body : bodyFromState;
+
+  if (_.isUndefined(noteBody) || engine.loading === LoadingStatus.PENDING) {
+    return <DendronSpinner />;
+  }
+
+  const maybeCollection =
+    note.custom?.has_collection && !_.isNull(collectionChildren)
+      ? collectionChildren.map((child: NoteProps) =>
+          DendronCollectionItem({ note: child, noteIndex })
+        )
+      : null;
+
+  const tocStyle = isMobile
+    ? { display: "none" }
+    : {
+        position: "absolute" as const,
+        top: HEADER.HEIGHT + 40,
+        right: 20,
+        marginTop: 20,
+        marginRight: 20,
+      };
+
+  return (
+    <>
+      <DendronSEO note={note} config={config} />
+      {customHeadContent && <DendronCustomHead content={customHeadContent} />}
+      <div style={tocStyle}>
+        <DendronTOC note={note} offsetTop={HEADER.HEIGHT} />
+      </div>
+      <Row>
+        <Col span={24}>
+          <DendronNote noteContent={noteBody} />
+          {maybeCollection}
+        </Col>
+      </Row>
+    </>
+  );
+}
--- packages/nextjs-template/components/DendronTOC.tsx
+++ packages/nextjs-template/components/DendronTOC.tsx
@@ -1,36 +1,43 @@
 import { NoteProps } from "@dendronhq/common-all";
 import { Anchor } from "antd";
+import _ from "lodash";
 import { ComponentProps } from "react";
 
 const Link = Anchor.Link;
+
 export const DendronTOC = ({
   note,
   ...rest
 }: {
   note: NoteProps;
-} & ComponentProps<typeof Anchor>) => (
-  <>
-    <Anchor style={{ zIndex: 1 }} {...rest}>
-      {Object.entries(note?.anchors).map(([key, entry]) =>
-        entry?.type === "header" ? (
-          <Link href={`#${key}`} title={entry?.text ?? entry?.value} />
+} & ComponentProps<typeof Anchor>) => {
+  return (
+    <>
+      <Anchor style={{ zIndex: 1 }} {...rest}>
+        {Object.entries(note?.anchors).map(([key, entry]) =>
+          entry?.type === "header" ? (
+            <Link
+              href={`#${key}`}
+              title={_.capitalize(entry?.text ?? entry?.value)}
+            />
+          ) : (
+            <></>
+          )
+        )}
+        {note?.links?.length > 0 &&
+        note?.links.some((link) => link.type === "backlink") ? (
+          <Link href="#backlinks" title="Backlinks" />
         ) : (
           <></>
-        )
-      )}
-      {note?.links?.length > 0 &&
-      note?.links.some((link) => link.type === "backlink") ? (
-        <Link href="#backlinks" title="Backlinks" />
-      ) : (
-        <></>
-      )}
-      {note?.children?.length > 0 ? (
-        <Link href="#children" title="Children" />
-      ) : (
-        <></>
-      )}
-    </Anchor>
-  </>
-);
+        )}
+        {note?.children?.length > 0 ? (
+          <Link href="#children" title="Children" />
+        ) : (
+          <></>
+        )}
+      </Anchor>
+    </>
+  );
+};
 
 export default DendronTOC;

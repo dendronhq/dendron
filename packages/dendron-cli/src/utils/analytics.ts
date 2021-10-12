363e6f5807d05c482a5e55799c145b160cb92231
Mark Choi <hikchoi@gmail.com>
2021-10-12 12:47:26 +0900

    chore: refactor notice display util

--- packages/dendron-cli/src/utils/analytics.ts
+++ packages/dendron-cli/src/utils/analytics.ts
@@ -10,22 +10,22 @@ export class CLIAnalyticsUtils {
     const cliVersion = process.env.npm_package_version!;
     SegmentUtils.identify({ type: "cli", cliVersion });
   }
-}
-
-/**
- * Show notice about telemetry
- */
-export function showTelemetryMessage() {
-  const message = [
-    "Dendron collects limited usage data to help improve the quality of our software.",
-    "",
-    "You can learn everything about our telemetry policies by visiting the following link: ",
-    "https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html",
-    "",
-    "If you would like to opt out, follow the instructions below: ",
-    "https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html#how-to-opt-out-of-data-collection",
-  ].join("\n");
-  const header = `\n===================\nTelemetry notice 🌱\n===================\n`;
-  const container = `${header}${message}`;
-  console.log(container);
+ 
+  /**
+   * Show notice about telemetry
+   */
+  static showTelemetryMessage() {
+    const message = [
+      "Dendron collects limited usage data to help improve the quality of our software.",
+      "",
+      "You can learn everything about our telemetry policies by visiting the following link: ",
+      "https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html",
+      "",
+      "If you would like to opt out, follow the instructions below: ",
+      "https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html#how-to-opt-out-of-data-collection",
+    ].join("\n");
+    const header = `\n===================\nTelemetry notice 🌱\n===================\n`;
+    const container = `${header}${message}`;
+    console.log(container);
+  }
 }

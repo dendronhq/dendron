872bd00a10ff6384b79ffcaa11c532552e2731eb
Mark Choi <hikchoi@gmail.com>
2021-10-12 11:18:22 +0900

    chore: make telemetry notice more visible

--- packages/dendron-cli/src/utils/analytics.ts
+++ packages/dendron-cli/src/utils/analytics.ts
@@ -16,10 +16,16 @@ export class CLIAnalyticsUtils {
  * Show notice about telemetry
  */
 export function showTelemetryMessage() {
-  console.log("Dendron collects limited usage data to help improve the quality of our software");
-  console.log("You can learn everything about our telemetry policies by visiting the following link: ");
-  console.log("https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html");
-  console.log("");
-  console.log("If you would like to opt out, follow the instructions below:");
-  console.log("https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html#how-to-opt-out-of-data-collection");
+  const message = [
+    "Dendron collects limited usage data to help improve the quality of our software.",
+    "",
+    "You can learn everything about our telemetry policies by visiting the following link: ",
+    "https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html",
+    "",
+    "If you would like to opt out, follow the instructions below: ",
+    "https://wiki.dendron.so/notes/84df871b-9442-42fd-b4c3-0024e35b5f3c.html#how-to-opt-out-of-data-collection",
+  ].join("\n");
+  const header = `\n===================\nTelemetry notice 🌱\n===================\n`;
+  const container = `${header}${message}`;
+  console.log(container);
 }

8f390628d49924cbc82c5353f63e6aa638933c91
Kaan Genç <kaan@dendron.so>
2021-10-14 10:28:21 -0400

    chore: skip failing WS init test on Windows (#1517)

--- packages/plugin-core/src/test/suite-integ/WorkspaceInit.test.ts
+++ packages/plugin-core/src/test/suite-integ/WorkspaceInit.test.ts
@@ -1,78 +1,85 @@
 import { WorkspaceType } from "@dendronhq/common-all";
 import { ENGINE_HOOKS } from "@dendronhq/engine-test-utils";
 import { getDWorkspace } from "../../workspace";
-import { describeMultiWS, setupBeforeAfter } from "../testUtilsV3";
+import {
+  describeMultiWS,
+  runSuiteButSkipForWindows,
+  setupBeforeAfter,
+} from "../testUtilsV3";
 import { _activate } from "../../_extension";
 import { expect } from "../testUtilsv2";
 
-suite("GIVEN testing code setupLegacyWorkspaceMulti", function () {
-  const ctx = setupBeforeAfter(this);
+runSuiteButSkipForWindows()(
+  "GIVEN testing code setupLegacyWorkspaceMulti",
+  function () {
+    const ctx = setupBeforeAfter(this);
 
-  describeMultiWS(
-    "WHEN configured for NATIVE workspace",
-    {
-      preSetupHook: ENGINE_HOOKS.setupBasic,
-      ctx,
-      workspaceType: WorkspaceType.NATIVE,
-    },
-    () => {
-      test("THEN initializes correctly", (done) => {
-        const { engine } = getDWorkspace();
-        const testNote = engine.notes["foo"];
-        expect(testNote).toBeTruthy();
-        done();
-      });
+    describeMultiWS(
+      "WHEN configured for NATIVE workspace",
+      {
+        preSetupHook: ENGINE_HOOKS.setupBasic,
+        ctx,
+        workspaceType: WorkspaceType.NATIVE,
+      },
+      () => {
+        test("THEN initializes correctly", (done) => {
+          const { engine } = getDWorkspace();
+          const testNote = engine.notes["foo"];
+          expect(testNote).toBeTruthy();
+          done();
+        });
 
-      test("THEN is of NATIVE type", (done) => {
-        const { type } = getDWorkspace();
-        expect(type).toEqual(WorkspaceType.NATIVE);
-        done();
-      });
-    }
-  );
+        test("THEN is of NATIVE type", (done) => {
+          const { type } = getDWorkspace();
+          expect(type).toEqual(WorkspaceType.NATIVE);
+          done();
+        });
+      }
+    );
 
-  describeMultiWS(
-    "WHEN configured for CODE workspace",
-    {
-      preSetupHook: ENGINE_HOOKS.setupBasic,
-      ctx,
-      workspaceType: WorkspaceType.CODE,
-    },
-    () => {
-      test("THEN initializes correctly", (done) => {
-        const { engine } = getDWorkspace();
-        const testNote = engine.notes["foo"];
-        expect(testNote).toBeTruthy();
-        done();
-      });
+    describeMultiWS(
+      "WHEN configured for CODE workspace",
+      {
+        preSetupHook: ENGINE_HOOKS.setupBasic,
+        ctx,
+        workspaceType: WorkspaceType.CODE,
+      },
+      () => {
+        test("THEN initializes correctly", (done) => {
+          const { engine } = getDWorkspace();
+          const testNote = engine.notes["foo"];
+          expect(testNote).toBeTruthy();
+          done();
+        });
 
-      test("THEN is of CODE type", (done) => {
-        const { type } = getDWorkspace();
-        expect(type).toEqual(WorkspaceType.CODE);
-        done();
-      });
-    }
-  );
+        test("THEN is of CODE type", (done) => {
+          const { type } = getDWorkspace();
+          expect(type).toEqual(WorkspaceType.CODE);
+          done();
+        });
+      }
+    );
 
-  describeMultiWS(
-    "WHEN workspace type is not specified",
-    {
-      preSetupHook: ENGINE_HOOKS.setupBasic,
-      ctx,
-    },
-    () => {
-      test("THEN initializes correctly", (done) => {
-        const { engine } = getDWorkspace();
-        const testNote = engine.notes["foo"];
-        expect(testNote).toBeTruthy();
-        done();
-      });
+    describeMultiWS(
+      "WHEN workspace type is not specified",
+      {
+        preSetupHook: ENGINE_HOOKS.setupBasic,
+        ctx,
+      },
+      () => {
+        test("THEN initializes correctly", (done) => {
+          const { engine } = getDWorkspace();
+          const testNote = engine.notes["foo"];
+          expect(testNote).toBeTruthy();
+          done();
+        });
 
-      test("THEN is of CODE type", (done) => {
-        const { type } = getDWorkspace();
-        expect(type).toEqual(WorkspaceType.CODE);
-        done();
-      });
-    }
-  );
-});
+        test("THEN is of CODE type", (done) => {
+          const { type } = getDWorkspace();
+          expect(type).toEqual(WorkspaceType.CODE);
+          done();
+        });
+      }
+    );
+  }
+);

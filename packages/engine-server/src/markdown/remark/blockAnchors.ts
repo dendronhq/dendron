084ba90904f404ad7f39d508e8479bb5f2bd8dd2
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2021-10-27 15:38:10 +0530

    chore: updated variable

--- packages/engine-server/src/markdown/remark/blockAnchors.ts
+++ packages/engine-server/src/markdown/remark/blockAnchors.ts
@@ -70,7 +70,7 @@ function attachParser(proc: Unified.Processor) {
   inlineMethods.splice(inlineMethods.indexOf("link"), 0, "blockAnchor");
 }
 
-function attachCompiler(proc: Unified.Processor, _opts?: PluginOpts) {
+function attachCompiler(proc: Unified.Processor, opts?: PluginOpts) {
   const Compiler = proc.Compiler;
   const visitors = Compiler.prototype.visitors;
 
@@ -82,7 +82,7 @@ function attachCompiler(proc: Unified.Processor, _opts?: PluginOpts) {
         case DendronASTDest.MD_DENDRON:
           return fullId;
         case DendronASTDest.MD_REGULAR:
-          return _opts?.hideBlockAnchors ? "" : fullId;
+          return opts?.hideBlockAnchors ? "" : fullId;
         case DendronASTDest.MD_ENHANCED_PREVIEW:
           return `<a aria-hidden="true" class="block-anchor anchor-heading" id="${fullId}" href="#${fullId}">^${fullId}</a>`;
         default:

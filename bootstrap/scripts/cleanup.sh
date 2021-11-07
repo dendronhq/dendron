f4c52682142a0723ba10bd2c4bec6200b38713d4
Kevin <kevin@dendron.so>
2021-11-07 10:17:09 -0800

    update cleanup script

--- bootstrap/scripts/cleanup.sh
+++ bootstrap/scripts/cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -rf node_modules
-#rm yarn-error.log  && echo
 find packages -name "node_modules" -type d -prune -exec rm -rf '{}' +
 find packages -type f -name yarn-error.log -exec rm {} +
+npx lerna run clean --parallel --scope "@dendronhq/{dendron-plugin-views,dendron-next-server}"

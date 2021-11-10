97bba98bdbda6ff8a84dfcff0d1520920cb55552
Kevin <kevin@dendron.so>
2021-11-09 17:33:42 -0800

    add tests

--- packages/engine-test-utils/src/__tests__/common-frontend/utils/tree-view.spec.tsx
+++ packages/engine-test-utils/src/__tests__/common-frontend/utils/tree-view.spec.tsx
@@ -3,6 +3,51 @@ import { createEngineFromServer, runEngineTestV5 } from "../../../engine";
 import { NoteTestUtilsV4 } from "@dendronhq/common-test-utils";
 
 describe("GIVEN tree-view", () => {
+  describe("WHEN nav_order is set", () => {
+    test("THEN tree-view respects nav_order", async () => {
+      await runEngineTestV5(
+        async ({ engine }) => {
+          const tree = TreeViewUtils.note2TreeDatanote({
+            noteId: "foo",
+            noteDict: engine.notes,
+            applyNavExclude: false,
+          });
+          expect(tree?.children).toBeDefined();
+          expect(tree!.children!.length).toEqual(3);
+          expect(tree!.children![0].title).toEqual("12");
+          expect(tree!.children![1].title).toEqual("10");
+          expect(tree!.children![2].title).toEqual("11");
+        },
+        {
+          expect,
+          preSetupHook: async ({ wsRoot, vaults }) => {
+            const vault = vaults[0];
+            NoteTestUtilsV4.createNote({ fname: "foo", vault, wsRoot });
+            NoteTestUtilsV4.createNote({
+              fname: "foo.10",
+              vault,
+              wsRoot,
+              props: {
+                custom: {
+                  nav_order: 3,
+                },
+              },
+            });
+            NoteTestUtilsV4.createNote({
+              fname: "foo.12",
+              vault,
+              wsRoot,
+              props: {
+                custom: { nav_order: 1 },
+              },
+            });
+            NoteTestUtilsV4.createNote({ fname: "foo.11", vault, wsRoot });
+          },
+          createEngine: createEngineFromServer,
+        }
+      );
+    });
+  });
   describe("WHEN notes are created out-of-order", () => {
     test("THEN tree-view sorts them based on titles", async () => {
       await runEngineTestV5(

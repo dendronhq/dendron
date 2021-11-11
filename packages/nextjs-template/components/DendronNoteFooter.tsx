55a197873de05799a60906898ffa90f60a0b3c6c
Felipe Janer <yo@felipejaner.com>
2021-11-11 14:25:06 -0500

    fix:(layout): position sidebar, main content and footer

--- packages/nextjs-template/components/DendronNoteFooter.tsx
+++ packages/nextjs-template/components/DendronNoteFooter.tsx
@@ -146,15 +146,6 @@ export function FooterText() {
           )}
         </Col>
       </Row>
-      <Col sm={24} md={12} style={{ textAlign: "right" }}>
-        <Text>
-          {" "}
-          🌱 with 💕 using{" "}
-          <Link href="https://www.dendron.so/" target="_blank">
-            Dendron 🌲
-          </Link>
-        </Text>
-      </Col>
     </Row>
   );
 }

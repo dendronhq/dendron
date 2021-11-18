af958409cd187b26c890e3a9a5b0e14d3d56a67c
Mark Choi <hikchoi@gmail.com>
2021-11-18 16:55:27 +0900

    chore: update ConfigUtils test

--- packages/engine-test-utils/src/__tests__/common-all/config.spec.ts
+++ packages/engine-test-utils/src/__tests__/common-all/config.spec.ts
@@ -1,47 +1,47 @@
-// import {
-//   ConfigUtils,
-//   DVault,
-//   genDefaultJournalConfig,
-//   IntermediateDendronConfig,
-//   StrictConfigV3,
-// } from "@dendronhq/common-all";
-// import _ from "lodash";
+import {
+  ConfigUtils,
+  DVault,
+  genDefaultJournalConfig,
+  IntermediateDendronConfig,
+  StrictConfigV4,
+} from "@dendronhq/common-all";
+import _ from "lodash";
 
-// describe("ConfigUtils", () => {
-//   describe("getProps", () => {
-//     describe("GIVEN v3 config", () => {
-//       let config: Partial<StrictConfigV3>;
-//       beforeEach(() => {
-//         config = ConfigUtils.genDefaultConfig() as Partial<StrictConfigV3>;
-//       });
+describe("ConfigUtils", () => {
+  describe("getProps", () => {
+    describe("GIVEN v4 config", () => {
+      let config: Partial<StrictConfigV4>;
+      beforeEach(() => {
+        config = ConfigUtils.genDefaultConfig() as Partial<StrictConfigV4>;
+      });
 
-//       test("WHEN given a v3 path AND value exists, THEN it returns the correct value", () => {
-//         const expected = [{ fsPath: "foo" }, { fsPath: "bar" }] as DVault[];
-//         ConfigUtils.setVaults(config as IntermediateDendronConfig, expected);
+      test("WHEN given a v4 path AND value exists, THEN it returns the correct value", () => {
+        const expected = [{ fsPath: "foo" }, { fsPath: "bar" }] as DVault[];
+        ConfigUtils.setVaults(config as IntermediateDendronConfig, expected);
 
-//         const vaults = ConfigUtils.getProp(
-//           config as IntermediateDendronConfig,
-//           "workspace"
-//         ).vaults;
+        const vaults = ConfigUtils.getProp(
+          config as IntermediateDendronConfig,
+          "workspace"
+        ).vaults;
 
-//         expect(vaults).toEqual(expected);
-//       });
+        expect(vaults).toEqual(expected);
+      });
 
-//       test("WHEN given v3 path AND value doesn't exists, THEN it returns v3 default", () => {
-//         const expected = genDefaultJournalConfig();
-//         // @ts-ignore
-//         delete config.workspace["journal"];
+      test("WHEN given v4 path AND value doesn't exists, THEN it returns v4 default", () => {
+        const expected = genDefaultJournalConfig();
+        // @ts-ignore
+        delete config.workspace["journal"];
 
-//         // testing for explicitly deleted key.
-//         expect(config.workspace!.journal).toBeUndefined();
+        // testing for explicitly deleted key.
+        expect(config.workspace!.journal).toBeUndefined();
 
-//         const journalConfig = ConfigUtils.getProp(
-//           config as IntermediateDendronConfig,
-//           "workspace"
-//         ).journal;
+        const journalConfig = ConfigUtils.getProp(
+          config as IntermediateDendronConfig,
+          "workspace"
+        ).journal;
 
-//         expect(journalConfig).toEqual(expected);
-//       });
-//     });
-//   });
-// });
+        expect(journalConfig).toEqual(expected);
+      });
+    });
+  });
+});

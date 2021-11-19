95f7ab55283b0a917c9d2e84641c463859142c3a
Mark Choi <hikchoi@gmail.com>
2021-11-19 17:27:50 +0900

    chore: fix config migration version to subsequent general release

--- packages/engine-server/src/migrations/migrations.ts
+++ packages/engine-server/src/migrations/migrations.ts
@@ -18,7 +18,7 @@ import { MigrationUtils, PATH_MAP } from "./utils";
 import { DEPRECATED_PATHS } from ".";
 
 export const CONFIG_MIGRATIONS: Migrations = {
-  version: "0.69.1",
+  version: "0.70.0",
   changes: [
     {
       name: "migrate config",

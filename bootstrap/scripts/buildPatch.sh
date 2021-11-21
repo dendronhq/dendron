0b60078571b3f68c3f07d543e017d68c8784acf1
Kevin <kevin@dendron.so>
2021-11-21 14:01:22 -0800

    restore old buildPatch

--- bootstrap/scripts/buildPatch.sh
+++ bootstrap/scripts/buildPatch.sh
@@ -16,12 +16,14 @@ fi
 
 SCRIPT_BUILD_ENV=${BUILD_ENV:-local}
 echo "building... upgrade: $UPGRADE_TYPE, endpoint: $PUBLISH_ENDPOINT build environment: $SCRIPT_BUILD_ENV"
-if [ $PUBLISH_ENDPOINT = "local" ]; then
-	echo "npm login with local account"
-	yarn setup:npmlogin:local
-elif [ $PUBLISH_ENDPOINT = "remote" ]; then
-	echo "npm login with remote npm registry account"
-	yarn setup:npmlogin:remote
+if [ $SCRIPT_BUILD_ENV = "ci" ]; then
+  if [ $PUBLISH_ENDPOINT = "local" ]; then
+  	echo "npm login with local account"
+	  yarn setup:npmlogin:local
+	elif [ $PUBLISH_ENDPOINT = "remote" ]; then
+  	echo "npm login with remote npm registry account"
+	  yarn setup:npmlogin:remote
+	fi
 fi
 
 DENDRON_CLI=dendron

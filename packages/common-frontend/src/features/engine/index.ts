ffe0fb8893e673502be85571f8bcb20cf42ea9b8
Kevin <kevin@dendron.so>
2021-12-03 20:56:12 -0800

    chore(build): when developing for browser mode, enable dynamic loading of the view
    [Docs](https://github.com/dendronhq/dendron-docs/commit/4f0b82772a493ced4124704a8d85f8b8ad6b228e)

--- packages/common-frontend/src/features/engine/index.ts
+++ packages/common-frontend/src/features/engine/index.ts
@@ -1,4 +1,5 @@
 import * as engineSlice from "./slice";
+
 export { engineSlice };
 export { store as engineStore, AppDispatch as EngineDispatch } from "./store";
 export * as engineHooks from "./hooks";

e9e2ceb762bb78a78f50350f6b8ec5e28aa4c3a5
Kevin <kevin@dendron.so>
2021-12-05 11:18:40 -0800

    update imports

--- packages/common-frontend/src/features/engine/utils.ts
+++ packages/common-frontend/src/features/engine/utils.ts
@@ -1,12 +1,10 @@
 import _ from "lodash";
 import { EngineState } from "./slice";
 
-export class EngineSliceUtils {
-  static hasInitialized(engine: EngineState) {
-    return (
-      engine.loading === "idle" &&
-      !_.isUndefined(engine.notes) &&
-      !_.isUndefined(engine.vaults)
-    );
-  }
-}
+export const hasInitialized = (engine: EngineState) => {
+  return (
+    engine.loading === "idle" &&
+    !_.isUndefined(engine.notes) &&
+    !_.isUndefined(engine.vaults)
+  );
+};

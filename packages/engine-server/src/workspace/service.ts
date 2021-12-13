f33a5c9dffbc7f056aec3931d2624676d9e5ce8a
Jonathan Yeung <jonathanyeung@outlook.com>
2021-12-13 13:46:55 +0800

    fix(workspace): simplify InitializeWorkspace command

--- packages/engine-server/src/workspace/service.ts
+++ packages/engine-server/src/workspace/service.ts
@@ -326,6 +326,16 @@ export class WorkspaceService implements Disposable {
         "# Welcome to Dendron",
         "",
         `This is the root of your dendron vault. If you decide to publish your entire vault, this will be your landing page. You are free to customize any part of this page except the frontmatter on top.`,
+        "",
+        "## Lookup",
+        "",
+        "This section contains useful links to related resources.",
+        "",
+        "- [Getting Started Guide](https://link.dendron.so/6b25)",
+        "- [Discord](https://link.dendron.so/6b23)",
+        "- [Home Page](https://wiki.dendron.so/)",
+        "- [Github](https://link.dendron.so/6b24)",
+        "- [Developer Docs](https://docs.dendron.so/)",
       ].join("\n"),
     });
     const schema = SchemaUtils.createRootModule({ vault });

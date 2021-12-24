c503bb486e6d22ea542e7f755d5ea4c882c6b8af
Mark Choi <hikchoi@gmail.com>
2021-12-25 04:32:41 +0900

    spike: basic sync in lookup view

--- packages/dendron-next-server/pages/vscode/lookup-view.tsx
+++ packages/dendron-next-server/pages/vscode/lookup-view.tsx
@@ -1,9 +1,110 @@
 import { DendronProps } from "../../lib/types";
+import { Form, Select, Switch } from "antd";
+import {
+  DMessageSource,
+  LookupEffectTypeEnum,
+  LookupFilterTypeEnum,
+  LookupNoteTypeEnum,
+  LookupSelectionType,
+  LookupSelectionTypeEnum,
+  LookupSplitTypeEnum,
+  LookupViewMessage,
+  LookupViewMessageEnum,
+} from "@dendronhq/common-all";
+import React from "react";
+import { postVSCodeMessage } from "@dendronhq/common-frontend";
 
-export default function Lookup({ engine }: DendronProps) {
+export default function Lookup({ ide }: DendronProps) {
+  const [form] = Form.useForm();
+  const { Option } = Select;
+  const selectionModifierChoices = Object.keys(LookupSelectionTypeEnum)
+    .filter((key) => key !== "none")
+    .map((key) => {
+      return <Option value={key}>{key}</Option>;
+    });
+  const noteModifierChoices = Object.keys(LookupNoteTypeEnum).map((key) => {
+    return <Option value={key}>{key}</Option>;
+  });
+  const effectModifierChoices = Object.keys(LookupEffectTypeEnum).map((key) => {
+    return <Option value={key}>{key}</Option>;
+  });
+
+  const pressed = ide.lookupModifiers
+    ? ide.lookupModifiers.filter((mod) => {
+        return mod.pressed;
+      })
+    : [];
+
+  const selectionModifierState = pressed.filter((mod) => {
+    if (Object.keys(LookupSelectionTypeEnum).includes(mod.type)) {
+      form.setFieldsValue({ selection: mod.type });
+      return true;
+    }
+    return false;
+  });
+
+  const noteModifierState = pressed.filter((mod) => {
+    if (Object.keys(LookupNoteTypeEnum).includes(mod.type)) {
+      form.setFieldsValue({ note: mod.type });
+      return true;
+    }
+    return false;
+  });
+
+  const effectModifierState = pressed.filter((mod) => {
+    return Object.keys(LookupEffectTypeEnum).includes(mod.type);
+  });
+
+  form.setFieldsValue({ effect: effectModifierState.map((mod) => mod.type) });
+
+  const isHorizontalSplit =
+    pressed.filter((mod) => {
+      return Object.keys(LookupSplitTypeEnum).includes(mod.type);
+    }).length === 1;
+
+  const isDirectChildOnly =
+    pressed.filter((mod) => {
+      return Object.keys(LookupFilterTypeEnum).includes(mod.type);
+    }).length === 1;
+
+  const onValuesChange = (changedValues: any, allValues: any) => {
+    postVSCodeMessage({
+      type: LookupViewMessageEnum.onValuesChange,
+      data: { changedValues, allValues },
+      source: DMessageSource.webClient,
+    } as LookupViewMessage);
+  };
+
+  const selectionTypeOnChange = (opts?: LookupSelectionTypeEnum) => {
+    console.log({ opts });
+  };
+
   return (
     <>
-      <h1>Lookup </h1>
+      <h4>Modifiers</h4>
+      <Form form={form}>
+        <Form.Item name="selection" label="Selection">
+          <Select
+            allowClear
+            onChange={selectionTypeOnChange}
+            placeholder="None selected"
+          >
+            {selectionModifierChoices}
+          </Select>
+        </Form.Item>
+        <Form.Item name="note" label="Note Type">
+          <Select>{noteModifierChoices}</Select>
+        </Form.Item>
+        <Form.Item name="effect" label="Effect Type">
+          <Select mode="multiple">{effectModifierChoices}</Select>
+        </Form.Item>
+        <Form.Item name="horizontalSplit" label="Split Horizontally">
+          <Switch checked={isHorizontalSplit} />
+        </Form.Item>
+        <Form.Item name="directChildOnly" label="Apply Direct Child Filter">
+          <Switch checked={isDirectChildOnly} />
+        </Form.Item>
+      </Form>
     </>
   );
 }
--- packages/plugin-core/src/views/LookupView.ts
+++ packages/plugin-core/src/views/LookupView.ts
@@ -1,13 +1,30 @@
-import { DendronTreeViewKey, DMessage } from "@dendronhq/common-all";
+import {
+  DendronTreeViewKey,
+  DMessage,
+  LookupModifierStatePayload,
+  LookupViewMessage,
+  LookupViewMessageEnum,
+} from "@dendronhq/common-all";
+import { Logger } from "../logger";
 import * as vscode from "vscode";
 import { WebViewUtils } from "./utils";
+import { LookupControllerV3 } from "../components/lookup/LookupControllerV3";
 
 export class LookupView implements vscode.WebviewViewProvider {
   public static readonly viewType = DendronTreeViewKey.LOOKUP_VIEW;
   private _view?: vscode.WebviewView;
+  private _controller?: LookupControllerV3;
 
   public postMessage(msg: DMessage) {
     this._view?.webview.postMessage(msg);
+  }
+
+  public registerController(controller: LookupControllerV3) {
+    this._controller = controller;
+  }
+
+  public deregisterController() {
+    this._controller = undefined;
   }
 
   public async resolveWebviewView(
@@ -23,6 +40,46 @@ export class LookupView implements vscode.WebviewViewProvider {
     webviewView.webview.html = await this._getHtmlForWebview(
       webviewView.webview
     );
+    webviewView.webview.onDidReceiveMessage(
+      this.onDidReceiveMessageHandler,
+      this
+    );
+  }
+
+  async onDidReceiveMessageHandler(msg: LookupViewMessage) {
+    const ctx = "onDidReceiveMessage";
+    Logger.info({ ctx, data: msg });
+    switch (msg.type) {
+      case LookupViewMessageEnum.onValuesChange: {
+        Logger.info({
+          ctx: `${ctx}:onValuesChange`,
+          data: msg.data,
+        });
+
+        const controllerState = this._controller?.state.buttons.map(
+          (button) => {
+            return { type: button.type, pressed: button.pressed };
+          }
+        );
+        console.log({ controllerState });
+
+        console.log({ bond: msg.data.allValues });
+        break;
+      }
+      case LookupViewMessageEnum.onUpdate:
+      default:
+        break;
+    }
+  }
+
+  public refresh(payload: LookupModifierStatePayload) {
+    if (this._view) {
+      this._view.webview.postMessage({
+        type: LookupViewMessageEnum.onUpdate,
+        data: { payload },
+        source: "vscode",
+      });
+    }
   }
 
   private _getHtmlForWebview(_webview: vscode.Webview) {

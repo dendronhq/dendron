fcb9bede7bd3b93c1537355d7c4731fb56330931
Nickolay Kondratyev <nickolay.kondratyev@gmail.com>
2021-12-29 14:10:49 +0700

    enhance: add auto completion to GoDown command and CreateTask command (#1987)

--- packages/plugin-core/src/test/suite-integ/AutoCompleterRegistrar.test.ts
+++ packages/plugin-core/src/test/suite-integ/AutoCompleterRegistrar.test.ts
@@ -1,12 +1,32 @@
 import { expect } from "../testUtilsv2";
 import { AutoCompletableRegistrar } from "../../utils/registers/AutoCompletableRegistrar";
 import { NoteLookupCommand } from "../../commands/NoteLookupCommand";
+import { AutoCompletable } from "../../utils/AutoCompletable";
+import { CodeCommandInstance } from "../../commands/base";
 
 suite("AutoCompleterRegistrar tests", function () {
   test(`WHEN accessing registered command THEN retrieve command`, () => {
     const key = "test-key";
-    AutoCompletableRegistrar.register("test-key", new NoteLookupCommand());
-    expect(AutoCompletableRegistrar.getCmd(key)).toBeTruthy();
+    const noteLookupCmd = new NoteLookupCommand();
+
+    const isACommandAndAutoCompletable = (
+      cmd: any
+    ): cmd is CodeCommandInstance & AutoCompletable => {
+      return (
+        (cmd as CodeCommandInstance & AutoCompletable).run !== undefined &&
+        (cmd as CodeCommandInstance & AutoCompletable).onAutoComplete !==
+          undefined
+      );
+    };
+
+    if (isACommandAndAutoCompletable(noteLookupCmd)) {
+      AutoCompletableRegistrar.register("test-key", noteLookupCmd);
+      expect(AutoCompletableRegistrar.getCmd(key)).toBeTruthy();
+    } else {
+      throw new Error(
+        `Could not cast to CodeCommandInstance & AutoCompletable`
+      );
+    }
   });
 
   test(`WHEN accessing unregistered command THEN throw`, () => {
--- packages/plugin-core/src/utils/registers/AutoCompletableRegistrar.ts
+++ packages/plugin-core/src/utils/registers/AutoCompletableRegistrar.ts
@@ -1,18 +1,41 @@
 import { AutoCompletable } from "../AutoCompletable";
-import { ErrorFactory } from "@dendronhq/common-all";
+import { DendronError, ErrorFactory } from "@dendronhq/common-all";
+import { CodeCommandInstance } from "../../commands/base";
+import { DENDRON_COMMANDS } from "../../constants";
+import { NoteLookupCommand } from "../../commands/NoteLookupCommand";
 
 /**
  * Populated during initialization with commands that implement function: cmd.onAutoComplete
  * */
 export class AutoCompletableRegistrar {
-  static _UI_AUTOCOMPLETE_COMMANDS = new Map<string, AutoCompletable>();
+  static _UI_AUTOCOMPLETE_COMMANDS = new Map<
+    string,
+    CodeCommandInstance & AutoCompletable
+  >();
 
-  static register = (key: string, cmd: AutoCompletable) => {
+  static register = (
+    key: string,
+    cmd: CodeCommandInstance & AutoCompletable
+  ) => {
     this._UI_AUTOCOMPLETE_COMMANDS.set(key, cmd);
   };
 
+  static getNoteLookupCmd(): NoteLookupCommand {
+    const isNoteLookup = (cmd: any): cmd is NoteLookupCommand => {
+      return (cmd as NoteLookupCommand).run !== undefined;
+    };
+
+    const noteLookupCmd = this.getCmd(DENDRON_COMMANDS.LOOKUP_NOTE.key);
+
+    if (isNoteLookup(noteLookupCmd)) {
+      return noteLookupCmd;
+    } else {
+      throw new DendronError({ message: `Could not get note lookup command.` });
+    }
+  }
+
   /** Retrieve command instance that is used by UI. */
-  static getCmd = (key: string): AutoCompletable => {
+  static getCmd = (key: string): CodeCommandInstance & AutoCompletable => {
     const cmd = this._UI_AUTOCOMPLETE_COMMANDS.get(key);
 
     if (cmd === undefined) {

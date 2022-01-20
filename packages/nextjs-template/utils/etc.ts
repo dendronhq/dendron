07cd41e2e91b2182f0b40d05f4afa7d62d7247e4
Kevin <kevin@dendron.so>
2022-01-19 18:06:32 -0800

    fix log level issue

--- packages/nextjs-template/utils/etc.ts
+++ packages/nextjs-template/utils/etc.ts
@@ -7,9 +7,14 @@ export function getNoteRouterQuery(router: NextRouter) {
 }
 
 export function getLogLevel(): LOG_LEVEL {
-  const logLevel = process.env.LOG_LEVEL || LOG_LEVEL.INFO;
-  if (!Object.values(LOG_LEVEL).includes(logLevel as LOG_LEVEL)) {
+  // TODO: issue with `process` being replaced. tracked in [[Process Being Replaced in Nextjs|dendron://private/task.2022.01.19.process-being-replaced-in-nextjs]]
+  try {
+    const logLevel = process.env.LOG_LEVEL || LOG_LEVEL.INFO;
+    if (!Object.values(LOG_LEVEL).includes(logLevel as LOG_LEVEL)) {
+      return LOG_LEVEL.INFO;
+    }
+    return logLevel as LOG_LEVEL;
+  } catch {
     return LOG_LEVEL.INFO;
   }
-  return logLevel as LOG_LEVEL;
 }

3f3fce7705baaa35fe3fb11ae9e9b57601b5768b
Mark Choi <hikchoi@gmail.com>
2022-01-28 19:07:02 +0900

    chore: use note lookup's run opts in journal and scratch note commands

--- packages/plugin-core/src/commands/LookupJournalNoteCommand.ts
+++ packages/plugin-core/src/commands/LookupJournalNoteCommand.ts
@@ -1,11 +1,14 @@
 import { Logger } from "../logger";
 import { DENDRON_COMMANDS } from "../constants";
 import { BasicCommand } from "./base";
-import { CommandOutput as NoteLookupOutput } from "./NoteLookupCommand";
+import {
+  CommandOutput as NoteLookupOutput,
+  CommandRunOpts as NoteLookupRunOpts,
+} from "./NoteLookupCommand";
 import { AutoCompletableRegistrar } from "../utils/registers/AutoCompletableRegistrar";
 import { LookupNoteTypeEnum } from "../components/lookup/types";
 
-type CommandOpts = {};
+type CommandOpts = NoteLookupRunOpts;
 type CommandOutput = NoteLookupOutput | undefined;
 
 export { CommandOpts as LookupJournalNoteOpts };
@@ -19,8 +22,10 @@ export class LookupJournalNoteCommand extends BasicCommand<
   async execute(opts: CommandOpts) {
     const ctx = "LookupJournalNote";
     Logger.info({ ctx, msg: "enter", opts });
-    return AutoCompletableRegistrar.getNoteLookupCmd().run({
+    const noteLookupRunOpts = {
+      ...opts,
       noteType: LookupNoteTypeEnum.journal,
-    });
+    } as NoteLookupRunOpts;
+    return AutoCompletableRegistrar.getNoteLookupCmd().run(noteLookupRunOpts);
   }
 }

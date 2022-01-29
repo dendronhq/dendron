e19c8a119d76cbb11a47e26d060188516d2bbeb6
Kevin Lin <kevinslin8@gmail.com>
2022-01-29 09:39:09 -0800

    chore(build): speed up build process (#2308)
    This change has several build optimizations that dramatically speed up the build process (~ >50%)
    
        instead of running build on bootstrap, introducebuildCI which skips clean and format of all repositories before build (this forces us to do a re-install of all packages despite loading from cache)
            NOTE: we still run a clean in plugin-core and engine-test-utils to make sure we clean up tests that have been deleted
        better caching of node modules
            upgrade setup-node action from v1 to v2
            cache node_modules based on top level yarn.lock
        enable typescript incremental build
        cache typescript compilation between builds using actions/cache@v2
        removal of frontend frameworks that are not being tested (eg. dendron-plugin-views, dendron-next-server)
    
    Some rough benchmarks:
    
        total CI time:
            before: 30min - 50min
            after: 20min
    
        install + build:
    
            before:
                linux: 7min - 8min
                mac: 12min - 15min
                windows: 9min - 12min
    
            after (install):
                linux: 1min 5s - 1min 24s
                mac: 2min 35s - 3min
                windows: 2min 48s - 3min37
    
            after (build):
                linux: 2min 8s - 2min 28s
                mac: 3min 28s - 2min 39s
                windows: 3min 55s - 3min 3s
    
            after (total):
                linux: 3min - 4min
                mac: 6min
                windows: 6min - 7min
    
        more details in [[Lerna Optimizations|dendron://private/task.2022.01.27.lerna-optimizations]]
    
    Future work:
    
        windows continues to be our slowest platform but the remainder of the gains will be made by fixing our tests which today are not deterministic and can hang across various test suites

---
+++ bootstrap/scripts/buildAllForTest.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-console */
+
+/**
+ * Compiles all code for Dendron Plugin
+ */
+
+const execa = require("execa");
+
+const $ = (cmd) => {
+  console.log(`$ ${cmd}`);
+  return execa.commandSync(cmd, { stdout: process.stdout, buffer: false });
+};
+
+const TEST_NEXT_TEMPLATE = process.env.TEST_NEXT_TEMPLATE;
+
+console.log("build all...");
+$(`npx lerna run buildCI --scope @dendronhq/common-all`);
+$(`npx lerna run buildCI --scope @dendronhq/common-server `);
+$(`npx lerna run buildCI --scope @dendronhq/engine-server `);
+$(`npx lerna run buildCI --scope @dendronhq/pods-core `);
+if (TEST_NEXT_TEMPLATE) {
+  $(
+    `npx lerna run buildCI --parallel --scope "@dendronhq/{common-test-utils,api-server,common-assets}"`
+  );
+} else {
+  $(
+    `npx lerna run buildCI --parallel --scope "@dendronhq/{common-test-utils,api-server}"`
+  );
+}
+$(
+  `npx lerna run buildCI --parallel --scope "@dendronhq/{common-frontend,dendron-cli}"`
+);
+if (TEST_NEXT_TEMPLATE) {
+  $(
+    `npx lerna run buildCI --scope "@dendronhq/{engine-test-utils,dendron-next-server}" `
+  );
+} else {
+  $(`npx lerna run buildCI --scope "@dendronhq/engine-test-utils" `);
+}
+
+$(`npx lerna run buildCI --scope "@dendronhq/plugin-core"`);
+
+if (TEST_NEXT_TEMPLATE) {
+  $(`npx lerna run build --scope "@dendronhq/dendron-plugin-views"`);
+  $(`npx yarn dendron dev sync_assets --fast`);
+}
+console.log("done");
--- bootstrap/scripts/cleanup.sh
+++ bootstrap/scripts/cleanup.sh
@@ -2,7 +2,6 @@
 
 # Remove all compiled assets
 
-rm -rf node_modules
-find packages -name "node_modules" -type d -prune -exec rm -rf '{}' +
-find packages -type f -name yarn-error.log -exec rm {} +
 npx lerna run clean --parallel --scope "@dendronhq/{dendron-plugin-views,dendron-next-server,plugin-core,nextjs-template}"
+find packages -name "node_modules" -type d -prune -exec rm -rf '{}' +
+# rm -rf node_modules

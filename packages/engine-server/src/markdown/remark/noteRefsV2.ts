eac3d9351e1ed9e199fdce523ee72c180355d594
Kaan Genc <kaan@dendron.so>
2022-01-31 03:06:49 -0500

    Revert "Revert "fix(views): hover preview containing local images on Windows  (#2047)""
    This reverts commit 7890c0108253eee34cef84af0437a11856da7fc8.

--- packages/engine-server/src/markdown/remark/noteRefsV2.ts
+++ packages/engine-server/src/markdown/remark/noteRefsV2.ts
@@ -36,7 +36,7 @@ import { MDUtilsV4, ParentWithIndex } from "../utils";
 import { MDUtilsV5, ProcMode } from "../utilsv5";
 import { LinkUtils } from "./utils";
 import { WikiLinksOpts } from "./wikiLinks";
-import { MdastUtils } from "..";
+import { MdastUtils, ProcFlavor } from "..";
 
 const LINK_REGEX = /^\!\[\[(.+?)\]\]/;
 
@@ -854,6 +854,7 @@ function convertNoteRefHelperAST(
         end ? end.index + 1 : undefined
       )
     );
+    const procFlavor = MDUtilsV5.getProcOpts(proc).flavor;
     // Copy the current proc to preserve all options
     let tmpProc = MDUtilsV4.procFull(procOpts);
     // but change the fname and vault to the referenced note, since we're inside that note now
@@ -863,7 +864,10 @@ function convertNoteRefHelperAST(
       vault: note.vault,
     });
     if (isV5Active) {
-      if (procOpts.dest === DendronASTDest.HTML) {
+      if (
+        procOpts.dest === DendronASTDest.HTML ||
+        procFlavor === ProcFlavor.HOVER_PREVIEW
+      ) {
         tmpProc = MDUtilsV5.procRemarkFull(
           {
             ...MDUtilsV5.getProcData(proc),
@@ -884,7 +888,7 @@ function convertNoteRefHelperAST(
       const out3 = tmpProc.runSync(out) as Parent;
       return { error: null, data: out3 };
     } else {
-      const out2 = tmpProc.stringify(out);
+      const out2 = tmpProc.stringify(tmpProc.runSync(out));
       out = tmpProc.parse(out2) as Parent;
       return { error: null, data: out };
     }

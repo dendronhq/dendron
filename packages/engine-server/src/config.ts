cf3a9dcd99aa8a5fd35609df6a911e68e6d31b4d
Mark Choi <hikchoi@gmail.com>
2022-02-04 01:29:32 +0900

    chore: revert cleanSiteConfig refactor and split into separate methods

--- packages/engine-server/src/config.ts
+++ packages/engine-server/src/config.ts
@@ -53,7 +53,9 @@ export class DConfig {
     return config;
   }
 
-  static getSiteIndex(sconfig: DendronSiteConfig | DendronPublishingConfig) {
+  static getSiteIndex(
+    sconfig: DendronSiteConfig | DendronPublishingConfig
+  ): string {
     const { siteIndex, siteHierarchies } = sconfig;
     return siteIndex || siteHierarchies[0];
   }
@@ -61,43 +63,71 @@ export class DConfig {
   /**
    * fill in defaults
    */
-  static cleanSiteConfig(
-    config: IntermediateDendronConfig
-  ): CleanDendronSiteConfig | CleanDendronPublishingConfig {
-    let out: DendronSiteConfig | DendronPublishingConfig;
-    if (configIsV4(config)) {
-      const siteConfig = ConfigUtils.getSite(config);
-      out = _.defaults(siteConfig, {
-        copyAssets: true,
-        usePrettyRefs: true,
-        siteNotesDir: "notes",
-        siteFaviconPath: "favicon.ico",
-        gh_edit_link: true,
-        gh_edit_link_text: "Edit this page on GitHub",
-        gh_edit_branch: "main",
-        gh_root: "docs/",
-        gh_edit_view_mode: "edit",
-        writeStubs: true,
-        description: "Personal knowledge space",
-      });
-    } else {
-      const publishingConfig = ConfigUtils.getPublishing(config);
-      out = _.defaultsDeep(publishingConfig, {
-        copyAssets: true,
-        enablePrettyRefs: true,
-        siteFaviconPath: "favicon.ico",
-        github: {
-          enableEditLink: true,
-          editLinkText: "Edit this page on Github",
-          editBranch: "main",
-          editViewMode: GithubEditViewModeEnum.edit,
-        },
-        writeStubs: true,
-        seo: {
-          description: "Personal Knowledge Space",
-        },
+
+  static cleanSiteConfig(config: DendronSiteConfig): CleanDendronSiteConfig {
+    let out: DendronSiteConfig = _.defaults(config, {
+      copyAssets: true,
+      usePrettyRefs: true,
+      siteNotesDir: "notes",
+      siteFaviconPath: "favicon.ico",
+      gh_edit_link: true,
+      gh_edit_link_text: "Edit this page on GitHub",
+      gh_edit_branch: "main",
+      gh_root: "docs/",
+      gh_edit_view_mode: "edit",
+      writeStubs: true,
+      description: "Personal knowledge space",
+    });
+    let { siteRootDir, siteHierarchies, siteIndex, siteUrl } = out;
+    if (process.env["SITE_URL"]) {
+      siteUrl = process.env["SITE_URL"];
+    }
+    if (!siteRootDir) {
+      throw `siteRootDir is undefined`;
+    }
+    if (!siteUrl && getStage() === "dev") {
+      // this gets overridden in dev so doesn't matter
+      siteUrl = "https://foo";
+    }
+    if (!siteUrl) {
+      throw DendronError.createFromStatus({
+        status: ERROR_STATUS.INVALID_CONFIG,
+        message:
+          "siteUrl is undefined. See https://dendron.so/notes/f2ed8639-a604-4a9d-b76c-41e205fb8713.html#siteurl for more details",
       });
     }
+    if (_.size(siteHierarchies) < 1) {
+      throw DendronError.createFromStatus({
+        status: ERROR_STATUS.INVALID_CONFIG,
+        message: `siteHiearchies must have at least one hiearchy`,
+      });
+    }
+    siteIndex = this.getSiteIndex(config);
+    return {
+      ...out,
+      siteIndex,
+      siteUrl,
+    };
+  }
+
+  static cleanPublishingConfig(
+    config: DendronPublishingConfig
+  ): CleanDendronPublishingConfig {
+    const out = _.defaultsDeep(config, {
+      copyAssets: true,
+      enablePrettyRefs: true,
+      siteFaviconPath: "favicon.ico",
+      github: {
+        enableEditLink: true,
+        editLinkText: "Edit this page on Github",
+        editBranch: "main",
+        editViewMode: GithubEditViewModeEnum.edit,
+      },
+      writeStubs: true,
+      seo: {
+        description: "Personal Knowledge Space",
+      },
+    });
     const { siteRootDir, siteHierarchies } = out;
     let { siteIndex, siteUrl } = out;
     if (process.env["SITE_URL"]) {
@@ -123,8 +153,7 @@ export class DConfig {
         message: `siteHiearchies must have at least one hiearchy`,
       });
     }
-    const publishingConfig = ConfigUtils.getPublishingConfig(config);
-    siteIndex = this.getSiteIndex(publishingConfig);
+    siteIndex = this.getSiteIndex(config);
     return {
       ...out,
       siteIndex,

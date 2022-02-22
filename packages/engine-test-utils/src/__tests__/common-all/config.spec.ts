0dfc14e6af04fd6079ecb751b1c32379d85a1be0
Mark Hyunik Choi <hikchoi@gmail.com>
2022-02-22 02:34:44 +0000

    chore: allow config client soft mapping (#2461)
    * chore: allow soft mapping of config client compatibility
    
    * chore: show warning but not exit when mismatch is from soft mapping
    
    * chore: add tests for soft mapping

--- packages/engine-test-utils/src/__tests__/common-all/config.spec.ts
+++ packages/engine-test-utils/src/__tests__/common-all/config.spec.ts
@@ -108,6 +108,46 @@ describe("ConfigUtils", () => {
         done();
       });
     });
+    describe("GIVEN config v4 and client version 0.83", () => {
+      test("THEN config is invalid but allowed because v5 is a soft mapping", (done) => {
+        // this tests for users who decided not to migrate on v83.
+        // because v4 outdated but still compatible, we will still display a notice but not exit the process in cli.
+        const resp = ConfigUtils.configIsValid({
+          clientVersion: "0.83.0",
+          configVersion: 4,
+        });
+        expect(resp.isValid).toBeFalsy();
+        expect(resp.reason).toEqual("config");
+        expect(resp.minCompatClientVersion).toEqual("0.70.0");
+        expect(resp.minCompatConfigVersion).toEqual("5");
+        expect(resp.isSoftMapping).toBeTruthy();
+        done();
+      });
+    });
+    describe("GIVEN config v5 and client version 0.83", () => {
+      test("THEN config is valid", (done) => {
+        const resp = ConfigUtils.configIsValid({
+          clientVersion: "0.83.0",
+          configVersion: 5,
+        });
+        expect(resp.isValid).toBeTruthy();
+        done();
+      });
+    });
+    describe("GIVEN config v5 and client version 0.82", () => {
+      test("THEN config is invalid because client is incompatible", (done) => {
+        // this tests for validation where user updated to v5, but hasn't update dendron-cli
+        const resp = ConfigUtils.configIsValid({
+          clientVersion: "0.82.0",
+          configVersion: 5,
+        });
+        expect(resp.isValid).toBeFalsy();
+        expect(resp.reason).toEqual("client");
+        expect(resp.minCompatClientVersion).toEqual("0.83.0");
+        expect(resp.minCompatConfigVersion).toEqual("4");
+        done();
+      });
+    });
   });
 
   describe("getProps", () => {

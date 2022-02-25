32f5ae61eccd80195eb8e32700c4f48dc516b54b
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2022-02-25 16:21:40 +0530

    fix(pods): md export v2 to acknowledge wikiLinkToURL for links inside noteRefs

--- packages/engine-server/src/markdown/remark/utils.ts
+++ packages/engine-server/src/markdown/remark/utils.ts
@@ -1158,21 +1158,13 @@ export class RemarkUtils {
           DendronASTTypes.WIKI_LINK,
           root
         ) as WikiLinkNoteV4[];
-
-        /** used findLinks to get vault of wikilink */
-        const links = LinkUtils.findLinks({ note, engine }).filter(
-          (linkNode) => linkNode.type === "wiki"
-        );
         let dirty = false;
-
-        links.forEach((linkNode, i) => {
+        wikiLinks.forEach((linkNode) => {
           let vault: DVault | undefined;
-
-          // If the link specifies a vault, we should only look at that vault
-          if (linkNode.to && !_.isUndefined(linkNode.to?.vaultName)) {
+          if (!_.isUndefined(linkNode.data.vaultName)) {
             vault = VaultUtils.getVaultByName({
               vaults: engine.vaults,
-              vname: linkNode.to?.vaultName,
+              vname: linkNode.data.vaultName,
             });
           }
           const existingNote = NoteUtils.getNoteFromMultiVault({
@@ -1187,12 +1179,12 @@ export class RemarkUtils {
               ConfigUtils.getPublishingConfig(dendronConfig);
             const urlRoot = publishingConfig.siteUrl || "";
             const { vault } = existingNote;
-            wikiLinks[i]["value"] = WorkspaceUtils.getNoteUrl({
+            linkNode.value = WorkspaceUtils.getNoteUrl({
               config: dendronConfig,
               note: existingNote,
               vault,
               urlRoot,
-              anchor: linkNode.to?.anchorHeader,
+              anchor: linkNode.data.anchorHeader,
             });
             dirty = true;
           }

30a4386a280e03deb2ec1be4d8fe3306b9399b69
Max Krieger <maxkrieger@users.noreply.github.com>
2022-02-25 22:59:04 -0800

    feat: add initial schema page defn

---
+++ packages/dendron-plugin-views/src/components/DendronSchemaPage.tsx
@@ -0,0 +1,5 @@
+import { DendronProps } from "../types";
+
+export default function DendronSchemaPage({}: DendronProps) {
+  return <div>Schema Page</div>;
+}
---
+++ packages/dendron-plugin-views/src/views/DendronSchemaPageView.tsx
@@ -0,0 +1,6 @@
+import { renderOnDOM } from "../bootstrap";
+import DendronSchemaPage from "../components/DendronSchemaPage";
+
+renderOnDOM(DendronSchemaPage, {});
+
+export default DendronSchemaPage;

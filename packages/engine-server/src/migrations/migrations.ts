5a6f4b110f691b3a159fb0840360cb477e69b48c
Mark Choi <hikchoi@gmail.com>
2022-03-04 10:23:13 +0900

    chore: remove unused export

--- packages/engine-server/src/migrations/migrations.ts
+++ packages/engine-server/src/migrations/migrations.ts
@@ -130,7 +130,7 @@ export const CONFIG_MIGRATIONS: Migrations = {
 /**
  * Migrations are sorted by version numbers, from greatest to least
  */
-export const ALL_MIGRATIONS: Migrations[] = [
+const ALL_MIGRATIONS: Migrations[] = [
   // CONFIG_MIGRATIONS,
   {
     version: "0.55.2",

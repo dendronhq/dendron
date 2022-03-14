000f3f60c90eec6c4796d7a59bdc1e0485d94ab6
Mark Choi <hikchoi@gmail.com>
2022-03-14 17:25:06 +0900

    chore: add better docstrings, allow name override in backup

--- packages/engine-server/src/backup/backupServiceInterface.ts
+++ packages/engine-server/src/backup/backupServiceInterface.ts
@@ -2,20 +2,57 @@ import { RespV3 } from "@dendronhq/common-all";
 import { BackupKeyEnum } from ".";
 
 export interface IBackupService {
-  // root directory
+  /**
+   * getter for backup root path.
+   * returns regardless of the existence of the directory.
+   */
   backupRoot: string;
 
-  // create backup root if it doesn't exist, add to gitignore
+  /**
+   * Makes sure the backup root ({@link backupRoot})
+   * and directory for each defined key ({@link BackupKeyEnum}) exists.
+   * Creates one otherwise.
+   * Adds an entry to gitignore of workspace root if it isn't added already.
+   */
   ensureBackupDir(): Promise<void>;
 
-  // given the options, generate a basename for the backup file
+  /**
+   * Given some options and a file name, generate a new file name to use for the backup.
+   *
+   * The format would be:
+   *
+   * `{file name without extension}.{yyyy.MM.dd.HHmmssS, if enabled}.{infix, if enabled}.{extension}`
+   *
+   * e.g.) Given `dendron.yml`, timestamp, and infix `migrate-config`,
+   * the resulting backup file name is `dendron.2022.03.14.3239848.migrate-config.yml`
+   *
+   * Note that with `timestamp: false` and no infix, this will return the same inputted filename.
+   *
+   * @param opts.fileName file name to generate backup name for. Assumes existence of extension.
+   * @param opts.timestamp flag to enable adding timestamp to name.
+   * @param opts.infix optional custom infix to append right before the extension.
+   * @returns generated backup file name.
+   */
   generateBackupFileName(opts: {
     fileName: string;
     timestamp?: boolean;
     infix?: string;
   }): string;
 
-  // backup given file
+  /**
+   * Back up {@link opts.pathToBackup} to directory `{@link backupRoot}/{@link opts.key}`
+   * with optional {@link opts.timestamp} and {@link opts.infix} in its name.
+   * Or use {@link opts.nameOverride} as the backup name.
+   *
+   * See {@link generateBackupFileName} to see how backup names are generated.
+   *
+   * @param opts.key key to use for backup.
+   * @param opts.pathToBackup path of file to back up.
+   * @param opts.timestamp flag to enable timestamp in backup file name.
+   * @param opts.infix optional custom infix to append right before the extension.
+   * @param opts.nameOverride if given, it will be used instead of calling {@link generateBackupFileName}.
+   * @returns A promise of response containing either the path of the backup or a DendronError
+   */
   backup(opts: {
     key: BackupKeyEnum;
     pathToBackup: string;
@@ -23,10 +60,16 @@ export interface IBackupService {
     infix?: string;
   }): Promise<RespV3<string>>;
 
-  // get a list of backups given the key
+  /**
+   * Given a {@link opts.key}, return all backups created with this key.
+   * @param opts.key backup key to use. One of {@link BackupKeyEnum} as string.
+   * @returns list of backups with given key.
+   */
   getBackupsWithKey(opts: { key: string }): string[];
 
-  // get all backups.
+  /**
+   * @returns all backups grouped by backup key defined by {@link BackupKeyEnum}.
+   */
   getAllBackups(): {
     key: string;
     backups: string[];
--- packages/engine-server/src/backup/service.ts
+++ packages/engine-server/src/backup/service.ts
@@ -41,16 +41,18 @@ export class BackupService implements Disposable, IBackupService {
   }
 
   /**
-   * returns backup root path.
+   * getter for backup root path.
+   * returns regardless of the existence of the directory.
    */
   get backupRoot(): string {
     return path.join(this.wsRoot, BACKUP_DIR_NAME);
   }
 
   /**
-   * Makes sure the backup root and directory for each defined key exists.
+   * Makes sure the backup root ({@link backupRoot})
+   * and directory for each defined key ({@link BackupKeyEnum}) exists.
    * Creates one otherwise.
-   * Add to gitignore if not already.
+   * Adds an entry to gitignore of workspace root if it isn't added already.
    */
   async ensureBackupDir(): Promise<void> {
     this.logger.info({ msg: "ensureBackupDir" });
@@ -66,9 +68,19 @@ export class BackupService implements Disposable, IBackupService {
 
   /**
    * Given some options and a file name, generate a new file name to use for the backup.
-   * @param fileName file name to generate backup name for. Assumes existence of extension.
-   * @param timestamp flag to enable adding timestamp to name.
-   * @param infix optional custom infix to append right before the extension.
+   *
+   * The format would be:
+   *
+   * `{file name without extension}.{yyyy.MM.dd.HHmmssS, if enabled}.{infix, if enabled}.{extension}`
+   *
+   * e.g.) Given `dendron.yml`, timestamp, and infix `migrate-config`,
+   * the resulting backup file name is `dendron.2022.03.14.3239848.migrate-config.yml`
+   *
+   * Note that with `timestamp: false` and no infix, this will return the same inputted filename.
+   *
+   * @param opts.fileName file name to generate backup name for. Assumes existence of extension.
+   * @param opts.timestamp flag to enable adding timestamp to name.
+   * @param opts.infix optional custom infix to append right before the extension.
    * @returns generated backup file name.
    */
   generateBackupFileName(opts: {
@@ -87,26 +99,35 @@ export class BackupService implements Disposable, IBackupService {
   }
 
   /**
+   * Back up {@link opts.pathToBackup} to directory `{@link backupRoot}/{@link opts.key}`
+   * with optional {@link opts.timestamp} and {@link opts.infix} in its name.
+   * Or use {@link opts.nameOverride} as the backup name.
    *
-   * @param key key to use for backup.
-   * @param pathToBackup path of file to back up.
-   * @param timestamp flag to enable timestamp in backup file name.
-   * @param infix optional custom infix to append right before the extension.
-   * @returns
+   * See {@link generateBackupFileName} to see how backup names are generated.
+   *
+   * @param opts.key key to use for backup.
+   * @param opts.pathToBackup path of file to back up.
+   * @param opts.timestamp flag to enable timestamp in backup file name.
+   * @param opts.infix optional custom infix to append right before the extension.
+   * @param opts.nameOverride if given, it will be used instead of calling {@link generateBackupFileName}.
+   * @returns A promise of response containing either the path of the backup or a DendronError
    */
   async backup(opts: {
     key: BackupKeyEnum;
     pathToBackup: string;
     timestamp?: boolean;
     infix?: string;
+    nameOverride?: string;
   }): Promise<RespV3<string>> {
-    const { key, pathToBackup, timestamp, infix } = opts;
+    const { key, pathToBackup, timestamp, infix, nameOverride } = opts;
     const backupDir = path.join(this.backupRoot, key);
     const fileName = path.basename(pathToBackup);
-    const backupPath = path.join(
-      backupDir,
-      this.generateBackupFileName({ fileName, timestamp, infix })
-    );
+    const backupPath =
+      nameOverride ||
+      path.join(
+        backupDir,
+        this.generateBackupFileName({ fileName, timestamp, infix })
+      );
     this.logger.info({ msg: "creating backup", backupPath, pathToBackup });
     await this.ensureBackupDir();
     fs.copyFileSync(pathToBackup, backupPath);
@@ -122,8 +143,8 @@ export class BackupService implements Disposable, IBackupService {
   }
 
   /**
-   *
-   * @param key backup key to use.
+   * Given a {@link opts.key}, return all backups created with this key.
+   * @param opts.key backup key to use. One of {@link BackupKeyEnum} as string.
    * @returns list of backups with given key.
    */
   getBackupsWithKey(opts: { key: string }): string[] {
@@ -141,8 +162,7 @@ export class BackupService implements Disposable, IBackupService {
   }
 
   /**
-   *
-   * @returns all backups grouped by backup key.
+   * @returns all backups grouped by backup key defined by {@link BackupKeyEnum}.
    */
   getAllBackups() {
     return Object.keys(BackupKeyEnum).map((key) => {

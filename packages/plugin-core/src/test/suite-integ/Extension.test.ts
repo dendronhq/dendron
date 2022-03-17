a11f60cfc6f0ce7fb33873337b2bcf1ac0028184
Kaan Genc <kaan@dendron.so>
2022-03-17 03:43:00 -0400

    fix tests failing due to timeout issues

--- packages/plugin-core/src/test/suite-integ/Extension.test.ts
+++ packages/plugin-core/src/test/suite-integ/Extension.test.ts
@@ -168,6 +168,7 @@ suite("GIVEN SetupWorkspace Command", function () {
   let homeDirStub: SinonStub;
   let userConfigDirStub: SinonStub;
   let wsFoldersStub: SinonStub;
+  this.timeout(6 * 1000);
 
   let ctx: ExtensionContext;
   beforeEach(async () => {
@@ -246,7 +247,130 @@ suite("GIVEN SetupWorkspace Command", function () {
           selfContained: false,
         });
         const resp = await readYAMLAsync(path.join(wsRoot, "dendron.yml"));
-        expect(resp).toBeTruthy();
+        expect(resp).toEqual({
+          version: 5,
+          dev: {
+            enablePreviewV2: true,
+          },
+          commands: {
+            lookup: {
+              note: {
+                selectionMode: "extract",
+                confirmVaultOnCreate: true,
+                vaultSelectionModeOnCreate: "smart",
+                leaveTrace: false,
+                bubbleUpCreateNew: true,
+                fuzzThreshold: 0.2,
+              },
+            },
+            randomNote: {},
+            copyNoteLink: {},
+            insertNote: {
+              initialValue: "templates",
+            },
+            insertNoteLink: {
+              aliasMode: "none",
+              enableMultiSelect: false,
+            },
+            insertNoteIndex: {
+              enableMarker: false,
+            },
+          },
+          workspace: {
+            vaults: [
+              {
+                fsPath: "vault",
+              },
+            ],
+            journal: {
+              dailyDomain: "daily",
+              name: "journal",
+              dateFormat: "y.MM.dd",
+              addBehavior: "childOfDomain",
+            },
+            scratch: {
+              name: "scratch",
+              dateFormat: "y.MM.dd.HHmmss",
+              addBehavior: "asOwnDomain",
+            },
+            task: {
+              name: "",
+              dateFormat: "",
+              addBehavior: "childOfCurrent",
+              statusSymbols: {
+                "": " ",
+                wip: "w",
+                done: "x",
+                assigned: "a",
+                moved: "m",
+                blocked: "b",
+                delegated: "l",
+                dropped: "d",
+                pending: "y",
+              },
+              prioritySymbols: {
+                H: "high",
+                M: "medium",
+                L: "low",
+              },
+              todoIntegration: false,
+              createTaskSelectionType: "selection2link",
+            },
+            graph: {
+              zoomSpeed: 1,
+            },
+            enableAutoCreateOnDefinition: false,
+            enableXVaultWikiLink: false,
+            enableRemoteVaultInit: true,
+            enableUserTags: true,
+            enableHashTags: true,
+            workspaceVaultSyncMode: "noCommit",
+            enableAutoFoldFrontmatter: false,
+            enableEditorDecorations: true,
+            maxPreviewsCached: 10,
+            maxNoteLength: 204800,
+          },
+          preview: {
+            enableFMTitle: true,
+            enableNoteTitleForLink: true,
+            enableFrontmatterTags: true,
+            enableHashesForFMTags: false,
+            enableMermaid: true,
+            enablePrettyRefs: true,
+            enableKatex: true,
+            automaticallyShowPreview: false,
+          },
+          publishing: {
+            enableFMTitle: true,
+            enableFrontmatterTags: true,
+            enableHashesForFMTags: false,
+            enableKatex: true,
+            enableMermaid: true,
+            enableNoteTitleForLink: true,
+            copyAssets: true,
+            enablePrettyRefs: true,
+            siteHierarchies: ["root"],
+            writeStubs: false,
+            siteRootDir: "docs",
+            seo: {
+              title: "Dendron",
+              description: "Personal Knowledge Space",
+            },
+            github: {
+              enableEditLink: true,
+              editLinkText: "Edit this page on GitHub",
+              editBranch: "main",
+              editViewMode: "tree",
+            },
+            enableSiteLastModified: true,
+            enableRandomlyColoredTags: true,
+            enablePrettyLinks: true,
+            duplicateNoteBehavior: {
+              action: "useVault",
+              payload: ["vault"],
+            },
+          },
+        });
 
         const dendronState = MetadataService.instance().getMeta();
         expect(isNotUndefined(dendronState.firstInstall)).toBeTruthy();
@@ -615,6 +739,7 @@ suite("GIVEN SetupWorkspace Command", function () {
 
 // These tests run on Windows too actually, but fail on the CI. Skipping for now.
 suite("GIVEN a native workspace", function () {
+  this.timeout(6 * 1000);
   runTestButSkipForWindows()("AND `dendron.yml` is nested in a folder", () => {
     let homeDirStub: SinonStub;
     let userConfigDirStub: SinonStub;

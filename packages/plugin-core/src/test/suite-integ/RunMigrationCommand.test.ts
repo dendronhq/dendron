cf95397bd108a5110fa27226eb038a462cc27059
Mark Hyunik Choi <hikchoi@gmail.com>
2022-03-28 16:23:46 +0000

    chore: fix RunMigrationCommand test (#2614)
    * chore: use correct describe name
    
    * spike: add logging
    
    * spike: catch error during beforeAll
    
    * spike: separate suite for test
    
    * chore: skip failing test for windows only
    
    * chore: cleanup
    
    * chore: remove unused import
    
    * chore: wrap describeMultiWS in runTestButSkipForWindows

--- packages/plugin-core/src/test/suite-integ/RunMigrationCommand.test.ts
+++ packages/plugin-core/src/test/suite-integ/RunMigrationCommand.test.ts
@@ -8,7 +8,7 @@ import sinon from "sinon";
 import { RunMigrationCommand } from "../../commands/RunMigrationCommand";
 import { CONFIG } from "../../constants";
 import { expect } from "../testUtilsv2";
-import { describeMultiWS } from "../testUtilsV3";
+import { describeMultiWS, runTestButSkipForWindows } from "../testUtilsV3";
 import { DConfig } from "@dendronhq/engine-server";
 import { ExtensionProvider } from "../../ExtensionProvider";
 
@@ -49,38 +49,40 @@ suite("RunMigrationCommand", function () {
     }
   );
 
-  describeMultiWS(
-    "GIVEN Code workspace",
-    {
-      modConfigCb: (config) => {
-        _.unset(config.commands, "lookup");
-        return config;
+  runTestButSkipForWindows()("", () => {
+    describeMultiWS(
+      "GIVEN Native workspace",
+      {
+        modConfigCb: (config) => {
+          _.unset(config.commands, "lookup");
+          return config;
+        },
+        workspaceType: WorkspaceType.NATIVE,
       },
-      workspaceType: WorkspaceType.NATIVE,
-    },
-    () => {
-      test("THEN migration runs as expected without looking for workspace config.", async () => {
-        const ext = ExtensionProvider.getExtension();
-        const cmd = new RunMigrationCommand(ext);
+      () => {
+        test("THEN migration runs as expected without looking for workspace config.", async () => {
+          const ext = ExtensionProvider.getExtension();
+          const cmd = new RunMigrationCommand(ext);
+          expect(ext.type).toEqual(WorkspaceType.NATIVE);
+          // testing for explicitly delete key.
+          const { wsRoot } = ext.getDWorkspace();
+          const rawConfig = DConfig.getRaw(wsRoot) as IntermediateDendronConfig;
+          expect(_.isUndefined(rawConfig.commands?.lookup)).toBeTruthy();
 
-        // testing for explicitly delete key.
-        const { wsRoot } = ext.getDWorkspace();
-        const rawConfig = DConfig.getRaw(wsRoot) as IntermediateDendronConfig;
-        expect(_.isUndefined(rawConfig.commands?.lookup)).toBeTruthy();
+          sinon.stub(cmd, "gatherInputs").resolves({ version: "0.83.0" });
+          const out = await cmd.run();
+          expect(out!.length).toEqual(1);
+          expect(out![0].data.version === "0.83.0");
 
-        sinon.stub(cmd, "gatherInputs").resolves({ version: "0.83.0" });
-        const out = await cmd.run();
-        expect(out!.length).toEqual(1);
-        expect(out![0].data.version === "0.83.0");
+          // test if no wsConfig was passed to migration
+          expect(out![0].data.wsConfig).toEqual(undefined);
 
-        // test if no wsConfig was passed to migration
-        expect(out![0].data.wsConfig).toEqual(undefined);
-
-        // test for existence of default key in the place where it was deleted.
-        const config = ext.getDWorkspace().config;
-        const lookupConfig = ConfigUtils.getLookup(config);
-        expect(lookupConfig.note.selectionMode).toEqual("extract");
-      });
-    }
-  );
+          // test for existence of default key in the place where it was deleted.
+          const config = ext.getDWorkspace().config;
+          const lookupConfig = ConfigUtils.getLookup(config);
+          expect(lookupConfig.note.selectionMode).toEqual("extract");
+        });
+      }
+    );
+  });
 });

ed1988abdf616053ed582353739280901bf2accf
Mark Choi <hikchoi@gmail.com>
2022-03-29 00:15:59 +0900

    chore: skip failing test for windows only

--- packages/plugin-core/src/test/suite-integ/RunMigrationCommand.test.ts
+++ packages/plugin-core/src/test/suite-integ/RunMigrationCommand.test.ts
@@ -8,11 +8,11 @@ import sinon from "sinon";
 import { RunMigrationCommand } from "../../commands/RunMigrationCommand";
 import { CONFIG } from "../../constants";
 import { expect } from "../testUtilsv2";
-import { describeMultiWS } from "../testUtilsV3";
+import { describeMultiWS, runTestButSkipForWindows } from "../testUtilsV3";
 import { DConfig } from "@dendronhq/engine-server";
 import { ExtensionProvider } from "../../ExtensionProvider";
 
-suite("RunMigrationCommand: Code", function () {
+suite("RunMigrationCommand", function () {
   describeMultiWS(
     "GIVEN Code workspace",
     {
@@ -48,9 +48,7 @@ suite("RunMigrationCommand: Code", function () {
       });
     }
   );
-});
 
-suite("RunMigrationCommand: Native", function () {
   describeMultiWS(
     "GIVEN Native workspace",
     {
@@ -61,27 +59,29 @@ suite("RunMigrationCommand: Native", function () {
       workspaceType: WorkspaceType.NATIVE,
     },
     () => {
-      test("THEN migration runs as expected without looking for workspace config.", async () => {
-        const ext = ExtensionProvider.getExtension();
-        const cmd = new RunMigrationCommand(ext);
-        expect(ext.type).toEqual(WorkspaceType.NATIVE);
-        // testing for explicitly delete key.
-        const { wsRoot } = ext.getDWorkspace();
-        const rawConfig = DConfig.getRaw(wsRoot) as IntermediateDendronConfig;
-        expect(_.isUndefined(rawConfig.commands?.lookup)).toBeTruthy();
+      runTestButSkipForWindows()("", () => {
+        test("THEN migration runs as expected without looking for workspace config.", async () => {
+          const ext = ExtensionProvider.getExtension();
+          const cmd = new RunMigrationCommand(ext);
+          expect(ext.type).toEqual(WorkspaceType.NATIVE);
+          // testing for explicitly delete key.
+          const { wsRoot } = ext.getDWorkspace();
+          const rawConfig = DConfig.getRaw(wsRoot) as IntermediateDendronConfig;
+          expect(_.isUndefined(rawConfig.commands?.lookup)).toBeTruthy();
 
-        sinon.stub(cmd, "gatherInputs").resolves({ version: "0.83.0" });
-        const out = await cmd.run();
-        expect(out!.length).toEqual(1);
-        expect(out![0].data.version === "0.83.0");
+          sinon.stub(cmd, "gatherInputs").resolves({ version: "0.83.0" });
+          const out = await cmd.run();
+          expect(out!.length).toEqual(1);
+          expect(out![0].data.version === "0.83.0");
 
-        // test if no wsConfig was passed to migration
-        expect(out![0].data.wsConfig).toEqual(undefined);
+          // test if no wsConfig was passed to migration
+          expect(out![0].data.wsConfig).toEqual(undefined);
 
-        // test for existence of default key in the place where it was deleted.
-        const config = ext.getDWorkspace().config;
-        const lookupConfig = ConfigUtils.getLookup(config);
-        expect(lookupConfig.note.selectionMode).toEqual("extract");
+          // test for existence of default key in the place where it was deleted.
+          const config = ext.getDWorkspace().config;
+          const lookupConfig = ConfigUtils.getLookup(config);
+          expect(lookupConfig.note.selectionMode).toEqual("extract");
+        });
       });
     }
   );

e36ffee8cdaf49e0dee7cc959b4e6af6ce4d4330
Mark Choi <hikchoi@gmail.com>
2022-03-30 22:13:32 +0900

    chore: fix test for initial survey prompt logic

--- packages/plugin-core/src/test/suite-integ/Survey.test.ts
+++ packages/plugin-core/src/test/suite-integ/Survey.test.ts
@@ -1,60 +1,183 @@
-import { describe } from "mocha";
-import sinon from "sinon";
+import {
+  InitialSurveyStatusEnum,
+  MetadataService,
+} from "@dendronhq/engine-server";
+import { TestEngineUtils } from "@dendronhq/engine-test-utils";
+import { describe, after, beforeEach, afterEach } from "mocha";
+import sinon, { SinonStub, SinonSpy } from "sinon";
 import * as vscode from "vscode";
+import { GLOBAL_STATE } from "../../constants";
+import { ExtensionProvider } from "../../ExtensionProvider";
 import { StateService } from "../../services/stateService";
 import { SurveyUtils } from "../../survey";
 import { VSCodeUtils } from "../../vsCodeUtils";
-import { getDWorkspace } from "../../workspace";
 import { TutorialInitializer } from "../../workspace/tutorialInitializer";
 import { showLapsedUserMessage } from "../../_extension";
-import { expect } from "../testUtilsv2";
-import { runLegacyMultiWorkspaceTest, setupBeforeAfter } from "../testUtilsV3";
+import { expect, resetCodeWorkspace } from "../testUtilsv2";
+import {
+  describeMultiWS,
+  runLegacyMultiWorkspaceTest,
+  setupBeforeAfter,
+} from "../testUtilsV3";
 
 suite("SurveyUtils", function () {
-  const ctx = setupBeforeAfter(this);
   describe("showInitialSurvey", () => {
-    describe("GIVEN: INITIAL_SURVEY_SUBMITTED is not set", () => {
-      test("THEN: showInitialSurvey is called", (done) => {
-        runLegacyMultiWorkspaceTest({
-          ctx,
-          preSetupHook: async () => {},
-          onInit: async () => {
-            sinon
-              .stub(StateService.instance(), "getGlobalState")
-              .resolves(undefined);
-            const surveySpy = sinon.spy(SurveyUtils, "showInitialSurvey");
+    let homeDirStub: SinonStub;
+    let stateStub: SinonStub;
+    let surveySpy: SinonSpy;
+    describeMultiWS(
+      "GIVEN INITIAL_SURVEY_SUBMITTED is not set",
+      {
+        beforeHook: async () => {
+          await resetCodeWorkspace();
+          homeDirStub = TestEngineUtils.mockHomeDir();
+        },
+      },
+      () => {
+        after(() => {
+          homeDirStub.restore();
+        });
+
+        beforeEach(() => {
+          stateStub = sinon
+            .stub(StateService.instance(), "getGlobalState")
+            .resolves(undefined);
+          surveySpy = sinon.spy(SurveyUtils, "showInitialSurvey");
+        });
+
+        afterEach(() => {
+          stateStub.restore();
+          surveySpy.restore();
+        });
+
+        describe("AND initialSurveyStatus is not set", () => {
+          test("THEN showInitialSurvey is called", async () => {
             const tutorialInitializer = new TutorialInitializer();
-            const ws = getDWorkspace();
+            const ws = ExtensionProvider.getDWorkspace();
+            // MetadataService.instance().setInitialSurveyStatus(InitialSurveyStatusEnum)
+            expect(
+              MetadataService.instance().getMeta().initialSurveyStatus
+            ).toEqual(undefined);
+            expect(
+              await StateService.instance().getGlobalState(
+                GLOBAL_STATE.INITIAL_SURVEY_SUBMITTED
+              )
+            ).toEqual(undefined);
             await tutorialInitializer.onWorkspaceOpen({ ws });
             expect(surveySpy.calledOnce).toBeTruthy();
-            done();
-          },
+          });
         });
-      });
-    });
 
-    describe("GIVEN: INITIAL_SURVEY_SUBMITTED is set", () => {
-      test("THEN: showInitialSurvey is not called", (done) => {
-        runLegacyMultiWorkspaceTest({
-          ctx,
-          preSetupHook: async () => {},
-          onInit: async () => {
-            sinon
-              .stub(StateService.instance(), "getGlobalState")
-              .resolves("submitted");
-            const surveySpy = sinon.spy(SurveyUtils, "showInitialSurvey");
+        describe("AND initialSurveyStatus is set to cancelled", () => {
+          test("THEN showInitialSurvey is called", async () => {
             const tutorialInitializer = new TutorialInitializer();
-            const ws = getDWorkspace();
+            const ws = ExtensionProvider.getDWorkspace();
+            MetadataService.instance().setInitialSurveyStatus(
+              InitialSurveyStatusEnum.cancelled
+            );
+            expect(
+              await StateService.instance().getGlobalState(
+                GLOBAL_STATE.INITIAL_SURVEY_SUBMITTED
+              )
+            ).toEqual(undefined);
+            await tutorialInitializer.onWorkspaceOpen({ ws });
+            expect(surveySpy.calledOnce).toBeTruthy();
+          });
+        });
+
+        describe("AND initialSurveyStatus is set to submitted", () => {
+          test("THEN showInitialSurvey is not called", async () => {
+            const tutorialInitializer = new TutorialInitializer();
+            const ws = ExtensionProvider.getDWorkspace();
+            MetadataService.instance().setInitialSurveyStatus(
+              InitialSurveyStatusEnum.submitted
+            );
+            expect(
+              await StateService.instance().getGlobalState(
+                GLOBAL_STATE.INITIAL_SURVEY_SUBMITTED
+              )
+            ).toEqual(undefined);
             await tutorialInitializer.onWorkspaceOpen({ ws });
             expect(surveySpy.calledOnce).toBeFalsy();
-            done();
-          },
+          });
         });
-      });
-    });
+      }
+    );
+
+    describeMultiWS(
+      "GIVEN INITIAL_SURVEY_SUBMITTED is set",
+      {
+        beforeHook: async () => {
+          await resetCodeWorkspace();
+          homeDirStub = TestEngineUtils.mockHomeDir();
+        },
+      },
+      () => {
+        after(() => {
+          homeDirStub.restore();
+        });
+
+        beforeEach(() => {
+          stateStub = sinon
+            .stub(StateService.instance(), "getGlobalState")
+            .resolves("submitted");
+          surveySpy = sinon.spy(SurveyUtils, "showInitialSurvey");
+        });
+
+        afterEach(() => {
+          stateStub.restore();
+          surveySpy.restore();
+        });
+
+        describe("AND initialSurveyStatus is not set", () => {
+          test("THEN metadata is backfilled AND showInitialSurvey is not called", async () => {
+            const tutorialInitializer = new TutorialInitializer();
+            const ws = ExtensionProvider.getDWorkspace();
+            // metadata is not set yet, we expect this to be backfilled
+            expect(
+              MetadataService.instance().getMeta().initialSurveyStatus
+            ).toEqual(undefined);
+            // global state is already set.
+            expect(
+              await StateService.instance().getGlobalState(
+                GLOBAL_STATE.INITIAL_SURVEY_SUBMITTED
+              )
+            ).toEqual("submitted");
+
+            await tutorialInitializer.onWorkspaceOpen({ ws });
+
+            expect(surveySpy.calledOnce).toBeFalsy();
+
+            // metadata is backfilled.
+            expect(
+              MetadataService.instance().getMeta().initialSurveyStatus
+            ).toEqual(InitialSurveyStatusEnum.submitted);
+          });
+        });
+
+        describe("AND initialSurveyStatus is set to submitted", () => {
+          test("THEN showInitialSurvey is not called", async () => {
+            const tutorialInitializer = new TutorialInitializer();
+            const ws = ExtensionProvider.getDWorkspace();
+
+            MetadataService.instance().setInitialSurveyStatus(
+              InitialSurveyStatusEnum.submitted
+            );
+            expect(
+              await StateService.instance().getGlobalState(
+                GLOBAL_STATE.INITIAL_SURVEY_SUBMITTED
+              )
+            ).toEqual("submitted");
+            await tutorialInitializer.onWorkspaceOpen({ ws });
+            expect(surveySpy.calledOnce).toBeFalsy();
+          });
+        });
+      }
+    );
   });
 
   describe("showLapsedUserSurvey", () => {
+    const ctx = setupBeforeAfter(this);
     describe("GIVEN: user rejects lapsed user message", () => {
       describe("WHEN: Global state for lapsed user survey isn't set", () => {
         test("THEN: showLapsedUserSurvey is called", (done) => {

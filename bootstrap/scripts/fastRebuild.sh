86f7ff9d3a80de14290ddda09cc44c9c0ddb1cd0
Kevin Lin <kevin@dendron.so>
2022-04-03 19:11:49 -0700

    update

--- bootstrap/scripts/fastRebuild.sh
+++ bootstrap/scripts/fastRebuild.sh
@@ -2,6 +2,7 @@
 
 echo "reset repo"
 git checkout package.json
+git checkout packages/plugin-core/package.json
 
 echo "remove compiled assets"
 npx lerna run clean --parallel --scope "@dendronhq/{dendron-plugin-views,dendron-next-server,plugin-core}"
@@ -9,8 +10,7 @@ npx lerna run clean --parallel --scope "@dendronhq/{dendron-plugin-views,dendron
 echo "link packages..."
 lerna bootstrap
 
-echo "re-building packages..."
-# NOTE: order matters, must run serially
+echo "re-building packages..." # NOTE: order matters, must run serially
 npx lerna run build --scope @dendronhq/dendron-next-server
 npx lerna run build --scope @dendronhq/dendron-plugin-views
 npx lerna run build --scope @dendronhq/plugin-core

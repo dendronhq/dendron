3f0ca6f375acbb30955e2187716476831def9c6c
Kevin Lin <kevin@dendron.so>
2022-04-04 10:19:56 -0700

    chore: fast rebuild script when building dendron plugin locally (#2679)
    * add fast rebuild script
    
    * update

---
+++ bootstrap/scripts/fastRebuild.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "reset repo"
+git checkout package.json
+git checkout packages/plugin-core/package.json
+
+echo "remove compiled assets"
+npx lerna run clean --parallel --scope "@dendronhq/{dendron-plugin-views,dendron-next-server,plugin-core}"
+
+echo "link packages..."
+lerna bootstrap
+
+echo "re-building packages..." # NOTE: order matters, must run serially
+npx lerna run build --scope @dendronhq/dendron-next-server
+npx lerna run build --scope @dendronhq/dendron-plugin-views
+npx lerna run build --scope @dendronhq/plugin-core

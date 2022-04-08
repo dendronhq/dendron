fd982036b7e6cfbbbafe4561efcc0a39e9dfa343
Mark Choi <hikchoi@gmail.com>
2022-04-08 23:58:54 +0900

    chore: add test

--- packages/plugin-core/src/test/suite-integ/Extension.test.ts
+++ packages/plugin-core/src/test/suite-integ/Extension.test.ts
@@ -976,6 +976,149 @@ suite("WHEN migrate config", function () {
   );
 });
 
+suite("GIVEN Dendron plugin activation", function () {
+  let setInitialInstallSpy: sinon.SinonSpy;
+  let showTelemetryNoticeSpy: sinon.SinonSpy;
+  const ctx: ExtensionContext = setupBeforeAfter(this);
+  let mockHomeDirStub: sinon.SinonStub;
+
+  function stubDendronWhenNotFirstInstall() {
+    MetadataService.instance().setInitialInstall();
+  }
+
+  function stubDendronGlobalVersionNotSet(ctx: ExtensionContext) {
+    ctx.globalState.update(GLOBAL_STATE.VERSION, undefined);
+  }
+
+  function setupSpies() {
+    setInitialInstallSpy = sinon.spy(
+      MetadataService.instance(),
+      "setInitialInstall"
+    );
+    showTelemetryNoticeSpy = sinon.spy(AnalyticsUtils, "showTelemetryNotice");
+  }
+
+  async function afterHook() {
+    mockHomeDirStub.restore();
+    sinon.restore();
+  }
+
+  describe("AND WHEN not first install", () => {
+    describeMultiWS(
+      "AND WHEN activate",
+      {
+        ctx,
+        preActivateHook: async () => {
+          mockHomeDirStub = TestEngineUtils.mockHomeDir();
+          stubDendronWhenNotFirstInstall();
+          setupSpies();
+        },
+        afterHook,
+        timeout: 1e4,
+      },
+      () => {
+        test("THEN set initial install not called", () => {
+          expect(setInitialInstallSpy.called).toBeFalsy();
+        });
+
+        test("THEN do not show telemetry notice", () => {
+          expect(showTelemetryNoticeSpy.called).toBeFalsy();
+        });
+      }
+    );
+    describeMultiWS(
+      "AND WHEN firstInstall not set for old user",
+      {
+        ctx,
+        preActivateHook: async () => {
+          mockHomeDirStub = TestEngineUtils.mockHomeDir();
+          stubDendronWhenNotFirstInstall();
+          setupSpies();
+          // when check for first install, should be empty
+          MetadataService.instance().deleteMeta("firstInstall");
+        },
+        afterHook,
+        timeout: 1e5,
+      },
+      () => {
+        test("THEN set initial install called", () => {
+          expect(
+            setInitialInstallSpy.calledWith(
+              Time.DateTime.fromISO("2021-06-22").toSeconds()
+            )
+          ).toBeTruthy();
+        });
+
+        test("THEN do not show telemetry notice", () => {
+          expect(showTelemetryNoticeSpy.called).toBeFalsy();
+        });
+      }
+    );
+  });
+
+  describe("AND WHEN first install", () => {
+    describeMultiWS(
+      "AND WHEN activate",
+      {
+        ctx,
+        preActivateHook: async ({ ctx }) => {
+          mockHomeDirStub = TestEngineUtils.mockHomeDir();
+          setupSpies();
+          stubDendronGlobalVersionNotSet(ctx);
+        },
+        afterHook,
+        timeout: 1e4,
+      },
+      () => {
+        test("THEN set initial install called", () => {
+          expect(setInitialInstallSpy.called).toBeTruthy();
+        });
+
+        test("THEN global version set", () => {
+          expect(ctx.globalState.get(GLOBAL_STATE.VERSION)).toNotEqual(
+            undefined
+          );
+        });
+        test("THEN show telemetry notice", () => {
+          expect(showTelemetryNoticeSpy.called).toBeTruthy();
+        });
+      }
+    );
+  });
+
+  describe.only("AND WHEN fresh install on new vscode instance", () => {
+    describeMultiWS(
+      "AND WHEN activate",
+      {
+        ctx,
+        preActivateHook: async ({ ctx }) => {
+          mockHomeDirStub = TestEngineUtils.mockHomeDir();
+          // new instance, so fresh user-data. global storage is clean slate.
+          stubDendronGlobalVersionNotSet(ctx);
+          // but we have first install already recorded in metadata.
+          stubDendronWhenNotFirstInstall();
+          setupSpies();
+        },
+        afterHook,
+        timeout: 1e4,
+      },
+      () => {
+        // we prevent this from happening in new vscode instances.
+        test("THEN set initial install is not called", () => {
+          expect(setInitialInstallSpy.called).toBeFalsy();
+        });
+
+        // but stil want to set this in the fresh globalStorage of the new vscode instance
+        test("THEN global version set", () => {
+          expect(ctx.globalState.get(GLOBAL_STATE.VERSION)).toNotEqual(
+            undefined
+          );
+        });
+      }
+    );
+  });
+});
+
 describe("shouldDisplayInactiveUserSurvey", () => {
   const ONE_WEEK = 604800;
   const NOW = Time.now().toSeconds();

d75507df76912cad753f805082a5086f1b11ba2d
Max Krieger <maxkrieger@users.noreply.github.com>
2022-04-08 20:02:09 -0700

    feat: Autocomplete schema view

--- packages/dendron-plugin-views/src/components/DendronSchemaPage.tsx
+++ packages/dendron-plugin-views/src/components/DendronSchemaPage.tsx
@@ -1,5 +1,5 @@
 import { DendronProps } from "../types";
-import { Select, Input, Card, Badge, Tag } from "antd";
+import { Select, Input, Card, Badge, Tag, AutoComplete, Tree } from "antd";
 import { useCallback, useEffect, useReducer, useState } from "react";
 import {
   SchemaModuleDict,
@@ -9,6 +9,7 @@ import {
   SchemaUtils,
 } from "@dendronhq/common-all";
 import Ribbon from "antd/lib/badge/Ribbon";
+import { DataNode } from "antd/lib/tree";
 
 function generatePath(module: SchemaModuleProps, schema: SchemaProps): string {
   const partString = schema.data.pattern
@@ -24,69 +25,53 @@ function generatePath(module: SchemaModuleProps, schema: SchemaProps): string {
   return partString;
 }
 
-const SchemaBox = ({
+function getCompletions(
+  schemaModule: SchemaModuleProps,
+  schema: SchemaProps
+): { label: string; value: string }[] {
+  const path = generatePath(schemaModule, schema);
+  return [
+    { label: path, value: path },
+    ...schema.children
+      .map((child) => getCompletions(schemaModule, schemaModule.schemas[child]))
+      .flat(),
+  ];
+}
+
+function SchemaBox({
   schema,
   schemaModule,
-  partial,
-  inner,
+  currentMatch,
 }: {
   schema: SchemaProps;
   schemaModule: SchemaModuleProps;
-  partial: boolean;
-  inner: boolean;
-}) => {
-  const { namespace } = schema.data;
-  const card = (
-    <Card
-      title={
-        <span>
-          {schema.title}
-          <span style={{ color: "#a9a9a9" }}>
-            {" "}
-            ({generatePath(schemaModule, schema)})
-          </span>
-        </span>
-      }
-      color={schema.color}
-      type={inner ? "inner" : undefined}
-    >
-      {schema.data.template && <div>template: {schema.data.template.id}</div>}
-      {schema.data.pattern && <div>pattern: {schema.data.pattern}</div>}
-      {schema.desc !== "" && <div>description: {schema.desc}</div>}
-      {!inner && schema.children.length > 0 && (
-        <div>
-          <h3>Children</h3>
-          {schema.children.map((child) => (
-            <SchemaBox
-              key={child}
-              schema={schemaModule.schemas[child]}
-              schemaModule={schemaModule}
-              inner={true}
-              partial={false}
-            />
-          ))}
-        </div>
-      )}
-    </Card>
-  );
-  if (inner) {
-    return card;
-  }
+  currentMatch: string | null;
+}) {
   return (
-    <Ribbon
-      color={partial ? "gold" : namespace ? "gray" : "green"}
-      text={
-        partial
-          ? "partial match"
-          : namespace
-          ? "namespace match"
-          : "exact match"
-      }
+    <div
+      style={{
+        backgroundColor: currentMatch !== schema.id ? "#FFFFFF" : "#b1ffb6",
+        borderRadius: "15px",
+        border: "1px solid #a9a9a9",
+        padding: "1em",
+        margin: "1em",
+      }}
     >
-      {card}
-    </Ribbon>
+      <h2>{schema.title}</h2>
+      <h3>({generatePath(schemaModule, schema)})</h3>
+      <div style={{ paddingLeft: "1em" }}>
+        {schema.children.map((childID) => (
+          <SchemaBox
+            key={childID}
+            schema={schemaModule.schemas[childID]}
+            schemaModule={schemaModule}
+            currentMatch={currentMatch}
+          />
+        ))}
+      </div>
+    </div>
   );
-};
+}
 
 export default function DendronSchemaPage({ engine }: DendronProps) {
   const [selectedDomain, setSelectedDomain] = useState<string | null>(null);
@@ -99,21 +84,23 @@ export default function DendronSchemaPage({ engine }: DendronProps) {
     });
   }, []);
   useEffect(() => {
+    if (selectedDomain !== null) {
+      setInputtedHierarchy(selectedDomain);
+    }
+  }, [selectedDomain]);
+  useEffect(() => {
     const schemas = Object.keys(engine.schemas);
     if (selectedDomain === null && schemas.length > 0) {
       setSelectedDomain(schemas[0]);
+      setInputtedHierarchy(schemas[0]);
     }
   }, [engine.schemas, selectedDomain]);
-  // next schemas at the level - show possibilities
-  // show the pattern field or id - literal pattern - namespace = * for child
-  // custom title; id; pattern
-  // autocomplete
   const schemaModule =
     selectedDomain !== null ? engine.schemas[selectedDomain] : null;
   const match =
     schemaModule !== null
       ? SchemaUtils.matchPathWithSchema({
-          notePath: `${selectedDomain}.${inputtedHierarchy}`,
+          notePath: inputtedHierarchy,
           matched: "",
           schemaCandidates: [schemaModule.schemas[schemaModule.root.id]],
           schemaModule,
@@ -138,27 +125,27 @@ export default function DendronSchemaPage({ engine }: DendronProps) {
       </div>
       {schemaModule !== null && (
         <div style={{ padding: "1em" }}>
-          <div style={{ margin: "1em" }}>
-            <Input
-              placeholder={`type a schema hierarchy under ${selectedDomain}`}
+          <div>
+            <AutoComplete
+              placeholder={`type a schema hierarchy starting with ${selectedDomain}`}
               size="large"
+              style={{ width: "100%" }}
               value={inputtedHierarchy}
-              onChange={(e) => setInputtedHierarchy(e.target.value)}
-              addonBefore={`${selectedDomain}.`}
+              onChange={setInputtedHierarchy}
+              options={
+                match !== undefined
+                  ? getCompletions(schemaModule, match.schema)
+                  : []
+              }
             />
           </div>
-          {match !== undefined ? (
+          <div style={{ padding: "1em" }}>
             <SchemaBox
-              schema={match.schema}
+              schema={schemaModule.schemas[schemaModule.root.id]}
               schemaModule={schemaModule}
-              partial={match.partial}
-              inner={false}
+              currentMatch={match !== undefined ? match.schema.id : null}
             />
-          ) : (
-            <div>
-              no matches found for {selectedDomain}.{inputtedHierarchy}
-            </div>
-          )}
+          </div>
         </div>
       )}
     </div>

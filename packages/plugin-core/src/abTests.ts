e46de41ffaa534463efabb2758caeb3d1ed6a704
Kaan Barmore-Genç <kaan@dendron.so>
2022-04-11 18:19:45 -0400

    Remove A/B test for upgrade view, add A/B test for upgrade toat button wording (#2730)

--- packages/plugin-core/src/abTests.ts
+++ packages/plugin-core/src/abTests.ts
@@ -1,27 +1,36 @@
 // ^xi5t1r2j51ot
 import { ABTest } from "@dendronhq/common-server";
 
-export enum UpgradeToastOrViewTestGroups {
-  /** Users who get a toast notification in the corner of VSCode. */
-  upgradeToast = "upgradeToast",
-  /** Users who get a upgrade webview pop up.  */
-  upgradeView = "upgradeView",
+export enum UpgradeToastWordingTestGroups {
+  /** The button on the upgrade toast will say "see what changed" */
+  seeWhatChanged = "seeWhatChanged",
+  /** The button on the upgrade toast will say "see what's new" */
+  seeWhatsNew = "seeWhatsNew",
+  /** The button on the upgrade toast will say "open the changelog" */
+  openChangelog = "openChangelog",
 }
 
 /** Test if showing a web view on an upgrade is more successful than showing a toast notification. */
-export const UPGRADE_TOAST_OR_VIEW_TEST = new ABTest("UpgradeToastOrViewTest", [
-  {
-    name: UpgradeToastOrViewTestGroups.upgradeToast,
-    weight: 1,
-  },
-  {
-    name: UpgradeToastOrViewTestGroups.upgradeView,
-    weight: 1,
-  },
-]);
+export const UPGRADE_TOAST_WORDING_TEST = new ABTest(
+  "UpgradeToastWordingTest",
+  [
+    {
+      name: UpgradeToastWordingTestGroups.seeWhatChanged,
+      weight: 1,
+    },
+    {
+      name: UpgradeToastWordingTestGroups.seeWhatsNew,
+      weight: 1,
+    },
+    {
+      name: UpgradeToastWordingTestGroups.openChangelog,
+      weight: 1,
+    },
+  ]
+);
 
 /** All A/B tests that are currently running.
  *
  * ^tkqhy45hflfd
  */
-export const CURRENT_AB_TESTS = [UPGRADE_TOAST_OR_VIEW_TEST];
+export const CURRENT_AB_TESTS = [UPGRADE_TOAST_WORDING_TEST];

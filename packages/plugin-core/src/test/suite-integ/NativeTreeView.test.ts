06d5ca4d0dea6e561977447e152c9cf0cc13b0ab
Mark Choi <hikchoi@gmail.com>
2022-04-20 12:24:16 +0900

    chore: add more tree view tests

--- packages/plugin-core/src/test/suite-integ/NativeTreeView.test.ts
+++ packages/plugin-core/src/test/suite-integ/NativeTreeView.test.ts
@@ -49,11 +49,25 @@ async function runRenameNote(opts: { noteId: string; newName: string }) {
   await renameCmd.execute(renameOpts);
 }
 
+async function getFullTree(opts: {
+  root: NoteProps;
+  provider: EngineNoteProvider;
+}): Promise<{ fname: string; childNodes: any }> {
+  const { root, provider } = opts;
+  const children = await (provider.getChildren(root) as Promise<NoteProps[]>);
+  const childNodes = await Promise.all(
+    children.map(async (child) => {
+      const tree = await getFullTree({ root: child, provider });
+      return tree;
+    })
+  );
+  return { fname: root.fname, childNodes };
+}
+
 suite("NativeTreeView tests", function () {
   this.timeout(2000);
 
   describe("Rename Note Command interactions", function () {
-    // when renaming note with top level hierarchy
     describeMultiWS(
       "WHEN renaming note with top level hierarchy",
       {
@@ -95,7 +109,7 @@ suite("NativeTreeView tests", function () {
         });
       }
     );
-    // when renaming note with stub parent
+
     describeMultiWS(
       "WHEN renaming note with stub parent",
       {
@@ -154,7 +168,7 @@ suite("NativeTreeView tests", function () {
         });
       }
     );
-    // when renaming note with non-stub parent
+
     describeMultiWS(
       "WHEN renaming note with non-stub parent",
       {
@@ -219,7 +233,7 @@ suite("NativeTreeView tests", function () {
         });
       }
     );
-    // when renaming note stub children
+
     describeMultiWS(
       "WHEN renaming note with stub children",
       {
@@ -305,9 +319,333 @@ suite("NativeTreeView tests", function () {
         });
       }
     );
-    // when renaming note with non-stub children
-    // when renaming note with a chain of ancestors that are only stubs
-    // when renaming note to a file name of an existing stub note
+
+    describeMultiWS(
+      "WHEN renaming note with non-stub children",
+      {
+        preSetupHook: async (opts) => {
+          const { wsRoot, vaults } = opts;
+          await NoteTestUtilsV4.createNote({
+            wsRoot,
+            vault: vaults[0],
+            fname: "foo",
+            genRandomId: true,
+          });
+          await NoteTestUtilsV4.createNote({
+            wsRoot,
+            vault: vaults[0],
+            fname: "foo.bar",
+            genRandomId: true,
+          });
+          await NoteTestUtilsV4.createNote({
+            wsRoot,
+            vault: vaults[0],
+            fname: "foo.baz",
+            genRandomId: true,
+          });
+        },
+      },
+      () => {
+        test("THEN tree view correctly displays renamed note", async () => {
+          const mockEvents = new MockEngineEvents();
+          const provider = new EngineNoteProvider(mockEvents);
+
+          const propsBefore = await (provider.getChildren() as Promise<
+            NoteProps[]
+          >);
+
+          const vaultOneRootPropsBefore = propsBefore[0];
+          const vaultOneRootId = propsBefore[0].id;
+
+          const childrenBefore = await (provider.getChildren(
+            vaultOneRootPropsBefore
+          ) as Promise<NoteProps[]>);
+          expect(childrenBefore.map((child) => child.fname)).toEqual(["foo"]);
+
+          const grandChildrenBefore = await (provider.getChildren(
+            childrenBefore[0]
+          ) as Promise<NoteProps[]>);
+          expect(grandChildrenBefore.map((gchild) => gchild.fname)).toEqual([
+            "foo.bar",
+            "foo.baz",
+          ]);
+
+          const engine = ExtensionProvider.getEngine();
+          await runRenameNote({
+            noteId: childrenBefore[0].id,
+            newName: "fooz",
+          });
+
+          const vault1RootPropsAfter = engine.notes[vaultOneRootId];
+          const childrenAfter = await (provider.getChildren(
+            vault1RootPropsAfter
+          ) as Promise<NoteProps[]>);
+          expect(
+            childrenAfter.map((child) => {
+              return { fname: child.fname, stub: child.stub };
+            })
+          ).toEqual([
+            { fname: "foo", stub: true },
+            { fname: "fooz", stub: undefined },
+          ]);
+
+          const grandChildrenAfter = await (provider.getChildren(
+            childrenAfter[0]
+          ) as Promise<NoteProps[]>);
+          expect(grandChildrenAfter.map((gchild) => gchild.fname)).toEqual([
+            "foo.bar",
+            "foo.baz",
+          ]);
+        });
+      }
+    );
+
+    describeMultiWS(
+      "WHEN renaming note with a chain of ancestors that are only stubs",
+      {
+        preSetupHook: async (opts) => {
+          const { wsRoot, vaults } = opts;
+          await NoteTestUtilsV4.createNote({
+            wsRoot,
+            vault: vaults[0],
+            fname: "one.two.three.foo",
+          });
+        },
+      },
+      () => {
+        test("THEN tree view correctly displays renamed note", async () => {
+          const mockEvents = new MockEngineEvents();
+          const provider = new EngineNoteProvider(mockEvents);
+
+          const engine = ExtensionProvider.getEngine();
+
+          const propsBefore = await (provider.getChildren() as Promise<
+            NoteProps[]
+          >);
+
+          const vaultOneRootPropsBefore = propsBefore[0];
+          const vaultOneRootId = propsBefore[0].id;
+          const fullTreeBefore = await getFullTree({
+            root: vaultOneRootPropsBefore,
+            provider,
+          });
+          expect(fullTreeBefore).toEqual({
+            fname: "root",
+            childNodes: [
+              {
+                fname: "one",
+                childNodes: [
+                  {
+                    fname: "one.two",
+                    childNodes: [
+                      {
+                        fname: "one.two.three",
+                        childNodes: [
+                          {
+                            fname: "one.two.three.foo",
+                            childNodes: [],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          });
+
+          await runRenameNote({
+            noteId: "one.two.three.foo",
+            newName: "zero",
+          });
+
+          const vaultOneRootPropsAfter = engine.notes[vaultOneRootId];
+          const fullTreeAfter = await getFullTree({
+            root: vaultOneRootPropsAfter,
+            provider,
+          });
+          expect(fullTreeAfter).toEqual({
+            fname: "root",
+            childNodes: [
+              {
+                fname: "zero",
+                childNodes: [],
+              },
+            ],
+          });
+        });
+      }
+    );
+
+    describeMultiWS(
+      "WHEN renaming note results in a chain of stub ancestors",
+      {
+        preSetupHook: async (opts) => {
+          const { wsRoot, vaults } = opts;
+          await NoteTestUtilsV4.createNote({
+            wsRoot,
+            vault: vaults[0],
+            fname: "foo",
+          });
+        },
+      },
+      () => {
+        test("THEN tree view correctly displays renamed note", async () => {
+          const mockEvents = new MockEngineEvents();
+          const provider = new EngineNoteProvider(mockEvents);
+
+          const engine = ExtensionProvider.getEngine();
+
+          const propsBefore = await (provider.getChildren() as Promise<
+            NoteProps[]
+          >);
+
+          const vaultOneRootPropsBefore = propsBefore[0];
+          const vaultOneRootId = propsBefore[0].id;
+          const fullTreeBefore = await getFullTree({
+            root: vaultOneRootPropsBefore,
+            provider,
+          });
+          expect(fullTreeBefore).toEqual({
+            fname: "root",
+            childNodes: [
+              {
+                fname: "foo",
+                childNodes: [],
+              },
+            ],
+          });
+
+          await runRenameNote({
+            noteId: "foo",
+            newName: "one.two.three.foo",
+          });
+
+          const vaultOneRootPropsAfter = engine.notes[vaultOneRootId];
+          const fullTreeAfter = await getFullTree({
+            root: vaultOneRootPropsAfter,
+            provider,
+          });
+          expect(fullTreeAfter).toEqual({
+            fname: "root",
+            childNodes: [
+              {
+                fname: "one",
+                childNodes: [
+                  {
+                    fname: "one.two",
+                    childNodes: [
+                      {
+                        fname: "one.two.three",
+                        childNodes: [
+                          {
+                            fname: "one.two.three.foo",
+                            childNodes: [],
+                          },
+                        ],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          });
+        });
+      }
+    );
+
+    describeMultiWS(
+      "WHEN renaming note to replace an existing stub note",
+      {
+        preSetupHook: async (opts) => {
+          const { wsRoot, vaults } = opts;
+          await NoteTestUtilsV4.createNote({
+            wsRoot,
+            vault: vaults[0],
+            fname: "foo",
+          });
+          await NoteTestUtilsV4.createNote({
+            wsRoot,
+            vault: vaults[0],
+            fname: "one.two.three",
+          });
+        },
+      },
+      () => {
+        test("THEN tree view correctly displays renamed note", async () => {
+          const mockEvents = new MockEngineEvents();
+          const provider = new EngineNoteProvider(mockEvents);
+
+          const engine = ExtensionProvider.getEngine();
+
+          const propsBefore = await (provider.getChildren() as Promise<
+            NoteProps[]
+          >);
+
+          const vaultOneRootPropsBefore = propsBefore[0];
+          const vaultOneRootId = propsBefore[0].id;
+          const fullTreeBefore = await getFullTree({
+            root: vaultOneRootPropsBefore,
+            provider,
+          });
+          expect(fullTreeBefore).toEqual({
+            fname: "root",
+            childNodes: [
+              {
+                fname: "foo",
+                childNodes: [],
+              },
+              {
+                fname: "one",
+                childNodes: [
+                  {
+                    fname: "one.two",
+                    childNodes: [
+                      {
+                        fname: "one.two.three",
+                        childNodes: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          });
+
+          await runRenameNote({
+            noteId: "foo",
+            newName: "one",
+          });
+
+          const vaultOneRootPropsAfter = engine.notes[vaultOneRootId];
+          const fullTreeAfter = await getFullTree({
+            root: vaultOneRootPropsAfter,
+            provider,
+          });
+          expect(fullTreeAfter).toEqual({
+            fname: "root",
+            childNodes: [
+              {
+                fname: "one",
+                childNodes: [
+                  {
+                    fname: "one.two",
+                    childNodes: [
+                      {
+                        fname: "one.two.three",
+                        childNodes: [],
+                      },
+                    ],
+                  },
+                ],
+              },
+            ],
+          });
+
+          expect(engine.notes["foo"].stub).toBeFalsy();
+        });
+      }
+    );
   });
 
   describe("filesystem change interactions", function () {});

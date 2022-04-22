8b043e7f09a18227534cc5ce07fc269232bc2cc6
Kevin <kevin@dendron.so>
2022-04-22 09:16:04 -0700

    update local sleep time

--- packages/dendron-cli/src/commands/devCLICommand.ts
+++ packages/dendron-cli/src/commands/devCLICommand.ts
@@ -363,8 +363,8 @@ export class DevCLICommand extends CLICommand<CommandOpts, CommandOutput> {
       );
       await new Promise((r) => setTimeout(r, 120000));
     } else {
-      this.print("sleeping 3s for local npm registry to have packages ready");
-      await new Promise((r) => setTimeout(r, 30000));
+      this.print("sleeping 6s for local npm registry to have packages ready");
+      await new Promise((r) => setTimeout(r, 60000));
     }
 
     this.print("install deps...");

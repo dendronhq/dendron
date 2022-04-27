c47db2f7bb60004e95e43c895b2f1ab11582f8a6
Mark Choi <hikchoi@gmail.com>
2022-04-28 02:26:54 +0900

    chore: prompt for removing deprecated config during startup

--- packages/common-all/src/analytics.ts
+++ packages/common-all/src/analytics.ts
@@ -111,6 +111,8 @@ export enum ConfigEvents {
   ConfigNotMigrated = "Config_Not_Migrated",
   EnabledExportPodV2 = "Enabled_Export_Pod_V2",
   ShowMissingDefaultConfigMessage = "Show_Missing_Default_Config_Message",
+  DeprecatedConfigMessageConfirm = "DeprecatedConfigMessageConfirm",
+  ShowDeprecatedConfigMessage = "ShowDeprecatedConfigMessage",
   MissingDefaultConfigMessageConfirm = "MissingDefaultConfigMessageConfirm",
   MissingSelfContainedVaultsMessageShow = "MissingSelfContainedVaultsMessageShow",
   MissingSelfContainedVaultsMessageAccept = "MissingSelfContainedVaultsMessageAccept",

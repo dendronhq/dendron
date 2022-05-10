4a2336508664486211d1cffece6b9c39be54eba7
Kaan Barmore-Genc <kaan@dendron.so>
2022-05-10 02:02:27 -0400

    fix published images missing leading forward slashes

--- packages/engine-test-utils/src/__tests__/engine-server/markdown/v5/image.spec.ts
+++ packages/engine-test-utils/src/__tests__/engine-server/markdown/v5/image.spec.ts
@@ -7,6 +7,31 @@ import { createProcCompileTests } from "../utils";
 import { getOpts, runTestCases } from "./utils";
 
 describe("GIVEN image link", () => {
+  describe("WHEN assetPrefix is NOT set", () => {
+    runTestCases(
+      createProcCompileTests({
+        name: "ASSET_PREFIX_SET",
+        setup: async (opts) => {
+          const { proc } = getOpts(opts);
+          const txt = `![some-image](some-image-link.png)`;
+          const resp = await proc.process(txt);
+          return { resp, proc };
+        },
+        verify: {
+          [DendronASTDest.HTML]: {
+            [ProcFlavor.PUBLISHING]: async ({ extra }) => {
+              const { resp } = extra;
+              expect(resp).toMatchSnapshot();
+              await checkString(resp.contents, "/some-image-link.png");
+            },
+          },
+        },
+        preSetupHook: async (opts) => {
+          await ENGINE_HOOKS.setupBasic({ ...opts, extra: { idv2: true } });
+        },
+      })
+    );
+  });
   describe("WHEN assetPrefix set", () => {
     runTestCases(
       createProcCompileTests({

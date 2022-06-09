18ba83c532dd397f8d901964ed91dd6f627cd4d0
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-06-09 18:31:22 -0400

    use dynamic import for es module package

--- packages/dendron-viz/src/index.jsx
+++ packages/dendron-viz/src/index.jsx
@@ -1,112 +1,133 @@
-import { exec } from '@actions/exec'
-import * as core from '@actions/core'
-import * as artifact from '@actions/artifact'
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import fs from "fs"
+import { exec } from "@actions/exec";
+import * as core from "@actions/core";
+import * as artifact from "@actions/artifact";
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import fs from "fs";
 
-import { processDir } from "./process-dir.js"
-import { Tree } from "./Tree.tsx"
+import { processDir } from "./process-dir.js";
+import { createTree } from "./Tree.tsx";
 
 const main = async () => {
-  core.info('[INFO] Usage https://github.com/githubocto/repo-visualizer#readme')
-
-  core.startGroup('Configuration')
-  const username = 'repo-visualizer'
-  await exec('git', ['config', 'user.name', username])
-  await exec('git', [
-    'config',
-    'user.email',
-    `${username}@users.noreply.github.com`,
-  ])
-
-  core.endGroup()
-
-
-  // const rootPath = core.getInput("root_path") || "../../workspaces/org-workspace-export"; 
-  const rootPath = core.getInput("root_path") || "../../workspaces/org-workspace-export/org-private"; 
-  const maxDepth = core.getInput("max_depth") || 9
-  const customFileColors = JSON.parse(core.getInput("file_colors") ||  '{}');
-  const colorEncoding = core.getInput("color_encoding") || "number-of-changes"
-  const commitMessage = core.getInput("commit_message") || "Repo visualizer: update diagram"
-  const excludedPathsString = core.getInput("excluded_paths") || "node_modules,bower_components,dist,out,build,eject,.next,.netlify,.yarn,.git,.vscode,package-lock.json,yarn.lock"
-  const excludedPaths = excludedPathsString.split(",").map(str => str.trim())
-
-  // Split on semicolons instead of commas since ',' are allowed in globs, but ';' are not + are not permitted in file/folder names.
-  const excludedGlobsString = core.getInput('excluded_globs') || '';
-  const excludedGlobs = excludedGlobsString.split(";");
-
-  const branch = core.getInput("branch")
-  
-  const data = await processDir(rootPath, excludedPaths, excludedGlobs);
-
-  const componentCodeString = ReactDOMServer.renderToStaticMarkup(
-    <Tree data={data} maxDepth={+maxDepth} colorEncoding={colorEncoding} customFileColors={customFileColors}/>
+  core.info(
+    "[INFO] Usage https://github.com/githubocto/repo-visualizer#readme"
   );
 
-  const outputFile = core.getInput("output_file") || "./diagram.svg"
+  core.startGroup("Configuration");
+  const username = "repo-visualizer";
+  await exec("git", ["config", "user.name", username]);
+  await exec("git", [
+    "config",
+    "user.email",
+    `${username}@users.noreply.github.com`,
+  ]);
 
-  core.setOutput('svg', componentCodeString)
+  core.endGroup();
 
-  await fs.writeFileSync(outputFile, componentCodeString)
+  // const rootPath = core.getInput("root_path") || "../../workspaces/org-workspace-export";
+  const rootPath =
+    core.getInput("root_path") ||
+    "../../workspaces/org-workspace-export/org-private";
+  const maxDepth = core.getInput("max_depth") || 9;
+  const customFileColors = JSON.parse(core.getInput("file_colors") || "{}");
+  const colorEncoding = core.getInput("color_encoding") || "number-of-changes";
+  const commitMessage =
+    core.getInput("commit_message") || "Repo visualizer: update diagram";
+  const excludedPathsString =
+    core.getInput("excluded_paths") ||
+    "node_modules,bower_components,dist,out,build,eject,.next,.netlify,.yarn,.git,.vscode,package-lock.json,yarn.lock";
+  const excludedPaths = excludedPathsString.split(",").map((str) => str.trim());
 
-  let doesBranchExist = true
+  // Split on semicolons instead of commas since ',' are allowed in globs, but ';' are not + are not permitted in file/folder names.
+  const excludedGlobsString = core.getInput("excluded_globs") || "";
+  const excludedGlobs = excludedGlobsString.split(";");
+
+  const branch = core.getInput("branch");
+
+  const data = await processDir(rootPath, excludedPaths, excludedGlobs);
+
+  const Tree = await createTree();
+
+  const componentCodeString = ReactDOMServer.renderToStaticMarkup(
+    <Tree
+      data={data}
+      maxDepth={+maxDepth}
+      colorEncoding={colorEncoding}
+      customFileColors={customFileColors}
+    />
+  );
+
+  const outputFile = core.getInput("output_file") || "./diagram.svg";
+
+  core.setOutput("svg", componentCodeString);
+
+  await fs.writeFileSync(outputFile, componentCodeString);
+
+  let doesBranchExist = true;
 
   if (branch) {
-    await exec('git', ['fetch'])
+    await exec("git", ["fetch"]);
 
     try {
-      await exec('git', ['rev-parse', '--verify', branch])
-      await exec('git', ['checkout', branch])
+      await exec("git", ["rev-parse", "--verify", branch]);
+      await exec("git", ["checkout", branch]);
     } catch {
-      doesBranchExist = false
-      core.info(`Branch ${branch} does not yet exist, creating ${branch}.`)
-      await exec('git', ['checkout', '-b', branch])
+      doesBranchExist = false;
+      core.info(`Branch ${branch} does not yet exist, creating ${branch}.`);
+      await exec("git", ["checkout", "-b", branch]);
     }
   }
 
-  await exec('git', ['add', outputFile])
-  const diff = await execWithOutput('git', ['status', '--porcelain', outputFile])
-  core.info(`diff: ${diff}`)
+  await exec("git", ["add", outputFile]);
+  const diff = await execWithOutput("git", [
+    "status",
+    "--porcelain",
+    outputFile,
+  ]);
+  core.info(`diff: ${diff}`);
   if (!diff) {
-    core.info('[INFO] No changes to the repo detected, exiting')
-    return
+    core.info("[INFO] No changes to the repo detected, exiting");
+    return;
   }
 
-  const shouldPush = core.getBooleanInput('should_push')
+  const shouldPush = core.getBooleanInput("should_push");
   if (shouldPush) {
-    core.startGroup('Commit and push diagram')
-    await exec('git', ['commit', '-m', commitMessage])
+    core.startGroup("Commit and push diagram");
+    await exec("git", ["commit", "-m", commitMessage]);
 
     if (doesBranchExist) {
-      await exec('git', ['push'])
+      await exec("git", ["push"]);
     } else {
-      await exec('git', ['push', '--set-upstream', 'origin', branch])
+      await exec("git", ["push", "--set-upstream", "origin", branch]);
     }
 
     if (branch) {
-      await exec('git', 'checkout', '-')
+      await exec("git", "checkout", "-");
     }
-    core.endGroup()
+    core.endGroup();
   }
 
-  const shouldUpload = core.getInput('artifact_name') !== ''
+  const shouldUpload = core.getInput("artifact_name") !== "";
   if (shouldUpload) {
-    core.startGroup('Upload diagram to artifacts')
-    const client = artifact.create()
-    const result = await client.uploadArtifact(core.getInput('artifact_name'), [outputFile], '.')
+    core.startGroup("Upload diagram to artifacts");
+    const client = artifact.create();
+    const result = await client.uploadArtifact(
+      core.getInput("artifact_name"),
+      [outputFile],
+      "."
+    );
     if (result.failedItems.length > 0) {
-      throw 'Artifact was not uploaded successfully.'
+      throw "Artifact was not uploaded successfully.";
     }
-    core.endGroup()
+    core.endGroup();
   }
 
-  console.log("All set!")
-}
+  console.log("All set!");
+};
 
 main().catch((e) => {
-  core.setFailed(e)
-})
+  core.setFailed(e);
+});
 
 function execWithOutput(command, args) {
   return new Promise((resolve, reject) => {
@@ -114,17 +135,17 @@ function execWithOutput(command, args) {
       exec(command, args, {
         listeners: {
           stdout: function (res) {
-            core.info(res.toString())
-            resolve(res.toString())
+            core.info(res.toString());
+            resolve(res.toString());
           },
           stderr: function (res) {
-            core.info(res.toString())
-            reject(res.toString())
-          }
-        }
-      })
+            core.info(res.toString());
+            reject(res.toString());
+          },
+        },
+      });
     } catch (e) {
-      reject(e)
+      reject(e);
     }
-  })
+  });
 }

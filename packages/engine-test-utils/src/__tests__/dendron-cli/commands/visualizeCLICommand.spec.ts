1f6528e2bb515732f5eacf0289d4501408cf1907
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-06-15 17:12:06 -0400

    transform esm modules for tests
    Co-authored-by: Kaan Barmore-Genç <kaan@bgenc.net>

--- packages/engine-test-utils/src/__tests__/dendron-cli/commands/visualizeCLICommand.spec.ts
+++ packages/engine-test-utils/src/__tests__/dendron-cli/commands/visualizeCLICommand.spec.ts
@@ -1,18 +1,20 @@
-import { NoteUtils, VaultUtils } from "@dendronhq/common-all";
-import { NoteTestUtilsV4 } from "@dendronhq/common-test-utils";
+// import { NoteUtils, VaultUtils } from "@dendronhq/common-all";
+// import { NoteTestUtilsV4 } from "@dendronhq/common-test-utils";
 import {
-  NoteCLICommand,
-  NoteCLICommandOpts,
-  NoteCLIOutput,
-  NoteCommandData,
-  NoteCommands,
+  // NoteCLICommand,
+  // NoteCLICommandOpts,
+  // NoteCLIOutput,
+  // NoteCommandData,
+  // NoteCommands,
   VisualizeCLICommand,
   VisualizeCLICommandOpts,
 } from "@dendronhq/dendron-cli";
+import { fstat } from "fs";
 import _ from "lodash";
 import { createEngineFromServer, runEngineTestV5 } from "../../../engine";
-import { ENGINE_HOOKS, ENGINE_HOOKS_MULTI } from "../../../presets";
-import { checkString } from "../../../utils";
+import * as fs from "fs";
+// import { ENGINE_HOOKS, ENGINE_HOOKS_MULTI } from "../../../presets";
+// import { checkString } from "../../../utils";
 
 const runCmd = (opts: Omit<VisualizeCLICommandOpts, "port" | "server">) => {
   const cmd = new VisualizeCLICommand();
@@ -30,6 +32,7 @@ describe("WHEN run 'visualize'", () => {
   describe("AND WHEN dendron ws root is set correctly", () => {
     test("THEN produce svg files", async () => {
       await runEngineTestV5(
+        //TODO: Where and how vaults are created?
         async ({ engine, wsRoot, vaults }) => {
           const vault = vaults[0];
           console.log("wsRoot:", wsRoot);
@@ -37,7 +40,28 @@ describe("WHEN run 'visualize'", () => {
             wsRoot,
             engine,
           });
+
           expect(1).toEqual(1);
+
+          // expect(fs.existsSync("diagram-vault1.svg")).toEqual(true);
+          // expect(fs.existsSync("diagram-vault1.svg")).toEqual(true);
+          // expect(fs.existsSync("diagram-vault1.svg")).toEqual(true);
+        },
+        {
+          //TODO: createEngineFromServer vs. createEngineFromEngine (default)
+          // createEngine: createEngineFromServer
+          expect,
+        }
+      );
+    });
+  });
+
+  describe("AND WHEN dendron ws root is not set correctly", () => {
+    test("THEN ???", async () => {
+      await runEngineTestV5(
+        async ({ engine, wsRoot, vaults }) => {
+          // Check if the error message is logged out correctly
+          //TODO: How to check log message? with sinon spy?
         },
         {
           // createEngine: createEngineFromServer,

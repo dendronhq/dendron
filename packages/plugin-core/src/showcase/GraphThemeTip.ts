2ff253e3620c812ba40b42c0930e132b583d8a42
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2022-06-20 22:42:03 +0530

    chore(internal): close show me how ab test for graph theme

--- packages/plugin-core/src/showcase/GraphThemeTip.ts
+++ packages/plugin-core/src/showcase/GraphThemeTip.ts
@@ -1,9 +1,4 @@
-import {
-  assertUnreachable,
-  GraphThemeFeatureShowcaseTestGroups,
-  GRAPH_THEME_FEATURE_SHOWCASE_TEST,
-} from "@dendronhq/common-all";
-import { SegmentClient } from "@dendronhq/common-server";
+import { assertUnreachable } from "@dendronhq/common-all";
 import { ShowcaseEntry } from "@dendronhq/engine-server";
 import * as vscode from "vscode";
 import { showMeHowView } from "../views/ShowMeHowView";
@@ -26,49 +21,22 @@ export class GraphThemeTip implements IFeatureShowcaseMessage {
     return ShowcaseEntry.GraphTheme;
   }
 
-  getDisplayMessage(displayLocation: DisplayLocation): string {
-    const ABUserGroup = GRAPH_THEME_FEATURE_SHOWCASE_TEST.getUserGroup(
-      SegmentClient.instance().anonymousId
-    );
-    const tipofTheDayMessage =
-      "Change the appearance of the note graph by clicking the config button on the top left corner of the graph view and selecting one of the built-in styles. You can even customize the appearance to your liking with css";
-    switch (displayLocation) {
-      case DisplayLocation.InformationMessage:
-        if (ABUserGroup === GraphThemeFeatureShowcaseTestGroups.showMeHow) {
-          return `Dendron now has new themes for Graph View. Check it out`;
-        }
-        return `New themes for Graph View! ${tipofTheDayMessage}`;
-      case DisplayLocation.TipOfTheDayView:
-        return tipofTheDayMessage;
-      default:
-        assertUnreachable(displayLocation);
-    }
+  getDisplayMessage(_displayLocation: DisplayLocation): string {
+    return `Dendron now has new themes for Graph View. Check it out`;
   }
 
   onConfirm() {
-    const ABUserGroup = GRAPH_THEME_FEATURE_SHOWCASE_TEST.getUserGroup(
-      SegmentClient.instance().anonymousId
-    );
-    if (ABUserGroup === GraphThemeFeatureShowcaseTestGroups.showMeHow) {
-      showMeHowView({
-        name: "Graph Theme",
-        src: "https://org-dendron-public-assets.s3.amazonaws.com/images/graph-theme.gif",
-        href: "https://www.loom.com/share/f2c53d2a5aeb48209b5587a3dfbb1015",
-        alt: "Click on menu icon in the Graph View to change themes",
-      });
-    } else {
-      vscode.commands.executeCommand("dendron.showNoteGraph");
-    }
+    showMeHowView({
+      name: "Graph Theme",
+      src: "https://org-dendron-public-assets.s3.amazonaws.com/images/graph-theme.gif",
+      href: "https://www.loom.com/share/f2c53d2a5aeb48209b5587a3dfbb1015",
+      alt: "Click on menu icon in the Graph View to change themes",
+    });
+    vscode.commands.executeCommand("dendron.showNoteGraph");
   }
 
   get confirmText(): string {
-    const ABUserGroup = GRAPH_THEME_FEATURE_SHOWCASE_TEST.getUserGroup(
-      SegmentClient.instance().anonymousId
-    );
-    if (ABUserGroup === GraphThemeFeatureShowcaseTestGroups.showMeHow) {
-      return "Show me how";
-    }
-    return "Open graph view";
+    return "Show me how";
   }
 
   get deferText(): string {

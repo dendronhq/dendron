3e1953ea142f2493c67b13ef55606335d839b89c
Mark Choi <hikchoi@gmail.com>
2022-06-23 02:46:35 +0900

    chore: fix tests for stats

--- packages/plugin-core/src/test/suite-integ/StatisticsUtils.test.ts
+++ packages/plugin-core/src/test/suite-integ/StatisticsUtils.test.ts
@@ -1,4 +1,4 @@
-import { DendronError, StatisticsUtils } from "@dendronhq/common-all";
+import { NonEmptyArray, StatisticsUtils } from "@dendronhq/common-all";
 import { describe } from "mocha";
 import { expect } from "../testUtilsv2";
 
@@ -6,35 +6,15 @@ suite("StatisticsUtils", function () {
   describe("stddev", () => {
     describe("given an array of numbers", () => {
       test("correctly outputs standard deviation", () => {
-        const arr = [1, 2, 3, 4, 5];
+        const arr: NonEmptyArray<number> = [1, 2, 3, 4, 5];
         const expected = Math.sqrt(2);
         const actual = StatisticsUtils.stddev(arr);
         expect(actual).toEqual(expected);
 
-        const arr2 = [1, 2, 3, 4, 5, 6];
+        const arr2: NonEmptyArray<number> = [1, 2, 3, 4, 5, 6];
         const expected2 = Math.sqrt(17.5 / 6);
         const actual2 = StatisticsUtils.stddev(arr2);
         expect(actual2).toEqual(expected2);
-      });
-    });
-
-    describe("given an empty array", () => {
-      test("throws error", () => {
-        const arr: number[] = [];
-        let errorThrown = false;
-        try {
-          StatisticsUtils.stddev(arr);
-        } catch (error) {
-          errorThrown = true;
-          const isDendronError = DendronError.isDendronError(error);
-          expect(isDendronError).toBeTruthy();
-          if (isDendronError) {
-            expect(error.message).toEqual(
-              "Cannot calculate standard deviation of empty array."
-            );
-          }
-        }
-        expect(errorThrown).toBeTruthy();
       });
     });
   });
@@ -42,35 +22,30 @@ suite("StatisticsUtils", function () {
   describe("median", () => {
     describe("given an array of number", () => {
       test("correctly outputs median value", () => {
-        const arr = [1, 2, 3, 4, 5];
+        const arr: NonEmptyArray<number> = [1, 2, 3, 4, 5];
         const expected = 3;
         const actual = StatisticsUtils.median(arr);
         expect(actual).toEqual(expected);
 
-        const arr2 = [1, 2, 3, 4, 5, 6];
+        const arr2: NonEmptyArray<number> = [1, 2, 3, 4, 5, 6];
         const expected2 = 3.5;
         const actual2 = StatisticsUtils.median(arr2);
         expect(actual2).toEqual(expected2);
       });
     });
+  });
 
+  describe("isNonEmptyArray", () => {
+    describe("given a non empty array", () => {
+      test("type guard correctly infers that an array is a NonEmptyArray", () => {
+        const arr: number[] = [1, 2, 3];
+        expect(StatisticsUtils.isNonEmptyArray(arr)).toBeTruthy();
+      });
+    });
     describe("given an empty array", () => {
-      test("throws error", () => {
+      test("type guard correctly infers that an array is not a NonEmptyArray", () => {
         const arr: number[] = [];
-        let errorThrown = false;
-        try {
-          StatisticsUtils.median(arr);
-        } catch (error) {
-          errorThrown = true;
-          const isDendronError = DendronError.isDendronError(error);
-          expect(isDendronError).toBeTruthy();
-          if (isDendronError) {
-            expect(error.message).toEqual(
-              "Cannot calculate median of empty array."
-            );
-          }
-        }
-        expect(errorThrown).toBeTruthy();
+        expect(StatisticsUtils.isNonEmptyArray(arr)).toBeFalsy();
       });
     });
   });

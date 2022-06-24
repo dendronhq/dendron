d69f02c0857b4fc594ee8e3f494bd1e5465be2e8
Kaan Barmore-Genc <kaan@dendron.so>
2022-06-24 16:20:56 -0400

    fix(workspace): duplicate note behavior is not updated when self contained vault is removed

--- packages/plugin-core/src/test/suite-integ/VaultRemoveCommand.test.ts
+++ packages/plugin-core/src/test/suite-integ/VaultRemoveCommand.test.ts
@@ -218,52 +218,42 @@ suite("GIVEN VaultRemoveCommand", function () {
     }
   );
 
-  // This test has been skipped because the checks were not working with self
-  // contained vaults. This should be fixed later.
-  describeSingleWS.skip(
-    "WHEN there's only one vault left after remove",
-    {},
-    () => {
-      test("THEN duplicateNoteBehavior is omitted", async () => {
-        const { wsRoot, vaults } = ExtensionProvider.getDWorkspace();
-        const configPath = DConfig.configPath(wsRoot as string);
+  describeSingleWS("WHEN there's only one vault left after remove", {}, () => {
+    test("THEN duplicateNoteBehavior is omitted", async () => {
+      const { wsRoot } = ExtensionProvider.getDWorkspace();
+      const configPath = DConfig.configPath(wsRoot as string);
 
-        // add a second vault
-        const vault2 = VaultUtils.getRelPath(vaults[1]);
-        const vpath2 = path.join(wsRoot, vault2);
+      // add a second vault
+      const vault2 = "vault2";
 
-        stubVaultInput({ sourceType: "local", sourcePath: vpath2 });
-        await new VaultAddCommand().run();
+      stubVaultInput({ sourceType: "local", sourcePath: vault2 });
+      await new VaultAddCommand().run();
 
-        const config = readYAML(configPath) as IntermediateDendronConfig;
-        // confirm that duplicateNoteBehavior option exists
-        const publishingConfig = ConfigUtils.getPublishingConfig(config);
-        expect(publishingConfig.duplicateNoteBehavior).toBeTruthy();
+      const config = readYAML(configPath) as IntermediateDendronConfig;
+      // confirm that duplicateNoteBehavior option exists
+      const publishingConfig = ConfigUtils.getPublishingConfig(config);
+      expect(publishingConfig.duplicateNoteBehavior).toBeTruthy();
 
-        const vaultsAfter = ExtensionProvider.getDWorkspace().vaults;
-        // @ts-ignore
-        VSCodeUtils.showQuickPick = () => {
-          return { data: vaultsAfter[1] };
-        };
-        await new VaultRemoveCommand().run();
+      const vaultsAfter = ExtensionProvider.getDWorkspace().vaults;
+      // @ts-ignore
+      VSCodeUtils.showQuickPick = () => {
+        return { data: vaultsAfter[1] };
+      };
+      await new VaultRemoveCommand().run();
 
-        const configNew = readYAML(configPath) as IntermediateDendronConfig;
-        // confirm that duplicateNoteBehavior setting is gone
-        const publishingConfigNew = ConfigUtils.getPublishingConfig(configNew);
-        expect(publishingConfigNew.duplicateNoteBehavior).toBeFalsy();
-      });
-    }
-  );
-
-  // This test has been skipped because the checks were not working with self
-  // contained vaults. This should be fixed later.
-  describeSingleWS.skip("WHEN a published vault is removed", {}, () => {
+      const configNew = readYAML(configPath) as IntermediateDendronConfig;
+      // confirm that duplicateNoteBehavior setting is gone
+      const publishingConfigNew = ConfigUtils.getPublishingConfig(configNew);
+      expect(publishingConfigNew.duplicateNoteBehavior).toBeFalsy();
+    });
+  });
+  describeSingleWS("WHEN a published vault is removed", {}, () => {
     test("THEN the vault is removed from duplicateNoteBehavior payload", async () => {
       const { wsRoot } = ExtensionProvider.getDWorkspace();
       // add two more vaults
 
-      const vpath2 = path.join(wsRoot, "vault2");
-      const vpath3 = path.join(wsRoot, "vault3");
+      const vpath2 = "vault2";
+      const vpath3 = "vault3";
 
       stubVaultInput({ sourceType: "local", sourcePath: vpath2 });
       await new VaultAddCommand().run();
@@ -293,8 +283,8 @@ suite("GIVEN VaultRemoveCommand", function () {
       // check that "vault2" is gone from payload
       const publishingConfig = ConfigUtils.getPublishingConfig(config);
       expect(publishingConfig.duplicateNoteBehavior!.payload).toEqual([
-        vaultsAfter[0].fsPath,
-        vaultsAfter[2].fsPath,
+        VaultUtils.getName(vaultsAfter[2]),
+        VaultUtils.getName(vaultsAfter[0]),
       ]);
     });
   });

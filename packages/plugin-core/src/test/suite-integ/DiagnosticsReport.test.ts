f3d6b1a21eab5c9b663e3e83d6fe786c658519bf
Kevin <kevin@dendron.so>
2022-06-23 19:34:58 -0700

    update diagnostics

--- packages/plugin-core/src/test/suite-integ/DiagnosticsReport.test.ts
+++ packages/plugin-core/src/test/suite-integ/DiagnosticsReport.test.ts
@@ -6,36 +6,30 @@ import path from "path";
 import { DiagnosticsReportCommand } from "../../commands/DiagnosticsReport";
 import { VSCodeUtils } from "../../vsCodeUtils";
 import { expect } from "../testUtilsv2";
-import { runLegacyMultiWorkspaceTest, setupBeforeAfter } from "../testUtilsV3";
+import { describeMultiWS } from "../testUtilsV3";
 
-suite("DiagnosticsReport", function () {
-  const ctx = setupBeforeAfter(this);
+suite("GIVEN DiagnosticsReport", function () {
+  describeMultiWS(
+    "WHEN run Diagnostics Report",
+    {
+      preSetupHook: ENGINE_HOOKS.setupBasic,
+    },
+    async () => {
+      const logDst = path.join(
+        path.dirname(env("LOG_DST")),
+        "dendron.server.log"
+      );
+      fs.writeFileSync(logDst, "foobar", { encoding: "utf8" });
+      const cmd = new DiagnosticsReportCommand();
+      await cmd.execute();
 
-  test("basic", (done) => {
-    runLegacyMultiWorkspaceTest({
-      ctx,
-      preSetupHook: async ({ wsRoot, vaults }) => {
-        // check for meta
-        ENGINE_HOOKS.setupBasic({ wsRoot, vaults });
-      },
-      onInit: async () => {
-        const logDst = path.join(
-          path.dirname(env("LOG_DST")),
-          "dendron.server.log"
-        );
-        fs.writeFileSync(logDst, "foobar", { encoding: "utf8" });
-        const cmd = new DiagnosticsReportCommand();
-        await cmd.execute();
-
-        const editor = VSCodeUtils.getActiveTextEditor();
-        const body = editor?.document.getText() as string;
-        const isInString = await AssertUtils.assertInString({
-          body,
-          match: ["foobar", "Dendron Confg", "Plugin Logs", "Workspace File"],
-        });
-        expect(isInString).toBeTruthy();
-        done();
-      },
-    });
-  });
+      const editor = VSCodeUtils.getActiveTextEditor();
+      const body = editor?.document.getText() as string;
+      const isInString = await AssertUtils.assertInString({
+        body,
+        match: ["foobar", "Dendron Confg", "Plugin Logs", "Workspace File"],
+      });
+      expect(isInString).toBeTruthy();
+    }
+  );
 });

49f6056b3a51446410d02f506abf2f2d106017eb
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-06-24 16:07:42 -0700

    chore: add some code from DendronNotePreview

--- packages/dendron-plugin-views/src/components/Visualization.tsx
+++ packages/dendron-plugin-views/src/components/Visualization.tsx
@@ -1,12 +1,30 @@
 import { DendronComponent } from "../types";
 import { getVisualizationContent } from "@dendronhq/dendron-viz";
-import { engineHooks } from "@dendronhq/common-frontend";
+import { createLogger, engineHooks } from "@dendronhq/common-frontend";
 import { useEffect, useState } from "react";
+import { useWorkspaceProps } from "../hooks";
 
-const { useEngine } = engineHooks;
+const { useEngine, useConfig } = engineHooks;
 
 const Visualization: DendronComponent = (props) => {
-  const { engine, workspace } = props;
+  const { engine } = props;
+
+  const ctx = "DendronNotePreview";
+  const logger = createLogger("DendronNotePreview");
+  const noteProps = props.ide.noteActive;
+  const config = props.engine.config;
+
+  const [workspace] = useWorkspaceProps();
+  const { useConfig } = engineHooks;
+  useConfig({ opts: workspace });
+
+  //TODO: Figure out how to use logger and what logger is for
+  logger.info({
+    ctx,
+    msg: "enter",
+    noteProps: noteProps ? noteProps.id : "no notes found",
+    config,
+  });
 
   useEngine({
     engineState: engine,
@@ -16,6 +34,9 @@ const Visualization: DendronComponent = (props) => {
   // Get current note (and vault), and display the corresponding visualization
   let noteActive = props.ide.noteActive;
   const activeVault = noteActive?.vault;
+
+  console.log("ide", props.ide);
+  console.log("noteActive", noteActive?.fname);
 
   const [component, setComponent] = useState<JSX.Element | undefined>(
     undefined

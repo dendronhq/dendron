fb9be4683fd03be61c052668ffbe5e8ee68fdfac
Jonathan Yeung <jonathanyeung@outlook.com>
2022-06-27 15:32:12 +0800

    fix(structure): hot reload in note traits + no template by default

--- packages/plugin-core/webpack-require-hack.js
+++ packages/plugin-core/webpack-require-hack.js
@@ -1,4 +1,8 @@
 const webpackRequire = (importPath) => {
+  // First delete the import from the node module cache in case it exists. This
+  // allows us to do 'hot-reloading' of the .js files in Traits.
+  delete require.cache[require.resolve(importPath)];
+
   const module = require(importPath);
   return module;
 };

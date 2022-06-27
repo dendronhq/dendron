390465552a6744495387aea6f49fa5392fb69b03
Jonathan Yeung <jonathanyeung@outlook.com>
2022-06-27 15:32:12 +0800

    fix(structure): hot reload in note traits + no template by default

--- packages/plugin-core/src/traits/webpack-require-hack.ts
+++ packages/plugin-core/src/traits/webpack-require-hack.ts
@@ -1,6 +1,10 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 // @ts-ignore
+
+// NOTE: This file is ONLY used during debugging. In the webpacked production
+// build, the file that is used is the version located at
+// PROJECT_ROOT/packages/plugin-core/webpack-require-hack.js
 const webpackRequire = (importPath) => {
   // First delete the import from the node module cache in case it exists. This
   // allows us to do 'hot-reloading' of the .js files in Traits.

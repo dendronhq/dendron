07a238d068ed140d6731db92ff9c43c78cad4c7c
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-06-30 11:06:35 -0700

    chore: test new command

---
+++ packages/plugin-core/src/test/suite-integ/ConfigureLocalOverride.test.ts
@@ -0,0 +1,50 @@
+import { DConfig, LocalConfigScope } from "@dendronhq/engine-server";
+import { describe } from "mocha";
+import { ConfigureLocalOverride } from "../../commands/ConfigureLocalOverride";
+import { ExtensionProvider } from "../../ExtensionProvider";
+import { VSCodeUtils } from "../../vsCodeUtils";
+import { expect } from "../testUtilsv2";
+import { describeSingleWS } from "../testUtilsV3";
+
+suite("ConfigureLocalOverrideCommand", function () {
+  describeSingleWS("WHEN run", {}, () => {
+    let cmd: ConfigureLocalOverride;
+
+    this.beforeEach(() => {
+      const ext = ExtensionProvider.getExtension();
+      cmd = new ConfigureLocalOverride(ext);
+    });
+
+    describe("AND scopoe is GLOBAL", () => {
+      test("THEN the configuration file for the user should open", async () => {
+        await cmd.run({ configScope: LocalConfigScope.GLOBAL });
+
+        const { wsRoot } = ExtensionProvider.getDWorkspace();
+        expect(
+          VSCodeUtils.getActiveTextEditor()?.document.uri.fsPath.toLowerCase()
+        ).toEqual(
+          DConfig.configOverridePath(
+            wsRoot,
+            LocalConfigScope.GLOBAL
+          ).toLowerCase()
+        );
+      });
+    });
+
+    describe("AND scope is LOCAL", () => {
+      test("THEN the configuration file for the workspace should open", async () => {
+        await cmd.run({ configScope: LocalConfigScope.WORKSPACE });
+
+        const { wsRoot } = ExtensionProvider.getDWorkspace();
+        expect(
+          VSCodeUtils.getActiveTextEditor()?.document.uri.fsPath.toLowerCase()
+        ).toEqual(
+          DConfig.configOverridePath(
+            wsRoot,
+            LocalConfigScope.WORKSPACE
+          ).toLowerCase()
+        );
+      });
+    });
+  });
+});

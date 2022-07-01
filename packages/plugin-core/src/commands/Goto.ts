e34f85184d385bf109e8d05d5dba0936779fa27c
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-06-30 21:32:40 -0700

    chore: move code for opening link to goto

--- packages/plugin-core/src/commands/Goto.ts
+++ packages/plugin-core/src/commands/Goto.ts
@@ -1,25 +1,40 @@
 import {
   DendronError,
   DVault,
+  ERROR_STATUS,
   NoteUtils,
   RespV3,
   VaultUtils,
 } from "@dendronhq/common-all";
+import { resolvePath, vault2Path } from "@dendronhq/common-server";
+import fs from "fs";
 import _ from "lodash";
+import open from "open";
+import path from "path";
+import { env, Uri } from "vscode";
+import { PickerUtilsV2 } from "../components/lookup/utils";
 import { DENDRON_COMMANDS } from "../constants";
 import { IDendronExtension } from "../dendronExtensionInterface";
 import { ExtensionProvider } from "../ExtensionProvider";
 import { getLinkFromSelectionWithWorkspace } from "../utils/editor";
+import { getURLAt } from "../utils/md";
+import { VSCodeUtils } from "../vsCodeUtils";
+import { getDWorkspace, getExtension } from "../workspace";
 import { BasicCommand } from "./base";
 import { GotoNoteCommand } from "./GotoNote";
 import { GoToNoteCommandOutput, TargetKind } from "./GoToNoteInterface";
 import { OpenLinkCommand } from "./OpenLink";
 
-type CommandOpts = {};
+type CommandOpts = {
+  externalLink?: string;
+};
 
 type CommandInput = {};
 
-type CommandOutput = RespV3<GoToNoteCommandOutput>;
+type CommandOutput =
+  | RespV3<GoToNoteCommandOutput>
+  | { error?: DendronError; fsPath?: string }
+  | undefined;
 
 const GOTO_KEY = "uri";
 
@@ -34,7 +49,10 @@ export class GotoCommand extends BasicCommand<CommandOpts, CommandOutput> {
     super();
   }
 
-  addAnalyticsPayload?(_opts?: CommandOpts, out?: CommandOutput) {
+  addAnalyticsPayload(
+    _opts?: CommandOpts,
+    out?: RespV3<GoToNoteCommandOutput>
+  ) {
     if (!out?.data) {
       return {};
     }
@@ -52,60 +70,123 @@ export class GotoCommand extends BasicCommand<CommandOpts, CommandOutput> {
   async gatherInputs(): Promise<CommandInput | undefined> {
     return {};
   }
-  async execute(): Promise<CommandOutput> {
+
+  async execute(opts?: CommandOpts): Promise<CommandOutput> {
     const { vaults, engine } = ExtensionProvider.getDWorkspace();
 
-    const link = await getLinkFromSelectionWithWorkspace();
-    if (!link) {
-      return {
-        error: new DendronError({ message: "selection is not a valid link" }),
-      };
-    }
+    const externalLink =
+      opts?.externalLink ?? getURLAt(VSCodeUtils.getActiveTextEditor());
+    const noteLink = await getLinkFromSelectionWithWorkspace();
 
-    // get vault
-    let vault: DVault | undefined;
-    const { anchorHeader, value: fname, vaultName } = link;
-    if (vaultName) {
-      vault = VaultUtils.getVaultByNameOrThrow({
-        vaults,
-        vname: vaultName,
+    /* If the link read is not a valid link, exit from the command */
+    if (!externalLink && !noteLink) {
+      const error = DendronError.createFromStatus({
+        status: ERROR_STATUS.INVALID_STATE,
+        message: `no valid path or URL selected`,
       });
+
+      this.L.error({ error });
+
+      return { error };
     }
 
-    // get note
-    const notes = NoteUtils.getNotesByFnameFromEngine({
-      fname,
-      engine,
-      vault,
-    });
-    if (notes.length === 0) {
-      return {
-        error: new DendronError({ message: "selection is not a note" }),
-      };
-    }
+    const goToExternalLink = async () => {
+      let assetPath: string;
 
-    // TODO: for now, get first note, in the future, show prompt
-    const note = notes[0];
+      if (
+        externalLink.indexOf(":/") !== -1 ||
+        externalLink.indexOf("/") === 0 ||
+        externalLink.indexOf(":\\") !== -1
+      ) {
+        // window.showInformationMessage(
+        //   "the selection reads as a full URI or filepath so an attempt will be made to open it"
+        // );
+        env.openExternal(Uri.parse(externalLink.replace("\\", "/"))); // make sure vscode doesn't choke on "\"s
+        assetPath = externalLink;
+      } else {
+        const wsRoot = getDWorkspace().wsRoot;
 
-    // if note doesn't have url, run goto note command
-    if (_.isUndefined(note.custom[GOTO_KEY])) {
-      const resp = await new GotoNoteCommand(this._ext).execute({
-        qs: note.fname,
-        vault: note.vault,
-        anchor: anchorHeader,
-      });
-      return { data: resp };
-    }
-
-    await this.openLink(note.custom[GOTO_KEY]);
-    // we found a link
-    return {
-      data: {
-        kind: TargetKind.LINK,
-        fullPath: note.custom[GOTO_KEY],
-        fromProxy: true,
-      },
+        if (externalLink.startsWith("asset")) {
+          const vault = PickerUtilsV2.getOrPromptVaultForOpenEditor();
+          assetPath = path.join(vault2Path({ vault, wsRoot }), externalLink);
+        } else {
+          assetPath = resolvePath(
+            externalLink,
+            getExtension().rootWorkspace.uri.fsPath
+          );
+        }
+        if (!fs.existsSync(assetPath)) {
+          const error = DendronError.createFromStatus({
+            status: ERROR_STATUS.INVALID_STATE,
+            message: `no valid path or URL selected`,
+          });
+          this.L.error({ error });
+          return { error };
+        }
+        await open(assetPath).catch((err) => {
+          const error = DendronError.createFromStatus({
+            status: ERROR_STATUS.UNKNOWN,
+            innerError: err,
+          });
+          this.L.error({ error });
+          return { error };
+        });
+      }
+      return { filepath: assetPath };
     };
+
+    const goToNoteLink = async () => {
+      // get vault
+      let vault: DVault | undefined;
+      const { anchorHeader, value: fname, vaultName } = noteLink!;
+      if (vaultName) {
+        vault = VaultUtils.getVaultByNameOrThrow({
+          vaults,
+          vname: vaultName,
+        });
+      }
+
+      // get note
+      const notes = NoteUtils.getNotesByFnameFromEngine({
+        fname,
+        engine,
+        vault,
+      });
+      if (notes.length === 0) {
+        return {
+          error: new DendronError({ message: "selection is not a note" }),
+        };
+      }
+
+      // TODO: for now, get first note, in the future, show prompt
+      const note = notes[0];
+
+      // if note doesn't have url, run goto note command
+      if (_.isUndefined(note.custom[GOTO_KEY])) {
+        const resp = await new GotoNoteCommand(this._ext).execute({
+          qs: note.fname,
+          vault: note.vault,
+          anchor: anchorHeader,
+        });
+        return { data: resp };
+      }
+
+      await this.openLink(note.custom[GOTO_KEY]);
+      // we found a link
+      return {
+        data: {
+          kind: TargetKind.LINK,
+          fullPath: note.custom[GOTO_KEY],
+          fromProxy: true,
+        },
+      };
+    };
+
+    if (externalLink) {
+      return goToExternalLink();
+    } else {
+      return goToNoteLink();
+    }
   }
 
   private openLink(uri: string) {

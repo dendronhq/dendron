804c3a01e0bd1987c64bb95e304a3f99e4578635
Hayata Suenaga <98560306+hayata-suenaga@users.noreply.github.com>
2022-07-05 10:41:06 -0700

    chore: get journal notes through parent property

--- packages/plugin-core/src/commands/GoToSiblingCommand.ts
+++ packages/plugin-core/src/commands/GoToSiblingCommand.ts
@@ -49,22 +49,26 @@ export class GoToSiblingCommand extends BasicCommand<
     // Get workspace
     const workspace = ExtensionProvider.getDWorkspace();
     // Get the active note
-    const note = await getActiveNote(workspace, textEditor, fname);
+    const note = await this.getActiveNote(workspace, textEditor, fname);
     if (!note) throw new Error(`${ctx}: ${UNKNOWN_ERROR_MSG}`);
     // Get the sibling note
     let siblingNote: NoteProps;
     // If the active note is a journal note, get the sibling note based on the chronological order
-    if (isJournalNote(note)) {
-      const allNotes: NoteProps[] = Object.values(workspace.engine.notes);
-      const { sibling, msg } = await getSiblingForJournalNote(
-        allNotes,
+    if (this.isJournalNote(note)) {
+      const { sibling, msg } = await this.getSiblingForJournalNote(
+        workspace.engine.notes,
         note,
         opts.direction
       );
       if (msg) return { msg };
       siblingNote = sibling!;
     } else {
-      const { sibling, msg } = getSibling(workspace, note, opts.direction, ctx);
+      const { sibling, msg } = this.getSibling(
+        workspace,
+        note,
+        opts.direction,
+        ctx
+      );
       if (msg) return { msg };
       siblingNote = sibling!;
     }
@@ -80,150 +84,177 @@ export class GoToSiblingCommand extends BasicCommand<
     // Return the success message for testing
     return { msg: "ok" as const };
   }
-}
 
-const getActiveNote = async (
-  { engine, vaults, wsRoot }: DWorkspaceV2,
-  textEditor: TextEditor,
-  fname: string
-): Promise<NoteProps | null> => {
-  const vault =
-    fname === "root"
-      ? VaultUtils.getVaultByFilePath({
-          vaults,
-          wsRoot,
-          fsPath: textEditor.document.uri.fsPath,
-        })
-      : PickerUtilsV2.getOrPromptVaultForOpenEditor();
-  const hitNotes = await engine.findNotes({ fname, vault });
-  return hitNotes.length !== 0 ? hitNotes[0] : null;
-};
+  async getActiveNote(
+    { engine, vaults, wsRoot }: DWorkspaceV2,
+    textEditor: TextEditor,
+    fname: string
+  ): Promise<NoteProps | null> {
+    const vault =
+      fname === "root"
+        ? VaultUtils.getVaultByFilePath({
+            vaults,
+            wsRoot,
+            fsPath: textEditor.document.uri.fsPath,
+          })
+        : PickerUtilsV2.getOrPromptVaultForOpenEditor();
+    const hitNotes = await engine.findNotes({ fname, vault });
+    return hitNotes.length !== 0 ? hitNotes[0] : null;
+  }
 
-const isJournalNote = (note: NoteProps): boolean => {
-  return NoteUtils.getNoteTraits(note).includes("journalNote");
-};
+  private isJournalNote(note: NoteProps): boolean {
+    return NoteUtils.getNoteTraits(note).includes("journalNote");
+  }
 
-const getSiblingForJournalNote = async (
-  allNotes: NoteProps[],
-  currNote: NoteProps,
-  direction: Direction
-): Promise<{ sibling: NoteProps | null; msg: CommandOutput["msg"] | null }> => {
-  const journalNotes = allNotes.filter((note) =>
-    NoteUtils.getNoteTraits(note).includes("journalNote")
-  );
-  // If the active note is the only journal note in the workspace, there is no sibling
-  if (journalNotes.length === 1) {
-    VSCodeUtils.showMessage(
-      MessageSeverity.WARN,
-      "There is no sibling journal note. Currently open note is the only journal note in the current workspace",
-      {}
-    );
-    return { sibling: null, msg: "no_siblings" };
-  }
-  // Sort all journal notes in the workspace
-  const sortedJournalNotes = _.sortBy(journalNotes, [
-    (note) => getDateFromJournalNote(note).valueOf(),
-  ]);
-  const currNoteIdx = _.findIndex(sortedJournalNotes, { id: currNote.id });
-  // Get the sibling based on the direction
-  let sibling: NoteProps;
-  if (direction === "next") {
-    sibling =
-      currNoteIdx !== sortedJournalNotes.length - 1
-        ? sortedJournalNotes[currNoteIdx + 1]
-        : sortedJournalNotes[0];
-  } else {
-    sibling =
-      currNoteIdx !== 0
-        ? sortedJournalNotes[currNoteIdx - 1]
-        : _.last(sortedJournalNotes)!;
-  }
-  return { sibling, msg: null };
-};
-
-const getSibling = (
-  workspace: DWorkspaceV2,
-  note: NoteProps,
-  direction: Direction,
-  ctx: string
-): { sibling: NoteProps | null; msg: CommandOutput["msg"] | null } => {
-  // Get sibling notes
-  const siblingNotes = getSiblings(workspace.engine.notes, note.parent!);
-  // Check if there is any sibling notes
-  if (siblingNotes.length <= 1) {
-    VSCodeUtils.showMessage(
-      MessageSeverity.INFO,
-      "There is no sibling note in this workspace",
-      {}
-    );
-    return { sibling: null, msg: "no_siblings" };
-  }
-  // Sort the sibling notes
-  const sortedSiblingNotes = sortNotes(siblingNotes);
-  // Get the index of the active note in the sorted notes
-  const idx = _.findIndex(sortedSiblingNotes, { fname: note.fname });
-  // Deal with the unexpected error
-  if (idx < 0) {
-    throw new Error(`${ctx}: ${UNKNOWN_ERROR_MSG}`);
-  }
-  // Get sibling based on the direction
-  let sibling: NoteProps;
-  if (direction === "next") {
-    sibling =
-      idx !== siblingNotes.length - 1
-        ? sortedSiblingNotes[idx + 1]
-        : sortedSiblingNotes[0];
-  } else {
-    sibling =
-      idx !== 0 ? sortedSiblingNotes[idx - 1] : _.last(sortedSiblingNotes)!;
-  }
-  return { sibling, msg: null };
-};
-
-const getSiblings = (
-  notes: NotePropsByIdDict,
-  parentNoteId: string
-): NoteProps[] => {
-  return notes[parentNoteId].children
-    .map((id) => notes[id])
-    .filter((ent) => _.isUndefined(ent.stub));
-};
-
-const sortNotes = (notes: NoteProps[]) => {
-  // check if there are numeric-only nodes
-  const numericNodes = _.filter(notes, (o) => {
-    const leafName = DNodeUtils.getLeafName(o);
-    return isNumeric(leafName);
-  });
-  // determine how much we want to zero-pad the numeric-only node names
-  let padLength = 0;
-  if (numericNodes.length > 0) {
-    const sortedNumericNodes = _.orderBy(
-      numericNodes,
-      (o) => {
-        return DNodeUtils.getLeafName(o)!.length;
-      },
-      "desc"
-    );
-    padLength = sortedNumericNodes[0].fname.length;
-  }
-  // zero-pad numeric-only nodes before sorting
-  return _.sortBy(notes, (o) => {
-    const leafName = DNodeUtils.getLeafName(o);
-    if (isNumeric(leafName)) {
-      return _.padStart(leafName, padLength, "0");
+  private async getSiblingForJournalNote(
+    allNotes: NotePropsByIdDict,
+    currNote: NoteProps,
+    direction: Direction
+  ): Promise<{ sibling: NoteProps | null; msg: CommandOutput["msg"] | null }> {
+    const journalNotes = this.getSiblingsForJournalNote(allNotes, currNote);
+    // If the active note is the only journal note in the workspace, there is no sibling
+    if (journalNotes.length === 1) {
+      VSCodeUtils.showMessage(
+        MessageSeverity.WARN,
+        "There is no sibling journal note. Currently open note is the only journal note in the current workspace",
+        {}
+      );
+      return { sibling: null, msg: "no_siblings" };
     }
-    return leafName;
-  });
-};
+    // Sort all journal notes in the workspace
+    const sortedJournalNotes = _.sortBy(journalNotes, [
+      (note) => this.getDateFromJournalNote(note).valueOf(),
+    ]);
+    const currNoteIdx = _.findIndex(sortedJournalNotes, { id: currNote.id });
+    // Get the sibling based on the direction
+    let sibling: NoteProps;
+    if (direction === "next") {
+      sibling =
+        currNoteIdx !== sortedJournalNotes.length - 1
+          ? sortedJournalNotes[currNoteIdx + 1]
+          : sortedJournalNotes[0];
+    } else {
+      sibling =
+        currNoteIdx !== 0
+          ? sortedJournalNotes[currNoteIdx - 1]
+          : _.last(sortedJournalNotes)!;
+    }
+    return { sibling, msg: null };
+  }
 
-const getDateFromJournalNote = (note: NoteProps): Date => {
-  const [year, month, date] = note.fname
-    .split("")
-    .slice(-3)
-    .map((str) => parseInt(str, 10));
-  return new Date(year, month - 1, date);
-};
+  private getSiblingsForJournalNote = (
+    notes: NotePropsByIdDict,
+    currNote: NoteProps
+  ): NoteProps[] => {
+    const month = notes[currNote.parent!];
+    const year = notes[month.parent!];
+    const parent = notes[year.parent!];
+    // Get all descendants of the parent. This includes stub notes
+    const allDescendants = this.getDescendantsRecursively(notes, parent);
+    // Filter out stub notes
+    return allDescendants.filter((note) => !note.stub);
+  };
+
+  private getDescendantsRecursively = (
+    notes: NotePropsByIdDict,
+    parent: NoteProps
+  ): NoteProps[] => {
+    // Get child notes
+    const children = parent.children.map((id) => notes[id]);
+    // Get all descendant notes of the given note, including stub notes
+    const descendants = [...children];
+    // Recursively get descendants
+    for (const child of children) {
+      const grandChild = this.getDescendantsRecursively(notes, child);
+      descendants.push(...grandChild);
+    }
+    return descendants;
+  };
+
+  private getSibling(
+    workspace: DWorkspaceV2,
+    note: NoteProps,
+    direction: Direction,
+    ctx: string
+  ): { sibling: NoteProps | null; msg: CommandOutput["msg"] | null } {
+    // Get sibling notes
+    const siblingNotes = this.getSiblings(workspace.engine.notes, note.parent!);
+    // Check if there is any sibling notes
+    if (siblingNotes.length <= 1) {
+      VSCodeUtils.showMessage(
+        MessageSeverity.INFO,
+        "There is no sibling note in this workspace",
+        {}
+      );
+      return { sibling: null, msg: "no_siblings" };
+    }
+    // Sort the sibling notes
+    const sortedSiblingNotes = this.sortNotes(siblingNotes);
+    // Get the index of the active note in the sorted notes
+    const idx = _.findIndex(sortedSiblingNotes, { id: note.id });
+    // Deal with the unexpected error
+    if (idx < 0) {
+      throw new Error(`${ctx}: ${UNKNOWN_ERROR_MSG}`);
+    }
+    // Get sibling based on the direction
+    let sibling: NoteProps;
+    if (direction === "next") {
+      sibling =
+        idx !== siblingNotes.length - 1
+          ? sortedSiblingNotes[idx + 1]
+          : sortedSiblingNotes[0];
+    } else {
+      sibling =
+        idx !== 0 ? sortedSiblingNotes[idx - 1] : _.last(sortedSiblingNotes)!;
+    }
+    return { sibling, msg: null };
+  }
+
+  private getSiblings(
+    notes: NotePropsByIdDict,
+    parentNoteId: string
+  ): NoteProps[] {
+    return notes[parentNoteId].children
+      .map((id) => notes[id])
+      .filter((ent) => _.isUndefined(ent.stub));
+  }
+
+  private sortNotes(notes: NoteProps[]) {
+    // check if there are numeric-only nodes
+    const numericNodes = _.filter(notes, (o) => {
+      const leafName = DNodeUtils.getLeafName(o);
+      return isNumeric(leafName);
+    });
+    // determine how much we want to zero-pad the numeric-only node names
+    let padLength = 0;
+    if (numericNodes.length > 0) {
+      const sortedNumericNodes = _.orderBy(
+        numericNodes,
+        (o) => {
+          return DNodeUtils.getLeafName(o)!.length;
+        },
+        "desc"
+      );
+      padLength = sortedNumericNodes[0].fname.length;
+    }
+    // zero-pad numeric-only nodes before sorting
+    return _.sortBy(notes, (o) => {
+      const leafName = DNodeUtils.getLeafName(o);
+      if (isNumeric(leafName)) {
+        return _.padStart(leafName, padLength, "0");
+      }
+      return leafName;
+    });
+  }
+
+  private getDateFromJournalNote(note: NoteProps): Date {
+    const [year, month, date] = note.fname
+      .split("")
+      .slice(-3)
+      .map((str) => parseInt(str, 10));
+    return new Date(year, month - 1, date);
+  }
+}
 
 /* Code for navigating journal notes across months */
 // const journalMonthCheck = async (

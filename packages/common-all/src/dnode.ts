bdfe74cdeb0ec5b6e6d5ed50506135d999c9b55a
tma66 <tuling.ma@gmail.com>
2022-07-19 13:14:58 -0700

    chore(internal): Fix bug where note is not properly deleted via writeNote

--- packages/common-all/src/dnode.ts
+++ packages/common-all/src/dnode.ts
@@ -52,7 +52,7 @@ import { NoteDictsUtils } from "./noteDictsUtils";
  * Utilities for dealing with nodes
  */
 export class DNodeUtils {
-  static addChild(parent: DNodeProps, child: DNodeProps) {
+  static addChild(parent: NotePropsMeta, child: NotePropsMeta) {
     parent.children = Array.from(new Set(parent.children).add(child.id));
     child.parent = parent.id;
   }
@@ -300,18 +300,10 @@ export class NoteUtils {
 
   static deleteChildFromParent(opts: {
     childToDelete: NoteProps;
-    notes: NotePropsByIdDict;
+    parent: NoteProps;
   }): NoteChangeEntry[] {
     const changed: NoteChangeEntry[] = [];
-    const { childToDelete, notes } = opts;
-    let parent;
-    if (childToDelete.parent) {
-      parent = notes[childToDelete.parent];
-    } else {
-      throw new DendronError({
-        message: `No parent found for ${childToDelete.fname}`,
-      });
-    }
+    const { childToDelete, parent } = opts;
 
     const prevParentState = { ...parent };
     parent.children = _.reject<string[]>(

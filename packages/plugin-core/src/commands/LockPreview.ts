fe8de543e0b173d811ff8ab518419060bbb29dee
Samuel Hobl <nam@hobl.at>
2022-07-28 14:25:32 +0200

    fix(views): make `LockPreview` command only lock preview
    And not toggle.

--- packages/plugin-core/src/commands/LockPreview.ts
+++ packages/plugin-core/src/commands/LockPreview.ts
@@ -1,4 +1,3 @@
-import { ExtensionProvider } from "../ExtensionProvider";
 import { PreviewProxy } from "../components/views/PreviewProxy";
 import { DENDRON_COMMANDS } from "../constants";
 import { BasicCommand } from "./base";
@@ -25,20 +24,15 @@ export class LockPreviewCommand extends BasicCommand<
     if (!this._panel || !this._panel.isVisible()) {
       return "No preview currently open";
     }
+    if (this._panel && this._panel.isLocked()) {
+      return "Preview is already locked";
+    }
     return;
   }
 
   async execute(_opts: CommandOpts) {
     if (this._panel) {
-      if (this._panel.isLocked()) {
-        this._panel.unlock();
-        const note = ExtensionProvider.getWSUtils().getActiveNote();
-        if (note) {
-          this._panel.show(note);
-        }
-      } else {
-        this._panel.lock();
-      }
+      this._panel.lock();
     }
 
     return {};

fff62969952d65e679d6b226157345e8f4f8f884
Samuel Hobl <nam@hobl.at>
2022-08-03 15:22:20 +0200

    chore(views): remove unnecessary explicit variable declaration

--- packages/dendron-plugin-views/src/components/DendronApp.tsx
+++ packages/dendron-plugin-views/src/components/DendronApp.tsx
@@ -224,13 +224,14 @@ export type DendronAppProps = {
 };
 
 function DendronApp(props: DendronAppProps) {
-  const style = {
-    padding: props.opts.padding,
-    backgroundColor: "var(--vscode-editor-background)",
-  } as React.CSSProperties;
   return (
     <Provider store={combinedStore}>
-      <Layout style={style}>
+      <Layout
+        style={{
+          padding: props.opts.padding,
+          backgroundColor: "var(--vscode-editor-background)",
+        }}
+      >
         <Content>
           <DendronVSCodeApp {...props} />
         </Content>

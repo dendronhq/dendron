023ac296ae01941558e35bf58253491076ee8400
Jonathan Yeung <jonathanyeung@outlook.com>
2022-08-05 16:32:56 +0800

    spike: ci testing

--- packages/plugin-core/src/web/commands/NoteLookupCmd.ts
+++ packages/plugin-core/src/web/commands/NoteLookupCmd.ts
@@ -27,50 +27,52 @@ export class NoteLookupCmd {
       return;
     }
 
-    result.items.forEach(async (value) => {
-      if (value.label === "Create New") {
-        const newNote = NoteUtils.create({
-          fname: value.fname,
-          vault: value.vault,
-        });
+    await Promise.all(
+      result.items.map(async (value) => {
+        if (value.label === "Create New") {
+          const newNote = NoteUtils.create({
+            fname: value.fname,
+            vault: value.vault,
+          });
 
-        // TODO: Add Schema and Template functionality
-        // const newNote = NoteUtils.createWithSchema({
-        //   noteOpts: {
-        //     fname: value.fname,
-        //     vault: value.vault,
-        //   },
-        //   engine: this.engine as DEngineClient, // TODO: Remove cast
-        // });
-        // await TemplateUtils.findAndApplyTemplate({
-        //   note: newNote,
-        //   engine: client,
-        //   pickNote: async (choices: NoteProps[]) => {
-        //     return WSUtilsV2.instance().promptForNoteAsync({
-        //       notes: choices,
-        //       quickpickTitle:
-        //         "Select which template to apply or press [ESC] to not apply a template",
-        //       nonStubOnly: true,
-        //     });
-        //   },
-        // });
-        // note = _.merge(newNote, overrides || {});
-        const res = await this.engine.writeNote(newNote);
+          // TODO: Add Schema and Template functionality
+          // const newNote = NoteUtils.createWithSchema({
+          //   noteOpts: {
+          //     fname: value.fname,
+          //     vault: value.vault,
+          //   },
+          //   engine: this.engine as DEngineClient, // TODO: Remove cast
+          // });
+          // await TemplateUtils.findAndApplyTemplate({
+          //   note: newNote,
+          //   engine: client,
+          //   pickNote: async (choices: NoteProps[]) => {
+          //     return WSUtilsV2.instance().promptForNoteAsync({
+          //       notes: choices,
+          //       quickpickTitle:
+          //         "Select which template to apply or press [ESC] to not apply a template",
+          //       nonStubOnly: true,
+          //     });
+          //   },
+          // });
+          // note = _.merge(newNote, overrides || {});
+          const res = await this.engine.writeNote(newNote);
 
-        if (res.error) {
-          vscode.window.showErrorMessage("Failed to write note to engine!");
+          if (res.error) {
+            vscode.window.showErrorMessage("Failed to write note to engine!");
+          }
         }
-      }
 
-      const doc = await vscode.workspace.openTextDocument(
-        Utils.joinPath(
-          this.wsRoot,
-          VaultUtils.getRelPath(value.vault),
-          value.fname + ".md"
-        )
-      );
+        const doc = await vscode.workspace.openTextDocument(
+          Utils.joinPath(
+            this.wsRoot,
+            VaultUtils.getRelPath(value.vault),
+            value.fname + ".md"
+          )
+        );
 
-      await vscode.window.showTextDocument(doc);
-    });
+        await vscode.window.showTextDocument(doc);
+      })
+    );
   }
 }
--- packages/plugin-core/src/web/test/suite/commands/NoteLookupCmd.test.ts
+++ packages/plugin-core/src/web/test/suite/commands/NoteLookupCmd.test.ts
@@ -11,9 +11,11 @@ import {
 import { NoteLookupCmd } from "../../../commands/NoteLookupCmd";
 import { IReducedEngineAPIService } from "../../../engine/IReducedEngineApiService";
 
+require("mocha/mocha");
+
 suite("GIVEN a NoteLookupCmd", () => {
   test("WHEN the user selects nothing THEN nothing is written to the engine", async () => {
-    const wsRoot = vscode.workspace.workspaceFile!;
+    const wsRoot = vscode.Uri.parse("tmp");
 
     const mockNoteProvider = stubInterface<ILookupProvider>();
 
@@ -48,7 +50,7 @@ suite("GIVEN a NoteLookupCmd", () => {
   });
 
   test("WHEN the user selects a note THEN that note is opened", async () => {
-    const wsRoot = vscode.workspace.workspaceFile!;
+    const wsRoot = vscode.Uri.parse("tmp");
 
     const mockNoteProvider = stubInterface<ILookupProvider>();
 
@@ -70,8 +72,9 @@ suite("GIVEN a NoteLookupCmd", () => {
     sinon.replace(factory, "showLookup", showLookupFake);
 
     const openTextDocumentFake = sinon.fake.resolves(undefined);
-    const showTextDocumentFake = sinon.fake.resolves(undefined);
     sinon.replace(vscode.workspace, "openTextDocument", openTextDocumentFake);
+
+    const showTextDocumentFake = sinon.fake.resolves(undefined);
     sinon.replace(vscode.window, "showTextDocument", showTextDocumentFake);
 
     const mockEngine = stubInterface<IReducedEngineAPIService>();
@@ -89,6 +92,63 @@ suite("GIVEN a NoteLookupCmd", () => {
     assert.strictEqual(mockEngine.writeNote.callCount, 0);
     assert.strictEqual(openTextDocumentFake.callCount, 1);
     assert.strictEqual(showTextDocumentFake.callCount, 1);
+
+    sinon.restore();
+  });
+
+  test("WHEN Create New is selected THEN a new note is written", async () => {
+    const wsRoot = vscode.Uri.parse("tmp");
+
+    const mockNoteProvider = stubInterface<ILookupProvider>();
+
+    const factory = {
+      showLookup: () => {
+        return Promise.resolve(0);
+      },
+    } as unknown as LookupQuickpickFactory;
+
+    const vault: DVault = {
+      selfContained: true,
+      fsPath: "path",
+    };
+    const lookupReturn: LookupAcceptPayload = {
+      items: [{ fname: "foo", vault, label: "Create New" } as NoteQuickInput],
+    };
+
+    const showLookupFake = sinon.fake.resolves(lookupReturn);
+    sinon.replace(factory, "showLookup", showLookupFake);
+
+    const openTextDocumentFake = sinon.fake.resolves(undefined);
+    sinon.replace(vscode.workspace, "openTextDocument", openTextDocumentFake);
+
+    const showTextDocumentFake = sinon.fake.resolves(undefined);
+    sinon.replace(vscode.window, "showTextDocument", showTextDocumentFake);
+
+    const mockEngine = stubInterface<IReducedEngineAPIService>();
+
+    mockEngine.writeNote.resolves({ error: null });
+
+    const cmd = new NoteLookupCmd(
+      factory,
+      wsRoot,
+      mockEngine,
+      mockNoteProvider
+    );
+
+    await cmd.run();
+
+    assert.strictEqual(showLookupFake.callCount, 1);
+    assert.strictEqual(mockEngine.writeNote.callCount, 1);
+    assert.strictEqual(
+      openTextDocumentFake.callCount,
+      1,
+      "OpenTextDocument called once"
+    );
+    assert.strictEqual(
+      showTextDocumentFake.callCount,
+      1,
+      "ShowTextDocument called once"
+    );
 
     sinon.restore();
   });

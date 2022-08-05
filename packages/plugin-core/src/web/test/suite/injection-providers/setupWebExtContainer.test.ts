5cfdf6eae6ed352d284247dd853e1f345c4c1d93
Jonathan Yeung <jonathanyeung@outlook.com>
2022-08-05 18:01:11 +0800

    spike: adding tests

---
+++ packages/plugin-core/src/web/test/suite/injection-providers/setupWebExtContainer.test.ts
@@ -0,0 +1,49 @@
+import assert from "assert";
+import _ from "lodash";
+import sinon from "sinon";
+import { container } from "tsyringe";
+import * as vscode from "vscode";
+import { Utils } from "vscode-uri";
+import { NoteLookupCmd } from "../../../commands/NoteLookupCmd";
+import { setupWebExtContainer } from "../../../injection-providers/setupWebExtContainer";
+import { createTestWorkspaceDirectory } from "../../helpers/createTestWorkspaceDirectory";
+import { WorkspaceHelpers } from "../../helpers/WorkspaceHelpers";
+
+async function setupEnvironment() {
+  const wsRoot = await createTestWorkspaceDirectory();
+
+  const config = {
+    workspace: {
+      vaults: [
+        {
+          fsPath: "test",
+          name: "test-name",
+        },
+      ],
+    },
+  };
+
+  await WorkspaceHelpers.createTestYAMLConfigFile(wsRoot, config);
+
+  sinon.replaceGetter(vscode.workspace, "workspaceFile", () =>
+    Utils.joinPath(wsRoot, "test.code-workspace")
+  );
+}
+
+suite(
+  "GIVEN an injection container for the Dendron Web Extension configuration",
+  () => {
+    test("WHEN command(s) are constructed THEN valid objects are returned without exceptions", async () => {
+      await setupEnvironment();
+      await setupWebExtContainer();
+
+      try {
+        const cmd = container.resolve(NoteLookupCmd);
+
+        assert(!_.isUndefined(cmd));
+      } catch (error) {
+        assert.fail(error as Error);
+      }
+    });
+  }
+);

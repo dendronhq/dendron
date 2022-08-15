7ba4e19deabfb4a40d4fe0da97aeb28cd10b4f49
Kevin <kevin@dendron.so>
2022-08-15 16:03:51 -0700

    add script to build prisma client

---
+++ packages/engine-server/copyPrismaClient.js
@@ -0,0 +1,12 @@
+const fs = require("fs-extra");
+const path = require("path");
+
+function main() {
+	const libPath = path.join(__dirname, "lib", "generated-prisma-client");
+	const srcPath = path.join("src", "generated-prisma-client");
+	fs.ensureDirSync(libPath);
+	fs.copy(srcPath, libPath, { overwrite: true });
+	console.log("done");
+}
+
+main();

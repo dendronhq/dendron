bbb2fc57a00e2183e308a439a391f8a72d070c68
Samuel Hobl <nam@hobl.at>
2022-09-02 08:59:34 +0200

    chore: remove debugging logs

--- packages/unified/src/rehype/wrap.ts
+++ packages/unified/src/rehype/wrap.ts
@@ -15,15 +15,11 @@ type PluginOpts = {
 };
 
 const plugin: Plugin<[PluginOpts]> = function plugin(this: Processor, opts) {
-  console.log("opts:", JSON.stringify(opts, null, 2));
   function transformer(tree: Node): void {
     const root = tree as HastNode;
     for (const match of selectAll(opts.selector, root)) {
       const wrapper = parseSelector(opts.wrapper);
-      console.log("wrapper:", wrapper);
-      console.log("match:", match);
       visit(tree, match, (node, i, parent) => {
-        console.log("node:", node);
         wrapper.children = [node];
         if (parent) {
           parent.children[i] = wrapper;

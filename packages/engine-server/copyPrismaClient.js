bf50c8c491460da8e2c349d36350e3c3b701e86a
Kevin <kevin@dendron.so>
2022-09-05 15:00:59 -0700

    update prisma copy script

--- packages/engine-server/copyPrismaClient.js
+++ packages/engine-server/copyPrismaClient.js
@@ -4,16 +4,24 @@ const os = require("os");
 
 function main() {
 	// copy client
-	const srcPath = path.join("/tmp", "generated-prisma-client");
+	// const srcPath = path.join("/tmp", "generated-prisma-client");
+	// const libPath = path.join(__dirname, "lib", "drivers", "generated-prisma-client");
+
 	const DENDRON_SYSTEM_ROOT = path.join(os.homedir(), ".dendron");
+	const srcPath = path.join("src", "drivers", "generated-prisma-client");
 	const libPath = path.join(DENDRON_SYSTEM_ROOT, "generated-prisma-client");
 	fs.ensureDirSync(DENDRON_SYSTEM_ROOT);
-	fs.removeSync(libPath);
 	fs.copy(srcPath, libPath, { overwrite: true });
 
 	// copy shim
-	const shimPathSrc = path.join(__dirname, "src", "drivers", "prisma-shim.js");
-	const shimPathDst = path.join(__dirname, "lib", "drivers", "prisma-shim.js");
+	let shimPathSrc = path.join(__dirname, "src", "drivers", "prisma-shim.js");
+	let shimPathDst = path.join(__dirname, "lib", "drivers", "prisma-shim.js");
+	fs.copy(shimPathSrc, shimPathDst, { overwrite: true });
+
+	// copy zip dep
+	const tgt = "adm-zip.js"
+	shimPathSrc = path.join(__dirname, "src", "drivers", tgt);
+	shimPathDst = path.join(__dirname, "lib", "drivers", tgt);
 	fs.copy(shimPathSrc, shimPathDst, { overwrite: true });
 
 	console.log("done");

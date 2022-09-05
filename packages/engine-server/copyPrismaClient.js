b4de4322677e3d327e9c1ce916453cd1d69f677e
Kevin <kevin@dendron.so>
2022-09-05 11:37:28 -0700

    make sure prisma client is initialized properly

--- packages/engine-server/copyPrismaClient.js
+++ packages/engine-server/copyPrismaClient.js
@@ -1,11 +1,21 @@
 const fs = require("fs-extra");
 const path = require("path");
+const os = require("os");
 
 function main() {
-	const libPath = path.join(__dirname, "lib", "drivers", "generated-prisma-client");
-	const srcPath = path.join("src", "drivers", "generated-prisma-client");
-	fs.ensureDirSync(libPath);
+	// copy client
+	const srcPath = path.join("/tmp", "generated-prisma-client");
+	const DENDRON_SYSTEM_ROOT = path.join(os.homedir(), ".dendron");
+	const libPath = path.join(DENDRON_SYSTEM_ROOT, "generated_prisma_client");
+	fs.ensureDirSync(DENDRON_SYSTEM_ROOT);
+	fs.removeSync(libPath);
 	fs.copy(srcPath, libPath, { overwrite: true });
+
+	// copy shim
+	const shimPathSrc = path.join(__dirname, "src", "drivers", "prisma-shim.js");
+	const shimPathDst = path.join(__dirname, "lib", "drivers", "prisma-shim.js");
+	fs.copy(shimPathSrc, shimPathDst, { overwrite: true });
+
 	console.log("done");
 }
 

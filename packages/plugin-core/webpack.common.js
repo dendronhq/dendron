ee63444b77e121e8b5ce23de5f54f1b6538820f3
Kevin <kevin@dendron.so>
2022-09-05 15:14:37 -0700

    exclude zip from packing

--- packages/plugin-core/webpack.common.js
+++ packages/plugin-core/webpack.common.js
@@ -31,7 +31,8 @@ const config = {
     },
     // see [[../packages/plugin-core/webpack-require-hack.js]] for more details
     /\.\/webpack-require-hack/,
-    /\.\/prisma-shim/
+    /\.\/prisma-shim/,
+    /\.\/adm-zip/,
   ],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],

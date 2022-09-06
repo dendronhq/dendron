a25aa70ff7eb42f85369efcdbd019b1ab5ab5581
Hunter Kievet <hkievet@gmail.com>
2022-09-06 14:38:55 -0700

    merge the spec files for noteRef

--- packages/engine-test-utils/src/__tests__/engine-server/markdown/noteRefv2.spec.ts
+++ packages/engine-test-utils/src/__tests__/engine-server/markdown/noteRefv2.spec.ts
@@ -14,9 +14,16 @@ import {
 } from "@dendronhq/common-test-utils";
 import { DConfig } from "@dendronhq/common-server";
 import { MDUtilsV5 } from "@dendronhq/unified";
+import path from "path";
 import { TestConfigUtils } from "../../../config";
 import { runEngineTestV5 } from "../../../engine";
 import { ENGINE_HOOKS, ENGINE_SERVER } from "../../../presets";
+import {
+  checkDir,
+  checkFile,
+  checkNotInString,
+  checkString,
+} from "../../../utils";
 import {
   checkNotInVFile,
   checkVFile,
@@ -2360,5 +2367,134 @@ describe("noteRefV2", () => {
         ...WITH_FOOTNOTES,
       ],
     });
+  });
+
+  describe("WHEN note ref missing", () => {
+    runTestCases(
+      createProcCompileTests({
+        name: "NOTE_REF_MISSING",
+        setup: async (opts) => {
+          const { proc } = getOpts(opts);
+          const txt = `![[alpha.md]]`;
+          const resp = await proc.process(txt);
+          return { resp, proc };
+        },
+        verify: {
+          [DendronASTDest.HTML]: {
+            [ProcFlavor.REGULAR]: async ({ extra }) => {
+              const { resp } = extra;
+              await checkString(resp.contents, "No note with name alpha found");
+            },
+            [ProcFlavor.PREVIEW]: ProcFlavor.REGULAR,
+            [ProcFlavor.PUBLISHING]: ProcFlavor.REGULAR,
+          },
+        },
+        preSetupHook: async (opts) => {
+          await ENGINE_HOOKS.setupBasic({ ...opts, extra: { idv2: true } });
+        },
+      })
+    );
+  });
+
+  describe("WHEN assetPrefix set", () => {
+    runTestCases(
+      createProcCompileTests({
+        name: "ASSET_PREFIX_SET",
+        setup: async (opts) => {
+          const { proc } = getOpts(opts);
+          const txt = `![[bar.md]]`;
+          const resp = await proc.process(txt);
+          return { resp, proc };
+        },
+        verify: {
+          [DendronASTDest.HTML]: {
+            [ProcFlavor.PUBLISHING]: async ({ extra }) => {
+              const { resp } = extra;
+              expect(resp).toMatchSnapshot();
+              await checkString(resp.contents, '<iframe src="/refs/bar---0"');
+            },
+          },
+        },
+        preSetupHook: async (opts) => {
+          await ENGINE_HOOKS.setupBasic({ ...opts, extra: { idv2: true } });
+          TestConfigUtils.withConfig(
+            (config) => {
+              ConfigUtils.setPublishProp(
+                config,
+                "assetsPrefix",
+                "/some-prefix"
+              );
+              return config;
+            },
+            { wsRoot: opts.wsRoot }
+          );
+        },
+      })
+    );
+  });
+
+  fdescribe("WHEN note ref to html AND prettyLinks = true", () => {
+    runTestCases(
+      createProcCompileTests({
+        name: "NOTE_REF_WITH_REHYPE",
+        setup: async (opts) => {
+          const { proc } = getOpts(opts);
+          const txt = `![[alpha.md]]`;
+          const resp = await proc.process(txt);
+          return { resp, proc };
+        },
+        verify: {
+          [DendronASTDest.HTML]: {
+            [ProcFlavor.REGULAR]: async ({ extra }) => {
+              const { resp } = extra;
+              expect(resp).toMatchSnapshot();
+              await checkVFile(
+                resp,
+                // should have id for link
+                `<a href="alpha-id"`,
+                // html quoted
+                `<p><a href="bar">Bar</a></p>`
+              );
+              await checkNotInString(
+                resp.contents,
+                // should not have title
+                `Alpha<h1>`
+              );
+            },
+            [ProcFlavor.PREVIEW]: ProcFlavor.REGULAR,
+            [ProcFlavor.PUBLISHING]: async ({ extra, engine }) => {
+              const { resp } = extra;
+              expect(resp).toMatchSnapshot();
+              await checkString(
+                resp.contents,
+                `<iframe src="/refs/alpha-id---0"`
+              );
+              const { wsRoot } = engine;
+              const refsDir = MDUtilsV5.getRefsRoot(wsRoot);
+              await checkDir({ fpath: refsDir, snapshot: true });
+              const refFile = path.join(refsDir, "alpha-id---0.json");
+              await checkFile({ fpath: refFile, snapshot: true });
+            },
+          },
+        },
+        preSetupHook: async (opts) => {
+          await ENGINE_HOOKS.setupBasic({ ...opts, extra: { idv2: true } });
+          await NoteTestUtilsV4.createNote({
+            fname: "alpha",
+            body: "[[bar]]",
+            vault: opts.vaults[0],
+            wsRoot: opts.wsRoot,
+            props: { id: "alpha-id" },
+          });
+          TestConfigUtils.withConfig(
+            (config) => {
+              ConfigUtils.setPublishProp(config, "enablePrettyLinks", true);
+              return config;
+            },
+            { wsRoot: opts.wsRoot }
+          );
+        },
+      })
+    );
   });
 });

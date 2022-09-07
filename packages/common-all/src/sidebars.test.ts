2b4e8af2e4ffae2b044786f50fe1ed6450012dde
Samuel Hobl <nam@hobl.at>
2022-09-07 17:12:22 +0200

    chore: mark test as todo

--- packages/common-all/src/sidebars.test.ts
+++ packages/common-all/src/sidebars.test.ts
@@ -1,9 +1,5 @@
-// see previous commit (refactor: remove unused `note2TreeDatanote` function) for reference implementation.
-
 describe("GIVEN sidebars object", () => {
   describe("WHEN parsing", () => {
-    test("THEN should do something", () => {
-      expect(true).toEqual(true);
-    });
+    test.todo("THEN should do something");
   });
 });

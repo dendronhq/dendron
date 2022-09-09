7fbea10baca9e9f5c2da771a0e4cc989bbfef078
Samuel Hobl <nam@hobl.at>
2022-09-09 08:45:43 +0200

    refactor(publish, sidebar): switch parsin library back to `zod`

--- packages/common-all/src/parse.ts
+++ packages/common-all/src/parse.ts
@@ -1,25 +1,25 @@
-import * as v from "@badrap/valita";
+import { z } from "zod";
 import { DendronError } from "./error";
 import { RespV3 } from "./types";
 
-export const parse = <T extends v.Type>(
+export const parse = <T extends z.ZodTypeAny>(
   schema: T,
   raw: unknown,
   msg?: string
-): RespV3<v.Infer<T>> => {
-  const parsed = schema.try(raw);
-  if (parsed.ok) {
-    return { data: parsed.value };
+): RespV3<z.infer<T>> => {
+  const parsed = schema.safeParse(raw);
+  if (parsed.success) {
+    return { data: parsed.data };
   } else {
     return {
       error: new DendronError({
         message: [
           [
             "Schema Parse Error",
-            ...(schema.name ? [`: ${schema.name}`] : []),
+            ...(schema.description ? [`: ${schema.description}`] : []),
           ].join(""),
           ...(msg ? [msg] : []),
-          JSON.stringify(parsed.issues, null, 2),
+          JSON.stringify(parsed.error.format(), null, 2),
         ].join("\n"),
       }),
     };

54f1142ab8677b3a6371dc71950bd69c1fde86b7
Harshita Joshi <84971366+Harshita-mindfire@users.noreply.github.com>
2022-09-19 10:50:11 +0530

    chore: ab test for video in launch tutorial welcome screen (#3528)
    * chore: ab test for video in launch tutorial welcome screen
    
    * update video url from aws and delete video from assets
    
    * fix PR comments

--- packages/plugin-core/src/WelcomeUtils.ts
+++ packages/plugin-core/src/WelcomeUtils.ts
@@ -1,8 +1,11 @@
 import {
-  TutorialEvents
+  TutorialEvents,
+  VideoTutorialTestGroups,
+  _2022_09_VIDEO_TUTORIAL_TEST,
 } from "@dendronhq/common-all";
-import { readMD } from "@dendronhq/common-server";
+import { readMD, SegmentClient } from "@dendronhq/common-server";
 import _ from "lodash";
+import semver from "semver";
 import * as vscode from "vscode";
 import { LaunchTutorialWorkspaceCommand } from "./commands/LaunchTutorialWorkspaceCommand";
 import { LaunchTutorialCommandInvocationPoint } from "./constants";
@@ -21,14 +24,35 @@ async function initWorkspace() {
 
 export function showWelcome(assetUri: vscode.Uri) {
   try {
-    // NOTE: this needs to be from extension because no workspace might exist at this point
-    const uri = VSCodeUtils.joinPath(
-      assetUri,
-      "dendron-ws",
-      "vault",
-      "welcome.html"
+    let content: string;
+    const ABUserGroup = _2022_09_VIDEO_TUTORIAL_TEST.getUserGroup(
+      SegmentClient.instance().anonymousId
     );
-    const { content } = readMD(uri.fsPath);
+    let testgroup: string;
+    if (
+      ABUserGroup === VideoTutorialTestGroups.video &&
+      semver.gte(vscode.version, "1.71.0")
+    ) {
+      const videoUri = VSCodeUtils.joinPath(
+        assetUri,
+        "dendron-ws",
+        "vault",
+        "welcome_video.html"
+      );
+      content = readMD(videoUri.fsPath).content;
+      testgroup = VideoTutorialTestGroups.video;
+    } else {
+      // NOTE: this needs to be from extension because no workspace might exist at this point
+      const uri = VSCodeUtils.joinPath(
+        assetUri,
+        "dendron-ws",
+        "vault",
+        "welcome.html"
+      );
+      content = readMD(uri.fsPath).content;
+      testgroup = VideoTutorialTestGroups.gif;
+    }
+
     const title = "Welcome to Dendron";
 
     const panel = vscode.window.createWebviewPanel(
@@ -39,13 +63,14 @@ export function showWelcome(assetUri: vscode.Uri) {
         enableScripts: true,
       }
     );
+
     panel.webview.html = content;
 
     panel.webview.onDidReceiveMessage(
       async (message) => {
         switch (message.command) {
           case "loaded":
-            AnalyticsUtils.track(TutorialEvents.WelcomeShow);
+            AnalyticsUtils.track(TutorialEvents.WelcomeShow, { testgroup });
             return;
 
           case "initializeWorkspace": {

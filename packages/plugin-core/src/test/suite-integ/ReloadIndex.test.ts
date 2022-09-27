d0c4864d720745b29662310e0843b5237d63a196
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2022-09-27 17:44:53 +0530

    chore(internal): fix reload index flaky test on windows

--- packages/plugin-core/src/test/suite-integ/ReloadIndex.test.ts
+++ packages/plugin-core/src/test/suite-integ/ReloadIndex.test.ts
@@ -10,7 +10,7 @@ import { expect } from "../testUtilsv2";
 import { describeMultiWS, describeSingleWS } from "../testUtilsV3";
 import { test, before } from "mocha";
 import { ExtensionProvider } from "../../ExtensionProvider";
-import { NoteUtils, VaultUtils } from "@dendronhq/common-all";
+import { VaultUtils } from "@dendronhq/common-all";
 import { MessageItem, window } from "vscode";
 import sinon from "sinon";
 import { VSCodeUtils } from "../../vsCodeUtils";
@@ -178,19 +178,17 @@ suite("GIVEN ReloadIndex", function () {
     before(async () => {
       const { wsRoot, vaults } = ExtensionProvider.getDWorkspace();
       const vaultPath = vault2Path({ vault: vaults[0], wsRoot });
-      await fs.rmdir(vaultPath, { recursive: true });
+      await fs.rm(vaultPath, { recursive: true, maxRetries: 2 });
     });
 
     test("THEN other vaults are still loaded", async () => {
       const engine = await new ReloadIndexCommand().run();
       const { vaults } = ExtensionProvider.getDWorkspace();
       expect(engine).toBeTruthy();
-      const notes = _.sortBy(
-        NoteUtils.getNotesByFnameFromEngine({
-          engine: engine!,
-          fname: "root",
-        }),
-        (note) => path.basename(note.vault.fsPath)
+      const allNotes = await engine?.findNotes({ fname: "root" });
+
+      const notes = _.sortBy(allNotes, (note) =>
+        path.basename(note.vault.fsPath)
       );
       expect(notes.length).toEqual(2);
       expect(VaultUtils.isEqualV2(notes[0].vault, vaults[1])).toBeTruthy();

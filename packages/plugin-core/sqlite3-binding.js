7915608557b7fdf1f4e48b654f5ad8b1f74ecbda
Jonathan Yeung <jonathanyeung@outlook.com>
2022-10-14 14:04:00 +0800

    spike: update sqlite3-binding

--- packages/plugin-core/sqlite3-binding.js
+++ packages/plugin-core/sqlite3-binding.js
@@ -1,12 +1,11 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 
-// const path = require("path");
-
-// const binding_path = path.resolve(path.join(__dirname, "node_sqlite3.node"));
-// const binding = require(binding_path);
-const binding = require("node_sqlite3.node");
-console.log("Inside custom binding");
+const path = require("path");
+const binding_path = path.resolve(path.join(__dirname, "node_sqlite3.node"));
+const binding = require(binding_path);
+// const binding = require("node_sqlite3.node");
+// console.log("Inside custom binding");
 
 // debugger;
 

89bb955b31976a9333fbe0ea888b2fffca25a169
Jonathan Yeung <jonathanyeung@outlook.com>
2022-10-17 18:19:58 +0800

    updating nightly workflow

--- packages/plugin-core/sqlite3-binding.js
+++ packages/plugin-core/sqlite3-binding.js
@@ -1,12 +1,15 @@
-/* eslint-disable global-require */
+/* eslint-disable no-multi-assign */
 /* eslint-disable import/no-dynamic-require */
 
+/**
+ * This is an override for the native module binding in the sqlite3 package. It
+ * is only used for webpack bundled versions of Dendron Local ext. This binding
+ * override is necessary because the native node module in the sqlite3 package
+ * cannot be resolved through webpack without some workarounds.
+ */
 const path = require("path");
-const binding_path = path.resolve(path.join(__dirname, "node_sqlite3.node"));
-const binding = require(binding_path);
-// const binding = require("node_sqlite3.node");
-// console.log("Inside custom binding");
 
-// debugger;
+const bindingPath = path.resolve(path.join(__dirname, "node_sqlite3.node"));
+const binding = require(bindingPath);
 
 module.exports = exports = binding;

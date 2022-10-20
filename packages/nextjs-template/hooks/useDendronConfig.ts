9e4b420787d5af5d8150dc9b839d5099a3d7906d
Samuel Hobl <samuel.hobl@gmail.com>
2022-10-20 17:35:22 +0200

    fix(publish): runtime error on undefined dendron config (#3688)
    This commit removes the unreachable assert which has been added using a
    false assumption, which was that the dendron config will have been
    loaded initially to the redux store.

--- packages/nextjs-template/hooks/useDendronConfig.ts
+++ packages/nextjs-template/hooks/useDendronConfig.ts
@@ -1,10 +1,6 @@
-import { assertUnreachable } from "@dendronhq/common-all";
 import { useEngineAppSelector } from "../features/engine/hooks";
 
 export const useDendronConfig = () => {
   const { config } = useEngineAppSelector((state) => state.engine);
-  if (!config) {
-    assertUnreachable(undefined as never);
-  }
   return config;
 };

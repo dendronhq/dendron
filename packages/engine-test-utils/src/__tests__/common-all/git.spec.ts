0e6b42ab7f8297e0a46fd3421e1dbc37b6b3e739
Samuel Hobl <samuel.hobl@gmail.com>
2022-10-28 11:52:10 +0200

    chore: add easy to add git utils tests

--- packages/engine-test-utils/src/__tests__/common-all/git.spec.ts
+++ packages/engine-test-utils/src/__tests__/common-all/git.spec.ts
@@ -12,7 +12,7 @@ const vault: DVault = {
   fsPath: "fooVault",
 };
 
-describe(`GIVEN GitUtils`, () => {
+describe("GIVEN GitUtils", () => {
   describe(`WHEN config is default`, () => {
     const config = ConfigUtils.genDefaultConfig();
     const note = NoteUtils.create({
@@ -20,11 +20,17 @@ describe(`GIVEN GitUtils`, () => {
       fname,
     });
 
-    test(`THEN canShowGitLink should be false`, () => {
+    test("THEN `canShowGitLink` should be false", () => {
       expect(GitUtils.canShowGitLink({ config, note })).toBeFalsy();
     });
+
+    test("THEN `getGithubEditUrl` should return github edit url", () => {
+      expect(GitUtils.getGithubEditUrl({ config, note })).toEqual(
+        "/tree/main/fooVault/foo.md"
+      );
+    });
   });
-  describe(`WHEN config contains github properties`, () => {
+  describe("WHEN config contains github properties", () => {
     const config = ConfigUtils.genDefaultConfig();
 
     config.publishing.github = {
@@ -35,12 +41,19 @@ describe(`GIVEN GitUtils`, () => {
       editViewMode: GithubEditViewModeEnum.edit,
     };
 
-    test(`THEN canShowGitLink should be true`, () => {
-      const note = NoteUtils.create({
-        vault,
-        fname,
-      });
+    const note = NoteUtils.create({
+      vault,
+      fname,
+    });
+
+    test("THEN `canShowGitLink` should be true", () => {
       expect(GitUtils.canShowGitLink({ config, note })).toBeTruthy();
+    });
+
+    test("THEN `getGithubEditUrl` should return github edit url", () => {
+      expect(GitUtils.getGithubEditUrl({ config, note })).toEqual(
+        "https://github.com/kevinslin/dendron-11ty-test/edit/main/fooVault/foo.md"
+      );
     });
 
     describe("AND note contains `gitNoLink` property", () => {
@@ -50,9 +63,26 @@ describe(`GIVEN GitUtils`, () => {
         fname,
       });
 
-      test(`THEN canShowGitLink should be false`, () => {
+      test("THEN canShowGitLink should be false", () => {
         expect(GitUtils.canShowGitLink({ config, note })).toBeFalsy();
       });
     });
+  });
+
+  test("THEN `git2Github` conversion", () => {
+    expect(
+      GitUtils.git2Github("git@github.com:kevinslin/dendron-vault.git")
+    ).toEqual("https://github.com/kevinslin/dendron-vault");
+  });
+
+  test("THEN `getGithubAccessTokenUrl` conversion", () => {
+    expect(
+      GitUtils.getGithubAccessTokenUrl({
+        remotePath: "https://github.com/kevinslin/dendron-11ty-test",
+        accessToken: "abc",
+      })
+    ).toEqual(
+      "https://abc:x-oauth-basic@github.com/kevinslin/dendron-11ty-test"
+    );
   });
 });

73d16c0ed567cbd35e0eb1596c4c9d910c31da88
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2022-11-07 17:31:20 +0530

    fix(workspace): decoration does not display correct custom color for hashtags

--- packages/unified/src/decorations/frontmatter.ts
+++ packages/unified/src/decorations/frontmatter.ts
@@ -24,7 +24,7 @@ export const decorateFrontmatter: Decorator<
   FrontmatterContent,
   DecorationsForDecorateFrontmatter
 > = async (opts) => {
-  const { node: frontmatter, config, engine, note } = opts;
+  const { node: frontmatter, config, engine } = opts;
   const { value: contents, position } = frontmatter;
   // Decorate the timestamps
 
@@ -71,7 +71,6 @@ export const decorateFrontmatter: Decorator<
         lineOffset,
         config,
         engine,
-        note,
       });
       tagDecorations.push(...decorations);
       errors.push(...errors);

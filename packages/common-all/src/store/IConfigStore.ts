36527ddc2cc992345c751c64327f3aa37a570305
Mark Choi <hikchoi@gmail.com>
2022-11-22 17:41:46 +0900

    chore: remove wsRoot from config service / store property and pass it as an argument on every method call instead

--- packages/common-all/src/store/IConfigStore.ts
+++ packages/common-all/src/store/IConfigStore.ts
@@ -1,13 +1,10 @@
 import { ResultAsync } from "neverthrow";
+import { URI } from "vscode-uri";
 import { IDendronError } from "../error";
 import { DendronConfig } from "../types";
 import { DeepPartial } from "../utils";
 
 export type ConfigValue = string | number | object;
-
-export type ConfigReadOpts = {
-  applyOverride?: boolean;
-};
 
 export interface IConfigStore {
   /**
@@ -15,19 +12,23 @@ export interface IConfigStore {
    * If a persistent dendron config exists, return an error
    */
   createConfig(
+    wsRoot: URI,
     defaults?: DeepPartial<DendronConfig>
   ): ResultAsync<DendronConfig, IDendronError>;
 
   /**
    * Read the entire dendron config as is
    */
-  readConfig(): ResultAsync<DeepPartial<DendronConfig>, IDendronError>;
+  readConfig(
+    wsRoot: URI
+  ): ResultAsync<DeepPartial<DendronConfig>, IDendronError>;
 
   /**
    * Given a dendron config, update the persistent dendron config with the given payload
    * If dendronrc.yml is found, content that is present in dendronrc.yml will be filtered out before writing
    */
   writeConfig(
+    wsRoot: URI,
     payload: DendronConfig
   ): ResultAsync<DendronConfig, IDendronError>;
 
@@ -35,6 +36,7 @@ export interface IConfigStore {
    * Given mode (workspace or global), read override config
    */
   readOverride(
+    wsRoot: URI,
     mode: "workspace" | "global"
   ): ResultAsync<string, IDendronError>;
 }

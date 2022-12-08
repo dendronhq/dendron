a92a56e4ce5c81213342257ec9ffa7aa923c45e1
Harshita-mindfire <harshitaj@mindfiresolutions.com>
2022-12-08 12:57:47 +0530

    resolve PR comment

--- packages/plugin-core/src/web/commands/CreateJournalNoteCmd.ts
+++ packages/plugin-core/src/web/commands/CreateJournalNoteCmd.ts
@@ -1,4 +1,4 @@
-import { container, inject, injectable } from "tsyringe";
+import { inject, injectable } from "tsyringe";
 import { LookupControllerCreateOpts } from "./lookup/LookupController";
 import { type ILookupProvider } from "./lookup/ILookupProvider";
 import { DENDRON_COMMANDS } from "../../constants";
@@ -12,18 +12,18 @@ export class CreateJournalNoteCmd {
   static key = DENDRON_COMMANDS.CREATE_JOURNAL.key;
   constructor(
     @inject("NoteProvider") private noteProvider: ILookupProvider,
-    @inject("ITelemetryClient") private analytics: ITelemetryClient
+    @inject("ITelemetryClient") private analytics: ITelemetryClient,
+    private lookupCmd: NoteLookupCmd
   ) {}
 
   async run() {
     this.analytics.track(CreateJournalNoteCmd.key);
-    const lookupCmd = container.resolve(NoteLookupCmd);
     const createJournalOpts: LookupControllerCreateOpts = {
       provider: this.noteProvider,
       noteType: LookupNoteTypeEnum.journal,
       buttons: [JournalBtn.create({ pressed: true, canToggle: false })],
       title: "Create Journal Note",
     };
-    await lookupCmd.run(createJournalOpts);
+    await this.lookupCmd.run(createJournalOpts);
   }
 }
